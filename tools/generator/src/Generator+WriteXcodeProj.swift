10cc96385699937e9d3d3b2fc0b562bcae7627b6
Brentley Jones <github@brentleyjones.com>
2022-02-28 13:45:29 -0600

    Write `.xcodeproj` (#38)

---
+++ tools/generator/src/Generator+WriteXcodeProj.swift
@@ -0,0 +1,48 @@
+import PathKit
+import XcodeProj
+
+extension Generator {
+    ///
+    static func writeXcodeProj(
+        _ xcodeProj: XcodeProj,
+        files: [FilePath: PBXFileElement],
+        internalDirectoryName: String,
+        to outputPath: Path
+    ) throws {
+        try xcodeProj.write(path: outputPath)
+
+        let internalOutputPath = outputPath + internalDirectoryName
+
+        // This will have to be improved when we eventually write different
+        // types of internal files
+        for (filePath, fileElement) in files.filter(\.key.isInternal) {
+            let path = internalOutputPath + filePath.path
+            if fileElement is PBXGroup {
+                try path.mkpath()
+            } else {
+                try path.parent().mkpath()
+                try path.write("")
+            }
+        }
+    }
+}
+
+private extension FilePath {
+    var isInternal: Bool {
+        switch self {
+        case .internal:
+            return true
+        default:
+            return false
+        }
+    }
+
+    var path: Path {
+        switch self {
+        case .internal(let path):
+            return path
+        case .input(let path):
+            return path
+        }
+    }
+}
