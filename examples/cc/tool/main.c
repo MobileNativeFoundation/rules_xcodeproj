fed062ae339868fd1f72133feb31638ccea132b7
Brentley Jones <github@brentleyjones.com>
2022-03-17 16:01:11 -0500

    Add same product name example
    Closes https://github.com/buildbuddy-io/rules_xcodeproj/issues/131.

--- examples/cc/tool/main.c
+++ examples/cc/tool/main.c
@@ -2,8 +2,10 @@
 #include <stdio.h>
 
 #include "examples/cc/lib/lib.h"
+#include "examples/cc/lib2/lib.h"
 
 int main(int argc, char *argv[]) {
     // Print the string returned from lib's greeting() function
     printf("%s, %s\n", greeting(), SECRET_2);
+    printf("%d\n", answer());
 }
