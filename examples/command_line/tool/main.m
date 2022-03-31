cc43a08bc45a44c98544b6b0f65abfecbc2f807a
Brentley Jones <github@brentleyjones.com>
2022-03-31 09:44:43 -0500

    Remove invalid framework usage in examples (#226)
    Command line tools can't use the dynamic frameworks, it crashes at launch.

--- examples/command_line/tool/main.m
+++ examples/command_line/tool/main.m
@@ -1,10 +1,7 @@
 #import <Foundation/Foundation.h>
 
-#import <ExternalFramework/ExternalFramework-Swift.h>
-
 #import "lib.h"
 
 int main(int argc, char *argv[]) {
-    // Print the string returned from lib's greeting() function
-    NSLog(@"%s, %s\n%@", greeting(), SECRET_2, Baz.bar);
+    NSLog(@"%s, %s", greeting(), SECRET_2);
 }
