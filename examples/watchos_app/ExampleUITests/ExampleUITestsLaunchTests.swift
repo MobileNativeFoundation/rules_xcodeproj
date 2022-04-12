b61147d8f47e225233cf1a2f1fde947ea9383710
Chuck Grindel <chuck.grindel@robinhood.com>
2022-04-12 13:04:51 -0600

    Removed overriden property for ExampleUITestsLaunchTests that is not supported on earlier versions of Swift/Xcode.

--- examples/watchos_app/ExampleUITests/ExampleUITestsLaunchTests.swift
+++ examples/watchos_app/ExampleUITests/ExampleUITestsLaunchTests.swift
@@ -1,10 +1,6 @@
 import XCTest
 
 class ExampleUITestsLaunchTests: XCTestCase {
-    override class var runsForEachTargetApplicationUIConfiguration: Bool {
-        true
-    }
-
     override func setUpWithError() throws {
         continueAfterFailure = false
     }
