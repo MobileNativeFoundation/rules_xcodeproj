1a6281d3b76a34eca67f92e8a21b9586e8cccd15
Chuck Grindel <chuck.grindel@robinhood.com>
2022-04-23 17:14:38 -0600

    Clean up.

--- examples/command_line/tool/main.m
+++ examples/command_line/tool/main.m
@@ -6,6 +6,5 @@
 int main(int argc, char *argv[]) {
     NSLog(@"%s, %s", greeting(), SECRET_2);
     NSLog(@"%@", SwiftGreetings.greeting);
-    /* NSLog(@"%@", SwiftParserExample.execute()); */
     NSLog(@"%@", SwiftParserExample.result);
 }
