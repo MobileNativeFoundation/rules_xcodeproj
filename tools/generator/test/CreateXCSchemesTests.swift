9cf18554b8574d39a863eefdac6209c34e9e9149
Chuck Grindel <chuck.grindel@robinhood.com>
2022-04-30 12:54:51 -0600

    Update placeholder tests.

--- tools/generator/test/CreateXCSchemesTests.swift
+++ tools/generator/test/CreateXCSchemesTests.swift
@@ -5,19 +5,23 @@ import XCTest
 @testable import generator
 
 class CreateXCSchemesTests: XCTestCase {
-    func test_NoTargets() throws {
+    func test_createXCSchemes_WithNoTargets() throws {
         XCTFail("IMPLEMENT ME!")
     }
 
-    func test_LibTarget() throws {
+    func test_createXCSchemes_WithTargets() throws {
         XCTFail("IMPLEMENT ME!")
     }
 
-    func test_LibAndTestTarget() throws {
+    func test_createXCScheme_LibTarget() throws {
         XCTFail("IMPLEMENT ME!")
     }
 
-    func test_AppTarget() throws {
+    func test_createXCScheme_LibAndTestTarget() throws {
+        XCTFail("IMPLEMENT ME!")
+    }
+
+    func test_createXCScheme_AppTarget() throws {
         XCTFail("IMPLEMENT ME!")
     }
 }
