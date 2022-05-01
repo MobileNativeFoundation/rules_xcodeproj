f99da4f6a8d0793829b5b03ade6c640a7ec662ba
Chuck Grindel <chuck.grindel@robinhood.com>
2022-05-01 13:30:32 -0600

    Brought over files from gh101_generate_schemes

---
+++ tools/generator/src/PBXTarget+Extensions.swift
@@ -0,0 +1,37 @@
+import Foundation
+import XcodeProj
+
+public extension PBXTarget {
+    var buildableName: String {
+        return productName ?? name
+    }
+
+    func createBuildableReference(
+        referencedContainer: XcodeProjContainerReference
+    ) -> XCScheme.BuildableReference {
+        return .init(
+            referencedContainer: "\(referencedContainer)",
+            blueprint: self,
+            buildableName: buildableName,
+            blueprintName: name
+        )
+    }
+
+    var schemeName: String {
+        // The XcodeProj write logic does not like slashes (/) in the scheme
+        // name. It fails to write with a missing folder error.
+        return buildableName.replacingOccurrences(
+            of: #"[/]"#,
+            with: "_",
+            options: .regularExpression
+        )
+    }
+
+    var isTestable: Bool {
+        return productType?.isTestBundle ?? false
+    }
+
+    var isLaunchable: Bool {
+        return productType?.isExecutable ?? false
+    }
+}
---
+++ tools/generator/test/PBXTarget+ExtensionsTests.swift
@@ -0,0 +1,70 @@
+import XcodeProj
+import XCTest
+
+@testable import generator
+
+class PBXTargetExtensionsTests: XCTestCase {
+    let pbxTarget = PBXTarget(name: "chicken", productName: "MyChicken")
+
+    func test_buildableName_WithProductName() throws {
+        XCTAssertEqual(pbxTarget.buildableName, "MyChicken")
+    }
+
+    func test_buildableName_WithoutProductName() throws {
+        pbxTarget.productName = nil
+        XCTAssertEqual(pbxTarget.buildableName, "chicken")
+    }
+
+    func test_createBuildableReference() throws {
+        let referencedContainer: XcodeProjContainerReference =
+            "container:Foo.xcodeproj"
+        let buildableReference = pbxTarget.createBuildableReference(
+            referencedContainer: referencedContainer
+        )
+        let expected = XCScheme.BuildableReference(
+            referencedContainer: "\(referencedContainer)",
+            blueprint: pbxTarget,
+            buildableName: pbxTarget.buildableName,
+            blueprintName: pbxTarget.name
+        )
+        XCTAssertEqual(buildableReference, expected)
+    }
+
+    func test_schemeName_WithSlashesInBuildableName() throws {
+        pbxTarget.name = "//examples/chicken:smidgen"
+        pbxTarget.productName = nil
+        XCTAssertEqual(pbxTarget.schemeName, "__examples_chicken:smidgen")
+    }
+
+    func test_schemeName_WithoutSlashesInBuildableName() throws {
+        XCTAssertEqual(pbxTarget.schemeName, "MyChicken")
+    }
+
+    func test_isTestable_WithoutProductType() throws {
+        XCTAssertFalse(pbxTarget.isTestable)
+    }
+
+    func test_isTestable_WhenIsTestBundle() throws {
+        pbxTarget.productType = .unitTestBundle
+        XCTAssertTrue(pbxTarget.isTestable)
+    }
+
+    func test_isTestable_WhenNotIsTestBundle() throws {
+        pbxTarget.productType = .application
+        XCTAssertFalse(pbxTarget.isTestable)
+    }
+
+    func test_isLaunchable_WithoutProductType() throws {
+        XCTAssertFalse(pbxTarget.isLaunchable)
+    }
+
+    func test_isLaunchable_WhenIsExecutable() throws {
+        pbxTarget.productType = .application
+        XCTAssertTrue(pbxTarget.isLaunchable)
+    }
+
+    func test_isLaunchable_WhenNotIsExecutable() throws {
+        pbxTarget.productType = .staticLibrary
+        XCTAssertFalse(pbxTarget.isLaunchable)
+    }
+}
