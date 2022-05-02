252e6481b3b03ea2b06223e6973b3a55615c62bc
Chuck Grindel <chuck.grindel@robinhood.com>
2022-05-02 16:13:32 -0600

    Updated with defaultBuildConfigurationName tests

--- tools/generator/src/PBXTarget+Extensions.swift
+++ tools/generator/src/PBXTarget+Extensions.swift
@@ -43,7 +43,6 @@ public extension PBXTarget {
             "Debug"
     }
 
-    // TODO(chuck): Add test
     var defaultBuildConfigurationName: String {
         return buildConfigurationList?.buildConfigurations.first?.name ??
             "Debug"
