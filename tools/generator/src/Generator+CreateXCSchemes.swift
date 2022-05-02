2045f3b710becd30b44a896b49955fa77d4bc9f6
Chuck Grindel <chuck.grindel@robinhood.com>
2022-05-02 16:06:13 -0600

    Add placeholders.

--- tools/generator/src/Generator+CreateXCSchemes.swift
+++ tools/generator/src/Generator+CreateXCSchemes.swift
@@ -1,10 +1,36 @@
 import XcodeProj
 
+// DEBUG BEGIN
+import Darwin
+// DEBUG END
+
 extension Generator {
     /// Creates an array of `XCScheme` entries for the specified targets.
     static func createXCSchemes(
-        disambiguatedTargets _: [TargetID: DisambiguatedTarget]
+        disambiguatedTargets: [TargetID: DisambiguatedTarget]
     ) -> [XCScheme] {
+        // DEBUG BEGIN
+        fputs("*** CHUCK disambiguatedTargets:\n", stderr)
+        for (idx, item) in disambiguatedTargets.enumerated() {
+            let disambiguatedTarget = item.1
+            let target = disambiguatedTarget.target
+            // fputs("*** CHUCK   \(idx) : \(String(reflecting: item.1))\n", stderr)
+            fputs("*** CHUCK   \(idx) : \(String(reflecting: item.0))\n", stderr)
+            fputs("*** CHUCK      disambiguatedTarget.name: \(String(reflecting: disambiguatedTarget.name))\n", stderr)
+            fputs("*** CHUCK      target.name: \(String(reflecting: target.name))\n", stderr)
+            fputs("*** CHUCK      target.product.type: \(String(reflecting: target.product.type))\n", stderr)
+            fputs("*** CHUCK      dependencies: \(String(reflecting: target.dependencies))\n", stderr)
+        }
+        // DEBUG END
+
+        // Need to group unit test target with its associated library for combo scheme
+        // The rest of the targets will get their own scheme
+
+        // Scheme actions: Build, Test, Run, Profile
+        for _, disambiguatedTarget in disambiguatedTargets {
+            // If
+        }
+
         // GH101: Implement logic to create schemes from targets.
         return []
     }
---
+++ tools/generator/test/CreateXCSchemesTests.swift
@@ -0,0 +1,23 @@
+import CustomDump
+import XcodeProj
+import XCTest
+
+@testable import generator
+
+class CreateXCSchemesTests: XCTestCase {
+    func test_NoTargets() throws {
+        fail("IMPLEMENT ME!")
+    }
+
+    func test_LibTarget() throws {
+        fail("IMPLEMENT ME!")
+    }
+
+    func test_LibAndTestTarget() throws {
+        fail("IMPLEMENT ME!")
+    }
+
+    func test_AppTarget() throws {
+        fail("IMPLEMENT ME!")
+    }
+}
