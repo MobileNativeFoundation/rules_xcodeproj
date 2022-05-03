6b6217d3a24b5184d9d799f8bc82e0e68910d304
Chuck Grindel <chuck@chuckgrindel.com>
2022-05-03 16:30:22 -0600

    Undo formatting

--- tools/generator/src/Generator.swift
+++ tools/generator/src/Generator.swift
@@ -46,7 +46,7 @@ class Generator {
         outputPath: Path
     ) throws {
         let filePathResolver = FilePathResolver(
-            internalDirectoryName: internalDirectoryName,
+            internalDirectoryName: internalDirectoryName, 
             workspaceOutputPath: workspaceOutputPath
         )
 
@@ -58,8 +58,8 @@ class Generator {
         )
         guard let pbxProject = pbxProj.rootObject else {
             throw PreconditionError(message: """
-            `rootObject` not set on `pbxProj`
-            """)
+`rootObject` not set on `pbxProj`
+""")
         }
         let mainGroup: PBXGroup = pbxProject.mainGroup
 
@@ -70,21 +70,21 @@ class Generator {
             guard let srcTarget = targets[src] else {
                 throw PreconditionError(
                     message: """
-                    Source target "\(src)" not found in `targets`
-                    """)
+Source target "\(src)" not found in `targets`
+""")
             }
             for destination in destinations {
                 guard let destTarget = targets[destination] else {
                     throw PreconditionError(message: """
-                    Destination target "\(destination)" not found in `targets`
-                    """)
+Destination target "\(destination)" not found in `targets`
+""")
                 }
                 logger.logWarning("""
-                Was unable to merge "\(srcTarget.label) \
-                (\(srcTarget.configuration))" into \
-                "\(destTarget.label) \
-                (\(destTarget.configuration))"
-                """)
+Was unable to merge "\(srcTarget.label) \
+(\(srcTarget.configuration))" into \
+"\(destTarget.label) \
+(\(destTarget.configuration))"
+""")
             }
         }
 
