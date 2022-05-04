ae8403051767f39d69c90ce192a7f72a569c6320
Chuck Grindel <chuck@chuckgrindel.com>
2022-05-04 08:35:36 -0600

    Add placeholder for PBXTargets+ExtensionsTests

---
+++ tools/generator/test/PBXTargets+ExtensionsTests.swift
@@ -0,0 +1,18 @@
+import XcodeProj
+import XCTest
+
+@testable import generator
+
+class PBXTargetsExtensionsTests: XCTestCase {
+    func test_nativeTarget_whereKeyDoesNotExist() throws {
+        XCTFail("IMPLEMENT ME!")
+    }
+
+    func test_nativeTarget_whereKeyExistsButNotNativeTarget() throws {
+        XCTFail("IMPLEMENT ME!")
+    }
+
+    func test_nativeTarget_whereKeyExistxAndIsNativeTarget() throws {
+        XCTFail("IMPLEMENT ME!")
+    }
+}
