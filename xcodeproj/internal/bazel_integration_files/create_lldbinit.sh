765cbc99849d28bbec8cd47a3322837193d4d104
Brentley Jones <github@brentleyjones.com>
2022-05-06 10:20:51 -0500

    Improve debugging of Bazel built projects
    This makes three improvements:
    
    1. Sets the working directory to the exec root instead of the project directory. When using `oso_prefix_is_pwd` the paths to archives start with `bazel-out`, which at the project root relies on the fact that the symlink exists, and isn't named something else. At the exec root `bazel-out` is the actual directory.
    
    2. Sets an `target.source-map` entry for `./bazel-out/` to allow breakpoints to hit for generated code.
    
    3. Changes the `target.source-map` entry for `./external/` to `$BAZEL_EXTERNAL`, ensuring that breakpoints work when set from opening a file in the project navigator.
    
    There are still some breakpoint improvements to be made. In particular, when opening a file via Jump to Definition, it will be the fully resolved path. Setting a breakpoint on these will result in a path that these source-maps won't match. I'm working on a fix for that now.

--- xcodeproj/internal/bazel_integration_files/create_lldbinit.sh
+++ xcodeproj/internal/bazel_integration_files/create_lldbinit.sh
@@ -2,16 +2,25 @@
 
 set -euo pipefail
 
-readonly external="$1"
+readonly exec_root="$1"
 
 # Load ~/.lldbinit if it exists
 if [[ -f "$HOME/.lldbinit" ]]; then
   echo "command source ~/.lldbinit"
 fi
 
-# Set `CWD` to `$SRCROOT` so relative paths in binaries work
-echo "platform settings -w \"$SRCROOT\""
+# Set `CWD` to `$exec_root` so relative paths in binaries work
+#
+# This is needed because we use the `oso_prefix_is_pwd` feature, which makes the
+# paths to archives relative to the exec root.
+echo "platform settings -w \"$exec_root\""
 
-# "Undo" `-debug-prefix-map`
-echo "settings set target.source-map ./external/ \"$external\""
+# "Undo" `-debug-prefix-map` for breakpoints
+#
+# This needs to cause the files to match exactly what Xcode set for breakpoints,
+# which is why we don't use `$exec_root` here. Xcode will set the path based
+# on the way you opened a file. If you open a file via the Project navigator,
+# it will use the paths specified below.
+echo "settings set target.source-map ./bazel-out/ \"$GEN_DIR\""
+echo "settings append target.source-map ./external/ \"$BAZEL_EXTERNAL\""
 echo "settings append target.source-map ./ \"$SRCROOT\""
