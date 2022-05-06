f1875dc1c9f643f81b4933b8ac9a57d1efa2a23c
Chuck Grindel <chuck.grindel@robinhood.com>
2022-05-06 11:06:29 -0600

    Add tests for normalizedName and normalizedLabel

--- tools/generator/src/Target+Extensions.swift
+++ tools/generator/src/Target+Extensions.swift
@@ -1,8 +1,12 @@
 extension Target {
+    /// The normalized name is used during target disambiguation. It allows the
+    /// logic to differentiate targets where the names only differ by case.
     var normalizedName: String {
         return name.lowercased()
     }
 
+    /// The normalized lable is used during target disambiguation. It allows the
+    /// logic to differentiate targets where the names only differ by case.
     var normalizedLabel: String {
         return label.lowercased()
     }
