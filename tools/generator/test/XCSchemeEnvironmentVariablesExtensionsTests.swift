b3cda3879c6d195d9c6815a34a14c5d4feeb28e6
Chuck Grindel <chuck.grindel@robinhood.com>
2022-05-13 10:23:57 -0600

    Save progress on env vars tests

--- tools/generator/test/XCSchemeEnvironmentVariablesExtensionsTests.swift
+++ tools/generator/test/XCSchemeEnvironmentVariablesExtensionsTests.swift
@@ -4,7 +4,25 @@ import XCTest
 @testable import generator
 
 class XCSchemeEnvironmentVariablesExtensionsTests: XCTestCase {
-    func testSomething() {
-        XCTFail("IMPLEMENT ME!")
+    func test_bazelLaunchVariables() throws {
+        let result: [XCScheme.EnvironmentVariable] = .bazelLaunchVariables
+        XCTAssertEqual(result.count, 2)
+
+        let variableNames = Set(result.map(\.variable))
+        XCTAssertEqual(
+            variableNames,
+            Set(["BUILD_WORKSPACE_DIRECTORY", "BUILD_WORKING_DIRECTORY"])
+        )
+    }
+
+    func test_createBazelTestVariables() throws {
+        let workspaceName = "MyBazelWorkspace"
+        let result: [XCScheme.EnvironmentVariable] = .createBazelTestVariables(
+            workspaceName: workspaceName
+        )
+        XCTAssertEqual(result.count, 2)
+
+        let variableNames = Set(result.map(\.variable))
+        XCTAssertEqual(variableNames, Set(["TEST_SRCDIR", "TEST_WORKSPACE"]))
     }
 }
