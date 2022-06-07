9ae4e5f01dbd2b8ef5683f4a063d4d94d94840a8
Brentley Jones <github@brentleyjones.com>
2022-06-07 14:21:26 -0500

    Pass through SwiftUI Preview thunk compilation

--- test/fixtures/generator/bwb.xcodeproj/rules_xcodeproj/bazel/ld.sh
+++ test/fixtures/generator/bwb.xcodeproj/rules_xcodeproj/bazel/ld.sh
@@ -2,6 +2,8 @@
 
 set -euo pipefail
 
+passthrough_args=("${@:1}")
+
 while test $# -gt 0
 do
   case $1 in
@@ -10,7 +12,18 @@ do
     printf "\0%s\0" "$ld_version" > "$1"
     break
     ;;
-  esac
 
+  -isysroot)
+    shift
+    # PATH/DEVELOPER_DIR isn't set when this is called, so we need to calculate
+    # the developer dir from the sysroot to invoke clang (used below)
+    developer_dir="${1/\/Platforms\/*/}"
+    ;;
+
+  *.preview-thunk.dylib)
+    # Pass through for SwiftUI Preview thunk compilation
+    exec "$developer_dir/Toolchains/XcodeDefault.xctoolchain/usr/bin/clang" "${passthrough_args[@]}"
+    ;;
+  esac
   shift
 done
