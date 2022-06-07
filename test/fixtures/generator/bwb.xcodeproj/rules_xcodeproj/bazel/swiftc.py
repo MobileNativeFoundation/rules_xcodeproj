64917c9b5dddafe1d12a9a8e08abe9831adeb33b
Brentley Jones <github@brentleyjones.com>
2022-06-07 16:41:11 -0500

    Pass through SwiftUI Preview thunk compilation and linking (#516)

--- test/fixtures/generator/bwb.xcodeproj/rules_xcodeproj/bazel/swiftc.py
+++ test/fixtures/generator/bwb.xcodeproj/rules_xcodeproj/bazel/swiftc.py
@@ -2,6 +2,7 @@
 
 import json
 import os
+import subprocess
 import sys
 from typing import List
 
@@ -9,6 +10,11 @@ def _main() -> None:
     if sys.argv[1:] == ["-v"]:
         os.system("swiftc -v")
         return
+
+    # Pass through for SwiftUI Preview thunk compilation
+    if (any(arg.endswith(".preview-thunk.swift") for arg in sys.argv) and
+        "-output-file-map" not in sys.argv):
+        exit(subprocess.run(["swiftc"] + sys.argv[1:], check=False).returncode)
 
     _touch_deps_files(sys.argv)
 
