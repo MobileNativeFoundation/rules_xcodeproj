1a07886df23274ed034bfb289ca6223152e59292
Brentley Jones <github@brentleyjones.com>
2022-06-08 17:17:13 -0500

    Use the correct version of swiftc for BwB SwiftUI Previews (#519)
    This is the same logic that we use in the ld stub, for the same reason: the `PATH` and `DEVELOPER_DIR` isn't set correctly for us, so we have to infer what `DEVELOPER_DIR` is. We also force the use of the default toolchain, which means this currently doesn't work correctly with custom toolchains.

--- xcodeproj/internal/bazel_integration_files/swiftc.py
+++ xcodeproj/internal/bazel_integration_files/swiftc.py
@@ -2,21 +2,36 @@
 
 import json
 import os
+import re
 import subprocess
 import sys
 from typing import List
 
+DEVELOPER_DIR_PATTERN = r'(.*?/Contents/Developer)/.*'
+
 def _main() -> None:
-    if sys.argv[1:] == ["-v"]:
+    args = sys.argv
+
+    if args[1:] == ["-v"]:
         os.system("swiftc -v")
         return
 
     # Pass through for SwiftUI Preview thunk compilation
-    if (any(arg.endswith(".preview-thunk.swift") for arg in sys.argv) and
-        "-output-file-map" not in sys.argv):
-        exit(subprocess.run(["swiftc"] + sys.argv[1:], check=False).returncode)
+    if (any(arg.endswith(".preview-thunk.swift") for arg in args) and
+        "-output-file-map" not in args):
+        flag = args.index("-sdk")
+        sdk_path = args[flag + 1]
+        match = re.fullmatch(DEVELOPER_DIR_PATTERN, sdk_path)
+        if not match:
+            raise RuntimeError("Failed to parse DEVELOPER_DIR from -sdk")
 
-    _touch_deps_files(sys.argv)
+        # TODO: Make this work with custom toolchains
+        developer_dir = match.group(1)
+        swiftc = f"{developer_dir}/Toolchains/XcodeDefault.xctoolchain/usr/bin/swiftc"
+
+        exit(subprocess.run([swiftc] + args[1:], check=False).returncode)
+
+    _touch_deps_files(args)
 
 
 def _touch_deps_files(args: List[str]) -> None:
