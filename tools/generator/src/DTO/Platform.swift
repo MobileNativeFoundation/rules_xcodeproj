95c8715ee4f47424e7bc62d07a7da6688e2d0951
Brentley Jones <github@brentleyjones.com>
2022-06-14 09:33:03 -0500

    Organize `generator` files a bit

--- tools/generator/src/DTO/Platform.swift
+++ tools/generator/src/DTO/Platform.swift
@@ -1,46 +1,3 @@
-import PathKit
-import XcodeProj
-
-struct Project: Equatable, Decodable {
-    let name: String
-    let bazelWorkspaceName: String
-    let label: String
-    let configuration: String
-    let buildSettings: [String: BuildSetting]
-    var targets: [TargetID: Target]
-    let targetMerges: [TargetID: Set<TargetID>]
-    let invalidTargetMerges: [TargetID: Set<TargetID>]
-    let extraFiles: Set<FilePath>
-}
-
-struct Target: Equatable, Decodable {
-    let name: String
-    let label: String
-    let configuration: String
-    var packageBinDir: Path
-    let platform: Platform
-    let product: Product
-    var isSwift: Bool
-    let testHost: TargetID?
-    var buildSettings: [String: BuildSetting]
-    var searchPaths: SearchPaths
-    var modulemaps: [FilePath]
-    var swiftmodules: [FilePath]
-    let resourceBundles: Set<FilePath>
-    var inputs: Inputs
-    var linkerInputs: LinkerInputs
-    var infoPlist: FilePath?
-    var entitlements: FilePath?
-    var dependencies: Set<TargetID>
-    var outputs: Outputs
-}
-
-struct Product: Equatable, Decodable {
-    let type: PBXProductType
-    let name: String
-    let path: FilePath
-}
-
 struct Platform: Equatable, Hashable, Decodable {
     enum OS: String, Decodable {
         case macOS = "macos"
@@ -55,6 +12,8 @@ struct Platform: Equatable, Hashable, Decodable {
     let minimumOsVersion: String
     let environment: String?
 }
+
+// MARK: - Comparable
 
 extension Platform: Comparable {
     static func < (lhs: Platform, rhs: Platform) -> Bool {
--- tools/generator/src/Generator/DisambiguateTargets.swift
+++ tools/generator/src/Generator/DisambiguateTargets.swift
@@ -472,6 +472,20 @@ struct EnvironmentSystemComponents {
     }
 }
 
+extension ConsolidatedTarget {
+    /// The normalized name is used during target disambiguation. It allows the
+    /// logic to differentiate targets where the names only differ by case.
+    var normalizedName: String {
+        return name.lowercased()
+    }
+
+    /// The normalized label is used during target disambiguation. It allows the
+    /// logic to differentiate targets where the names only differ by case.
+    var normalizedLabel: String {
+        return label.lowercased()
+    }
+}
+
 private extension Target {
     /// A key that corresponds to the most-distinguished string that
     /// `ProductTypeComponents.distinguisher()` can return for this
