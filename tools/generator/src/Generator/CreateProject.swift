29ab6d5140b34299d8eef4e32d45b5afd4bf5da4
Brentley Jones <github@brentleyjones.com>
2022-06-30 09:31:28 -0500

    Use less confusing `INDEX_ENABLE_BUILD_ARENA` resolution
    This sets the relevant values on the `NO` case, and has the unresolved case redirect to it, instead of the other way around.

--- tools/generator/src/Generator/CreateProject.swift
+++ tools/generator/src/Generator/CreateProject.swift
@@ -36,17 +36,17 @@ $(INDEXING_DEPLOYMENT_LOCATION__$(INDEX_ENABLE_BUILD_ARENA)),
             "DSTROOT": "$(PROJECT_TEMP_DIR)",
             "GEN_DIR": "$(LINKS_DIR)/gen_dir",
             "LINKS_DIR": "$(INTERNAL_DIR)/links",
-            "INDEXING_BUILT_PRODUCTS_DIR__": "$(BUILD_DIR)",
-            "INDEXING_BUILT_PRODUCTS_DIR__NO": """
-$(INDEXING_BUILT_PRODUCTS_DIR__)
+            "INDEXING_BUILT_PRODUCTS_DIR__": """
+$(INDEXING_BUILT_PRODUCTS_DIR__NO)
 """,
+            "INDEXING_BUILT_PRODUCTS_DIR__NO": "$(BUILD_DIR)",
             // Index Build doesn't respect `DEPLOYMENT_LOCATION`, but we also
             // don't need the `DYLD_LIBRARY_PATH` fix for it
             "INDEXING_BUILT_PRODUCTS_DIR__YES": "$(CONFIGURATION_BUILD_DIR)",
-            "INDEXING_DEPLOYMENT_LOCATION__": true,
-            "INDEXING_DEPLOYMENT_LOCATION__NO": """
-$(INDEXING_DEPLOYMENT_LOCATION__)
+            "INDEXING_DEPLOYMENT_LOCATION__": """
+$(INDEXING_DEPLOYMENT_LOCATION__NO)
 """,
+            "INDEXING_DEPLOYMENT_LOCATION__NO": true,
             "INDEXING_DEPLOYMENT_LOCATION__YES": false,
             "INSTALL_PATH": "$(BAZEL_PACKAGE_BIN_DIR)/$(TARGET_NAME)/bin",
             "INTERNAL_DIR": """
