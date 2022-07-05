d9a8b13b201e4e07fa67024325d734e415ef86bd
Chuck Grindel <chuck.grindel@robinhood.com>
2022-07-05 15:56:09 -0600

    Make actions optional in XcodeScheme

--- tools/generator/src/DTO/XcodeScheme.swift
+++ tools/generator/src/DTO/XcodeScheme.swift
@@ -1,6 +1,6 @@
 struct XcodeScheme: Equatable, Decodable {
     let name: String
-    let buildAction: XcodeScheme.BuildAction
-    let testAction: XcodeScheme.TestAction
-    let launchAction: XcodeScheme.LaunchAction
+    let buildAction: XcodeScheme.BuildAction?
+    let testAction: XcodeScheme.TestAction?
+    let launchAction: XcodeScheme.LaunchAction?
 }
