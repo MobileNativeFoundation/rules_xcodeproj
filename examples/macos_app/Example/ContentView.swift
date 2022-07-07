4b5724d95dc748a00ea4df7cc23adfe42e69cfd0
Brentley Jones <github@brentleyjones.com>
2022-07-07 09:49:57 -0500

    Add `macos_application` example (#672)
    Also move the `apple_dynamic_framework_import` use case to this, as it can't be used by a command line tool.

---
+++ examples/macos_app/Example/ContentView.swift
@@ -0,0 +1,16 @@
+import ExampleFramework
+import SwiftUI
+
+
+struct ContentView: View {
+    var body: some View {
+        Text(Bar().baz())
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
