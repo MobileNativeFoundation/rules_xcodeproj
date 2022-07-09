f83ead373c3fdf3d2dca3cd9ae202c441fe5c311
Chuck Grindel <chuck.grindel@robinhood.com>
2022-07-09 15:41:04 -0600

    Remove weird comma for now

--- tools/generator/test/Fixtures.swift
+++ tools/generator/test/Fixtures.swift
@@ -1567,7 +1567,7 @@ cp "${SCRIPT_INPUT_FILE_0}" "${SCRIPT_OUTPUT_FILE_0}"
                 PBXSourcesBuildPhase(
                     files: buildFiles([PBXBuildFile(
                         file: elements[.external("a_repo/a.swift")]!
-                    ), ])
+                    )])
                 ),
                 createGeneratedHeaderShellScript(),
             ],
