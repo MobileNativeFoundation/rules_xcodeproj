24f133e7a2c1905baeb8ff46e273ee1b210b1795
Chuck Grindel <chuck.grindel@robinhood.com>
2022-07-11 18:49:37 -0600

    First pass with logic

--- tools/generator/src/Extensions/XcodeScheme+Extensions.swift
+++ tools/generator/src/Extensions/XcodeScheme+Extensions.swift
@@ -19,17 +19,17 @@ extension XcodeScheme.TargetWithID: Comparable {
     }
 }
 
-extension XcodeScheme.TargetWithID: Hashable {
-    func hash(into hasher: inout Hasher) {
-        hasher.combine(id)
-    }
-}
+// extension XcodeScheme.TargetWithID: Hashable {
+//     func hash(into hasher: inout Hasher) {
+//         hasher.combine(id)
+//     }
+// }
 
 // MARK: BazelLabel
 
 extension XcodeScheme {
     struct SchemeLabel {
-        let label: String
+        let label: LabelValue
         let isTopLevel: Bool
     }
 }
@@ -37,48 +37,72 @@ extension XcodeScheme {
 // MARK: Resolve TargetIDs
 
 extension XcodeScheme {
-    // typealias ByBuildSettingC
-    // // The Bazel label as a string
-    // typealias ByLabelValue = String
+    // The Bazel label as a string (Target.label)
+    typealias LabelValue = String
+    // The configuration string (Target.configuration)
+    typealias Configuration = String
 
-    // private func groupTargetWithIDsByLabel(targetWithIDs: [TargetWithID]) -> [String: [Targetg]]
+    typealias LabelValueTargetInfo = (
+        byConfiguration: [Configuration: TargetWithID],
+        // inPlatformOrder: [TargetWithID]
+        best: TargetWithID
+    )
 
-    func resolveTargetIDs(targets _: [TargetID: Target]) -> [String: Set<TargetID>] {
-        // TODO: Consider how to reduce recreating topLevelTargetLabels multiple times. Perhaps,
-        // create TargetID resolver.
-
-        // let targetWithIDs = targets.map { id, target in
-        //     TargetWithID(id: id, target: target)
-        // }
-        // // let targetWithIDsByLabel = Dictionary(grouping: targetWithIDs, by: { $0.target.label })
-
-        // let allTargets = allTargets
-        // let topLevelTargets = allTargets.filter(\.isTopLevel)
-        // let configurations = Set<String>(
-        //     topLevelTargets.isEmpty ? [] : topLevelTargetLabels.map(\.configuration)
-        // )
-
-        // // If no configurations, then pick the "best" configuration
-        // if configurations.isEmpty {
-        //     // TODO: Pick the best config
-        // } else {
-        //     // TODO: Select targets based upon the top-level configurations
-
-        //     // For each label in the scheme,
-        // }
-
-        // Get all of the scheme labels
-        // Collect top-level configurations
-        // For each schemeLabel,
-            // If schemeLabel is top-level, then get the Target-TargetID with the "best" platform
-            // If schemeLabel is not top-level, then collect all of the Target-TargetID for the
-            // top-level configurations
-
-        // let resolvedTargetIDs: [String: Set<TargetID>]
-        // return resolvedTargetIDs
-
+    private func collectTargetInfoByLabelValue(
+        targetWithIDs _: [TargetWithID]
+    ) -> [LabelValue: LabelValueTargetInfo] {
         // TODO: IMPLEMENT ME!
         return [:]
+    }
+
+    func resolveTargetIDs(targets: [TargetID: Target]) throws -> [LabelValue: [TargetID]] {
+        // Get all of the scheme labels
+        let allSchemeLabels = allSchemeLabels
+
+        // Create a list of TargetWithID values for the labels that we care about
+        let allLabelValues = Set(allSchemeLabels.map(\.label))
+        let targetWithIDs = targets
+            // We only need target info for labels explicitly mentioned in the scheme
+            .filter { _, target in
+                allLabelValues.contains(target.label)
+            }
+            .map { id, target in
+                TargetWithID(id: id, target: target)
+            }
+        let targetInfoByLabelValue = collectTargetInfoByLabelValue(targetWithIDs: targetWithIDs)
+
+        // Collect top-level configurations
+        let topLevelLabelValues = allSchemeLabels.filter(\.isTopLevel).map(\.label)
+        let topLevelConfigurations = Set(
+            targetWithIDs
+                .filter { topLevelLabelValues.contains($0.target.label) }
+                .map(\.target.configuration)
+        )
+
+        // For each schemeLabel,
+        var resolvedTargetIDs = [LabelValue: [TargetID]]()
+        for schemeLabel in allSchemeLabels {
+            guard let targetInfo = targetInfoByLabelValue[schemeLabel.label] else {
+                throw PreconditionError(message: """
+Did not find `targetInfo` for label "\(schemeLabel.label)"
+""")
+            }
+
+            let targetIDs: [TargetID]
+            if schemeLabel.isTopLevel {
+                // If schemeLabel is top-level, then get the Target-TargetID with the "best" platform
+                targetIDs = [targetInfo.best.id]
+            } else {
+                // If schemeLabel is not top-level, then collect all of the Target-TargetID for the
+                // top-level configurations
+                targetIDs = topLevelConfigurations
+                    .compactMap { targetInfo.byConfiguration[$0]?.id }
+                    // .flatMap { $0 }
+            }
+            resolvedTargetIDs[schemeLabel.label] = targetIDs
+        }
+
+        return resolvedTargetIDs
     }
 }
 
@@ -94,7 +118,7 @@ extension XcodeScheme {
         return results
     }
 
-    var allLabels: [SchemeLabel] {
+    var allSchemeLabels: [SchemeLabel] {
         let topLevelTargetLabels = topLevelTargetLabels
 
         var results = [SchemeLabel]()
