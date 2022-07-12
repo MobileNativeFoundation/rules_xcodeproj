b45e09eebb96e908a719bc62814c88bdf0be214c
Chuck Grindel <chuck.grindel@robinhood.com>
2022-07-12 10:05:46 -0600

    Add platforms to test

--- tools/generator/test/XcodeScheme+ExtensionsTests.swift
+++ tools/generator/test/XcodeScheme+ExtensionsTests.swift
@@ -29,12 +29,54 @@ class XcodeSchemeExtensionsTests: XCTestCase {
     let applebinwatchOSwatchOSarm64Configuration = "applebin_watchos-watchos_arm64_32-dbg-ST-ffdc9fd07085"
     let applebinwatchOSwatchOSx8664Configuration = "applebin_watchos-watchos_x86_64-dbg-ST-cd006600ac60"
 
-    let iosarm64Platform = Platform(
-        name: "ios-arm64-min15.0",
+    let appletvOSPlatform = Platform(
+        name: "appletvos",
+        os: .tvOS,
+        arch: "arm64",
+        minimumOsVersion: "15.0",
+        environment: nil
+    )
+    let appletvSimulatorPlatform = Platform(
+        name: "appletvsimulator",
+        os: .tvOS,
+        arch: "x86_64",
+        minimumOsVersion: "15.0",
+        environment: Optional("Simulator")
+    )
+    let iphoneOSPlatform = Platform(
+        name: "iphoneos",
         os: .iOS,
         arch: "arm64",
         minimumOsVersion: "15.0",
         environment: nil
+    )
+    let iphoneSimulatorPlatform = Platform(
+        name: "iphonesimulator",
+        os: .iOS,
+        arch: "x86_64",
+        minimumOsVersion: "15.0",
+        environment: Optional("Simulator")
+    )
+    let macOSPlatform = Platform(
+        name: "macosx",
+        os: .macOS,
+        arch: "x86_64",
+        minimumOsVersion: "11.0",
+        environment: nil
+    )
+    let watchOSPlatform = Platform(
+        name: "watchos",
+        os: .watchOS,
+        arch: "arm64_32",
+        minimumOsVersion: "7.0",
+        environment: nil
+    )
+    let watchSimulatorPlatform = Platform(
+        name: "watchsimulator",
+        os: .watchOS,
+        arch: "x86_64",
+        minimumOsVersion: "7.0",
+        environment: Optional("Simulator")
     )
 
     lazy var libIosarm64TargetID: TargetID = .init("\(libLabel) \(iOSarm64Configuration)")
@@ -45,21 +87,4 @@ class XcodeSchemeExtensionsTests: XCTestCase {
     // )
 
     let targets: [TargetID: Target] = [:]
-
-  // examples/multiplatform/Lib:Lib ios-arm64-min15.0-applebin_ios-ios_arm64-dbg-ST-2427ca916465 : "//examples/multiplatform/Lib:Lib"
-  // examples/multiplatform/Lib:Lib ios-x86_64-min15.0-applebin_ios-ios_x86_64-dbg-ST-d619bc5eae76 : "//examples/multiplatform/Lib:Lib"
-  // examples/multiplatform/Lib:Lib macos-x86_64-min11.0-applebin_macos-darwin_x86_64-dbg-ST-7373f6dcb398 : "//examples/multiplatform/Lib:Lib"
-  // examples/multiplatform/Lib:Lib tvos-arm64-min15.0-applebin_tvos-tvos_arm64-dbg-ST-90aac610cf68 : "//examples/multiplatform/Lib:Lib"
-  // examples/multiplatform/Lib:Lib tvos-x86_64-min15.0-applebin_tvos-tvos_x86_64-dbg-ST-9d824d5ada9f : "//examples/multiplatform/Lib:Lib"
-  // examples/multiplatform/Lib:Lib watchos-arm64_32-min7.0-applebin_watchos-watchos_arm64_32-dbg-ST-ffdc9fd07085 : "//examples/multiplatform/Lib:Lib"
-  // examples/multiplatform/Lib:Lib watchos-x86_64-min7.0-applebin_watchos-watchos_x86_64-dbg-ST-cd006600ac60 : "//examples/multiplatform/Lib:Lib"
-  // examples/multiplatform/Tool:Tool applebin_macos-darwin_x86_64-dbg-ST-7373f6dcb398 : "//examples/multiplatform/Tool:Tool"
-  // examples/multiplatform/iOSApp:iOSApp applebin_ios-ios_arm64-dbg-ST-2427ca916465 : "//examples/multiplatform/iOSApp:iOSApp"
-  // examples/multiplatform/iOSApp:iOSApp applebin_ios-ios_x86_64-dbg-ST-d619bc5eae76 : "//examples/multiplatform/iOSApp:iOSApp"
-  // examples/multiplatform/tvOSApp:tvOSApp applebin_tvos-tvos_arm64-dbg-ST-90aac610cf68 : "//examples/multiplatform/tvOSApp:tvOSApp"
-  // examples/multiplatform/tvOSApp:tvOSApp applebin_tvos-tvos_x86_64-dbg-ST-9d824d5ada9f : "//examples/multiplatform/tvOSApp:tvOSApp"
-  // examples/multiplatform/watchOSApp:watchOSApp applebin_watchos-watchos_arm64_32-dbg-ST-ffdc9fd07085 : "//examples/multiplatform/watchOSApp:watchOSApp"
-  // examples/multiplatform/watchOSApp:watchOSApp applebin_watchos-watchos_x86_64-dbg-ST-cd006600ac60 : "//examples/multiplatform/watchOSApp:watchOSApp"
-  // examples/multiplatform/watchOSAppExtension:watchOSAppExtension applebin_watchos-watchos_arm64_32-dbg-ST-ffdc9fd07085 : "//examples/multiplatform/watchOSAppExtension:watchOSAppExtension"
-  // examples/multiplatform/watchOSAppExtension:watchOSAppExtension applebin_watchos-watchos_x86_64-dbg-ST-cd006600ac60 : "//examples/multiplatform/watchOSAppExtension:watchOSAppExtension"
 }
