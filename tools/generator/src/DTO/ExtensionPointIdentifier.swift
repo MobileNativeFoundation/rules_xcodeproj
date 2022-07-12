10925995f9612a6e16c217f71752ba11ea3dffa5
Brentley Jones <github@brentleyjones.com>
2022-07-12 07:50:26 -0500

    Fix parsing of unknown extension point identifiers

--- tools/generator/src/DTO/ExtensionPointIdentifier.swift
+++ tools/generator/src/DTO/ExtensionPointIdentifier.swift
@@ -1,4 +1,10 @@
 enum ExtensionPointIdentifier: String, Decodable {
-    case watchKit = "com.apple.watchkit"
     case widgetKitExtension = "com.apple.widgetkit-extension"
+    case unknown = ""
+
+    init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        let value = try container.decode(String.self)
+        self = .init(rawValue: value) ?? .unknown
+    }
 }
