4e209f80bf66435ef256cf095f7b6d65ca88e979
Chuck Grindel <chuck.grindel@robinhood.com>
2022-07-13 10:46:24 -0600

    First pass at findFirst implementation

--- tools/generator/src/Extensions/Collection+Extensions.swift
+++ tools/generator/src/Extensions/Collection+Extensions.swift
@@ -1,10 +1,27 @@
 extension Collection where Index == TargetID, Element == Target {
     func findFirst<StartTargets: Sequence>(
         // TODO: Switch to BazelLabel
-        label _: XcodeScheme.LabelValue,
-        under _: StartTargets
+        label: XcodeScheme.LabelValue,
+        under startTargetIDs: StartTargets
     ) -> TargetID? where StartTargets.Element == TargetID {
-        // TODO: IMPLEMENT ME!
-        return nil
+        // Collect the targets for the specified targetID values
+        let targetIDPairs = startTargetIDs.map { ($0, self[$0]) }
+
+        // Check if any of the start targets are the desired label.
+        // If not, add their dependencies to the set of targetIDs to check next.
+        var newStartIDs = Set<TargetID>()
+        for (targetID, target) in targetIDPairs {
+            if target.label == label {
+                return targetID
+            }
+            newStartIDs.formUnion(target.dependencies)
+        }
+
+        // If there are no more targetIDs to check, then we are done
+        // Otherwise, keep searching
+        if newStartIDs.isEmpty {
+            return nil
+        }
+        return findFirst(label: label, under: newStartIDs)
     }
 }
