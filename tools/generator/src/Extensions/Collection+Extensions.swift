1ab0c5ec4dc82f3f678edf77b02841ed794fc797
Chuck Grindel <chuck.grindel@robinhood.com>
2022-07-13 11:25:44 -0600

    Update to firstTargetID with a predicate

--- tools/generator/src/Extensions/Collection+Extensions.swift
+++ tools/generator/src/Extensions/Collection+Extensions.swift
@@ -1,17 +1,16 @@
 extension Collection where Index == TargetID, Element == Target {
-    func findFirst<StartTargets: Sequence>(
-        // TODO: Switch to BazelLabel
-        label: XcodeScheme.LabelValue,
-        under startTargetIDs: StartTargets
-    ) -> TargetID? where StartTargets.Element == TargetID {
+    func firstTargetID<StartTargets: Sequence>(
+        under startTargetIDs: StartTargets,
+        where predicate: (TargetID, Target) throws -> Bool
+    ) rethrows -> TargetID? where StartTargets.Element == TargetID {
         // Collect the targets for the specified targetID values
         let targetIDPairs = startTargetIDs.map { ($0, self[$0]) }
 
-        // Check if any of the start targets are the desired label.
+        // Check if any of the start targets satisfy the predicate.
         // If not, add their dependencies to the set of targetIDs to check next.
         var newStartIDs = Set<TargetID>()
         for (targetID, target) in targetIDPairs {
-            if target.label == label {
+            if try predicate(targetID, target) {
                 return targetID
             }
             newStartIDs.formUnion(target.dependencies)
@@ -22,6 +21,32 @@ extension Collection where Index == TargetID, Element == Target {
         if newStartIDs.isEmpty {
             return nil
         }
-        return findFirst(label: label, under: newStartIDs)
+        return try firstTargetID(under: newStartIDs, where: predicate)
     }
+
+    // func firstTargetID<StartTargets: Sequence>(
+    //     // TODO: Switch to BazelLabel
+    //     label: XcodeScheme.LabelValue,
+    //     under startTargetIDs: StartTargets
+    // ) -> TargetID? where StartTargets.Element == TargetID {
+    //     // Collect the targets for the specified targetID values
+    //     let targetIDPairs = startTargetIDs.map { ($0, self[$0]) }
+
+    //     // Check if any of the start targets are the desired label.
+    //     // If not, add their dependencies to the set of targetIDs to check next.
+    //     var newStartIDs = Set<TargetID>()
+    //     for (targetID, target) in targetIDPairs {
+    //         if target.label == label {
+    //             return targetID
+    //         }
+    //         newStartIDs.formUnion(target.dependencies)
+    //     }
+
+    //     // If there are no more targetIDs to check, then we are done
+    //     // Otherwise, keep searching
+    //     if newStartIDs.isEmpty {
+    //         return nil
+    //     }
+    //     return firstTargetID(label: label, under: newStartIDs)
+    // }
 }
--- tools/generator/test/Collection+ExtensionsTests.swift
+++ tools/generator/test/Collection+ExtensionsTests.swift
@@ -2,8 +2,92 @@ import XCTest
 
 @testable import generator
 
+extension CollectionExtensionsTests {
+    func test_firstTargetID_breadthFirstSearch() throws {
+        XCTFail("IMPLEMENT ME!")
+    }
+
+    func test_firstTargetID_doesNotExist() throws {
+        XCTFail("IMPLEMENT ME!")
+    }
+}
+
 class CollectionExtensionsTests: XCTestCase {
-  func testSomething() {
-      XCTFail("IMPLEMENT ME!")
-  }
+    //  A    B
+    //  |    |
+    //  C    D
+    //       |
+    //       E
+    // Targets C and E have the same label, but different configurations
+
+    let fooLabel = "//:foo"
+    let barLabel = "//:bar"
+    let helloLabel = "//:hello"
+    let goodbyeLabel = "//:goodbye"
+
+    let chickenConfiguration = "chicken"
+    let beefConfiguration = "beef"
+
+    lazy var aTargetID: TargetID = .init("\(fooLabel) \(chickenConfiguration)")
+    lazy var aTarget = Target.mock(
+        label: fooLabel,
+        configuration: chickenConfiguration,
+        product: .init(
+            type: .staticLibrary,
+            name: "a",
+            path: .generated("z/A.a")
+        )
+    )
+
+    lazy var bTargetID: TargetID = .init("\(barLabel) \(beefConfiguration)")
+    lazy var bTarget = Target.mock(
+        label: barLabel,
+        configuration: beefConfiguration,
+        product: .init(
+            type: .staticLibrary,
+            name: "a",
+            path: .generated("z/A.a")
+        )
+    )
+
+    lazy var cTargetID: TargetID = .init("\(helloLabel) \(chickenConfiguration)")
+    lazy var cTarget = Target.mock(
+        label: helloLabel,
+        configuration: chickenConfiguration,
+        product: .init(
+            type: .staticLibrary,
+            name: "a",
+            path: .generated("z/A.a")
+        )
+    )
+
+    lazy var dTargetID: TargetID = .init("\(goodbyeLabel) \(beefConfiguration)")
+    lazy var dTarget = Target.mock(
+        label: goodbyeLabel,
+        configuration: beefConfiguration,
+        product: .init(
+            type: .staticLibrary,
+            name: "a",
+            path: .generated("z/A.a")
+        )
+    )
+
+    lazy var eTargetID: TargetID = .init("\(helloLabel) \(beefConfiguration)")
+    lazy var eTarget = Target.mock(
+        label: helloLabel,
+        configuration: beefConfiguration,
+        product: .init(
+            type: .staticLibrary,
+            name: "a",
+            path: .generated("z/A.a")
+        )
+    )
+
+    lazy var targets: [TargetID: Target] = [
+        aTargetID: aTarget,
+        bTargetID: bTarget,
+        cTargetID: cTarget,
+        dTargetID: dTarget,
+        eTargetID: eTarget,
+    ]
 }
