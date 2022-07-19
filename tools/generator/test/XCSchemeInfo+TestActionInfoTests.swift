f277d94bf82fdb38afc3c850fd807ca2b863f04c
Chuck Grindel <chuck.grindel@robinhood.com>
2022-07-19 17:59:23 -0600

    Add test placeholders for TestActionInfo

---
+++ tools/generator/test/XCSchemeInfo+TestActionInfoTests.swift
@@ -0,0 +1,26 @@
+import XcodeProj
+import XCTest
+
+@testable import generator
+
+extension XCSchemeInfoTestActionInfoTests {
+    func test_init_withEmptyTargetInfos() throws {
+        XCTFail("IMPLEMENT ME!")
+    }
+
+    func test_init_withNoTestables() throws {
+        XCTFail("IMPLEMENT ME!")
+    }
+
+    func test_init_allIsWell() throws {
+        XCTFail("IMPLEMENT ME!")
+    }
+}
+
+extension XCSchemeInfoTestActionInfoTests {
+    func test_hostResolution() throws {
+        XCTFail("IMPLEMENT ME!")
+    }
+}
+
+class XCSchemeInfoTestActionInfoTests: XCTestCase {}
