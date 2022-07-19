6bb2e0edad1e13ed2fee01bf81d03ddfdab88af0
Chuck Grindel <chuck.grindel@robinhood.com>
2022-07-19 17:55:23 -0600

    Add test placeholders for ProfileActionInfo

---
+++ tools/generator/test/XCSchemeInfo+ProfileActionInfoTests.swift
@@ -0,0 +1,18 @@
+import XcodeProj
+import XCTest
+
+@testable import generator
+
+extension XCSchemeInfoProfileActionInfoTests {
+    func test_hostResolution() throws {
+        XCTFail("IMPLEMENT ME!")
+    }
+}
+
+extension XCSchemeInfoProfileActionInfoTests {
+    func test_runnable() throws {
+        XCTFail("IMPLEMENT ME!")
+    }
+}
+
+class XCSchemeInfoProfileActionInfoTests: XCTestCase {}
