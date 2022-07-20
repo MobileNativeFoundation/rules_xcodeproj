9d45e7eaff7c29069bbbe1b5d09c3e563e6fc77f
Chuck Grindel <chuck.grindel@robinhood.com>
2022-07-20 12:37:36 -0600

    Add test for host resolution for TestActionInfo

--- tools/generator/test/XCSchemeInfo+TestActionInfoTests.swift
+++ tools/generator/test/XCSchemeInfo+TestActionInfoTests.swift
@@ -19,8 +19,53 @@ extension XCSchemeInfoTestActionInfoTests {
 
 extension XCSchemeInfoTestActionInfoTests {
     func test_hostResolution() throws {
-        XCTFail("IMPLEMENT ME!")
+        let actionInfo = try XCSchemeInfo.TestActionInfo(
+            resolveHostsFor: .init(
+                buildConfigurationName: buildConfigurationName,
+                targetInfos: [unitTestTargetInfo]
+            ),
+            topLevelTargetInfos: []
+        )
+        guard let testActionInfo = actionInfo else {
+            XCTFail("Expected a TestActionInfo.")
+            return
+        }
+        XCTAssertEqual(testActionInfo.buildConfigurationName, buildConfigurationName)
+        for targetInfo in testActionInfo.targetInfos {
+            XCTAssertNotEqual(targetInfo.hostResolution, .unresolved)
+        }
     }
 }
 
-class XCSchemeInfoTestActionInfoTests: XCTestCase {}
+class XCSchemeInfoTestActionInfoTests: XCTestCase {
+    let buildConfigurationName = "Foo"
+
+    lazy var filePathResolver = FilePathResolver(
+        internalDirectoryName: "rules_xcodeproj",
+        workspaceOutputPath: "examples/foo/Foo.xcodeproj"
+    )
+
+    lazy var pbxTargetsDict: [ConsolidatedTarget.Key: PBXTarget] =
+        Fixtures.pbxTargets(
+            in: Fixtures.pbxProj(),
+            consolidatedTargets: Fixtures.consolidatedTargets
+        )
+        .0
+
+    lazy var libraryTarget = pbxTargetsDict["A 1"]!
+    lazy var unitTestTarget = pbxTargetsDict["B 2"]!
+
+    lazy var libraryTargetInfo = XCSchemeInfo.TargetInfo(
+        pbxTarget: libraryTarget,
+        referencedContainer: filePathResolver.containerReference,
+        hostInfos: [],
+        extensionPointIdentifiers: []
+    )
+
+    lazy var unitTestTargetInfo = XCSchemeInfo.TargetInfo(
+        pbxTarget: unitTestTarget,
+        referencedContainer: filePathResolver.containerReference,
+        hostInfos: [],
+        extensionPointIdentifiers: []
+    )
+}
