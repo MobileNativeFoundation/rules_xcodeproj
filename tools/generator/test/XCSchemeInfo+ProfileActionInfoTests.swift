1c82c5dec34eb672a66baa38870444a57eb3ee02
Chuck Grindel <chuck.grindel@robinhood.com>
2022-07-20 14:22:01 -0600

    Add host resolution tests for ProfileActionInfo

--- tools/generator/test/XCSchemeInfo+ProfileActionInfoTests.swift
+++ tools/generator/test/XCSchemeInfo+ProfileActionInfoTests.swift
@@ -4,8 +4,27 @@ import XCTest
 @testable import generator
 
 extension XCSchemeInfoProfileActionInfoTests {
-    func test_hostResolution() throws {
-        XCTFail("IMPLEMENT ME!")
+    func test_hostResolution_withoutProfileActionInfo() throws {
+        let actionInfo = XCSchemeInfo.ProfileActionInfo(
+            resolveHostsFor: nil,
+            topLevelTargetInfos: []
+        )
+        XCTAssertNil(actionInfo)
+    }
+
+    func test_hostResolution_withProfileActionInfo() throws {
+        let actionInfo = XCSchemeInfo.ProfileActionInfo(
+            resolveHostsFor: .init(
+                buildConfigurationName: buildConfigurationName,
+                targetInfo: appTargetInfo
+            ),
+            topLevelTargetInfos: []
+        )
+        guard let profileActionInfo = actionInfo else {
+            XCTFail("Expected a ProfileActionInfo.")
+            return
+        }
+        XCTAssertNotEqual(profileActionInfo.targetInfo.hostResolution, .unresolved)
     }
 }
 
@@ -15,4 +34,27 @@ extension XCSchemeInfoProfileActionInfoTests {
     }
 }
 
-class XCSchemeInfoProfileActionInfoTests: XCTestCase {}
+class XCSchemeInfoProfileActionInfoTests: XCTestCase {
+    let buildConfigurationName = "Foo"
+
+    lazy var filePathResolver = FilePathResolver(
+        internalDirectoryName: "rules_xcodeproj",
+        workspaceOutputPath: "examples/foo/Foo.xcodeproj"
+    )
+
+    lazy var pbxTargetsDict: [ConsolidatedTarget.Key: PBXTarget] =
+        Fixtures.pbxTargets(
+            in: Fixtures.pbxProj(),
+            consolidatedTargets: Fixtures.consolidatedTargets
+        )
+        .0
+
+    lazy var appTarget = pbxTargetsDict["A 2"]!
+
+    lazy var appTargetInfo = XCSchemeInfo.TargetInfo(
+        pbxTarget: appTarget,
+        referencedContainer: filePathResolver.containerReference,
+        hostInfos: [],
+        extensionPointIdentifiers: []
+    )
+}
