93a7dcca1b0674f636203ba98a00b8277b108757
Chuck Grindel <chuck.grindel@robinhood.com>
2022-07-20 16:04:13 -0600

    Add more init tests for SchemeInfo

--- tools/generator/test/XCSchemeInfoTests.swift
+++ tools/generator/test/XCSchemeInfoTests.swift
@@ -5,11 +5,81 @@ import XCTest
 
 extension XCSchemeInfoTests {
     func test_init_noActionInfos() throws {
-        XCTFail("IMPLEMENT ME!")
+        var thrown: Error?
+        XCTAssertThrowsError(try XCSchemeInfo(name: schemeName)) {
+            thrown = $0
+        }
+        guard let preconditionError = thrown as? PreconditionError else {
+            XCTFail("Expected a PreconditionError")
+            return
+        }
+        XCTAssertEqual(preconditionError.message, """
+An `XCSchemeInfo` (\(schemeName)) should have at least one of the following: `buildActionInfo`, \
+`testActionInfo`, `launchActionInfo`.
+""")
+    }
+
+    func test_init_noName() throws {
+        var thrown: Error?
+        XCTAssertThrowsError(try XCSchemeInfo(
+            buildActionInfo: .init(
+                targetInfos: [libraryTargetInfo]
+            )
+        )) {
+            thrown = $0
+        }
+        guard let preconditionError = thrown as? PreconditionError else {
+            XCTFail("Expected a PreconditionError")
+            return
+        }
+        XCTAssertEqual(preconditionError.message, """
+An `XCSchemeInfo` should have at least one of the following: `name`, `nameClosure`.
+""")
     }
 
     func test_init_withActionInfos() throws {
-        XCTFail("IMPLEMENT ME!")
+        let schemeInfo = try XCSchemeInfo(
+            name: schemeName,
+            buildActionInfo: .init(
+                targetInfos: [libraryTargetInfo]
+            ),
+            testActionInfo: .init(
+                buildConfigurationName: buildConfigurationName,
+                targetInfos: [unitTestTargetInfo]
+            ),
+            launchActionInfo: .init(
+                buildConfigurationName: buildConfigurationName,
+                targetInfo: appTargetInfo
+            ),
+            profileActionInfo: .init(
+                buildConfigurationName: buildConfigurationName,
+                targetInfo: appTargetInfo
+            ),
+            analyzeActionInfo: .init(
+                buildConfigurationName: buildConfigurationName
+            ),
+            archiveActionInfo: .init(
+                buildConfigurationName: buildConfigurationName
+            )
+        )
+        XCTAssertEqual(schemeInfo.name, schemeName)
+        XCTAssertNotNil(schemeInfo.buildActionInfo)
+        XCTAssertNotNil(schemeInfo.testActionInfo)
+        XCTAssertNotNil(schemeInfo.launchActionInfo)
+        XCTAssertNotNil(schemeInfo.analyzeActionInfo)
+        XCTAssertNotNil(schemeInfo.archiveActionInfo)
+    }
+
+    func test_init_withNameClosure() throws {
+        let customName = "Sally"
+        let schemeInfo = try XCSchemeInfo(
+            buildActionInfo: .init(
+                targetInfos: [libraryTargetInfo]
+            )
+        ) { _, _, _, _ in
+            return customName
+        }
+        XCTAssertEqual(schemeInfo.name, customName)
     }
 }
 
@@ -29,4 +99,48 @@ extension XCSchemeInfoTests {
     }
 }
 
-class XCSchemeInfoTests: XCTestCase {}
+class XCSchemeInfoTests: XCTestCase {
+    let schemeName = "Foo"
+    let buildConfigurationName = "Bar"
+
+    lazy var filePathResolver = FilePathResolver(
+        internalDirectoryName: "rules_xcodeproj",
+        workspaceOutputPath: "examples/foo/Foo.xcodeproj"
+    )
+
+    lazy var pbxTargetsDict: [ConsolidatedTarget.Key: PBXTarget] =
+        Fixtures.pbxTargets(
+            in: Fixtures.pbxProj(),
+            consolidatedTargets: Fixtures.consolidatedTargets
+        )
+        .0
+
+    lazy var libraryTarget = pbxTargetsDict["A 1"]!
+    lazy var appTarget = pbxTargetsDict["A 2"]!
+    lazy var unitTestTarget = pbxTargetsDict["B 2"]!
+
+    lazy var appHostInfo = XCSchemeInfo.HostInfo(
+        pbxTarget: appTarget,
+        referencedContainer: filePathResolver.containerReference,
+        index: 0
+    )
+
+    lazy var libraryTargetInfo = XCSchemeInfo.TargetInfo(
+        pbxTarget: libraryTarget,
+        referencedContainer: filePathResolver.containerReference,
+        hostInfos: [],
+        extensionPointIdentifiers: []
+    )
+    lazy var appTargetInfo = XCSchemeInfo.TargetInfo(
+        pbxTarget: appTarget,
+        referencedContainer: filePathResolver.containerReference,
+        hostInfos: [],
+        extensionPointIdentifiers: []
+    )
+    lazy var unitTestTargetInfo = XCSchemeInfo.TargetInfo(
+        pbxTarget: unitTestTarget,
+        referencedContainer: filePathResolver.containerReference,
+        hostInfos: [appHostInfo],
+        extensionPointIdentifiers: []
+    )
+}
