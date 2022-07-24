49fcd6d011527d30b79a2906fdd7343e1cb24fd9
Chuck Grindel <chuck@chuckgrindel.com>
2022-07-23 20:36:39 -0600

    Implement inStableOrder for TargetInfo sequence

--- tools/generator/src/Generator/XCSchemeInfo+TargetInfo.swift
+++ tools/generator/src/Generator/XCSchemeInfo+TargetInfo.swift
@@ -162,9 +162,15 @@ extension XCSchemeInfo.TargetInfo {
 // MARK: Sequence Extensions
 
 extension Sequence where Element == XCSchemeInfo.TargetInfo {
+    var inStableOrder: [XCSchemeInfo.TargetInfo] {
+        return sorted { $0.pbxTarget.name < $1.pbxTarget.name }
+    }
+}
+
+extension Sequence where Element == XCSchemeInfo.TargetInfo {
     /// Return all of the buildable references for all of the target infos.
     var buildableReferences: [XCScheme.BuildableReference] {
-        return flatMap(\.buildableReferences)
+        return inStableOrder.flatMap(\.buildableReferences)
     }
 }
 
@@ -177,9 +183,7 @@ extension Sequence where Element == XCSchemeInfo.TargetInfo {
 
 extension Sequence where Element == XCSchemeInfo.TargetInfo {
     func buildPreActions(buildMode: BuildMode) throws -> [XCScheme.ExecutionAction] {
-        // We sort the list so that the actions are serialized in a consistent order.
-        let sorted = sorted { $0.pbxTarget.name < $1.pbxTarget.name }
-        let preActions = try sorted.compactMap { try $0.buildPreAction(buildMode: buildMode) }
+        let preActions = try inStableOrder.compactMap { try $0.buildPreAction(buildMode: buildMode) }
         return [.initBazelBuildOutputGroupsFile] + preActions
     }
 }
