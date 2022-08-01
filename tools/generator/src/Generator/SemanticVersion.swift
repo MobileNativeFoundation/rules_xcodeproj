8dfcf65409d05b9d8f9dc0a102a26b0ebd0521d9
Chuck Grindel <chuck@chuckgrindel.com>
2022-08-01 17:02:03 -0600

    Start adding semantic version to Platform

---
+++ tools/generator/src/Generator/SemanticVersion.swift
@@ -0,0 +1,51 @@
+import Foundation
+
+// Inspired by https://gist.github.com/mjdescy/a805b5b4c49ed79fb240d3886815d5a2
+public struct SemanticVersionNumber: Equatable {
+    public let major: Int
+    public let minor: Int
+    public let patch: Int
+
+    public init?(_ version: String) {
+        let components = version.split(separator: ".")
+        guard components.count == 3 else { return nil }
+
+        self.init(major: String(components[0]),
+                  minor: String(components[1]),
+                  patch: String(components[2]))
+    }
+
+    public init?(major: String, minor: String, patch: String) {
+        guard
+            let majorAsInt = Int(major),
+            let minorAsInt = Int(minor),
+            let patchAsInt = Int(patch)
+            else {
+                return nil
+        }
+
+        self.init(major: majorAsInt,
+                  minor: minorAsInt,
+                  patch: patchAsInt)
+    }
+
+    public init(major: Int, minor: Int, patch: Int) {
+        self.major = major
+        self.minor = minor
+        self.patch = patch
+    }
+}
+
+extension SemanticVersionNumber: Comparable {
+    public static func < (lhs: SemanticVersionNumber, rhs: SemanticVersionNumber) -> Bool {
+        return (lhs.major < rhs.major)
+            || (lhs.major == rhs.major && lhs.minor < rhs.minor)
+            || (lhs.major == rhs.major && lhs.minor == rhs.minor && lhs.patch < rhs.patch)
+    }
+}
+
+extension SemanticVersionNumber: CustomStringConvertible {
+    public var description: String {
+        return "\(major).\(minor).\(patch)"
+    }
+}
