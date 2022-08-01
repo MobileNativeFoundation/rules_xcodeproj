84583638b68738443133e1eda88a971c4d7ebc01
Brentley Jones <github@brentleyjones.com>
2022-08-01 11:19:39 -0500

    Remove unused `SRCROOT` symlink

--- tools/generator/test/Fixtures.swift
+++ tools/generator/test/Fixtures.swift
@@ -1360,9 +1360,8 @@ cd "$BUILD_DIR"
 rm -rf external
 rm -rf bazel-exec-root
 
-ln -sf "$external" external
-ln -sf "$exec_root" bazel-exec-root
-ln -sfn "$PROJECT_DIR" SRCROOT
+ln -s "$external" external
+ln -s "$exec_root" bazel-exec-root
 
 # Create parent directories of generated files, so the project navigator works
 # better faster
