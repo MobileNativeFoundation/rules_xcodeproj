e55d863f400d7dab31eb7fbdd037b22dbc80ad37
Brentley Jones <github@brentleyjones.com>
2022-08-03 16:08:44 -0500

    Fix over-collection of `lldb_context`

--- test/fixtures/tvos_app/bwb.xcodeproj/rules_xcodeproj/swift_debug_settings.py
+++ test/fixtures/tvos_app/bwb.xcodeproj/rules_xcodeproj/swift_debug_settings.py
@@ -34,7 +34,7 @@ _SETTINGS = {
     ]
   },
   "x86_64-apple-tvos15.0.0-simulator ExampleUITests.xctest/ExampleUITests" : {
-    "clang" : "-iquote \"$(PROJECT_DIR)\" -iquote \"$(BAZEL_OUT)/tvos-x86_64-min15.0-applebin_tvos-tvos_x86_64-dbg-ST-d2947f5560a1/bin\" -O0 -DDEBUG=1 -fstack-protector -fstack-protector-all -iquote \"$(PROJECT_DIR)\" -iquote \"$(BAZEL_OUT)/tvos-x86_64-min15.0-applebin_tvos-tvos_x86_64-dbg-ST-d2947f5560a1/bin\" -O0 -fstack-protector -fstack-protector-all",
+    "clang" : "-iquote \"$(PROJECT_DIR)\" -iquote \"$(BAZEL_OUT)/tvos-x86_64-min15.0-applebin_tvos-tvos_x86_64-dbg-ST-d2947f5560a1/bin\" -O0 -DDEBUG=1 -fstack-protector -fstack-protector-all",
     "frameworks" : [
       "$(PLATFORM_DIR)/Developer/Library/Frameworks",
       "$(SDKROOT)/Developer/Library/Frameworks"
