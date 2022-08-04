129aa9cb35bcbe5932f467d507f429463e92003f
Brentley Jones <github@brentleyjones.com>
2022-08-04 14:16:28 -0500

    Upgrade swift-custom-dump to 0.5.0

--- test/fixtures/generator/bwb.xcodeproj/rules_xcodeproj/swift_debug_settings.py
+++ test/fixtures/generator/bwb.xcodeproj/rules_xcodeproj/swift_debug_settings.py
@@ -21,8 +21,8 @@ _SETTINGS = {
     "includes" : [
       "$(BAZEL_OUT)/darwin_x86_64-dbg-ST-5a06a31acc4f/bin/external/com_github_apple_swift_collections",
       "$(BAZEL_OUT)/darwin_x86_64-dbg-ST-5a06a31acc4f/bin/external/com_github_tuist_xcodeproj",
-      "$(BAZEL_OUT)/darwin_x86_64-dbg-ST-5a06a31acc4f/bin/external/com_github_tadija_aexml",
-      "$(BAZEL_OUT)/darwin_x86_64-dbg-ST-5a06a31acc4f/bin/external/com_github_kylef_pathkit"
+      "$(BAZEL_OUT)/darwin_x86_64-dbg-ST-5a06a31acc4f/bin/external/com_github_kylef_pathkit",
+      "$(BAZEL_OUT)/darwin_x86_64-dbg-ST-5a06a31acc4f/bin/external/com_github_tadija_aexml"
     ]
   },
   "x86_64-apple-macosx11.0.0 tests.xctest/Contents/MacOS/tests" : {
@@ -37,8 +37,8 @@ _SETTINGS = {
       "$(BAZEL_OUT)/darwin_x86_64-dbg-ST-5a06a31acc4f/bin/external/com_github_pointfreeco_swift_custom_dump",
       "$(BAZEL_OUT)/darwin_x86_64-dbg-ST-5a06a31acc4f/bin/external/com_github_apple_swift_collections",
       "$(BAZEL_OUT)/darwin_x86_64-dbg-ST-5a06a31acc4f/bin/external/com_github_tuist_xcodeproj",
-      "$(BAZEL_OUT)/darwin_x86_64-dbg-ST-5a06a31acc4f/bin/external/com_github_tadija_aexml",
       "$(BAZEL_OUT)/darwin_x86_64-dbg-ST-5a06a31acc4f/bin/external/com_github_kylef_pathkit",
+      "$(BAZEL_OUT)/darwin_x86_64-dbg-ST-5a06a31acc4f/bin/external/com_github_tadija_aexml",
       "$(BAZEL_OUT)/darwin_x86_64-dbg-ST-5a06a31acc4f/bin/external/com_github_pointfreeco_xctest_dynamic_overlay"
     ]
   }
