45492562671e16213021d450cdf48e3edba98447
Chuck Grindel <chuck@chuckgrindel.com>
2022-08-04 14:52:31 -0600

    Add HostInfo test file

--- tools/generator/src/Generator/XCSchemeInfo+HostInfo.swift
+++ tools/generator/src/Generator/XCSchemeInfo+HostInfo.swift
@@ -3,11 +3,26 @@ import XcodeProj
 extension XCSchemeInfo {
     struct HostInfo: Equatable, Hashable {
         let pbxTarget: PBXTarget
-        let platforms: Set<Platform>
+        // The platforms property is a sorted list of unique platforms.
+        let platforms: [Platform]
         let buildableReference: XCScheme.BuildableReference
         let index: Int
+
+        init<Platforms: Sequence>(
+            pbxTarget: PBXTarget,
+            platforms: Platforms,
+            buildableReference: XCScheme.BuildableReference,
+            index: Int
+        ) where Platforms.Element == Platform {
+             self.pbxTarget = pbxTarget
+             self.platforms = Set(platforms).sorted()
+             self.buildableReference = buildableReference
+             self.index = index
+        }
     }
 }
+
+// MARK: Initializers
 
 extension XCSchemeInfo.HostInfo {
     init<Platforms: Sequence>(
@@ -16,13 +31,15 @@ extension XCSchemeInfo.HostInfo {
         referencedContainer: String,
         index: Int
     ) where Platforms.Element == Platform {
-         self.pbxTarget = pbxTarget
-         self.platforms = Set(platforms)
-         buildableReference = .init(
-             pbxTarget: pbxTarget,
-             referencedContainer: referencedContainer
-         )
-         self.index = index
+        self.init(
+            pbxTarget: pbxTarget,
+            platforms: platforms,
+            buildableReference: .init(
+                pbxTarget: pbxTarget,
+                referencedContainer: referencedContainer
+            ),
+            index: index
+        )
     }
 }
 
@@ -34,5 +51,21 @@ extension XCSchemeInfo.HostInfo {
             buildableReference: pbxTargetInfo.buildableReference,
             index: index
         )
+    }
+}
+
+// MARK: `Comparable`
+
+extension XCSchemeInfo.HostInfo: Comparable {
+    static func < (lhs: XCSchemeInfo.HostInfo, rhs: XCSchemeInfo.HostInfo) -> Bool {
+        // The platforms for HostInfo are sorted with the preferred/best platform first.
+        // Sort HostInfo instances based upon the best platform
+        guard let lhsFirstPlatform = lhs.platforms.first else {
+            return false
+        }
+        guard let rhsFirstPlatform = rhs.platforms.first else {
+            return true
+        }
+        return lhsFirstPlatform < rhsFirstPlatform
     }
 }
---
+++ tools/generator/test/XCSchemeInfo+HostInfoTests.swift
@@ -0,0 +1,17 @@
+import XCTest
+
+@testable import generator
+
+extension XCSchemeInfoHostInfoTests {
+    func test_init_storesUniqSortedPlatforms() throws {
+        XCTFail("IMPLEMENT ME!")
+    }
+}
+
+extension XCSchemeInfoHostInfoTests {
+    func test_Comparable() throws {
+        XCTFail("IMPLEMENT ME!")
+    }
+}
+
+class XCSchemeInfoHostInfoTests: XCTestCase {}
