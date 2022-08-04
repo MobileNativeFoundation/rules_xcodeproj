a666ece1e91d63cac37fbcd5f9a7658329906974
Chuck Grindel <chuck.grindel@gmail.com>
2022-08-04 09:06:42 -0600

    Add default context info to `Dictionary` and `Optional` helpers (#881)
    Add default context info to Dictionary.value(for:context:message:), if no context or message is provided. The default context includes the function name, filename, and line number.
    Add default error message to Optional.orThrow(). The default error message includes the function name, filename, and line number.

--- tools/generator/src/Extensions/Dictionary+Extensions.swift
+++ tools/generator/src/Extensions/Dictionary+Extensions.swift
@@ -3,15 +3,21 @@ extension Dictionary {
     func value(
         for key: Key,
         context: @autoclosure () -> String = "",
-        message: @autoclosure () -> String = ""
+        message: @autoclosure () -> String = "",
+        file: String = #file,
+        line: Int = #line,
+        function: String = #function
     ) throws -> Value {
         return try self[key].orThrow({
             let userMessage = message()
             guard userMessage != "" else {
                 let contextStr = context()
                 let endOfMsg = contextStr.isEmpty ? "" : ", while \(contextStr)"
+                let postMsg = endOfMsg.isEmpty ?
+                    " (function: \(function), file: \(file), line: \(line))" :
+                    ""
                 return """
-Unable to find the `\(Value.self)` for the `\(Key.self)`, "\(key)"\(endOfMsg).
+Unable to find the `\(Value.self)` for the `\(Key.self)`, "\(key)"\(endOfMsg).\(postMsg)
 """
             }
             return userMessage
--- tools/generator/src/Extensions/Optional+Extensions.swift
+++ tools/generator/src/Extensions/Optional+Extensions.swift
@@ -1,7 +1,18 @@
 extension Optional {
-    func orThrow(_ message: @autoclosure () -> String) throws -> Wrapped {
+    func orThrow(
+        _ message: @autoclosure () -> String = "",
+        file: String = #file,
+        line: Int = #line,
+        function: String = #function
+    ) throws -> Wrapped {
         guard let value = self else {
-            throw PreconditionError(message: message())
+            var errMsg = message()
+            if errMsg == "" {
+                errMsg = """
+Expected non-nil value. (function: \(function), file: \(file), line: \(line))
+"""
+            }
+            throw PreconditionError(message: errMsg)
         }
         return value
     }
