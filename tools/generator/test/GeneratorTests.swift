5dfcac93fd1a7fd866ab03a1ef9acb684774d8c2
Chuck Grindel <chuck@chuckgrindel.com>
2022-08-04 09:58:37 -0600

    Rename pbxTarget

--- tools/generator/test/GeneratorTests.swift
+++ tools/generator/test/GeneratorTests.swift
@@ -145,7 +145,7 @@ final class GeneratorTests: XCTestCase {
         let productsGroup = PBXGroup(name: "42")
         let bazelDependenciesTarget = PBXAggregateTarget(name: "BD")
         let pbxTargets: [ConsolidatedTarget.Key: PBXTarget] = [
-            "A": PBXNativeTarget(name: "A (3456a)"),
+            "Y": PBXNativeTarget(name: "Y (3456a)"),
             "I 1": PBXNativeTarget(name: "I1 (3456a)"),
             "I 2": PBXNativeTarget(name: "I2 (3456a)"),
             "WKE": PBXNativeTarget(name: "WKE (3456a)"),
