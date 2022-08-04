ad262f147a8a919bbfa6d5d6ff076a64dde67d55
Chuck Grindel <chuck@chuckgrindel.com>
2022-08-04 15:15:13 -0600

    Add tests for HostInfo

--- tools/generator/test/XCSchemeInfo+HostInfoTests.swift
+++ tools/generator/test/XCSchemeInfo+HostInfoTests.swift
@@ -4,14 +4,42 @@ import XCTest
 
 extension XCSchemeInfoHostInfoTests {
     func test_init_storesUniqSortedPlatforms() throws {
-        XCTFail("IMPLEMENT ME!")
+        let hostInfo = XCSchemeInfo.HostInfo(
+            pbxTarget: try targetResolver.pbxTargetInfos.value(for: "I").pbxTarget,
+            // This is purposely not sorted.
+            platforms: [iOSDevicePlatform, iOSSimulatorPlatform],
+            referencedContainer: filePathResolver.containerReference,
+            index: 0
+        )
+        XCTAssertEqual(hostInfo.platforms, [iOSSimulatorPlatform, iOSDevicePlatform])
     }
 }
 
 extension XCSchemeInfoHostInfoTests {
     func test_Comparable() throws {
-        XCTFail("IMPLEMENT ME!")
+        let iOSApp = XCSchemeInfo.HostInfo(
+            pbxTargetInfo: try targetResolver.pbxTargetInfos.value(for: "I"),
+            index: 0
+        )
+        let macApp = XCSchemeInfo.HostInfo(
+            pbxTargetInfo: try targetResolver.pbxTargetInfos.value(for: "A 2"),
+            index: 1
+        )
+        XCTAssertFalse(iOSApp < macApp, "iOSApp should not be less than macApp")
+        XCTAssertTrue(macApp < iOSApp, "macApp should be less than iOSApp")
     }
 }
 
-class XCSchemeInfoHostInfoTests: XCTestCase {}
+class XCSchemeInfoHostInfoTests: XCTestCase {
+    let iOSDevicePlatform = Platform.device(os: .iOS)
+    let iOSSimulatorPlatform = Platform.simulator(os: .iOS)
+
+    lazy var filePathResolver = FilePathResolver(
+        internalDirectoryName: "rules_xcodeproj",
+        workspaceOutputPath: "examples/foo/Foo.xcodeproj"
+    )
+
+    lazy var targetResolver = Fixtures.targetResolver(
+        referencedContainer: filePathResolver.containerReference
+    )
+}
