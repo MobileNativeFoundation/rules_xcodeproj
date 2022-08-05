b4d1f009ea58c6291ce0a4754e2b06fd15cd3ce4
Chuck Grindel <chuck@chuckgrindel.com>
2022-08-05 10:44:27 -0600

    Add tests for resolve

--- tools/generator/test/XCSchemeInfo+HostInfoTests.swift
+++ tools/generator/test/XCSchemeInfo+HostInfoTests.swift
@@ -37,8 +37,41 @@ extension XCSchemeInfoHostInfoTests {
 // MARK: Host Resolution Tests
 
 extension XCSchemeInfoHostInfoTests {
-    func test_resolve() throws {
-        XCTFail("IMPLEMENT ME!")
+    func test_Collection_resolve_isEmpty() throws {
+        let hostInfos = [XCSchemeInfo.HostInfo]()
+        XCTAssertEqual(hostInfos.resolve(topLevelTargetInfos: topLevelTargetInfos), .none)
+    }
+
+    func test_Collection_resolve_matchTopLevelTarget() throws {
+        // Make sure the expected host info is not first
+        let hostInfos = [watchOSAppHostInfo, macOSAppHostInfo]
+        let result = hostInfos.resolve(topLevelTargetInfos: topLevelTargetInfos)
+        guard case let .selected(selected) = result else {
+            XCTFail("Expected host resolution selected, but was \(result)")
+            return
+        }
+        XCTAssertEqual(selected, macOSAppHostInfo)
+    }
+
+    func test_Collection_resolve_matchTopLevelPlatform() throws {
+        // Make sure the expected host info is not first
+        let hostInfos = [watchOSAppHostInfo, anotheriOSAppHostInfo]
+        let result = hostInfos.resolve(topLevelTargetInfos: topLevelTargetInfos)
+        guard case let .selected(selected) = result else {
+            XCTFail("Expected host resolution selected, but was \(result)")
+            return
+        }
+        XCTAssertEqual(selected, anotheriOSAppHostInfo)
+    }
+
+    func test_Collection_resolve_matchFirst() throws {
+        let hostInfos = [watchOSAppHostInfo]
+        let result = hostInfos.resolve(topLevelTargetInfos: topLevelTargetInfos)
+        guard case let .selected(selected) = result else {
+            XCTFail("Expected host resolution selected, but was \(result)")
+            return
+        }
+        XCTAssertEqual(selected, watchOSAppHostInfo)
     }
 }
 
@@ -57,5 +90,37 @@ class XCSchemeInfoHostInfoTests: XCTestCase {
 
     lazy var targetResolver = Fixtures.targetResolver(
         referencedContainer: filePathResolver.containerReference
+    )
+
+    lazy var macOSAppPBXTargetInfo = targetResolver.pbxTargetInfos["A 2"]!
+    lazy var iOSAppPBXTargetInfo = targetResolver.pbxTargetInfos["I"]!
+    lazy var anotheriOSAppPBXTargetInfo = targetResolver.pbxTargetInfos["AC"]!
+    lazy var watchOSAppPBXTargetInfo = targetResolver.pbxTargetInfos["W"]!
+
+    lazy var macOSAppTargetInfo = XCSchemeInfo.TargetInfo(
+        pbxTargetInfo: macOSAppPBXTargetInfo,
+        hostInfos: []
+    )
+    lazy var iOSAppTargetInfo = XCSchemeInfo.TargetInfo(
+        pbxTargetInfo: iOSAppPBXTargetInfo,
+        hostInfos: []
+    )
+    lazy var topLevelTargetInfos = [macOSAppTargetInfo, iOSAppTargetInfo]
+
+    lazy var macOSAppHostInfo = XCSchemeInfo.HostInfo(
+        pbxTargetInfo: macOSAppPBXTargetInfo,
+        index: 3
+    )
+    lazy var iOSAppHostInfo = XCSchemeInfo.HostInfo(
+        pbxTargetInfo: iOSAppPBXTargetInfo,
+        index: 2
+    )
+    lazy var anotheriOSAppHostInfo = XCSchemeInfo.HostInfo(
+        pbxTargetInfo: anotheriOSAppPBXTargetInfo,
+        index: 4
+    )
+    lazy var watchOSAppHostInfo = XCSchemeInfo.HostInfo(
+        pbxTargetInfo: watchOSAppPBXTargetInfo,
+        index: 1
     )
 }
