MobileNativeFoundation/rules_xcodeproj

commit 51ec0f82416a26f801ba7f8331572ec12ae7714e

Add more docs to `xcodeproj` (#969)

xcodeproj/internal/xcodeproj_macro.bzl
@@ -4,37 +4,60 @@ load(":bazel_labels.bzl", "bazel_labels")
 load(":xcode_schemes.bzl", "xcode_schemes")
 load(":xcodeproj_rule.bzl", _xcodeproj = "xcodeproj")
 
-def xcodeproj(*, name, xcodeproj_rule = _xcodeproj, schemes = None, **kwargs):
-    """Creates an .xcodeproj file in the workspace when run.
+def xcodeproj(
+        *,
+        name,
+        focused_targets = [],
+        project_name = None,
+        schemes = [],
+        top_level_targets,
+        unfocused_targets = [],
+        **kwargs):
+    """Creates an `.xcodeproj` file in the workspace when run.
+
+    The is a wrapper macro for the
+    [actual `xcodeproj` rule](../xcodeproj/internal/xcodeproj_rule.bzl), which
+    can't be used directly. All public API is documented below. The `kwargs`
+    argument will pass forward values for globally available attributes (e.g.
+    `visibility`, `features`, etc.) to the underlying rule.
 
     Args:
-        name: The name of the target.
-        xcodeproj_rule: The actual `xcodeproj` rule. This is overridden during
-            fixture testing. You shouldn't need to set it yourself.
+        name: A unique name for this target.
+        focused_targets: Optional. A `list` of target labels as `string` values.
+            If specified, only these targets will be included in the generated
+            project; all other targets will be excluded, as if they were
+            listed explicitly in the `unfocused_targets` argument. The labels
+            must match transitive dependencies of the targets specified in the
+            `top_level_targets` argument.
+        project_name: Optional. The name to use for the `.xcodeproj` file. If
+            not specified, the value of the `name` argument is used.
         schemes: Optional. A `list` of values returned by
             `xcode_schemes.scheme`.
-        **kwargs: Additional arguments to pass to `xcodeproj_rule`.
+        top_level_targets: A `list` of top-level targets labels.
+        unfocused_targets: Optional. A `list` of target labels as `string`
+            values. Any targets in the transitive dependencies of the targets
+            specified in the `top_level_targets` argument with a matching
+            label will be excluded from the generated project. This overrides
+            any targets specified in the `focused_targets` argument.
+        **kwargs: Additional arguments to pass to the underlying `xcodeproj`
+            rule specified by `xcodeproj_rule`.
     """
     testonly = kwargs.pop("testonly", True)
 
-    project = kwargs.get("project_name", name)
+    if not top_level_targets:
+        fail("`top_level_targets` cannot be empty.")
 
     focused_targets = [
         bazel_labels.normalize(t)
-        for t in kwargs.pop("focused_targets", [])
+        for t in focused_targets
     ]
     unfocused_targets = [
         bazel_labels.normalize(t)
-        for t in kwargs.pop("unfocused_targets", [])
+        for t in unfocused_targets
     ]
 
-    # Combine targets that are specified directly and implicitly via the schemes
-    top_level_targets = [
-        bazel_labels.normalize(t)
-        for t in kwargs.pop("top_level_targets", [])
-    ]
     schemes_json = None
-    if schemes != None:
+    if schemes:
         if unfocused_targets:
             schemes = xcode_schemes.unfocus_schemes(
                 schemes = schemes,
@@ -57,15 +80,18 @@ def xcodeproj(*, name, xcodeproj_rule = _xcodeproj, schemes = None, **kwargs):
     toplevel_cache_buster = native.glob(
         [
             "{}.xcodeproj/rules_xcodeproj/toplevel_cache_buster".format(
-                project,
+                project_name,
             ),
         ],
         allow_empty = True,
     )
 
+    xcodeproj_rule = kwargs.pop("xcodeproj_rule", _xcodeproj)
+
     xcodeproj_rule(
         name = name,
         focused_targets = focused_targets,
+        project_name = project_name,
         schemes_json = schemes_json,
         testonly = testonly,
         top_level_targets = top_level_targets,

xcodeproj/internal/xcodeproj_rule.bzl
@@ -621,24 +621,54 @@ def make_xcodeproj_rule(
             default = False,
         ),
         "bazel_path": attr.string(
+            doc = """\
+The path to the `bazel` binary or wrapper script. If the path is relative it
+will be resolved using the `PATH` environment variable (which is set to
+`/opt/homebrew/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin` in Xcode). If
+you wan to specify a path to a workspace-relative binary, you must prepend the
+path with `./` (e.g. `"./bazelw"`).
+""",
             default = "bazel",
         ),
         "build_mode": attr.string(
+            doc = """\
+The build mode the generated project should use.
+
+If this is set to `"xcode"`, the project will use the Xcode build system to
+build targets. Generated files and unfocused targets (see the `focused_targets`
+and `unfocused_targets` attributes) will be built with Bazel.
+
+If this is set to `"bazel"`, the project will use Bazel to build targets, inside
+of Xcode. The Xcode build system still unavoidably orchestrates some things at a
+high level.
+""",
             default = "xcode",
             values = ["xcode", "bazel"],
         ),
         "focused_targets": attr.string_list(
+            doc = """\
+A `list` of target labels as `string` values. If specified, only these targets
+will be included in the generated project; all other targets will be excluded,
+as if they were listed explicitly in the `unfocused_targets` attribute. The
+labels must match transitive dependencies of the targets specified in the
+`top_level_targets` attribute.
+""",
             default = [],
         ),
-        "project_name": attr.string(),
+        "project_name": attr.string(
+            doc = """\
+The name to use for the `.xcodeproj` file. If not specified, the value of the
+`name` attribute is used.
+""",
+        ),
         "scheme_autogeneration_mode": attr.string(
-            default = "auto",
             doc = "Specifies how Xcode schemes are automatically generated.",
+            default = "auto",
             values = ["auto", "none", "all"],
         ),
         "schemes_json": attr.string(
             doc = """\
-A JSON string representing a list of Xcode schemes to create.\
+A JSON string representing a list of Xcode schemes to create.
 """,
         ),
         "top_level_targets": attr.label_list(
@@ -649,19 +679,29 @@ A JSON string representing a list of Xcode schemes to create.\
             providers = [XcodeProjInfo],
         ),
         "toplevel_cache_buster": attr.label_list(
+            doc = "For internal use only. Do not set this value yourself.",
             allow_empty = True,
             allow_files = True,
-            doc = "For internal use only. Do not set this value yourself.",
         ),
         "unfocused_targets": attr.string_list(
+            doc = """\
+A `list` of target labels as `string` values. Any targets in the transitive
+dependencies of the targets specified in the `top_level_targets` attribute with
+a matching label will be excluded from the generated project. This overrides any
+targets specified in the `focused_targets` attribute.
+""",
             default = [],
         ),
         "_allowlist_function_transition": attr.label(
-            default = "@bazel_tools//tools/allowlists/function_transition_allowlist",
+            default = Label(
+                "@bazel_tools//tools/allowlists/function_transition_allowlist",
+            ),
         ),
         "_base_integration_files": attr.label(
             allow_files = True,
-            default = Label("//xcodeproj/internal/bazel_integration_files:base_integration_files"),
+            default = Label(
+                "//xcodeproj/internal/bazel_integration_files:base_integration_files",
+            ),
         ),
         "_bazel_integration_files": attr.label(
             allow_files = True,
@@ -694,6 +734,7 @@ A JSON string representing a list of Xcode schemes to create.\
     }
 
     return rule(
+        doc = "Creates an `.xcodeproj` file in the workspace when run.",
         cfg = xcodeproj_transition,
         implementation = _xcodeproj_impl,
         attrs = attrs,

commit 581de648e20a9f399d47b0d6dee9613b0f0b5a60

Fix toplevel_cache_buster
Bug introduced with 51ec0f82416a26f801ba7f8331572ec12ae7714e.

xcodeproj/internal/xcodeproj_macro.bzl
@@ -44,6 +44,9 @@ def xcodeproj(
     """
     testonly = kwargs.pop("testonly", True)
 
+    if not project_name:
+        project_name = name
+
     if not top_level_targets:
         fail("`top_level_targets` cannot be empty.")
 