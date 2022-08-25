f847fa80e3abe97eb7d61fa2597429130620d03f
Brentley Jones <github@brentleyjones.com>
2022-08-25 13:11:06 -0500

    Add ability to specify extra Bazel flags

--- xcodeproj/internal/runner.template.sh
+++ xcodeproj/internal/runner.template.sh
@@ -3,6 +3,7 @@
 set -euo pipefail
 
 readonly bazelrc="$PWD/%bazelrc%"
+readonly extra_flags_bazelrc="$PWD/%extra_flags_bazelrc%"
 
 cd "$BUILD_WORKSPACE_DIRECTORY"
 
@@ -13,6 +14,9 @@ bazelrcs=(
 if [[ -s ".bazelrc" ]]; then
   bazelrcs+=("--bazelrc=.bazelrc")
 fi
+if [[ -s "$extra_flags_bazelrc" ]]; then
+  bazelrcs+=("--bazelrc=$extra_flags_bazelrc")
+fi
 
 echo 'Generating "%project_name%.xcodeproj"'
 
@@ -20,5 +24,7 @@ echo 'Generating "%project_name%.xcodeproj"'
   "${bazelrcs[@]}" \
   run \
   --config=rules_xcodeproj_generator \
+  %extra_generator_flags% \
   "%generator_label%" \
-  -- "$@"
+  -- "$@" \
+  --extra_flags_bazelrc "$extra_flags_bazelrc"
