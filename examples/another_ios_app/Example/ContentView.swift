427c77faab6fe41c3fc28550383dca20a7603d88
chuckgrindel <chuck.grindel@robinhood.com>
2022-08-28 14:55:17 -0600

    Create another_ios_app

---
+++ examples/another_ios_app/Example/ContentView.swift
@@ -0,0 +1,17 @@
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Hello, World!")
+                .padding()
+            Text("rules_xcodeproj_key")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
