248625b2a0f6bab782c8eb6d747ceb56faea19da
chiragramani <chirag.ramani7@gmail.com>
2022-09-16 10:48:26 -0700

    Thread Sanitizer Support
    TSan support

--- examples/integration/AppClip/AppClip.swift
+++ examples/integration/AppClip/AppClip.swift
@@ -2,9 +2,27 @@ import SwiftUI
 
 @main
 struct AppClip: App {
+    
+    init() {
+        ThreadSanitizer().test()
+    }
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
     }
 }
+
+// TODO: Once the base approach is approved,
+// create a different test suite for sanitizers that will
+// host all the sanitizers related test-cases.
+struct ThreadSanitizer {
+    func test() {
+        var name = ""
+        DispatchQueue.global().async {
+            name.append("Thread Sanitizer Test")
+        }
+        print(name)
+    }
+}
+
