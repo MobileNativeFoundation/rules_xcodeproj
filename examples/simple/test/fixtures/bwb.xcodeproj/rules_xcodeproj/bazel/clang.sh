e109345d57548b6ed7a72946566dccc3730de61b
Chirag Ramani <chirag.ramani7@gmail.com>
2022-09-21 16:35:24 -0700

    Add version handling in the clang stub (#1137)

--- examples/simple/test/fixtures/bwb.xcodeproj/rules_xcodeproj/bazel/clang.sh
+++ examples/simple/test/fixtures/bwb.xcodeproj/rules_xcodeproj/bazel/clang.sh
@@ -16,6 +16,13 @@ do
   *.o)
     break
     ;;
+  -v)
+    # TODO: Make this work with custom toolchains
+    DEV_DIR_PREFIX=$(awk '{ sub(/.*-isysroot /, ""); sub(/.Contents\/Developer.*/, ""); print}' <<< "${@:1}")
+    clang="$DEV_DIR_PREFIX/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/clang"
+    "$clang" "${@:1}"
+    break
+    ;;
   esac
 
   shift
