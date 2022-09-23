90629457ad0b9191967f1185bc56da86d6170d6e
Brentley Jones <github@brentleyjones.com>
2022-09-23 18:13:46 -0500

    Typo

--- examples/simple/test/fixtures/bwb.xcodeproj/rules_xcodeproj/bazel/calculate_output_groups.py
+++ examples/simple/test/fixtures/bwb.xcodeproj/rules_xcodeproj/bazel/calculate_output_groups.py
@@ -69,12 +69,12 @@ def _calculate_label_and_target_ids(
         for target in build_request["configuredTargets"]:
             label = guid_labels.get(target["guid"])
             if not label:
-                # `BazelDependency`` and the like
+                # `BazelDependency` and the like
                 continue
             full_target_target_ids = guid_target_ids[target["guid"]]
             target_target_ids = (
                 full_target_target_ids.get(command) or
-                # Will only be `null` if `command == "buildFiles"`` and there
+                # Will only be `null` if `command == "buildFiles"` and there
                 # isn't a different compile target id
                 full_target_target_ids["build"]
             )
@@ -161,7 +161,7 @@ def _calculate_guid_labels_and_target_ids(base_objroot):
                     label = value
 
         if not label:
-            # `BazelDependency`` and the like
+            # `BazelDependency` and the like
             continue
 
         target_ids = {
