2f6ccda5665cf45482447265c35d639f88b51f9d
Brentley Jones <github@brentleyjones.com>
2022-10-10 13:45:40 -0500

    Remove unneeded `--experimental_convenience_symlinks=ignore` (#1253)
    It's already set in `xcodeproj.template.bazelrc`.

--- xcodeproj/internal/bazel_integration_files/bazel_build.sh
+++ xcodeproj/internal/bazel_integration_files/bazel_build.sh
@@ -236,7 +236,6 @@ touch "$build_marker"
   --config="$config" \
   --color=yes \
   ${toolchain:+--define=SWIFT_CUSTOM_TOOLCHAIN="$toolchain"} \
-  --experimental_convenience_symlinks=ignore \
   --symlink_prefix=/ \
   "$output_groups_flag" \
   "$GENERATOR_LABEL" \
