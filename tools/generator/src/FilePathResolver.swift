MobileNativeFoundation/rules_xcodeproj

commit 5febe757f6c771e32b82bf640f3db5b2f65d34ab

Memoize `FilePathResolver` results (#1337)
Saves about 2 seconds in a large real world project.

tools/generator/src/FilePathResolver.swift
@@ -1,13 +1,24 @@
 import PathKit
 
-struct FilePathResolver: Equatable {
+// TODO: Make thread safe if we ever go concurrent
+var memoizedPaths: [MemoizationKey: Path] = [:]
+
+struct MemoizationKey: Equatable, Hashable {
+    let resolver: FilePathResolver
+    let transformedFilePath: FilePath
+    let useBazelOut: Bool?
+    let forceFullBuildSettingPath: Bool
+    let mode: FilePathResolver.Mode
+}
+
+struct FilePathResolver: Equatable, Hashable {
     enum Mode {
         case buildSetting
         case script
         case srcRoot
     }
 
-    struct Directories: Equatable {
+    struct Directories: Equatable, Hashable {
         let workspace: Path
         let workspaceComponents: [String]
         let workspaceOutput: Path
@@ -88,12 +99,31 @@ container:\(workspace + directories.workspaceOutput)
         forceFullBuildSettingPath: Bool = false,
         mode: Mode = .buildSetting
     ) throws -> Path {
+        func memoizationKey(_ transformedFilePath: FilePath) -> MemoizationKey {
+            return .init(
+                resolver: self,
+                transformedFilePath: transformedFilePath,
+                useBazelOut: useBazelOut,
+                forceFullBuildSettingPath: forceFullBuildSettingPath,
+                mode: mode
+            )
+        }
+
+        let key: MemoizationKey
+        let path: Path
         switch filePath.type {
         case .project:
             guard filePath.path.normalize() != "." else {
                 // We need to use Bazel's execution root for ".", since includes
                 // can reference things like "external/" and "bazel-out"
                 return "$(PROJECT_DIR)"
+            }
+
+            let transformedFilePath = transform(filePath)
+
+            key = memoizationKey(transformedFilePath)
+            if let memoized = memoizedPaths[key] {
+                return memoized
             }
 
             let projectDir: Path
@@ -105,8 +135,15 @@ container:\(workspace + directories.workspaceOutput)
             case .srcRoot:
                 projectDir = ""
             }
-            return projectDir + transform(filePath).path
+            path = projectDir + transformedFilePath.path
         case .external:
+            let transformedFilePath = transform(filePath)
+
+            key = memoizationKey(transformedFilePath)
+            if let memoized = memoizedPaths[key] {
+                return memoized
+            }
+
             let externalDir: Path
             switch mode {
             case .buildSetting:
@@ -117,7 +154,7 @@ container:\(workspace + directories.workspaceOutput)
             case .srcRoot:
                 externalDir = directories.external
             }
-            return externalDir + transform(filePath).path
+            path = externalDir + transformedFilePath.path
         case .generated:
             let actuallyUseBazelOut: Bool
             let generatedFilePath: FilePath
@@ -137,6 +174,11 @@ container:\(workspace + directories.workspaceOutput)
                 generatedFilePath = transform(filePath)
             }
 
+            key = memoizationKey(generatedFilePath)
+            if let memoized = memoizedPaths[key] {
+                return memoized
+            }
+
             if actuallyUseBazelOut {
                 let bazelOutDir: Path
                 switch mode {
@@ -148,7 +190,7 @@ container:\(workspace + directories.workspaceOutput)
                 case .srcRoot:
                     bazelOutDir = directories.bazelOut
                 }
-                return bazelOutDir + generatedFilePath.path
+                path = bazelOutDir + generatedFilePath.path
             } else {
                 let buildDir: Path
                 switch mode {
@@ -161,9 +203,16 @@ container:\(workspace + directories.workspaceOutput)
 `useBuildDir = true` and `mode` == `.srcRoot`
 """)
                 }
-                return buildDir + "bazel-out" + generatedFilePath.path
+                path = buildDir + "bazel-out" + generatedFilePath.path
             }
         case .internal:
+            let transformedFilePath = transform(filePath)
+
+            key = memoizationKey(transformedFilePath)
+            if let memoized = memoizedPaths[key] {
+                return memoized
+            }
+
             let internalDir: Path
             switch mode {
             case .buildSetting:
@@ -173,7 +222,10 @@ container:\(workspace + directories.workspaceOutput)
             case .srcRoot:
                 internalDir = directories.internal
             }
-            return internalDir + transform(filePath).path
+            path = internalDir + transformedFilePath.path
         }
+
+        memoizedPaths[key] = path
+        return path
     }
 }

commit feda40ca6d42dca7b7278850d22c6807968d80e8

Fix horrible BwX `FilePathResolver` performance (#1360)
Fixes #1359.

Regressed with 5febe757f6c771e32b82bf640f3db5b2f65d34ab.

We were hashing `FilePathResolver` as part of `MemoizationKey`, to
ensure that if there were multiple versions of `FilePathResolver` then
the values wouldn't conflict. That itself isn't much of an issue, except
in BwX mode that would include a large `xcodeGeneratedFiles` dictionary.
Changing `FilePathResolver` to a class and embedding the memoization
into the instance side-steps this issue. In the future
`FilePathResolver` will be in Starlark, removing all of this anyway...

tools/generator/src/FilePathResolver.swift
@@ -1,25 +1,13 @@
 import PathKit
 
-// TODO: Make thread safe if we ever go concurrent
-var memoizedPaths: [MemoizationKey: Path] = [:]
-
-struct MemoizationKey: Equatable, Hashable {
-    let resolver: FilePathResolver
-    let filePath: FilePath
-    let transformedFilePath: FilePath
-    let useBazelOut: Bool?
-    let forceFullBuildSettingPath: Bool
-    let mode: FilePathResolver.Mode
-}
-
-struct FilePathResolver: Equatable, Hashable {
+final class FilePathResolver {
     enum Mode {
         case buildSetting
         case script
         case srcRoot
     }
 
-    struct Directories: Equatable, Hashable {
+    struct Directories: Equatable {
         let workspace: Path
         let workspaceComponents: [String]
         let workspaceOutput: Path
@@ -58,6 +46,17 @@ struct FilePathResolver: Equatable, Hashable {
             }
         }
     }
+
+    struct MemoizationKey: Equatable, Hashable {
+        let filePath: FilePath
+        let transformedFilePath: FilePath
+        let useBazelOut: Bool?
+        let forceFullBuildSettingPath: Bool
+        let mode: FilePathResolver.Mode
+    }
+
+    // TODO: Make thread safe if we ever go concurrent
+    private var memoizedPaths: [MemoizationKey: Path] = [:]
 
     private let directories: Directories
 
@@ -99,7 +98,6 @@ container:\(workspace + directories.workspaceOutput)
     ) throws -> Path {
         func memoizationKey(_ transformedFilePath: FilePath) -> MemoizationKey {
             return .init(
-                resolver: self,
                 filePath: filePath,
                 transformedFilePath: transformedFilePath,
                 useBazelOut: useBazelOut,

tools/generator/test/GeneratorTests.swift
@@ -489,7 +489,6 @@ final class GeneratorTests: XCTestCase {
             let forceBazelDependencies: Bool
             let indexImport: FilePath
             let files: [FilePath: File]
-            let filePathResolver: FilePathResolver
             let bazelConfig: String
             let generatorLabel: BazelLabel
             let generatorConfiguration: String
@@ -521,7 +520,6 @@ final class GeneratorTests: XCTestCase {
                 forceBazelDependencies: forceBazelDependencies,
                 indexImport: indexImport,
                 files: files,
-                filePathResolver: filePathResolver,
                 bazelConfig: bazelConfig,
                 generatorLabel: generatorLabel,
                 generatorConfiguration: generatorConfiguration,
@@ -539,7 +537,6 @@ final class GeneratorTests: XCTestCase {
                 forceBazelDependencies: project.forceBazelDependencies,
                 indexImport: project.indexImport,
                 files: files,
-                filePathResolver: filePathResolver,
                 bazelConfig: project.bazelConfig,
                 generatorLabel: project.generatorLabel,
                 generatorConfiguration: project.configuration,
@@ -557,7 +554,6 @@ final class GeneratorTests: XCTestCase {
             let buildMode: BuildMode
             let products: Products
             let files: [FilePath: File]
-            let filePathResolver: FilePathResolver
             let bazelDependenciesTarget: PBXAggregateTarget?
         }
 
@@ -577,7 +573,6 @@ final class GeneratorTests: XCTestCase {
                 buildMode: buildMode,
                 products: products,
                 files: files,
-                filePathResolver: filePathResolver,
                 bazelDependenciesTarget: bazelDependenciesTarget
             ))
             return pbxTargets
@@ -589,7 +584,6 @@ final class GeneratorTests: XCTestCase {
             buildMode: buildMode,
             products: products,
             files: files,
-            filePathResolver: filePathResolver,
             bazelDependenciesTarget: bazelDependenciesTarget
         )]
 
@@ -604,7 +598,6 @@ final class GeneratorTests: XCTestCase {
             let hostIDs: [TargetID: [TargetID]]
             let hasBazelDependencies: Bool
             let bazelRemappedFiles: [FilePath: FilePath]
-            let filePathResolver: FilePathResolver
         }
 
         var setTargetConfigurationsCalled: [SetTargetConfigurationsCalled] = []
@@ -627,8 +620,7 @@ final class GeneratorTests: XCTestCase {
                 pbxTargets: pbxTargets,
                 hostIDs: hostIDs,
                 hasBazelDependencies: hasBazelDependencies,
-                bazelRemappedFiles: bazelRemappedFiles,
-                filePathResolver: filePathResolver
+                bazelRemappedFiles: bazelRemappedFiles
             ))
         }
 
@@ -641,8 +633,7 @@ final class GeneratorTests: XCTestCase {
                 pbxTargets: pbxTargets,
                 hostIDs: project.targetHosts,
                 hasBazelDependencies: true,
-                bazelRemappedFiles: bazelRemappedFiles,
-                filePathResolver: filePathResolver
+                bazelRemappedFiles: bazelRemappedFiles
             ),
         ]
 