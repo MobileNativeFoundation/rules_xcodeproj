MobileNativeFoundation/rules_xcodeproj

commit 5aec77f2831a70164406d461811bedab7a46e6aa

Set `PROJECT_DIR` to Bazel's execution root (#1260)
By anchoring the project in Bazel's execution root, we can reduce the
amount of path manipulation we need to perform on Bazel provided
compiler and linker flags. This will also allow us to support multiple
output bases for various modes (e.g. SwiftUI Previews or Runtime
Sanitizers), as the `.sourceRoot` (really `PROJECT_DIR`) relative
`PBXBuildFile`'s will adjust as we change `PROJECT_DIR` (which we can
change based on things like `ENABLE_PREVIEWS`).

tools/generator/src/Generator/CreateFilesAndGroups.swift
@@ -358,9 +358,9 @@ extension Generator {
             }
 
             let group = PBXGroup(
-                sourceTree: directories.external.sourceTree,
+                sourceTree: .sourceRoot,
                 name: "Bazel External Repositories",
-                path: directories.external.string
+                path: "../../external"
             )
             pbxProj.add(object: group)
             groups[.external("")] = group
@@ -376,9 +376,9 @@ extension Generator {
             }
 
             let group = PBXGroup(
-                sourceTree: directories.bazelOut.sourceTree,
+                sourceTree: .sourceRoot,
                 name: "Bazel Generated Files",
-                path: directories.bazelOut.string
+                path: "bazel-out"
             )
             pbxProj.add(object: group)
             groups[.generated("")] = group

commit e4e7b471a380d8074f94546fe104154158de4c01

Fix `local_repository` regression
Regressed with 5aec77f2831a70164406d461811bedab7a46e6aa.

`.srcRoot` now means Bazel's execution root. So just like `mainGroup`, we need this to be an absolute path all the time now.

tools/generator/src/Generator/CreateFilesAndGroups.swift
@@ -82,6 +82,7 @@ extension Generator {
     static func createFilesAndGroups(
         in pbxProj: PBXProj,
         buildMode: BuildMode,
+        forFixtures: Bool,
         forceBazelDependencies: Bool,
         targets: [TargetID: Target],
         extraFiles: Set<FilePath>,
@@ -118,7 +119,7 @@ extension Generator {
                 let workspaceDirectoryComponents = directories
                     .workspaceComponents
                 let symlinkComponents = symlinkDest.components
-                if symlinkComponents.starts(
+                if forFixtures && symlinkComponents.starts(
                     with: directories.workspaceComponents
                 ) {
                     let relativeComponents = symlinkComponents.suffix(

tools/generator/src/Generator/Environment.swift
@@ -33,6 +33,7 @@ struct Environment {
     let createFilesAndGroups: (
         _ pbxProj: PBXProj,
         _ buildMode: BuildMode,
+        _ forFixtures: Bool,
         _ forceBazelDependencies: Bool,
         _ targets: [TargetID: Target],
         _ extraFiles: Set<FilePath>,

tools/generator/src/Generator/Generator.swift
@@ -88,6 +88,7 @@ class Generator {
         ) = try environment.createFilesAndGroups(
             pbxProj,
             buildMode,
+            forFixtures,
             project.forceBazelDependencies,
             targets,
             project.extraFiles,

tools/generator/test/CreateFilesAndGroupsTests.swift
@@ -65,6 +65,7 @@ final class CreateFilesAndGroupsTests: XCTestCase {
         ) = try Generator.createFilesAndGroups(
             in: pbxProj,
             buildMode: .xcode,
+            forFixtures: false,
             forceBazelDependencies: false,
             targets: targets,
             extraFiles: extraFiles,
@@ -169,6 +170,7 @@ final class CreateFilesAndGroupsTests: XCTestCase {
         ) = try Generator.createFilesAndGroups(
             in: pbxProj,
             buildMode: .xcode,
+            forFixtures: false,
             forceBazelDependencies: false,
             targets: targets,
             extraFiles: extraFiles,
@@ -283,6 +285,7 @@ final class CreateFilesAndGroupsTests: XCTestCase {
         ) = try Generator.createFilesAndGroups(
             in: pbxProj,
             buildMode: .bazel,
+            forFixtures: false,
             forceBazelDependencies: false,
             targets: targets,
             extraFiles: extraFiles,

tools/generator/test/GeneratorTests.swift
@@ -340,6 +340,7 @@ final class GeneratorTests: XCTestCase {
         func createFilesAndGroups(
             in pbxProj: PBXProj,
             buildMode: BuildMode,
+            _forFixtures: Bool,
             forceBazelDependencies: Bool,
             targets: [TargetID: Target],
             extraFiles: Set<FilePath>,