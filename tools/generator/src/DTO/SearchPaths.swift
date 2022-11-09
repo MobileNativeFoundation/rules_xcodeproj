67e441dd09705f0a92ea7ebe69e09ffa99ce5e4b
Brentley Jones <github@brentleyjones.com>
2022-11-09 10:17:08 -0600

    Set `FRAMEWORK_SEARCH_PATHS` in Starlark
    This is progress towards moving all file path resolution into Starlark.

--- tools/generator/src/DTO/SearchPaths.swift
+++ tools/generator/src/DTO/SearchPaths.swift
@@ -1,10 +1,8 @@
 struct SearchPaths: Equatable {
     let hasIncludes: Bool
-    let frameworkIncludes: [FilePath]
 
-    init(hasIncludes: Bool = false, frameworkIncludes: [FilePath] = []) {
+    init(hasIncludes: Bool = false) {
         self.hasIncludes = hasIncludes
-        self.frameworkIncludes = frameworkIncludes
     }
 }
 
@@ -13,19 +11,11 @@ struct SearchPaths: Equatable {
 extension SearchPaths: Decodable {
     enum CodingKeys: String, CodingKey {
         case hasIncludes
-        case frameworkIncludes
     }
 
     init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
 
         hasIncludes = try container.decode(Bool.self, forKey: .hasIncludes)
-        frameworkIncludes = try container.decodeFilePaths(.frameworkIncludes)
-    }
-}
-
-private extension KeyedDecodingContainer where K == SearchPaths.CodingKeys {
-    func decodeFilePaths(_ key: K) throws -> [FilePath] {
-        return try decodeIfPresent([FilePath].self, forKey: key) ?? []
     }
 }
