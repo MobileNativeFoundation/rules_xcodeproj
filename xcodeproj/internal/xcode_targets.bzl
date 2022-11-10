MobileNativeFoundation/rules_xcodeproj

commit ca39a4fe58f2b67f45331866bafc7c300f737c39

Set `FRAMEWORK_SEARCH_PATHS` in Starlark (#1426)
This is progress towards moving all file path resolution into Starlark.

xcodeproj/internal/xcode_targets.bzl
@@ -10,7 +10,6 @@ load(
     "file_path",
     "file_path_to_dto",
     "normalized_file_path",
-    "parsed_file_path",
 )
 load(":lldb_contexts.bzl", "lldb_contexts")
 load(":platform.bzl", "platform_info")
@@ -339,7 +338,13 @@ def _set_bazel_outputs_product(
         absolute_path = False,
     )
 
-def _set_search_paths(*, build_settings, search_paths_intermediate):
+def _set_search_paths(
+        *,
+        build_mode,
+        build_settings,
+        search_paths_intermediate,
+        xcode_generated_paths,
+        xcode_target):
     _prepend_array_build_setting(
         build_settings = build_settings,
         key = "USER_HEADER_SEARCH_PATHS",
@@ -363,6 +368,61 @@ def _set_search_paths(*, build_settings, search_paths_intermediate):
             build_setting_path(path = path)
             for path in search_paths_intermediate.system_includes
         ],
+    )
+
+    if xcode_target.linker_inputs:
+        frameworks = xcode_target.linker_inputs.dynamic_frameworks
+    else:
+        frameworks = []
+
+    framework_build_setting_paths = {}
+    for file in frameworks:
+        search_path = file.dirname
+        xcode_generated_path = xcode_generated_paths.get(file)
+        if xcode_generated_path:
+            framework_build_setting_paths.setdefault(search_path, {})[True] = (
+                paths.dirname(xcode_generated_path)
+            )
+        else:
+            if build_mode == "xcode":
+                # Since we use `$(FRAMEWORK_SEARCH_PATHS)` in
+                # `PREVIEWS_LD_RUNPATH_SEARCH_PATHS__YES`, we need to fully
+                #  qualify the paths
+                bs_path = build_setting_path(
+                    file = file,
+                    path = search_path,
+                    absolute_path = True,
+                )
+            else:
+                bs_path = search_path
+            framework_build_setting_paths.setdefault(search_path, {})[False] = (
+                bs_path
+            )
+
+    framework_search_paths = []
+    for path in search_paths_intermediate.framework_includes:
+        search_paths = framework_build_setting_paths.get(path)
+        if not search_paths:
+            framework_search_paths.append(
+                build_setting_path(
+                    path = path,
+                    absolute_path = build_mode == "xcode",
+                ),
+            )
+            continue
+
+        xcode_path = search_paths.get(True)
+        if xcode_path:
+            framework_search_paths.append(xcode_path)
+
+        bazel_path = search_paths.get(False)
+        if bazel_path:
+            framework_search_paths.append(bazel_path)
+
+    set_if_true(
+        build_settings,
+        "FRAMEWORK_SEARCH_PATHS",
+        framework_search_paths,
     )
 
 def _set_other_swift_flags(*, build_settings, xcode_target):
@@ -518,32 +578,17 @@ def _xcode_target_to_dto(
         compile_target = xcode_target._compile_target,
     )
 
-    build_settings = structs.to_dict(xcode_target._build_settings)
-    _set_bazel_outputs_product(
-        build_mode = build_mode,
-        build_settings = build_settings,
-        xcode_target = xcode_target,
+    set_if_true(
+        dto,
+        "build_settings",
+        _build_settings_to_dto(
+            build_mode = build_mode,
+            linker_products_map = linker_products_map,
+            search_paths_intermediate = search_paths_intermediate,
+            xcode_generated_paths = xcode_generated_paths,
+            xcode_target = xcode_target,
+        ),
     )
-    _set_other_swift_flags(
-        build_settings = build_settings,
-        xcode_target = xcode_target,
-    )
-    _set_preview_framework_paths(
-        build_mode = build_mode,
-        build_settings = build_settings,
-        linker_products_map = linker_products_map,
-        xcode_target = xcode_target,
-    )
-    _set_search_paths(
-        build_settings = build_settings,
-        search_paths_intermediate = search_paths_intermediate,
-    )
-    _set_swift_include_paths(
-        build_settings = build_settings,
-        xcode_generated_paths = xcode_generated_paths,
-        xcode_target = xcode_target,
-    )
-    set_if_true(dto, "build_settings", build_settings)
 
     set_if_true(
         dto,
@@ -631,6 +676,43 @@ def _xcode_target_to_dto(
     )
 
     return dto, replaced_dependencies
+
+def _build_settings_to_dto(
+        *,
+        build_mode,
+        linker_products_map,
+        search_paths_intermediate,
+        xcode_generated_paths,
+        xcode_target):
+    build_settings = structs.to_dict(xcode_target._build_settings)
+    _set_bazel_outputs_product(
+        build_mode = build_mode,
+        build_settings = build_settings,
+        xcode_target = xcode_target,
+    )
+    _set_other_swift_flags(
+        build_settings = build_settings,
+        xcode_target = xcode_target,
+    )
+    _set_preview_framework_paths(
+        build_mode = build_mode,
+        build_settings = build_settings,
+        linker_products_map = linker_products_map,
+        xcode_target = xcode_target,
+    )
+    _set_search_paths(
+        build_mode = build_mode,
+        build_settings = build_settings,
+        search_paths_intermediate = search_paths_intermediate,
+        xcode_generated_paths = xcode_generated_paths,
+        xcode_target = xcode_target,
+    )
+    _set_swift_include_paths(
+        build_settings = build_settings,
+        xcode_generated_paths = xcode_generated_paths,
+        xcode_target = xcode_target,
+    )
+    return build_settings
 
 def _inputs_to_dto(inputs):
     """Generates a target DTO value for inputs.
@@ -845,14 +927,6 @@ def _search_paths_intermediate_to_dto(search_paths_intermediate):
                              search_paths_intermediate.system_includes or
                              search_paths_intermediate.framework_includes),
     }
-    set_if_true(
-        dto,
-        "framework_includes",
-        [
-            file_path_to_dto(parsed_file_path(path))
-            for path in search_paths_intermediate.framework_includes
-        ],
-    )
     return dto
 
 def _swift_to_dto(outputs):

commit 19fd9dc037e6cece2a5f2a5f55ccc59becc30e18

Fix BwX `FRAMEWORK_SEARCH_PATH` calculation
Regressed with ca39a4fe58f2b67f45331866bafc7c300f737c39.

xcodeproj/internal/xcode_targets.bzl
@@ -373,8 +373,8 @@ def _set_search_paths(
 
     framework_build_setting_paths = {}
     for file in frameworks:
-        search_path = file.dirname
-        xcode_generated_path = xcode_generated_paths.get(file)
+        search_path = paths.dirname(file.dirname)
+        xcode_generated_path = xcode_generated_paths.get(file.path)
         if xcode_generated_path:
             framework_build_setting_paths.setdefault(search_path, {})[True] = (
                 paths.dirname(xcode_generated_path)
@@ -398,6 +398,7 @@ def _set_search_paths(
     for path in search_paths_intermediate.framework_includes:
         search_paths = framework_build_setting_paths.get(path)
         if not search_paths:
+            # Imported frameworks
             if build_mode == "xcode":
                 framework_search_paths.append(build_setting_path(path = path))
             else:

xcodeproj/internal/xcodeproj_rule.bzl
@@ -533,7 +533,7 @@ actual targets: {}
         for file in product.additional_product_files:
             xcode_generated_paths[file.path] = xcode_product_path
         for file in product.framework_files.to_list():
-            xcode_generated_paths[file.dirname] = (
+            xcode_generated_paths[file.path] = (
                 xcode_product_path
             )
 

examples/integration/test/fixtures/bwx.xcodeproj/project.pbxproj
@@ -8212,8 +8212,8 @@
 				ENABLE_TESTING_SEARCH_PATHS = YES;
 				FRAMEWORK_SEARCH_PATHS = (
 					"$(BAZEL_EXTERNAL)/com_github_krzyzanowskim_cryptoswift/CryptoSwift.xcframework/watchos-arm64_i386_x86_64-simulator",
-					"$(BAZEL_OUT)/applebin_watchos-watchos_x86_64-dbg-ST-7b12038e3673/bin/UI/frameworks",
-					"$(BAZEL_OUT)/applebin_watchos-watchos_x86_64-dbg-ST-7b12038e3673/bin/Lib/frameworks",
+					"$(BUILD_DIR)/bazel-out/applebin_watchos-watchos_x86_64-dbg-ST-7b12038e3673/bin/UI",
+					"$(BUILD_DIR)/bazel-out/applebin_watchos-watchos_x86_64-dbg-ST-7b12038e3673/bin/Lib",
 				);
 				GCC_OPTIMIZATION_LEVEL = 0;
 				INFOPLIST_FILE = "$(BAZEL_OUT)/applebin_watchos-watchos_x86_64-dbg-ST-7b12038e3673/bin/watchOSAppExtension/Test/UnitTests/rules_xcodeproj/watchOSAppExtensionUnitTests.__internal__.__test_bundle/Info.plist";
@@ -8452,11 +8452,11 @@
 				EXCLUDED_SOURCE_FILE_NAMES = "$(APPLETVOS_FILES) $(APPLETVSIMULATOR_FILES)";
 				FRAMEWORK_SEARCH_PATHS = (
 					"$(BAZEL_EXTERNAL)/com_github_krzyzanowskim_cryptoswift/CryptoSwift.xcframework/tvos-arm64_x86_64-simulator",
-					"$(BAZEL_OUT)/applebin_tvos-tvos_x86_64-dbg-ST-c922fa386514/bin/Lib/frameworks",
+					"$(BUILD_DIR)/bazel-out/applebin_tvos-tvos_x86_64-dbg-ST-c922fa386514/bin/Lib",
 				);
 				"FRAMEWORK_SEARCH_PATHS[sdk=appletvos*]" = (
 					"$(BAZEL_EXTERNAL)/com_github_krzyzanowskim_cryptoswift/CryptoSwift.xcframework/tvos-arm64",
-					"$(BAZEL_OUT)/applebin_tvos-tvos_arm64-dbg-ST-4104480a806c/bin/Lib/frameworks",
+					"$(BUILD_DIR)/bazel-out/applebin_tvos-tvos_arm64-dbg-ST-4104480a806c/bin/Lib",
 				);
 				GCC_OPTIMIZATION_LEVEL = 0;
 				INCLUDED_SOURCE_FILE_NAMES = "";
@@ -8597,18 +8597,18 @@
 					"$(BAZEL_EXTERNAL)/com_google_google_maps/GoogleMapsCore.xcframework/ios-arm64_x86_64-simulator",
 					"$(BAZEL_EXTERNAL)/com_google_google_maps/GoogleMapsBase.xcframework/ios-arm64_x86_64-simulator",
 					"$(BAZEL_EXTERNAL)/com_github_krzyzanowskim_cryptoswift/CryptoSwift.xcframework/ios-arm64_i386_x86_64-simulator",
-					"$(BAZEL_OUT)/applebin_ios-ios_x86_64-dbg-ST-5e5f1c985307/bin/iOSApp/Source/CoreUtilsObjC/frameworks",
-					"$(BAZEL_OUT)/applebin_ios-ios_x86_64-dbg-ST-5e5f1c985307/bin/UI/frameworks",
-					"$(BAZEL_OUT)/applebin_ios-ios_x86_64-dbg-ST-5e5f1c985307/bin/Lib/frameworks",
+					"$(BUILD_DIR)/bazel-out/applebin_ios-ios_x86_64-dbg-ST-5e5f1c985307/bin/iOSApp/Source/CoreUtilsObjC",
+					"$(BUILD_DIR)/bazel-out/applebin_ios-ios_x86_64-dbg-ST-5e5f1c985307/bin/UI",
+					"$(BUILD_DIR)/bazel-out/applebin_ios-ios_x86_64-dbg-ST-5e5f1c985307/bin/Lib",
 				);
 				"FRAMEWORK_SEARCH_PATHS[sdk=iphoneos*]" = (
 					"$(BAZEL_EXTERNAL)/com_google_google_maps/GoogleMaps.xcframework/ios-arm64",
 					"$(BAZEL_EXTERNAL)/com_google_google_maps/GoogleMapsCore.xcframework/ios-arm64",
 					"$(BAZEL_EXTERNAL)/com_google_google_maps/GoogleMapsBase.xcframework/ios-arm64",
 					"$(BAZEL_EXTERNAL)/com_github_krzyzanowskim_cryptoswift/CryptoSwift.xcframework/ios-arm64_armv7",
-					"$(BAZEL_OUT)/applebin_ios-ios_arm64-dbg-ST-a79bc2d21871/bin/iOSApp/Source/CoreUtilsObjC/frameworks",
-					"$(BAZEL_OUT)/applebin_ios-ios_arm64-dbg-ST-a79bc2d21871/bin/UI/frameworks",
-					"$(BAZEL_OUT)/applebin_ios-ios_arm64-dbg-ST-a79bc2d21871/bin/Lib/frameworks",
+					"$(BUILD_DIR)/bazel-out/applebin_ios-ios_arm64-dbg-ST-a79bc2d21871/bin/iOSApp/Source/CoreUtilsObjC",
+					"$(BUILD_DIR)/bazel-out/applebin_ios-ios_arm64-dbg-ST-a79bc2d21871/bin/UI",
+					"$(BUILD_DIR)/bazel-out/applebin_ios-ios_arm64-dbg-ST-a79bc2d21871/bin/Lib",
 				);
 				GCC_OPTIMIZATION_LEVEL = 0;
 				HEADER_SEARCH_PATHS = (
@@ -9014,13 +9014,13 @@
 				EXCLUDED_SOURCE_FILE_NAMES = "$(APPLETVOS_FILES) $(APPLETVSIMULATOR_FILES)";
 				FRAMEWORK_SEARCH_PATHS = (
 					"$(BAZEL_EXTERNAL)/com_github_krzyzanowskim_cryptoswift/CryptoSwift.xcframework/tvos-arm64_x86_64-simulator",
-					"$(BAZEL_OUT)/applebin_tvos-tvos_x86_64-dbg-ST-c922fa386514/bin/UI/frameworks",
-					"$(BAZEL_OUT)/applebin_tvos-tvos_x86_64-dbg-ST-c922fa386514/bin/Lib/frameworks",
+					"$(BUILD_DIR)/bazel-out/applebin_tvos-tvos_x86_64-dbg-ST-c922fa386514/bin/UI",
+					"$(BUILD_DIR)/bazel-out/applebin_tvos-tvos_x86_64-dbg-ST-c922fa386514/bin/Lib",
 				);
 				"FRAMEWORK_SEARCH_PATHS[sdk=appletvos*]" = (
 					"$(BAZEL_EXTERNAL)/com_github_krzyzanowskim_cryptoswift/CryptoSwift.xcframework/tvos-arm64",
-					"$(BAZEL_OUT)/applebin_tvos-tvos_arm64-dbg-ST-4104480a806c/bin/UI/frameworks",
-					"$(BAZEL_OUT)/applebin_tvos-tvos_arm64-dbg-ST-4104480a806c/bin/Lib/frameworks",
+					"$(BUILD_DIR)/bazel-out/applebin_tvos-tvos_arm64-dbg-ST-4104480a806c/bin/UI",
+					"$(BUILD_DIR)/bazel-out/applebin_tvos-tvos_arm64-dbg-ST-4104480a806c/bin/Lib",
 				);
 				GCC_OPTIMIZATION_LEVEL = 0;
 				INCLUDED_SOURCE_FILE_NAMES = "";
@@ -9225,9 +9225,9 @@
 					"$(BAZEL_EXTERNAL)/com_google_google_maps/GoogleMapsCore.xcframework/ios-arm64_x86_64-simulator",
 					"$(BAZEL_EXTERNAL)/com_google_google_maps/GoogleMapsBase.xcframework/ios-arm64_x86_64-simulator",
 					"$(BAZEL_EXTERNAL)/com_github_krzyzanowskim_cryptoswift/CryptoSwift.xcframework/ios-arm64_i386_x86_64-simulator",
-					"$(BAZEL_OUT)/applebin_ios-ios_x86_64-dbg-ST-5e5f1c985307/bin/iOSApp/Source/CoreUtilsObjC/frameworks",
-					"$(BAZEL_OUT)/applebin_ios-ios_x86_64-dbg-ST-5e5f1c985307/bin/UI/frameworks",
-					"$(BAZEL_OUT)/applebin_ios-ios_x86_64-dbg-ST-5e5f1c985307/bin/Lib/frameworks",
+					"$(BUILD_DIR)/bazel-out/applebin_ios-ios_x86_64-dbg-ST-5e5f1c985307/bin/iOSApp/Source/CoreUtilsObjC",
+					"$(BUILD_DIR)/bazel-out/applebin_ios-ios_x86_64-dbg-ST-5e5f1c985307/bin/UI",
+					"$(BUILD_DIR)/bazel-out/applebin_ios-ios_x86_64-dbg-ST-5e5f1c985307/bin/Lib",
 				);
 				GCC_OPTIMIZATION_LEVEL = 0;
 				GCC_PREPROCESSOR_DEFINITIONS = (
@@ -9357,11 +9357,11 @@
 				EXCLUDED_SOURCE_FILE_NAMES = "$(WATCHOS_FILES) $(WATCHSIMULATOR_FILES)";
 				FRAMEWORK_SEARCH_PATHS = (
 					"$(BAZEL_EXTERNAL)/com_github_krzyzanowskim_cryptoswift/CryptoSwift.xcframework/watchos-arm64_i386_x86_64-simulator",
-					"$(BAZEL_OUT)/applebin_watchos-watchos_x86_64-dbg-ST-7b12038e3673/bin/Lib/frameworks",
+					"$(BUILD_DIR)/bazel-out/applebin_watchos-watchos_x86_64-dbg-ST-7b12038e3673/bin/Lib",
 				);
 				"FRAMEWORK_SEARCH_PATHS[sdk=watchos*]" = (
 					"$(BAZEL_EXTERNAL)/com_github_krzyzanowskim_cryptoswift/CryptoSwift.xcframework/watchos-arm64_32_armv7k",
-					"$(BAZEL_OUT)/applebin_watchos-watchos_arm64_32-dbg-ST-f389b73ae0f8/bin/Lib/frameworks",
+					"$(BUILD_DIR)/bazel-out/applebin_watchos-watchos_arm64_32-dbg-ST-f389b73ae0f8/bin/Lib",
 				);
 				GCC_OPTIMIZATION_LEVEL = 0;
 				INCLUDED_SOURCE_FILE_NAMES = "";
@@ -9999,13 +9999,13 @@
 				EXCLUDED_SOURCE_FILE_NAMES = "$(WATCHOS_FILES) $(WATCHSIMULATOR_FILES)";
 				FRAMEWORK_SEARCH_PATHS = (
 					"$(BAZEL_EXTERNAL)/com_github_krzyzanowskim_cryptoswift/CryptoSwift.xcframework/watchos-arm64_i386_x86_64-simulator",
-					"$(BAZEL_OUT)/applebin_watchos-watchos_x86_64-dbg-ST-7b12038e3673/bin/UI/frameworks",
-					"$(BAZEL_OUT)/applebin_watchos-watchos_x86_64-dbg-ST-7b12038e3673/bin/Lib/frameworks",
+					"$(BUILD_DIR)/bazel-out/applebin_watchos-watchos_x86_64-dbg-ST-7b12038e3673/bin/UI",
+					"$(BUILD_DIR)/bazel-out/applebin_watchos-watchos_x86_64-dbg-ST-7b12038e3673/bin/Lib",
 				);
 				"FRAMEWORK_SEARCH_PATHS[sdk=watchos*]" = (
 					"$(BAZEL_EXTERNAL)/com_github_krzyzanowskim_cryptoswift/CryptoSwift.xcframework/watchos-arm64_32_armv7k",
-					"$(BAZEL_OUT)/applebin_watchos-watchos_arm64_32-dbg-ST-f389b73ae0f8/bin/UI/frameworks",
-					"$(BAZEL_OUT)/applebin_watchos-watchos_arm64_32-dbg-ST-f389b73ae0f8/bin/Lib/frameworks",
+					"$(BUILD_DIR)/bazel-out/applebin_watchos-watchos_arm64_32-dbg-ST-f389b73ae0f8/bin/UI",
+					"$(BUILD_DIR)/bazel-out/applebin_watchos-watchos_arm64_32-dbg-ST-f389b73ae0f8/bin/Lib",
 				);
 				GCC_OPTIMIZATION_LEVEL = 0;
 				INCLUDED_SOURCE_FILE_NAMES = "";
@@ -10082,9 +10082,9 @@
 					"$(BAZEL_EXTERNAL)/com_google_google_maps/GoogleMapsCore.xcframework/ios-arm64_x86_64-simulator",
 					"$(BAZEL_EXTERNAL)/com_google_google_maps/GoogleMapsBase.xcframework/ios-arm64_x86_64-simulator",
 					"$(BAZEL_EXTERNAL)/com_github_krzyzanowskim_cryptoswift/CryptoSwift.xcframework/ios-arm64_i386_x86_64-simulator",
-					"$(BAZEL_OUT)/applebin_ios-ios_x86_64-dbg-ST-5e5f1c985307/bin/iOSApp/Source/CoreUtilsObjC/frameworks",
-					"$(BAZEL_OUT)/applebin_ios-ios_x86_64-dbg-ST-5e5f1c985307/bin/UI/frameworks",
-					"$(BAZEL_OUT)/applebin_ios-ios_x86_64-dbg-ST-5e5f1c985307/bin/Lib/frameworks",
+					"$(BUILD_DIR)/bazel-out/applebin_ios-ios_x86_64-dbg-ST-5e5f1c985307/bin/iOSApp/Source/CoreUtilsObjC",
+					"$(BUILD_DIR)/bazel-out/applebin_ios-ios_x86_64-dbg-ST-5e5f1c985307/bin/UI",
+					"$(BUILD_DIR)/bazel-out/applebin_ios-ios_x86_64-dbg-ST-5e5f1c985307/bin/Lib",
 				);
 				GCC_OPTIMIZATION_LEVEL = 0;
 				GCC_PREPROCESSOR_DEFINITIONS = AWESOME;
@@ -10448,8 +10448,8 @@
 				ENABLE_TESTING_SEARCH_PATHS = YES;
 				FRAMEWORK_SEARCH_PATHS = (
 					"$(BAZEL_EXTERNAL)/com_github_krzyzanowskim_cryptoswift/CryptoSwift.xcframework/tvos-arm64_x86_64-simulator",
-					"$(BAZEL_OUT)/applebin_tvos-tvos_x86_64-dbg-ST-c922fa386514/bin/UI/frameworks",
-					"$(BAZEL_OUT)/applebin_tvos-tvos_x86_64-dbg-ST-c922fa386514/bin/Lib/frameworks",
+					"$(BUILD_DIR)/bazel-out/applebin_tvos-tvos_x86_64-dbg-ST-c922fa386514/bin/UI",
+					"$(BUILD_DIR)/bazel-out/applebin_tvos-tvos_x86_64-dbg-ST-c922fa386514/bin/Lib",
 				);
 				GCC_OPTIMIZATION_LEVEL = 0;
 				INFOPLIST_FILE = "$(BAZEL_OUT)/applebin_tvos-tvos_x86_64-dbg-ST-c922fa386514/bin/tvOSApp/Test/UnitTests/rules_xcodeproj/tvOSAppUnitTests.__internal__.__test_bundle/Info.plist";
@@ -10549,11 +10549,11 @@
 				EXCLUDED_SOURCE_FILE_NAMES = "$(IPHONEOS_FILES) $(IPHONESIMULATOR_FILES)";
 				FRAMEWORK_SEARCH_PATHS = (
 					"$(BAZEL_EXTERNAL)/com_github_krzyzanowskim_cryptoswift/CryptoSwift.xcframework/ios-arm64_i386_x86_64-simulator",
-					"$(BAZEL_OUT)/applebin_ios-ios_x86_64-dbg-ST-5e5f1c985307/bin/Lib/frameworks",
+					"$(BUILD_DIR)/bazel-out/applebin_ios-ios_x86_64-dbg-ST-5e5f1c985307/bin/Lib",
 				);
 				"FRAMEWORK_SEARCH_PATHS[sdk=iphoneos*]" = (
 					"$(BAZEL_EXTERNAL)/com_github_krzyzanowskim_cryptoswift/CryptoSwift.xcframework/ios-arm64_armv7",
-					"$(BAZEL_OUT)/applebin_ios-ios_arm64-dbg-ST-a79bc2d21871/bin/Lib/frameworks",
+					"$(BUILD_DIR)/bazel-out/applebin_ios-ios_arm64-dbg-ST-a79bc2d21871/bin/Lib",
 				);
 				GCC_OPTIMIZATION_LEVEL = 0;
 				INCLUDED_SOURCE_FILE_NAMES = "";