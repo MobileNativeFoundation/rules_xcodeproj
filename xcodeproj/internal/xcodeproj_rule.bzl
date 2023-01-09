MobileNativeFoundation/rules_xcodeproj

commit a7cc6e1de67cf4b12cc4839c76b76f7a062ea0ab

Generate `link.params` via output groups (#1522)
Resolves #1020.

This makes the file generation on-demand, which means only BwX or
SwiftUI Previews will create the file.

xcodeproj/internal/xcodeproj_rule.bzl
@@ -200,6 +200,7 @@ def _process_extra_files(
 
 def _process_targets(
         *,
+        ctx,
         build_mode,
         is_fixture,
         configuration,
@@ -623,7 +624,9 @@ actual targets: {}
             )
 
     target_dtos = {}
-    for xcode_target in focused_targets.values():
+    target_dependencies = {}
+    target_link_params = {}
+    for index, xcode_target in enumerate(focused_targets.values()):
         transitive_dependencies = (
             target_transitive_dependencies[xcode_target.id]
         )
@@ -637,7 +640,8 @@ actual targets: {}
         else:
             additional_scheme_target_ids = None
 
-        dto, replaced_dependencies = xcode_targets.to_dto(
+        dto, replaced_dependencies, link_params = xcode_targets.to_dto(
+            ctx = ctx,
             xcode_target = xcode_target,
             is_fixture = is_fixture,
             additional_scheme_target_ids = additional_scheme_target_ids,
@@ -645,13 +649,56 @@ actual targets: {}
             include_lldb_context = include_lldb_context,
             is_unfocused_dependency = xcode_target.id in unfocused_dependencies,
             linker_products_map = linker_products_map,
+            params_index = index,
             should_include_outputs = should_include_outputs(build_mode),
             unfocused_targets = unfocused_targets,
             target_merges = target_merges,
             xcode_generated_paths = xcode_generated_paths,
         )
         target_dtos[xcode_target.id] = dto
+        target_dependencies[xcode_target.id] = (
+            transitive_dependencies,
+            replaced_dependencies,
+        )
+        if link_params:
+            target_link_params[xcode_target.id] = depset([link_params])
 
+    for xcode_target in focused_targets.values():
+        (
+            transitive_dependencies,
+            replaced_dependencies,
+        ) = target_dependencies[xcode_target.id]
+
+        transitive_link_params = []
+
+        link_params = target_link_params.get(xcode_target.id)
+        if link_params:
+            transitive_link_params.append(link_params)
+
+        for id in transitive_dependencies:
+            merge = target_merges.get(id)
+            if merge:
+                id = merge[0]
+                if id == xcode_target.id:
+                    continue
+            link_params = target_link_params.get(id)
+            if link_params:
+                transitive_link_params.append(link_params)
+
+        compiling_output_group_name = (
+            xcode_target.inputs.compiling_output_group_name
+        )
+        indexstores_output_group_name = (
+            xcode_target.inputs.indexstores_output_group_name
+        )
+        linking_output_group_name = (
+            xcode_target.inputs.linking_output_group_name
+        )
+        bwb_linking_output_group_name = (
+            xcode_target.outputs.linking_output_group_name
+        )
+
+        additional_linking_files = []
         for id in replaced_dependencies:
             if id in transitive_dependencies:
                 continue
@@ -659,17 +706,12 @@ actual targets: {}
             # The replaced dependency is not a transitive dependency, so we
             # need to add its merge in its output groups
 
-            compiling_output_group_name = (
-                xcode_target.inputs.compiling_output_group_name
-            )
-            indexstores_output_group_name = (
-                xcode_target.inputs.indexstores_output_group_name
-            )
-            linking_output_group_name = (
-                xcode_target.inputs.linking_output_group_name
-            )
+            link_params = target_link_params.get(id, None)
+            if link_params:
+                transitive_link_params.append(link_params)
 
             dep_target = focused_targets[id]
+
             dep_compiling_output_group_name = (
                 dep_target.inputs.compiling_output_group_name
             )
@@ -710,11 +752,20 @@ actual targets: {}
                     dep_linking_output_group_name,
                     [],
                 )
-                set_if_true(
-                    additional_generated,
-                    linking_output_group_name,
-                    additional_linking_files,
+
+        if transitive_link_params:
+            if linking_output_group_name:
+                additional_linking_files.extend(transitive_link_params)
+            if bwb_linking_output_group_name:
+                additional_outputs[bwb_linking_output_group_name] = (
+                    transitive_link_params
                 )
+
+        set_if_true(
+            additional_generated,
+            linking_output_group_name,
+            additional_linking_files,
+        )
 
     return (
         focused_targets,
@@ -1235,6 +1286,7 @@ def _xcodeproj_impl(ctx):
         replacement_labels_by_label,
         configurations_map,
     ) = _process_targets(
+        ctx = ctx,
         build_mode = build_mode,
         is_fixture = is_fixture,
         configuration = configuration,

examples/cc/test/fixtures/bwb.xcodeproj/project.pbxproj
@@ -101,6 +101,7 @@
 		889162C3C2543E5EA1748185 /* private.h */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.c.h; path = private.h; sourceTree = "<group>"; };
 		929141189779333F1DDD490C /* BUILD */ = {isa = PBXFileReference; explicitFileType = text.script.python; path = BUILD; sourceTree = "<group>"; };
 		9E3FDAF45933E360CDCB3BAC /* lib.c */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.c.c; path = lib.c; sourceTree = "<group>"; };
+		A1891B8AF0EBEE66588DA614 /* tool.7.link.params */ = {isa = PBXFileReference; path = tool.7.link.params; sourceTree = "<group>"; };
 		D14C8349EFC6309D19C1FFFD /* lib.c */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.c.c; path = lib.c; sourceTree = "<group>"; };
 		D20093170A54E92A741D748D /* BUILD */ = {isa = PBXFileReference; explicitFileType = text.script.python; path = BUILD; sourceTree = "<group>"; };
 		E475F19E9E0AF14E1F46E023 /* liblib_impl.a */ = {isa = PBXFileReference; explicitFileType = "compiled.mach-o.dylib"; includeInIndex = 0; name = liblib_impl.a; path = "bazel-out/darwin_x86_64-dbg-STABLE-1/bin/external/examples_cc_external/liblib_impl.a"; sourceTree = BUILT_PRODUCTS_DIR; };
@@ -108,6 +109,30 @@
 /* End PBXFileReference section */
 
 /* Begin PBXGroup section */
+		0AA83B642ECA8286582764C1 /* fixtures */ = {
+			isa = PBXGroup;
+			children = (
+				1D48CBCAC6B7EE027B759A78 /* xcodeproj_bwb.generator-params */,
+			);
+			path = fixtures;
+			sourceTree = "<group>";
+		};
+		1BEF0788635BF32C08EDB7E2 /* test */ = {
+			isa = PBXGroup;
+			children = (
+				0AA83B642ECA8286582764C1 /* fixtures */,
+			);
+			path = test;
+			sourceTree = "<group>";
+		};
+		1D48CBCAC6B7EE027B759A78 /* xcodeproj_bwb.generator-params */ = {
+			isa = PBXGroup;
+			children = (
+				A1891B8AF0EBEE66588DA614 /* tool.7.link.params */,
+			);
+			path = "xcodeproj_bwb.generator-params";
+			sourceTree = "<group>";
+		};
 		37E3120547BEDCC42886C663 /* lib2 */ = {
 			isa = PBXGroup;
 			children = (
@@ -117,6 +142,15 @@
 			);
 			path = lib2;
 			sourceTree = "<group>";
+		};
+		38D12FD81F17842A5E5276F9 /* Bazel Generated Files */ = {
+			isa = PBXGroup;
+			children = (
+				EE297CCC0A7F8A721EA38521 /* darwin_x86_64-dbg-STABLE-0 */,
+			);
+			name = "Bazel Generated Files";
+			path = "bazel-out";
+			sourceTree = SOURCE_ROOT;
 		};
 		3BCB286303B532709B354800 /* includes */ = {
 			isa = PBXGroup;
@@ -181,6 +215,7 @@
 				F1024C07D3F609275C658D3F /* test */,
 				5C981C5BDF60FD9AF790B0BB /* tool */,
 				E35FE3EB7C97DE1172A312B1 /* Bazel External Repositories */,
+				38D12FD81F17842A5E5276F9 /* Bazel Generated Files */,
 				891FF4EFB55BD0CCB8B61ED5 /* Products */,
 				E41C6FEE91F3B36078EC9630 /* Frameworks */,
 			);
@@ -193,6 +228,14 @@
 				412E979B4EDBD9EAE95D73AB /* private.inc */,
 			);
 			path = private;
+			sourceTree = "<group>";
+		};
+		C1030DAB4E164A910DF09385 /* bin */ = {
+			isa = PBXGroup;
+			children = (
+				1BEF0788635BF32C08EDB7E2 /* test */,
+			);
+			path = bin;
 			sourceTree = "<group>";
 		};
 		C22F6A101E0D226EBF37333E /* examples_cc_external */ = {
@@ -220,6 +263,14 @@
 			children = (
 			);
 			name = Frameworks;
+			sourceTree = "<group>";
+		};
+		EE297CCC0A7F8A721EA38521 /* darwin_x86_64-dbg-STABLE-0 */ = {
+			isa = PBXGroup;
+			children = (
+				C1030DAB4E164A910DF09385 /* bin */,
+			);
+			path = "darwin_x86_64-dbg-STABLE-0";
 			sourceTree = "<group>";
 		};
 		F1024C07D3F609275C658D3F /* test */ = {
@@ -544,7 +595,7 @@
 			);
 			runOnlyForDeploymentPostprocessing = 0;
 			shellPath = /bin/sh;
-			shellScript = "set -x\n\nif [[ \"${ENABLE_PREVIEWS:-}\" == \"YES\" ]]; then\nperl -pe 's/^(\"?)(.*\\$\\(.*\\).*?)(\"?)$/\"$2\"/ ; s/\\$(\\()?([a-zA-Z_]\\w*)(?(1)\\))/$ENV{$2}/g' \\\n  \"$SCRIPT_INPUT_FILE_0\" > \"$SCRIPT_OUTPUT_FILE_0\"\nelse\n  touch \"$SCRIPT_OUTPUT_FILE_0\"\nfi\n";
+			shellScript = "set -euo pipefail\n\nif [[ \"${ENABLE_PREVIEWS:-}\" == \"YES\" ]]; then\nperl -pe 's/^(\"?)(.*\\$\\(.*\\).*?)(\"?)$/\"$2\"/ ; s/\\$(\\()?([a-zA-Z_]\\w*)(?(1)\\))/$ENV{$2}/g' \\\n  \"$SCRIPT_INPUT_FILE_0\" > \"$SCRIPT_OUTPUT_FILE_0\"\nelse\n  touch \"$SCRIPT_OUTPUT_FILE_0\"\nfi\n";
 			showEnvVarsInLog = 0;
 		};
 		8AA0267F679300ED1FA6DF71 /* Copy Bazel Outputs / Generate Bazel Dependencies (Index Build) */ = {
@@ -574,6 +625,7 @@
 			name = "Bazel Build";
 			outputFileListPaths = (
 				"$(INTERNAL_DIR)/external.xcfilelist",
+				"$(INTERNAL_DIR)/generated.xcfilelist",
 			);
 			outputPaths = (
 			);
@@ -943,7 +995,7 @@
 					"SECRET_2=\\\"World!\\\"",
 					"EXTERNAL_SECRET_2=\\\"World?\\\"",
 				);
-				LINK_PARAMS_FILE = "$(INTERNAL_DIR)/targets/darwin_x86_64-dbg-STABLE-1/tool/tool.link.params";
+				LINK_PARAMS_FILE = "$(BAZEL_OUT)/darwin_x86_64-dbg-STABLE-0/bin/test/fixtures/xcodeproj_bwb.generator-params/tool.7.link.params";
 				MACOSX_DEPLOYMENT_TARGET = 12.0;
 				OTHER_CFLAGS = (
 					"-ivfsoverlay",

examples/cc/test/fixtures/bwb.xcodeproj/rules_xcodeproj/generated.xcfilelist
@@ -1,0 +1,1 @@
+$(BAZEL_OUT)/darwin_x86_64-dbg-STABLE-0/bin/test/fixtures/xcodeproj_bwb.generator-params/tool.7.link.params

examples/cc/test/fixtures/bwx.xcodeproj/project.pbxproj
@@ -11,7 +11,7 @@
 			isa = PBXAggregateTarget;
 			buildConfigurationList = A4C19F2824EAB3EBB5D7EE24 /* Build configuration list for PBXAggregateTarget "BazelDependencies" */;
 			buildPhases = (
-				790733B6E675C2785F8020AC /* Fetch External Repositories */,
+				773C70B69E7F801B38FDC01C /* Generate Files */,
 				2FB8CA190C5FD287F83F39E3 /* Create swift_debug_settings.py */,
 			);
 			dependencies = (
@@ -139,6 +139,7 @@
 /* Begin PBXFileReference section */
 		10D21E69D7E12FFDA82B1273 /* lib.h */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.c.h; path = lib.h; sourceTree = "<group>"; };
 		1F8D330113018E003269E782 /* liblib_impl.a */ = {isa = PBXFileReference; explicitFileType = "compiled.mach-o.dylib"; includeInIndex = 0; name = liblib_impl.a; path = "bazel-out/darwin_x86_64-dbg-STABLE-1/bin/external/examples_cc_external/liblib_impl.a"; sourceTree = BUILT_PRODUCTS_DIR; };
+		50EE84464C0D09BEA535AD55 /* tool.7.link.params */ = {isa = PBXFileReference; path = tool.7.link.params; sourceTree = "<group>"; };
 		655197F36E3561F5EBB2B456 /* lib.h */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.c.h; path = lib.h; sourceTree = "<group>"; };
 		82E1AC498ACAE9E123276B7A /* BUILD */ = {isa = PBXFileReference; explicitFileType = text.script.python; path = BUILD; sourceTree = "<group>"; };
 		9EA769DDCBCA01C4A4A674AF /* lib.c */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.c.c; path = lib.c; sourceTree = "<group>"; };
@@ -165,6 +166,7 @@
 				CE093984B3218CD2FC972D09 /* test */,
 				8EC0A5AD173DDF13D9361EBE /* tool */,
 				29CFFE07E6D74CFE2F61B31F /* Bazel External Repositories */,
+				6DEE7775A87F3AD8AD281743 /* Bazel Generated Files */,
 				C5DA04B2480EBE8D895B5826 /* Products */,
 				2A84A6FD612A2BEF44B2014C /* Frameworks */,
 			);
@@ -188,11 +190,27 @@
 			path = ../../external;
 			sourceTree = SOURCE_ROOT;
 		};
+		2A74A0BABCA13EF4011BFD00 /* bin */ = {
+			isa = PBXGroup;
+			children = (
+				9401D751AA6B1781111B8489 /* test */,
+			);
+			path = bin;
+			sourceTree = "<group>";
+		};
 		2A84A6FD612A2BEF44B2014C /* Frameworks */ = {
 			isa = PBXGroup;
 			children = (
 			);
 			name = Frameworks;
+			sourceTree = "<group>";
+		};
+		362DEFEF499FD1FCF27C95D3 /* fixtures */ = {
+			isa = PBXGroup;
+			children = (
+				BE534ACECD6842E3D0C7EDA6 /* xcodeproj_bwx.generator-params */,
+			);
+			path = fixtures;
 			sourceTree = "<group>";
 		};
 		6033DA143D6CA2B97C1808E7 /* private */ = {
@@ -202,6 +220,15 @@
 			);
 			path = private;
 			sourceTree = "<group>";
+		};
+		6DEE7775A87F3AD8AD281743 /* Bazel Generated Files */ = {
+			isa = PBXGroup;
+			children = (
+				E9FB62E17E9CD717293583B0 /* darwin_x86_64-dbg-STABLE-0 */,
+			);
+			name = "Bazel Generated Files";
+			path = "bazel-out";
+			sourceTree = SOURCE_ROOT;
 		};
 		8C751C8E5D51A45F0E89CB8F /* private */ = {
 			isa = PBXGroup;
@@ -220,6 +247,14 @@
 			path = tool;
 			sourceTree = "<group>";
 		};
+		9401D751AA6B1781111B8489 /* test */ = {
+			isa = PBXGroup;
+			children = (
+				362DEFEF499FD1FCF27C95D3 /* fixtures */,
+			);
+			path = test;
+			sourceTree = "<group>";
+		};
 		B93A94C580A3732A5B346592 /* examples_cc_external */ = {
 			isa = PBXGroup;
 			children = (
@@ -230,6 +265,14 @@
 			name = examples_cc_external;
 			path = external;
 			sourceTree = SOURCE_ROOT;
+		};
+		BE534ACECD6842E3D0C7EDA6 /* xcodeproj_bwx.generator-params */ = {
+			isa = PBXGroup;
+			children = (
+				50EE84464C0D09BEA535AD55 /* tool.7.link.params */,
+			);
+			path = "xcodeproj_bwx.generator-params";
+			sourceTree = "<group>";
 		};
 		C5DA04B2480EBE8D895B5826 /* Products */ = {
 			isa = PBXGroup;
@@ -256,6 +299,14 @@
 				B13DA8B967470B62463A7C36 /* BUILD */,
 			);
 			path = fixtures;
+			sourceTree = "<group>";
+		};
+		E9FB62E17E9CD717293583B0 /* darwin_x86_64-dbg-STABLE-0 */ = {
+			isa = PBXGroup;
+			children = (
+				2A74A0BABCA13EF4011BFD00 /* bin */,
+			);
+			path = "darwin_x86_64-dbg-STABLE-0";
 			sourceTree = "<group>";
 		};
 		F1B323372508EB9B5C75D7CE /* lib2 */ = {
@@ -591,7 +642,7 @@
 			shellScript = "\"$SCRIPT_INPUT_FILE_0\"\n";
 			showEnvVarsInLog = 0;
 		};
-		790733B6E675C2785F8020AC /* Fetch External Repositories */ = {
+		773C70B69E7F801B38FDC01C /* Generate Files */ = {
 			isa = PBXShellScriptBuildPhase;
 			alwaysOutOfDate = 1;
 			buildActionMask = 2147483647;
@@ -599,9 +650,10 @@
 			);
 			inputPaths = (
 			);
-			name = "Fetch External Repositories";
+			name = "Generate Files";
 			outputFileListPaths = (
 				"$(INTERNAL_DIR)/external.xcfilelist",
+				"$(INTERNAL_DIR)/generated.xcfilelist",
 			);
 			outputPaths = (
 			);
@@ -641,7 +693,7 @@
 			);
 			runOnlyForDeploymentPostprocessing = 0;
 			shellPath = /bin/sh;
-			shellScript = "perl -pe 's/^(\"?)(.*\\$\\(.*\\).*?)(\"?)$/\"$2\"/ ; s/\\$(\\()?([a-zA-Z_]\\w*)(?(1)\\))/$ENV{$2}/g' \\\n  \"$SCRIPT_INPUT_FILE_0\" > \"$SCRIPT_OUTPUT_FILE_0\"\n";
+			shellScript = "set -euo pipefail\n\nperl -pe 's/^(\"?)(.*\\$\\(.*\\).*?)(\"?)$/\"$2\"/ ; s/\\$(\\()?([a-zA-Z_]\\w*)(?(1)\\))/$ENV{$2}/g' \\\n  \"$SCRIPT_INPUT_FILE_0\" > \"$SCRIPT_OUTPUT_FILE_0\"\n";
 			showEnvVarsInLog = 0;
 		};
 		E881916100D775EF5F50A2C6 /* Create compiling dependencies */ = {
@@ -1312,7 +1364,7 @@
 					"EXTERNAL_SECRET_2=\\\"World?\\\"",
 				);
 				GENERATE_INFOPLIST_FILE = YES;
-				LINK_PARAMS_FILE = "$(INTERNAL_DIR)/targets/darwin_x86_64-dbg-STABLE-1/tool/tool.link.params";
+				LINK_PARAMS_FILE = "$(BAZEL_OUT)/darwin_x86_64-dbg-STABLE-0/bin/test/fixtures/xcodeproj_bwx.generator-params/tool.7.link.params";
 				MACOSX_DEPLOYMENT_TARGET = 12.0;
 				OTHER_CFLAGS = (
 					"-ivfsoverlay",

examples/cc/test/fixtures/bwx.xcodeproj/rules_xcodeproj/generated.xcfilelist
@@ -1,0 +1,1 @@
+$(BAZEL_OUT)/darwin_x86_64-dbg-STABLE-0/bin/test/fixtures/xcodeproj_bwx.generator-params/tool.7.link.params

commit 57e692d1d9bd0c4bb2057d3178e37a01be66b9ee

Fix `xl` output group calculation
Regressed with a7cc6e1de67cf4b12cc4839c76b76f7a062ea0ab.

xcodeproj/internal/xcodeproj_rule.bzl
@@ -393,88 +393,11 @@ targets.
 
     additional_generated = {}
     additional_outputs = {}
-    target_transitive_dependencies = {}
     for xcode_target in focused_targets.values():
-        transitive_dependencies = {
-            id: None
-            for id in xcode_target.transitive_dependencies.to_list()
-        }
-        target_transitive_dependencies[xcode_target.id] = (
-            transitive_dependencies
-        )
-
-        additional_compiling_files = []
-        additional_indexstores_files = []
-        additional_linking_files = []
-        for dependency in transitive_dependencies:
-            unfocused_dependency = unfocused_dependencies.get(dependency)
-            if not unfocused_dependency:
-                continue
-            unfocused_compiling_files = (
-                unfocused_dependency.inputs.unfocused_generated_compiling
-            )
-            unfocused_indexstores_files = (
-                unfocused_dependency.inputs.unfocused_generated_indexstores
-            )
-            unfocused_linking_files = (
-                unfocused_dependency.inputs.unfocused_generated_linking
-            )
-            if unfocused_compiling_files:
-                additional_compiling_files.append(
-                    depset(unfocused_compiling_files),
-                )
-            if unfocused_indexstores_files:
-                additional_indexstores_files.append(
-                    depset(unfocused_indexstores_files),
-                )
-            if unfocused_linking_files:
-                additional_linking_files.append(
-                    depset(unfocused_linking_files),
-                )
-
-        compiling_output_group_name = (
-            xcode_target.inputs.compiling_output_group_name
-        )
-        indexstores_output_group_name = (
-            xcode_target.inputs.indexstores_output_group_name
-        )
-        if compiling_output_group_name:
-            set_if_true(
-                additional_generated,
-                compiling_output_group_name,
-                additional_compiling_files,
-            )
-        if indexstores_output_group_name:
-            set_if_true(
-                additional_generated,
-                indexstores_output_group_name,
-                additional_indexstores_files,
-            )
-
         label = replacement_labels.get(
             xcode_target.id,
             xcode_target.label,
         )
-        target_infoplists = infoplists.get(label)
-        if target_infoplists:
-            additional_linking_files.extend(target_infoplists)
-            products_output_group_name = (
-                xcode_target.outputs.products_output_group_name
-            )
-            if products_output_group_name:
-                additional_outputs[products_output_group_name] = (
-                    target_infoplists
-                )
-
-        linking_output_group_name = (
-            xcode_target.inputs.linking_output_group_name
-        )
-        if linking_output_group_name:
-            set_if_true(
-                additional_generated,
-                linking_output_group_name,
-                additional_linking_files,
-            )
 
         invalid_extra_files_targets = sets.to_list(
             sets.difference(
@@ -627,9 +550,10 @@ actual targets: {}
     target_dependencies = {}
     target_link_params = {}
     for index, xcode_target in enumerate(focused_targets.values()):
-        transitive_dependencies = (
-            target_transitive_dependencies[xcode_target.id]
-        )
+        transitive_dependencies = {
+            id: None
+            for id in xcode_target.transitive_dependencies.to_list()
+        }
 
         if include_swiftui_previews_scheme_targets:
             additional_scheme_target_ids = _calculate_swiftui_preview_targets(
@@ -698,7 +622,51 @@ actual targets: {}
             xcode_target.outputs.linking_output_group_name
         )
 
+        additional_compiling_files = []
+        additional_indexstores_files = []
         additional_linking_files = []
+
+        label = replacement_labels.get(
+            xcode_target.id,
+            xcode_target.label,
+        )
+        target_infoplists = infoplists.get(label)
+        if target_infoplists:
+            additional_linking_files.extend(target_infoplists)
+            products_output_group_name = (
+                xcode_target.outputs.products_output_group_name
+            )
+            if products_output_group_name:
+                additional_outputs[products_output_group_name] = (
+                    target_infoplists
+                )
+
+        for dependency in transitive_dependencies:
+            unfocused_dependency = unfocused_dependencies.get(dependency)
+            if not unfocused_dependency:
+                continue
+            unfocused_compiling_files = (
+                unfocused_dependency.inputs.unfocused_generated_compiling
+            )
+            unfocused_indexstores_files = (
+                unfocused_dependency.inputs.unfocused_generated_indexstores
+            )
+            unfocused_linking_files = (
+                unfocused_dependency.inputs.unfocused_generated_linking
+            )
+            if unfocused_compiling_files:
+                additional_compiling_files.append(
+                    depset(unfocused_compiling_files),
+                )
+            if unfocused_indexstores_files:
+                additional_indexstores_files.append(
+                    depset(unfocused_indexstores_files),
+                )
+            if unfocused_linking_files:
+                additional_linking_files.append(
+                    depset(unfocused_linking_files),
+                )
+
         for id in replaced_dependencies:
             if id in transitive_dependencies:
                 continue
@@ -728,11 +696,6 @@ actual targets: {}
                     [],
                 )
                 additional_compiling_files.append(dep_target.inputs.generated)
-                set_if_true(
-                    additional_generated,
-                    compiling_output_group_name,
-                    additional_compiling_files,
-                )
             if (indexstores_output_group_name and
                 dep_indexstores_output_group_name):
                 additional_indexstores_files = additional_generated.get(
@@ -741,11 +704,6 @@ actual targets: {}
                 )
                 additional_indexstores_files.append(
                     dep_target.inputs.indexstores,
-                )
-                set_if_true(
-                    additional_generated,
-                    indexstores_output_group_name,
-                    additional_indexstores_files,
                 )
             if linking_output_group_name and dep_linking_output_group_name:
                 additional_linking_files = additional_generated.get(
@@ -761,11 +719,24 @@ actual targets: {}
                     transitive_link_params
                 )
 
-        set_if_true(
-            additional_generated,
-            linking_output_group_name,
-            additional_linking_files,
-        )
+        if compiling_output_group_name:
+            set_if_true(
+                additional_generated,
+                compiling_output_group_name,
+                additional_compiling_files,
+            )
+        if indexstores_output_group_name:
+            set_if_true(
+                additional_generated,
+                indexstores_output_group_name,
+                additional_indexstores_files,
+            )
+        if linking_output_group_name:
+            set_if_true(
+                additional_generated,
+                linking_output_group_name,
+                additional_linking_files,
+            )
 
     return (
         focused_targets,