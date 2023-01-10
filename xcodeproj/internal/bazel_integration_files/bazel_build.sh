MobileNativeFoundation/rules_xcodeproj

commit df9425a497a1b871885573cef2882d9dd75dcf11

Move indexstores filelist into target's package (#1458)
Fixes #1459.

Users have reported that the current indexstore filelist path can grow
to be too large. This is because it currently encodes the path to the
target, and the file will be at the path of the generator. Now the file
will be at the path of the target, and the filename is a fixes size.

xcodeproj/internal/bazel_integration_files/bazel_build.sh
@@ -6,8 +6,6 @@
 # - BAZEL_PATH
 # - DEVELOPER_DIR
 # - GENERATOR_LABEL
-# - GENERATOR_TARGET_NAME
-# - GENERATOR_PACKAGE_BIN_DIR
 # - HOME
 # - INTERNAL_DIR
 # - OBJROOT
@@ -125,15 +123,28 @@ touch "$build_marker"
 
 # Collect indexstore filelists
 
+readonly outputgroup_regex='([^\ ]+) @?(.*)//(.*):(.*) ([^\ ]+)$'
+
 indexstores_filelists=()
 for output_group in "${output_groups[@]}"; do
-  if ! [[ "$output_group" =~ ^(xi|bi) ]]; then
-    continue
-  fi
+  if [[ $output_group =~ $outputgroup_regex ]]; then
+    output_type="${BASH_REMATCH[1]}"
 
-  filelist="$GENERATOR_TARGET_NAME-${output_group//\//_}"
-  filelist="${filelist/#/$output_path/$GENERATOR_PACKAGE_BIN_DIR/}"
-  filelist="${filelist/%/.filelist}"
+    if [[ "$output_type" != 'xi' && "$output_type" != 'bi' ]]; then
+      continue
+    fi
+
+    repo="${BASH_REMATCH[2]}"
+    package="${BASH_REMATCH[3]}"
+    target="${BASH_REMATCH[4]}"
+    configuration="${BASH_REMATCH[5]}"
+    filelist="$output_path/$configuration/bin/${repo:+"external/$repo/"}$package/$target-${output_type}.filelist"
+  else
+    echo "error: output group doesn't match regex. Please file a bug report" \
+"here: https://github.com/buildbuddy-io/rules_xcodeproj/issues/new?template=bug.md" \
+      >&2
+    exit 1
+  fi
 
   if [[ ! -f "$filelist" ]]; then
     echo "error: Bazel didn't create the indexstore filelist (\"$filelist\")." \

examples/cc/test/fixtures/bwb.xcodeproj/project.pbxproj
@@ -761,8 +761,6 @@
 				DSTROOT = "$(PROJECT_TEMP_DIR)";
 				ENABLE_DEFAULT_SEARCH_PATHS = NO;
 				GENERATOR_LABEL = "@//test/fixtures:xcodeproj_bwb.generator";
-				GENERATOR_PACKAGE_BIN_DIR = "darwin_x86_64-dbg-ST-14942f8a2d44/bin/test/fixtures";
-				GENERATOR_TARGET_NAME = xcodeproj_bwb.generator;
 				INDEXING_BUILT_PRODUCTS_DIR__ = "$(INDEXING_BUILT_PRODUCTS_DIR__NO)";
 				INDEXING_BUILT_PRODUCTS_DIR__NO = "$(BUILD_DIR)";
 				INDEXING_BUILT_PRODUCTS_DIR__YES = "$(CONFIGURATION_BUILD_DIR)";

examples/cc/test/fixtures/bwx.xcodeproj/project.pbxproj
@@ -732,8 +732,6 @@
 				DSTROOT = "$(PROJECT_TEMP_DIR)";
 				ENABLE_DEFAULT_SEARCH_PATHS = NO;
 				GENERATOR_LABEL = "@//test/fixtures:xcodeproj_bwx.generator";
-				GENERATOR_PACKAGE_BIN_DIR = "darwin_x86_64-dbg-ST-14942f8a2d44/bin/test/fixtures";
-				GENERATOR_TARGET_NAME = xcodeproj_bwx.generator;
 				INDEXING_BUILT_PRODUCTS_DIR__ = "$(INDEXING_BUILT_PRODUCTS_DIR__NO)";
 				INDEXING_BUILT_PRODUCTS_DIR__NO = "$(BUILD_DIR)";
 				INDEXING_BUILT_PRODUCTS_DIR__YES = "$(CONFIGURATION_BUILD_DIR)";

examples/integration/test/fixtures/bwb.xcodeproj/project.pbxproj
@@ -8090,8 +8090,6 @@
 				DSTROOT = "$(PROJECT_TEMP_DIR)";
 				ENABLE_DEFAULT_SEARCH_PATHS = NO;
 				GENERATOR_LABEL = "@//test/fixtures:xcodeproj_bwb.generator";
-				GENERATOR_PACKAGE_BIN_DIR = "darwin_x86_64-dbg-ST-0880460ee850/bin/test/fixtures";
-				GENERATOR_TARGET_NAME = xcodeproj_bwb.generator;
 				INDEXING_BUILT_PRODUCTS_DIR__ = "$(INDEXING_BUILT_PRODUCTS_DIR__NO)";
 				INDEXING_BUILT_PRODUCTS_DIR__NO = "$(BUILD_DIR)";
 				INDEXING_BUILT_PRODUCTS_DIR__YES = "$(CONFIGURATION_BUILD_DIR)";

examples/integration/test/fixtures/bwx.xcodeproj/project.pbxproj
@@ -9687,8 +9687,6 @@
 				DSTROOT = "$(PROJECT_TEMP_DIR)";
 				ENABLE_DEFAULT_SEARCH_PATHS = NO;
 				GENERATOR_LABEL = "@//test/fixtures:xcodeproj_bwx.generator";
-				GENERATOR_PACKAGE_BIN_DIR = "darwin_x86_64-dbg-ST-0880460ee850/bin/test/fixtures";
-				GENERATOR_TARGET_NAME = xcodeproj_bwx.generator;
 				INDEXING_BUILT_PRODUCTS_DIR__ = "$(INDEXING_BUILT_PRODUCTS_DIR__NO)";
 				INDEXING_BUILT_PRODUCTS_DIR__NO = "$(BUILD_DIR)";
 				INDEXING_BUILT_PRODUCTS_DIR__YES = "$(CONFIGURATION_BUILD_DIR)";

examples/sanitizers/test/fixtures/bwb.xcodeproj/project.pbxproj
@@ -723,8 +723,6 @@
 				DSTROOT = "$(PROJECT_TEMP_DIR)";
 				ENABLE_DEFAULT_SEARCH_PATHS = NO;
 				GENERATOR_LABEL = "@//test/fixtures:xcodeproj_bwb.generator";
-				GENERATOR_PACKAGE_BIN_DIR = "darwin_x86_64-dbg-ST-14942f8a2d44/bin/test/fixtures";
-				GENERATOR_TARGET_NAME = xcodeproj_bwb.generator;
 				INDEXING_BUILT_PRODUCTS_DIR__ = "$(INDEXING_BUILT_PRODUCTS_DIR__NO)";
 				INDEXING_BUILT_PRODUCTS_DIR__NO = "$(BUILD_DIR)";
 				INDEXING_BUILT_PRODUCTS_DIR__YES = "$(CONFIGURATION_BUILD_DIR)";

examples/sanitizers/test/fixtures/bwx.xcodeproj/project.pbxproj
@@ -736,8 +736,6 @@
 				DSTROOT = "$(PROJECT_TEMP_DIR)";
 				ENABLE_DEFAULT_SEARCH_PATHS = NO;
 				GENERATOR_LABEL = "@//test/fixtures:xcodeproj_bwx.generator";
-				GENERATOR_PACKAGE_BIN_DIR = "darwin_x86_64-dbg-ST-14942f8a2d44/bin/test/fixtures";
-				GENERATOR_TARGET_NAME = xcodeproj_bwx.generator;
 				INDEXING_BUILT_PRODUCTS_DIR__ = "$(INDEXING_BUILT_PRODUCTS_DIR__NO)";
 				INDEXING_BUILT_PRODUCTS_DIR__NO = "$(BUILD_DIR)";
 				INDEXING_BUILT_PRODUCTS_DIR__YES = "$(CONFIGURATION_BUILD_DIR)";

examples/simple/test/fixtures/bwb.xcodeproj/project.pbxproj
@@ -348,8 +348,6 @@
 				DSTROOT = "$(PROJECT_TEMP_DIR)";
 				ENABLE_DEFAULT_SEARCH_PATHS = NO;
 				GENERATOR_LABEL = "@//test/fixtures:xcodeproj_bwb.generator";
-				GENERATOR_PACKAGE_BIN_DIR = "darwin_x86_64-dbg-ST-14942f8a2d44/bin/test/fixtures";
-				GENERATOR_TARGET_NAME = xcodeproj_bwb.generator;
 				INDEXING_BUILT_PRODUCTS_DIR__ = "$(INDEXING_BUILT_PRODUCTS_DIR__NO)";
 				INDEXING_BUILT_PRODUCTS_DIR__NO = "$(BUILD_DIR)";
 				INDEXING_BUILT_PRODUCTS_DIR__YES = "$(CONFIGURATION_BUILD_DIR)";

commit 82178595e713b973928c6e85ac843002842ddcf7

Fix BwX Index Build failure
Regressed with df9425a497a1b871885573cef2882d9dd75dcf11.

xcodeproj/internal/bazel_integration_files/bazel_build.sh
@@ -142,9 +142,12 @@ for output_group in "${output_groups[@]}"; do
     target="${BASH_REMATCH[4]}"
     configuration="${BASH_REMATCH[5]}"
     filelist="$output_path/$configuration/bin/${repo:+"external/$repo/"}$package/$target-${output_type}.filelist"
+  elif [[ "$output_group" == "all_xc" ]]; then
+    continue
   else
-    echo "error: output group doesn't match regex. Please file a bug report" \
-"here: https://github.com/buildbuddy-io/rules_xcodeproj/issues/new?template=bug.md" \
+    echo "error: output group ($output_group) doesn't match regex. Please" \
+"file a bug report here:" \
+"https://github.com/buildbuddy-io/rules_xcodeproj/issues/new?template=bug.md" \
       >&2
     exit 1
   fi