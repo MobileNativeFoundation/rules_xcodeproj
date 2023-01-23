MobileNativeFoundation/rules_xcodeproj

commit 021c78f968cb5d484b48624b587f9c44a7a20207

Fix incorrect BwB copy path for macOS tests (#1593)
Fixes #1586.

Regressed with 0ad6162d7c97e218d07c4892b864ceaaff150e5a.

xcodeproj/internal/top_level_targets.bzl
@@ -90,6 +90,7 @@ def process_top_level_properties(
             archive_file_path = file_path(bundle_file)
 
             if tree_artifact_enabled:
+                bundle_path = bundle_file.path
                 bundle_file_path = archive_file_path
             else:
                 bundle_extension = bundle_info.bundle_extension
@@ -109,9 +110,10 @@ def process_top_level_properties(
         elif product_type.startswith("com.apple.product-type.framework"):
             # Some rules only set the binary for static frameworks
             bundle_file = bundle_info.binary
+            bundle_path = bundle_file.dirname
             archive_file_path = file_path(
                 bundle_file,
-                path = bundle_file.dirname,
+                path = bundle_path,
             )
             bundle_file_path = archive_file_path
         else:
@@ -142,20 +144,22 @@ def process_top_level_properties(
 
             # "some/test.xctest/binary" -> "some/test.xctest"
             xctest_path = bundle_file.path
-            path = xctest_path[:-(len(xctest_path.split(".xctest/")[1]) + 1)]
+            bundle_path = xctest_path[:-(len(xctest_path.split(".xctest/")[1]) + 1)]
             bundle_file_path = file_path(
                 bundle_file,
-                path = path,
+                path = bundle_path,
             )
             archive_file_path = bundle_file_path
         else:
             product_type = "com.apple.product-type.tool"
+            bundle_path = None
             bundle_file_path = None
             archive_file_path = None
 
     return struct(
         archive_file_path = archive_file_path,
         bundle_file = bundle_file,
+        bundle_path = bundle_path,
         bundle_file_path = bundle_file_path,
         executable_name = executable_name,
         product_name = product_name,
@@ -377,6 +381,7 @@ def process_top_level_target(
         product_name = props.product_name,
         product_type = props.product_type,
         bundle_file = props.bundle_file,
+        bundle_path = props.bundle_path,
         bundle_file_path = props.bundle_file_path,
         archive_file_path = props.archive_file_path,
         executable_name = props.executable_name,

examples/sanitizers/test/fixtures/bwb.xcodeproj/project.pbxproj
@@ -625,7 +625,7 @@
 				BAZEL_COMPILE_TARGET_ID = "//UndefinedBehaviorSanitizerApp:UndefinedBehaviorSanitizerApp.library ios-x86_64-min15.0-applebin_ios-ios_x86_64-dbg-STABLE-1";
 				"BAZEL_COMPILE_TARGET_ID[sdk=iphonesimulator*]" = "$(BAZEL_COMPILE_TARGET_ID)";
 				BAZEL_LABEL = "@//UndefinedBehaviorSanitizerApp:UndefinedBehaviorSanitizerApp";
-				BAZEL_OUTPUTS_PRODUCT = "bazel-out/applebin_ios-ios_x86_64-dbg-STABLE-2/bin/UndefinedBehaviorSanitizerApp/UndefinedBehaviorSanitizerApp.ipa";
+				BAZEL_OUTPUTS_PRODUCT = "bazel-out/applebin_ios-ios_x86_64-dbg-STABLE-2/bin/UndefinedBehaviorSanitizerApp/UndefinedBehaviorSanitizerApp_archive-root/Payload/UndefinedBehaviorSanitizerApp.app";
 				BAZEL_PACKAGE_BIN_DIR = "bazel-out/applebin_ios-ios_x86_64-dbg-STABLE-2/bin/UndefinedBehaviorSanitizerApp";
 				BAZEL_TARGET_ID = "//UndefinedBehaviorSanitizerApp:UndefinedBehaviorSanitizerApp applebin_ios-ios_x86_64-dbg-STABLE-2";
 				"BAZEL_TARGET_ID[sdk=iphonesimulator*]" = "$(BAZEL_TARGET_ID)";
@@ -803,7 +803,7 @@
 				BAZEL_COMPILE_TARGET_ID = "//ThreadSanitizerApp:ThreadSanitizerApp.library ios-x86_64-min15.0-applebin_ios-ios_x86_64-dbg-STABLE-1";
 				"BAZEL_COMPILE_TARGET_ID[sdk=iphonesimulator*]" = "$(BAZEL_COMPILE_TARGET_ID)";
 				BAZEL_LABEL = "@//ThreadSanitizerApp:ThreadSanitizerApp";
-				BAZEL_OUTPUTS_PRODUCT = "bazel-out/applebin_ios-ios_x86_64-dbg-STABLE-2/bin/ThreadSanitizerApp/ThreadSanitizerApp.ipa";
+				BAZEL_OUTPUTS_PRODUCT = "bazel-out/applebin_ios-ios_x86_64-dbg-STABLE-2/bin/ThreadSanitizerApp/ThreadSanitizerApp_archive-root/Payload/ThreadSanitizerApp.app";
 				BAZEL_PACKAGE_BIN_DIR = "bazel-out/applebin_ios-ios_x86_64-dbg-STABLE-2/bin/ThreadSanitizerApp";
 				BAZEL_TARGET_ID = "//ThreadSanitizerApp:ThreadSanitizerApp applebin_ios-ios_x86_64-dbg-STABLE-2";
 				"BAZEL_TARGET_ID[sdk=iphonesimulator*]" = "$(BAZEL_TARGET_ID)";
@@ -847,7 +847,7 @@
 				BAZEL_COMPILE_TARGET_ID = "//AddressSanitizerApp:AddressSanitizerApp.library ios-x86_64-min15.0-applebin_ios-ios_x86_64-dbg-STABLE-1";
 				"BAZEL_COMPILE_TARGET_ID[sdk=iphonesimulator*]" = "$(BAZEL_COMPILE_TARGET_ID)";
 				BAZEL_LABEL = "@//AddressSanitizerApp:AddressSanitizerApp";
-				BAZEL_OUTPUTS_PRODUCT = "bazel-out/applebin_ios-ios_x86_64-dbg-STABLE-2/bin/AddressSanitizerApp/AddressSanitizerApp.ipa";
+				BAZEL_OUTPUTS_PRODUCT = "bazel-out/applebin_ios-ios_x86_64-dbg-STABLE-2/bin/AddressSanitizerApp/AddressSanitizerApp_archive-root/Payload/AddressSanitizerApp.app";
 				BAZEL_PACKAGE_BIN_DIR = "bazel-out/applebin_ios-ios_x86_64-dbg-STABLE-2/bin/AddressSanitizerApp";
 				BAZEL_TARGET_ID = "//AddressSanitizerApp:AddressSanitizerApp applebin_ios-ios_x86_64-dbg-STABLE-2";
 				"BAZEL_TARGET_ID[sdk=iphonesimulator*]" = "$(BAZEL_TARGET_ID)";

test/fixtures/generator/bwb.xcodeproj/project.pbxproj
@@ -3224,7 +3224,7 @@
 				BAZEL_COMPILE_TARGET_ID = "//tools/generator/test:tests.library macos-x86_64-min12.0-applebin_macos-darwin_x86_64-dbg-STABLE-1";
 				"BAZEL_COMPILE_TARGET_ID[sdk=macosx*]" = "$(BAZEL_COMPILE_TARGET_ID)";
 				BAZEL_LABEL = "@//tools/generator/test:tests";
-				BAZEL_OUTPUTS_PRODUCT = "bazel-out/applebin_macos-darwin_x86_64-dbg-STABLE-2/bin/tools/generator/test/tests.__internal__.__test_bundle.zip";
+				BAZEL_OUTPUTS_PRODUCT = "bazel-out/applebin_macos-darwin_x86_64-dbg-STABLE-2/bin/tools/generator/test/tests.__internal__.__test_bundle_archive-root/tests.xctest";
 				BAZEL_PACKAGE_BIN_DIR = "bazel-out/applebin_macos-darwin_x86_64-dbg-STABLE-2/bin/tools/generator/test";
 				BAZEL_TARGET_ID = "//tools/generator/test:tests.__internal__.__test_bundle applebin_macos-darwin_x86_64-dbg-STABLE-2";
 				"BAZEL_TARGET_ID[sdk=macosx*]" = "$(BAZEL_TARGET_ID)";

xcodeproj/internal/output_files.bzl
@@ -218,6 +218,7 @@ def _get_outputs(*, id, product, swift_info):
         id = id,
         is_framework = is_framework,
         product = product.file if product else None,
+        product_path = product.path if product else None,
         product_file_path = product.actual_file_path if product else None,
         swift = swift,
     )

xcodeproj/internal/product.bzl
@@ -66,6 +66,7 @@ def process_product(
         product_type,
         is_resource_bundle = False,
         bundle_file = None,
+        bundle_path = None,
         bundle_file_path = None,
         archive_file_path = None,
         executable_name = None,
@@ -83,6 +84,8 @@ def process_product(
         is_resource_bundle: Whether the product is a resource bundle.
         bundle_file: If the product is a bundle, this is `File` for the bundle,
             otherwise `None`.
+        bundle_path: If the product is a bundle, this is the path to
+            the bundle, when not in an archive, otherwise `None`.
         bundle_file_path: If the product is a bundle, this is the `file_path` to
             the bundle, when not in an archive, otherwise `None`.
         archive_file_path: If the product is a bundle, this is
@@ -114,6 +117,13 @@ def process_product(
         fp = None
         actual_fp = None
 
+    if bundle_path:
+        path = bundle_path
+    elif file:
+        path = file.path
+    else:
+        path = None
+
     if target and apple_common.AppleDynamicFramework in target:
         framework_files = (
             target[apple_common.AppleDynamicFramework].framework_files
@@ -132,6 +142,7 @@ def process_product(
         name = product_name,
         framework_files = framework_files,
         file = file,
+        path = path,
         file_path = fp,
         actual_file_path = actual_fp,
         type = product_type,

xcodeproj/internal/resource_target.bzl
@@ -25,10 +25,8 @@ def _process_resource_bundle(bundle, *, information):
     )
 
     package_bin_dir = bundle.package_bin_dir
-    bundle_file_path = parsed_file_path(paths.join(
-        package_bin_dir,
-        "{}.bundle".format(name),
-    ))
+    bundle_path = paths.join(package_bin_dir, "{}.bundle".format(name))
+    bundle_file_path = parsed_file_path(bundle_path)
 
     product = process_product(
         ctx = None,
@@ -37,6 +35,7 @@ def _process_resource_bundle(bundle, *, information):
         product_type = "com.apple.product-type.bundle",
         is_resource_bundle = True,
         bundle_file = None,
+        bundle_path = bundle_path,
         bundle_file_path = bundle_file_path,
         linker_inputs = None,
     )

xcodeproj/internal/xcode_targets.bzl
@@ -182,6 +182,9 @@ def _to_xcode_target_outputs(outputs):
         product_file = (
             direct_outputs.product if direct_outputs else None
         ),
+        product_path = (
+            direct_outputs.product_path if direct_outputs else None
+        ),
         products_output_group_name = outputs.products_output_group_name,
         swiftmodule = swiftmodule,
         swift_generated_header = swift_generated_header,
@@ -289,6 +292,7 @@ def _merge_xcode_target_outputs(*, src, dest):
         swiftmodule = src.swiftmodule,
         swift_generated_header = src.swift_generated_header,
         product_file = dest.product_file,
+        product_path = dest.product_path,
         products_output_group_name = dest.products_output_group_name,
         transitive_infoplists = dest.transitive_infoplists,
     )
@@ -326,11 +330,11 @@ def _set_bazel_outputs_product(
     if build_mode != "bazel":
         return
 
-    file = xcode_target.outputs.product_file
-    if not file:
+    path = xcode_target.outputs.product_path
+    if not path:
         return
 
-    build_settings["BAZEL_OUTPUTS_PRODUCT"] = file.path
+    build_settings["BAZEL_OUTPUTS_PRODUCT"] = path
 
 def _set_search_paths(
         *,

commit bb7f790e0423d63b1ec422addb77e6ba9c110154

Fix stale BwB installs
Regressed with 021c78f968cb5d484b48624b587f9c44a7a20207.

xcodeproj/internal/top_level_targets.bzl
@@ -83,28 +83,29 @@ def process_top_level_properties(
         product_type = bundle_info.product_type
 
         bundle_file = bundle_info.archive
-
-        bundle_file = bundle_info.archive
         if bundle_file:
+            bundle_path = bundle_file.path
             archive_file_path = file_path(bundle_file)
 
             if tree_artifact_enabled:
-                bundle_path = bundle_file.path
                 bundle_file_path = archive_file_path
             else:
                 bundle_extension = bundle_info.bundle_extension
                 bundle = "{}{}".format(bundle_name, bundle_extension)
                 if bundle_extension == ".app":
-                    bundle_path = paths.join(
+                    bundle_file_path_path = paths.join(
                         bundle_info.archive_root,
                         "Payload",
                         bundle,
                     )
                 else:
-                    bundle_path = paths.join(bundle_info.archive_root, bundle)
+                    bundle_file_path_path = paths.join(
+                        bundle_info.archive_root,
+                        bundle,
+                    )
                 bundle_file_path = file_path(
                     bundle_file,
-                    path = bundle_path,
+                    path = bundle_file_path_path,
                 )
         elif product_type.startswith("com.apple.product-type.framework"):
             # Some rules only set the binary for static frameworks

examples/sanitizers/test/fixtures/bwb.xcodeproj/project.pbxproj
@@ -625,7 +625,7 @@
 				BAZEL_COMPILE_TARGET_ID = "//UndefinedBehaviorSanitizerApp:UndefinedBehaviorSanitizerApp.library ios-x86_64-min15.0-applebin_ios-ios_x86_64-dbg-STABLE-1";
 				"BAZEL_COMPILE_TARGET_ID[sdk=iphonesimulator*]" = "$(BAZEL_COMPILE_TARGET_ID)";
 				BAZEL_LABEL = "@//UndefinedBehaviorSanitizerApp:UndefinedBehaviorSanitizerApp";
-				BAZEL_OUTPUTS_PRODUCT = "bazel-out/applebin_ios-ios_x86_64-dbg-STABLE-2/bin/UndefinedBehaviorSanitizerApp/UndefinedBehaviorSanitizerApp_archive-root/Payload/UndefinedBehaviorSanitizerApp.app";
+				BAZEL_OUTPUTS_PRODUCT = "bazel-out/applebin_ios-ios_x86_64-dbg-STABLE-2/bin/UndefinedBehaviorSanitizerApp/UndefinedBehaviorSanitizerApp.ipa";
 				BAZEL_PACKAGE_BIN_DIR = "bazel-out/applebin_ios-ios_x86_64-dbg-STABLE-2/bin/UndefinedBehaviorSanitizerApp";
 				BAZEL_TARGET_ID = "//UndefinedBehaviorSanitizerApp:UndefinedBehaviorSanitizerApp applebin_ios-ios_x86_64-dbg-STABLE-2";
 				"BAZEL_TARGET_ID[sdk=iphonesimulator*]" = "$(BAZEL_TARGET_ID)";
@@ -770,7 +770,7 @@
 				BAZEL_COMPILE_TARGET_ID = "//ThreadSanitizerApp:ThreadSanitizerApp.library ios-x86_64-min15.0-applebin_ios-ios_x86_64-dbg-STABLE-1";
 				"BAZEL_COMPILE_TARGET_ID[sdk=iphonesimulator*]" = "$(BAZEL_COMPILE_TARGET_ID)";
 				BAZEL_LABEL = "@//ThreadSanitizerApp:ThreadSanitizerApp";
-				BAZEL_OUTPUTS_PRODUCT = "bazel-out/applebin_ios-ios_x86_64-dbg-STABLE-2/bin/ThreadSanitizerApp/ThreadSanitizerApp_archive-root/Payload/ThreadSanitizerApp.app";
+				BAZEL_OUTPUTS_PRODUCT = "bazel-out/applebin_ios-ios_x86_64-dbg-STABLE-2/bin/ThreadSanitizerApp/ThreadSanitizerApp.ipa";
 				BAZEL_PACKAGE_BIN_DIR = "bazel-out/applebin_ios-ios_x86_64-dbg-STABLE-2/bin/ThreadSanitizerApp";
 				BAZEL_TARGET_ID = "//ThreadSanitizerApp:ThreadSanitizerApp applebin_ios-ios_x86_64-dbg-STABLE-2";
 				"BAZEL_TARGET_ID[sdk=iphonesimulator*]" = "$(BAZEL_TARGET_ID)";
@@ -812,7 +812,7 @@
 				BAZEL_COMPILE_TARGET_ID = "//AddressSanitizerApp:AddressSanitizerApp.library ios-x86_64-min15.0-applebin_ios-ios_x86_64-dbg-STABLE-1";
 				"BAZEL_COMPILE_TARGET_ID[sdk=iphonesimulator*]" = "$(BAZEL_COMPILE_TARGET_ID)";
 				BAZEL_LABEL = "@//AddressSanitizerApp:AddressSanitizerApp";
-				BAZEL_OUTPUTS_PRODUCT = "bazel-out/applebin_ios-ios_x86_64-dbg-STABLE-2/bin/AddressSanitizerApp/AddressSanitizerApp_archive-root/Payload/AddressSanitizerApp.app";
+				BAZEL_OUTPUTS_PRODUCT = "bazel-out/applebin_ios-ios_x86_64-dbg-STABLE-2/bin/AddressSanitizerApp/AddressSanitizerApp.ipa";
 				BAZEL_PACKAGE_BIN_DIR = "bazel-out/applebin_ios-ios_x86_64-dbg-STABLE-2/bin/AddressSanitizerApp";
 				BAZEL_TARGET_ID = "//AddressSanitizerApp:AddressSanitizerApp applebin_ios-ios_x86_64-dbg-STABLE-2";
 				"BAZEL_TARGET_ID[sdk=iphonesimulator*]" = "$(BAZEL_TARGET_ID)";

test/fixtures/generator/bwb.xcodeproj/project.pbxproj
@@ -3140,7 +3140,7 @@
 				BAZEL_COMPILE_TARGET_ID = "//tools/generator/test:tests.library macos-x86_64-min12.0-applebin_macos-darwin_x86_64-dbg-STABLE-1";
 				"BAZEL_COMPILE_TARGET_ID[sdk=macosx*]" = "$(BAZEL_COMPILE_TARGET_ID)";
 				BAZEL_LABEL = "@//tools/generator/test:tests";
-				BAZEL_OUTPUTS_PRODUCT = "bazel-out/applebin_macos-darwin_x86_64-dbg-STABLE-2/bin/tools/generator/test/tests.__internal__.__test_bundle_archive-root/tests.xctest";
+				BAZEL_OUTPUTS_PRODUCT = "bazel-out/applebin_macos-darwin_x86_64-dbg-STABLE-2/bin/tools/generator/test/tests.__internal__.__test_bundle.zip";
 				BAZEL_PACKAGE_BIN_DIR = "bazel-out/applebin_macos-darwin_x86_64-dbg-STABLE-2/bin/tools/generator/test";
 				BAZEL_TARGET_ID = "//tools/generator/test:tests.__internal__.__test_bundle applebin_macos-darwin_x86_64-dbg-STABLE-2";
 				"BAZEL_TARGET_ID[sdk=macosx*]" = "$(BAZEL_TARGET_ID)";