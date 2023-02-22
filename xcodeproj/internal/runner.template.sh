2b2b0029f172e5b372b0749cb3e2620022f0e95b
Brentley Jones <github@brentleyjones.com>
2023-02-21 21:17:13 -0600

    Fix

--- xcodeproj/internal/runner.template.sh
+++ xcodeproj/internal/runner.template.sh
@@ -131,6 +131,10 @@ if [[ %is_fixture% -eq 1 && %is_bazel_6% -eq 1 ]]; then
     # bzlmod adjust labels in a way that we can't account for yet
     "--noenable_bzlmod"
   )
+
+  if [[ "${BUILD_WORKSPACE_DIRECTORY##*/}" == "integration" ]]; then
+    pre_config_flags+=("--platform_mappings=platform_mappings")
+  fi
 fi
 
 readonly bazel_cmd=(
