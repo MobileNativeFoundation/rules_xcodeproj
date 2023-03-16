e9ea725b350260d1e3f63bfc971282cceb9c3b5b
Brentley Jones <github@brentleyjones.com>
2023-03-16 15:44:56 -0500

    Fix output base hashing (#1886)
    We need to use the outer output base, so `Index Build` can find the
    generator as well.

--- xcodeproj/internal/runner.template.sh
+++ xcodeproj/internal/runner.template.sh
@@ -112,8 +112,8 @@ readonly output_base="${execution_root%/*/*}"
 readonly nested_output_base="$output_base/rules_xcodeproj/build_output_base"
 
 # Create files for the generator target
-nested_output_base_hash=$(/sbin/md5 -q -s "$nested_output_base")
-readonly generator_package_directory="/tmp/rules_xcodeproj/generated/$nested_output_base_hash/generator"
+output_base_hash=$(/sbin/md5 -q -s "$output_base")
+readonly generator_package_directory="/tmp/rules_xcodeproj/generated/$output_base_hash/generator"
 
 mkdir -p "$generator_package_directory"
 cp "$generator_build_file" "$generator_package_directory/BUILD"
