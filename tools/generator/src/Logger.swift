7b74b06633d944cc7ac401c82b544650723e4a46
JP Simard <jp@jpsim.com>
2023-03-22 23:09:21 -0400

    Revert "Make `DefaultLogger` use stderr/stdout by default."
    This reverts commit b1ee330dc7bb7cc37da7da5f8ec92e51d48b1aa7.
    
    Signed-off-by: JP Simard <jp@jpsim.com>

--- tools/generator/src/Logger.swift
+++ tools/generator/src/Logger.swift
@@ -23,11 +23,11 @@ final class StdoutOutputStream: TextOutputStream {
 }
 
 /// The logger that is used when not running tests.
-final class DefaultLogger: Logger {
-    private var standardError: any TextOutputStream
-    private var standardOutput: any TextOutputStream
+final class DefaultLogger<E: TextOutputStream, O: TextOutputStream>: Logger {
+    private var standardError: E
+    private var standardOutput: O
 
-    init(standardError: TextOutputStream = StderrOutputStream(), standardOutput: TextOutputStream = StdoutOutputStream()) {
+    init(standardError: E, standardOutput: O) {
         self.standardError = standardError
         self.standardOutput = standardOutput
     }
