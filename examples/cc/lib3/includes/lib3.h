59199129fd29e34d7d322d95428ecf5e79b92f0f
Brentley Jones <github@brentleyjones.com>
2023-04-04 07:04:26 -0500

    Add `cc_binary.linkshared = True` (i.e. dylib) example (#1949)
    Resolves #1922.
    
    The `lib3` target is unfocused for now, until we can create dynamic
    library targets.
    
    ---------
    
    Signed-off-by: Brentley Jones <github@brentleyjones.com>

---
+++ examples/cc/lib3/includes/lib3.h
@@ -0,0 +1,1 @@
+const char *lib3Version();
--- examples/cc/tool/main.c
+++ examples/cc/tool/main.c
@@ -1,13 +1,15 @@
 
 #include <stdio.h>
 
+#include "lib.h"
 #include "lib/lib.h"
 #include "lib2_prefix/lib2.h"
-#include "lib.h"
+#include "lib3/lib3.h"
 
 int main(int argc, char *argv[]) {
     // Print the string returned from lib's greeting() function
     printf("%s, %s\n", greeting(), SECRET_2);
     printf("%s, %s\n", externalGreeting(), EXTERNAL_SECRET_2);
     printf("%d\n", answer());
+    printf("lib3 version: %s\n", lib3Version());
 }
