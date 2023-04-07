458270d8f90402af0000962267d691e9394cd55e
Brentley Jones <github@brentleyjones.com>
2023-04-07 18:23:35 -0500

    Use execution root relative paths for `{EXCLUDED,INCLUDED}_SOURCE_FILE_NAMES` (#1986)
    Part of #1854.
    
    1. These paths are shorter
    2. This is needed in order to remove `FilePath.PathType` down the line
    (as this function will just be removed in that case)
    
    ---------
    
    Signed-off-by: Brentley Jones <github@brentleyjones.com>

--- tools/generator/src/FilePathResolver.swift
+++ tools/generator/src/FilePathResolver.swift
@@ -21,6 +21,21 @@ enum FilePathResolver {
         return path
     }
 
+    static func resolveRelativeToExecutionRoot(_ filePath: FilePath) -> String {
+        let path: String
+        switch filePath.type {
+        case .generated:
+            path = "bazel-out/\(filePath.path)"
+        case .external:
+            path = "external/\(filePath.path)"
+        case .project:
+            // We could check for `"."`, but this is only called on actual paths
+            path = filePath.path.string
+        }
+
+        return path
+    }
+
     static func resolveExternal(_ path: Path) -> String {
         return "$(BAZEL_EXTERNAL)/\(path)"
     }
