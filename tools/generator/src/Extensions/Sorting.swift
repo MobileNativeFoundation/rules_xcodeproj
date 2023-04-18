MobileNativeFoundation/rules_xcodeproj

commit 9041dc2af622df7b3c0221add5ff1cbf07dc5b1b

Rewrite `createFilesAndGroups` (#2042)
It’s around 5 times faster now (e.g. on a particular large project it
went from 5.61s to 1.13s).

Signed-off-by: Brentley Jones <github@brentleyjones.com>

tools/generator/src/Extensions/Sorting.swift
@@ -1,29 +1,9 @@
 import Foundation
 import XcodeProj
 
-private extension PBXFileElement {
+extension PBXFileElement {
     private static var cache: [String: String] = [:]
     private static let cacheLock = NSRecursiveLock()
-
-    var sortOrder: Int {
-        switch self {
-        case is PBXVariantGroup:
-            // Localized containers should be treated as files
-            return 0
-        case is XCVersionGroup:
-            // Code Data containers should be treated as files
-            return 0
-        case is PBXGroup:
-            return -1
-        default:
-            if let reference = self as? PBXFileReference {
-                // Folders should be treated as groups
-                return reference.lastKnownFileType == "folder" ? -1 : 0
-            } else {
-                return 0
-            }
-        }
-    }
 
     var namePathSortString: String {
         Self.cacheLock.lock()
@@ -64,7 +44,17 @@ extension Sequence {
     }
 }
 
-extension Sequence where Element: PBXFileElement {
+extension Array where Element == PBXFileElement {
+    mutating func sortLocalizedStandard(
+        _ keyPath: KeyPath<Element, String>
+    ) {
+        sort(by: Self.sortByLocalizedStandard(keyPath))
+    }
+
+    mutating func sortLocalizedStandard() {
+        sortLocalizedStandard(\.namePathSortString)
+    }
+
     func sortedLocalizedStandard() -> [Element] {
         return sortedLocalizedStandard(\.namePathSortString)
     }
@@ -73,28 +63,6 @@ extension Sequence where Element: PBXFileElement {
 extension Sequence where Element == PBXBuildFile {
     func sortedLocalizedStandard() -> [Element] {
         return sortedLocalizedStandard(\.file!.namePathSortString)
-    }
-}
-
-extension Array where Element: PBXFileElement {
-    mutating func sortGroupedLocalizedStandard() {
-        let fileSort = Self.sortByLocalizedStandard(\.namePathSortString)
-        sort { lhs, rhs in
-            let lhsSortOrder = lhs.sortOrder
-            let rhsSortOrder = rhs.sortOrder
-            if lhsSortOrder != rhsSortOrder {
-                // Groups and folders before files
-                return lhsSortOrder < rhsSortOrder
-            } else {
-                // Files alphabetically
-                return fileSort(lhs, rhs)
-            }
-        }
-        for element in self {
-            if let group = element as? PBXGroup {
-                group.children.sortGroupedLocalizedStandard()
-            }
-        }
     }
 }
 

test/fixtures/generator/bwb.xcodeproj/project.pbxproj
@@ -95,13 +95,11 @@
 		39FE8D6098E291F1E003ED19 /* OrderedDictionary+Codable.swift in Sources */ = {isa = PBXBuildFile; fileRef = DDB00AEB74C1EBA77CB5A346 /* OrderedDictionary+Codable.swift */; };
 		3A8C3CE14F1EBD5EFBD1FC14 /* PBXProj+Testing.swift in Sources */ = {isa = PBXBuildFile; fileRef = 1A9526E2C4F63F4404FE5CA7 /* PBXProj+Testing.swift */; };
 		3ABAC9D6BBC6012C1B94F255 /* PBXBatchUpdater.swift in Sources */ = {isa = PBXBuildFile; fileRef = 9A1ED07486D10DDAA9F681F7 /* PBXBatchUpdater.swift */; };
-		3AFF507A5BAEE00DE63F13C6 /* PathExtensionsTests.swift in Sources */ = {isa = PBXBuildFile; fileRef = 98FB3331A657CE14F517D2F9 /* PathExtensionsTests.swift */; };
 		3C5BC1F53D377D3611F86354 /* DisambiguateTargets.swift in Sources */ = {isa = PBXBuildFile; fileRef = 0AE0BA76CA15BF4A07E11120 /* DisambiguateTargets.swift */; };
 		3CFDABB5BECD7DFCC386F79F /* OrderedSet+UnorderedView.swift in Sources */ = {isa = PBXBuildFile; fileRef = 12E9F366756D2F90DB8EA58C /* OrderedSet+UnorderedView.swift */; };
 		3D960F091B3C378AD19971F4 /* OrderedSet+ExpressibleByArrayLiteral.swift in Sources */ = {isa = PBXBuildFile; fileRef = E28C1C02F6539D5429492028 /* OrderedSet+ExpressibleByArrayLiteral.swift */; };
 		3EC565A9409AC2547D47D831 /* ConsolidateTargetsTests.swift in Sources */ = {isa = PBXBuildFile; fileRef = A1AD646F676F07910B661BBD /* ConsolidateTargetsTests.swift */; };
 		3EE801C86E4315C14C6EDC19 /* String.swift in Sources */ = {isa = PBXBuildFile; fileRef = 7E3410784B3DCFECBA6F4ADD /* String.swift */; };
-		400825480781AE867F62DC0C /* PathExtensionsTests.swift in Sources */ = {isa = PBXBuildFile; fileRef = 98FB3331A657CE14F517D2F9 /* PathExtensionsTests.swift */; };
 		4205D8169B745E194D3F9B7E /* PBXFileElement.swift in Sources */ = {isa = PBXBuildFile; fileRef = 79635341E1D4711836249329 /* PBXFileElement.swift */; };
 		426BDD5BA5ECCE96EAC03184 /* OrderedSet+Initializers.swift in Sources */ = {isa = PBXBuildFile; fileRef = 02303D78B9A2B3A3BEEC055C /* OrderedSet+Initializers.swift */; };
 		428996F06DD5289E7BE01B56 /* PBXCopyFilesBuildPhase.swift in Sources */ = {isa = PBXBuildFile; fileRef = 9E3216CC232BADA4DD21C08F /* PBXCopyFilesBuildPhase.swift */; };
@@ -673,7 +671,6 @@
 		97E066BC8246F6C0364DC257 /* XCSchemeInfo+LaunchActionInfo.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = "XCSchemeInfo+LaunchActionInfo.swift"; sourceTree = "<group>"; };
 		980978C8B7B8892A00D59EBF /* XCSchemeInfo+ProfileActionInfo.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = "XCSchemeInfo+ProfileActionInfo.swift"; sourceTree = "<group>"; };
 		98CA01BCABAF4205F1E15E97 /* swiftc */ = {isa = PBXFileReference; explicitFileType = "compiled.mach-o.executable"; includeInIndex = 0; path = swiftc; sourceTree = BUILT_PRODUCTS_DIR; };
-		98FB3331A657CE14F517D2F9 /* PathExtensionsTests.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = PathExtensionsTests.swift; sourceTree = "<group>"; };
 		995B5304CE18F36C413D225E /* ZippyJSONCFamily.15.cxx.compile.params */ = {isa = PBXFileReference; path = ZippyJSONCFamily.15.cxx.compile.params; sourceTree = "<group>"; };
 		996B2A115E843C1C37CE8EE1 /* SchemeAutogenerationMode.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = SchemeAutogenerationMode.swift; sourceTree = "<group>"; };
 		9A1ED07486D10DDAA9F681F7 /* PBXBatchUpdater.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = PBXBatchUpdater.swift; sourceTree = "<group>"; };
@@ -1683,7 +1680,6 @@
 				E4362F2D9C31A8E10398D412 /* Fixtures.swift */,
 				17A46E74D5EB9195A2F8109C /* GeneratorTests.swift */,
 				68E58AE38A15AAD67492BF95 /* Optional+ExtensionsTests.swift */,
-				98FB3331A657CE14F517D2F9 /* PathExtensionsTests.swift */,
 				CBF35A052C2CD5B4B4818DF7 /* PBXProductTypeExtensionsTests.swift */,
 				1A9526E2C4F63F4404FE5CA7 /* PBXProj+Testing.swift */,
 				436B26F3C1112775ADD28474 /* PBXTarget+ExtensionsTests.swift */,
@@ -2785,7 +2781,6 @@
 				9DCB4FD46510916EB3335EA1 /* PBXProductTypeExtensionsTests.swift in Sources */,
 				3A8C3CE14F1EBD5EFBD1FC14 /* PBXProj+Testing.swift in Sources */,
 				57FB92F0DB221BFFDD6B3A51 /* PBXTarget+ExtensionsTests.swift in Sources */,
-				3AFF507A5BAEE00DE63F13C6 /* PathExtensionsTests.swift in Sources */,
 				AF81566CC76AA1AFEB229F18 /* Platform+ExtensionsTests.swift in Sources */,
 				344D9568BBF4B290F575E2B6 /* PopulateMainGroupTests.swift in Sources */,
 				273B0203CC70B7CA76D68C6C /* SemanticVersionTests.swift in Sources */,
@@ -3058,7 +3053,6 @@
 				F8E3983B3B81463B0A3EF7D3 /* PBXProductTypeExtensionsTests.swift in Sources */,
 				E9A02207EC62ACE0F9B83B8C /* PBXProj+Testing.swift in Sources */,
 				A286CB9F8B178FD81B4B9535 /* PBXTarget+ExtensionsTests.swift in Sources */,
-				400825480781AE867F62DC0C /* PathExtensionsTests.swift in Sources */,
 				875FED97D6829CDAFFB77ACA /* Platform+ExtensionsTests.swift in Sources */,
 				E964E40E9CA2E9FCC25318A4 /* PopulateMainGroupTests.swift in Sources */,
 				CA84DA21A25A7371D3477969 /* SemanticVersionTests.swift in Sources */,

test/fixtures/generator/bwx.xcodeproj/project.pbxproj
@@ -103,7 +103,6 @@
 		44BB89423A6306778E9F8375 /* XCWorkspaceData.swift in Sources */ = {isa = PBXBuildFile; fileRef = 504B069348A0605F21611FC4 /* XCWorkspaceData.swift */; };
 		44E1BDF8DE81B7CDBB4BE268 /* XcodeScheme+BuildForTests.swift in Sources */ = {isa = PBXBuildFile; fileRef = 34B813E13CE46E2FC2BD8BF1 /* XcodeScheme+BuildForTests.swift */; };
 		4515CA63533DFA2BB369285A /* BuildSettingConditionalTests.swift in Sources */ = {isa = PBXBuildFile; fileRef = A8E9CD80363AD803C9D7DE9D /* BuildSettingConditionalTests.swift */; };
-		4581DB6B70A358DFEE4438B3 /* PathExtensionsTests.swift in Sources */ = {isa = PBXBuildFile; fileRef = D28B7FC6715E7326C616241F /* PathExtensionsTests.swift */; };
 		46EACED5CD1929982C9543CE /* Directories.swift in Sources */ = {isa = PBXBuildFile; fileRef = E614119A7EDEBA96CCEA90A8 /* Directories.swift */; };
 		4875C26C37CC43659754FDED /* ConsolidatedTargetTests.swift in Sources */ = {isa = PBXBuildFile; fileRef = 29E7D384959A44AD2F611FBE /* ConsolidatedTargetTests.swift */; };
 		4890FC12D7D77C0C680FB9DE /* OrderedSet+SubSequence.swift in Sources */ = {isa = PBXBuildFile; fileRef = C378FEA4A01A92CC1AC06961 /* OrderedSet+SubSequence.swift */; };
@@ -146,7 +145,6 @@
 		66571FBAF69CE48BCAD0CA5B /* OrderedDictionary+Sequence.swift in Sources */ = {isa = PBXBuildFile; fileRef = B22BF45078ABF5D37BCB8303 /* OrderedDictionary+Sequence.swift */; };
 		6669E7ADE0CEC22FE5DDAA3B /* BuildSettingConditional.swift in Sources */ = {isa = PBXBuildFile; fileRef = BB5B8734422420CE74FDA1B5 /* BuildSettingConditional.swift */; };
 		670756F490E6AB32E314E01A /* CreateFilesAndGroupsTests.swift in Sources */ = {isa = PBXBuildFile; fileRef = 9A338CC77A69EE4BEF7EF192 /* CreateFilesAndGroupsTests.swift */; };
-		67A706C255DEA6EBB993B83C /* PathExtensionsTests.swift in Sources */ = {isa = PBXBuildFile; fileRef = D28B7FC6715E7326C616241F /* PathExtensionsTests.swift */; };
 		67FD348ABD8A53CBF11F42EE /* DisambiguateTargetsTests.swift in Sources */ = {isa = PBXBuildFile; fileRef = FEBDF5F30F45F0E6875E0101 /* DisambiguateTargetsTests.swift */; };
 		681771D6CCE15E492E5CC268 /* SetTargetDependenciesTests.swift in Sources */ = {isa = PBXBuildFile; fileRef = 99790B19EF9550743B230806 /* SetTargetDependenciesTests.swift */; };
 		68611051F587C6CDD275C6FA /* XcodeScheme+ExtensionsTests.swift in Sources */ = {isa = PBXBuildFile; fileRef = EE17FB22568407A03773476A /* XcodeScheme+ExtensionsTests.swift */; };
@@ -822,7 +820,6 @@
 		D03142A5FFF9B448765CB8CB /* OrderedSet+Initializers.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = "OrderedSet+Initializers.swift"; sourceTree = "<group>"; };
 		D101AB74A281F3C445463559 /* XcodeSchemeTests.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = XcodeSchemeTests.swift; sourceTree = "<group>"; };
 		D12DFF31D1BF48A2BB5D6FDF /* Path+Extensions.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = "Path+Extensions.swift"; sourceTree = "<group>"; };
-		D28B7FC6715E7326C616241F /* PathExtensionsTests.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = PathExtensionsTests.swift; sourceTree = "<group>"; };
 		D30974060706FEC409D7B86F /* XCScheme+LaunchAction.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = "XCScheme+LaunchAction.swift"; sourceTree = "<group>"; };
 		D3D5E0DB48F31A95C46CDE83 /* XCSharedData.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = XCSharedData.swift; sourceTree = "<group>"; };
 		D4532CA4F1662785402A7517 /* JJLISO8601DateFormatter.14.c.compile.params */ = {isa = PBXFileReference; path = JJLISO8601DateFormatter.14.c.compile.params; sourceTree = "<group>"; };
@@ -1381,7 +1378,6 @@
 				CE62022748A657FD096CED2F /* Fixtures.swift */,
 				DA18D77F4E0B4775D7E5D32C /* GeneratorTests.swift */,
 				743EE4E9856631C27E98DFDD /* Optional+ExtensionsTests.swift */,
-				D28B7FC6715E7326C616241F /* PathExtensionsTests.swift */,
 				E67D48D1E753E4D6E083F0E4 /* PBXProductTypeExtensionsTests.swift */,
 				898AFFFF8F10EA2BC4FB3E48 /* PBXProj+Testing.swift */,
 				29530BB3D0BD84B670B43375 /* PBXTarget+ExtensionsTests.swift */,
@@ -2745,7 +2741,6 @@
 				0854EFF4099ADC206BB6FA37 /* PBXProductTypeExtensionsTests.swift in Sources */,
 				E3EFF1BFDF032EB58DFBC9CF /* PBXProj+Testing.swift in Sources */,
 				FDAD88491C97B2FB5F5CC11C /* PBXTarget+ExtensionsTests.swift in Sources */,
-				4581DB6B70A358DFEE4438B3 /* PathExtensionsTests.swift in Sources */,
 				CE20576C4B5E28582026348C /* Platform+ExtensionsTests.swift in Sources */,
 				0E98F74DA1E999E218104F6F /* PopulateMainGroupTests.swift in Sources */,
 				27CA110FE916F197F02BB48B /* SemanticVersionTests.swift in Sources */,
@@ -2801,7 +2796,6 @@
 				30CAB221FD2E5BE95D5CEEF0 /* PBXProductTypeExtensionsTests.swift in Sources */,
 				97D5D55D7F645755F86D1FFE /* PBXProj+Testing.swift in Sources */,
 				0A48DA478A37D1605519F04F /* PBXTarget+ExtensionsTests.swift in Sources */,
-				67A706C255DEA6EBB993B83C /* PathExtensionsTests.swift in Sources */,
 				DECDB6ED68373F706F7EED0F /* Platform+ExtensionsTests.swift in Sources */,
 				36465DE2B19E995A2D500C52 /* PopulateMainGroupTests.swift in Sources */,
 				AA7450B194C329CE6259AAA9 /* SemanticVersionTests.swift in Sources */,

tools/generator/src/Extensions/Path+Extensions.swift
@@ -1,64 +1,6 @@
 import PathKit
 import XcodeProj
 
-extension Path {
-    var isFolderTypeFileSource: Bool {
-        return isXCAssets
-            || isFramework
-            || isBundle
-            || isDocCArchive
-            || isSceneKitAssets
-            || isCoreDataModel
-    }
-
-    var isCoreDataContainer: Bool { self.extension == "xcdatamodeld" }
-    var isCoreDataModel: Bool { self.extension == "xcdatamodel" }
-    var isLocalizedContainer: Bool { self.extension == "lproj" }
-
-    var isBazelBuildFile: Bool {
-        lastComponent == "BUILD" || lastComponent == "BUILD.bazel"
-    }
-    var isPodfile: Bool {
-        lastComponent == "Podfile"
-    }
-
-    var explicitFileType: String? {
-        if isBazelBuildFile{
-            return Xcode.filetype(extension: "py")
-        }
-        if isPodfile {
-            return Xcode.filetype(extension: "rb")
-        }
-        return nil
-    }
-
-    var lastKnownFileType: String? {
-        // XcodeProj treats `.inc` files as Pascal source files, but
-        // they're commonly C/C++ headers, so map them as such here.
-        if self.extension == "inc", let ext = Xcode.filetype(extension: "h") {
-            return ext
-        }
-        return self.extension.flatMap { Xcode.filetype(extension: $0) }
-    }
-
-    var versionGroupType: String? {
-        switch self.extension {
-        case "xcdatamodeld":
-            return "wrapper.xcdatamodel"
-        case let fileExtension?:
-            return Xcode.filetype(extension: fileExtension)
-        default:
-            return nil
-        }
-    }
-
-    private var isBundle: Bool { self.extension == "bundle" }
-    private var isDocCArchive: Bool { self.extension == "docc" }
-    private var isFramework: Bool { self.extension == "framework" }
-    private var isSceneKitAssets: Bool { self.extension == "scnassets" }
-    private var isXCAssets: Bool { self.extension == "xcassets" }
-}
-
 extension String {
     /// Wraps the path in quotes if it needs it.
     var quoted: String {

tools/generator/test/CreateFilesAndGroupsTests.swift
@@ -139,8 +139,8 @@ final class CreateFilesAndGroupsTests: XCTestCase {
             expectedElements["Assets.xcassets"]!,
             expectedElements["b.c"]!,
             expectedElements["d.h"]!,
-            expectedElements["Example.xib"]!,
-            expectedElements["Localized.strings"]!,
+            expectedElements["Base.lproj/Example.xib"]!,
+            expectedElements["en.lproj/Localized.strings"]!,
             expectedElements["z.h"]!,
             expectedElements["z.mm"]!,
             // Then Bazel External Repositories
@@ -248,8 +248,8 @@ final class CreateFilesAndGroupsTests: XCTestCase {
             expectedElements["Assets.xcassets"]!,
             expectedElements["b.c"]!,
             expectedElements["d.h"]!,
-            expectedElements["Example.xib"]!,
-            expectedElements["Localized.strings"]!,
+            expectedElements["Base.lproj/Example.xib"]!,
+            expectedElements["en.lproj/Localized.strings"]!,
             expectedElements["z.h"]!,
             expectedElements["z.mm"]!,
             // Then Bazel External Repositories

tools/generator/test/Fixtures.swift
@@ -767,7 +767,7 @@ enum Fixtures {
             lastKnownFileType: "file.xib",
             path: "Base.lproj/Example.xib"
         )
-        elements["Example.xib"] = PBXVariantGroup(
+        let example_xib_variant_group = PBXVariantGroup(
             children: [
                 base_lproj_example_xib,
                 en_lproj_example_strings,
@@ -776,9 +776,9 @@ enum Fixtures {
             sourceTree: .group,
             name: "Example.xib"
         )
-        elements["Base.lproj/Example.xib"] = elements["Example.xib"]!
-        elements["en.lproj/Example.strings"] = elements["Example.xib"]!
-        elements["es.lproj/Example.strings"] = elements["Example.xib"]!
+        elements["Base.lproj/Example.xib"] = example_xib_variant_group
+        elements["en.lproj/Example.strings"] = example_xib_variant_group
+        elements["es.lproj/Example.strings"] = example_xib_variant_group
 
         let en_lproj_localized_strings = PBXFileReference(
             sourceTree: .group,
@@ -792,7 +792,7 @@ enum Fixtures {
             lastKnownFileType: "text.plist.strings",
             path: "es.lproj/Localized.strings"
         )
-        elements["Localized.strings"] = PBXVariantGroup(
+        let localized_strings_variant_group = PBXVariantGroup(
             children: [
                 en_lproj_localized_strings,
                 es_lproj_localized_strings,
@@ -800,8 +800,8 @@ enum Fixtures {
             sourceTree: .group,
             name: "Localized.strings"
         )
-        elements["en.lproj/Localized.strings"] = elements["Localized.strings"]!
-        elements["es.lproj/Localized.strings"] = elements["Localized.strings"]!
+        elements["en.lproj/Localized.strings"] = localized_strings_variant_group
+        elements["es.lproj/Localized.strings"] = localized_strings_variant_group
 
         // r1/X.txt
 
@@ -1472,8 +1472,10 @@ touch "$SCRIPT_OUTPUT_FILE_1"
                 ),
                 PBXResourcesBuildPhase(
                     files: buildFiles([
-                        PBXBuildFile(file: elements["Example.xib"]!),
-                        PBXBuildFile(file: elements["Localized.strings"]!),
+                        PBXBuildFile(file: elements["Base.lproj/Example.xib"]!),
+                        PBXBuildFile(
+                            file: elements["es.lproj/Localized.strings"]!
+                        ),
                         PBXBuildFile(file: products
                             .byFilePath[.generated("r1/R1.bundle")]!
                         ),
@@ -2345,5 +2347,11 @@ extension FilePath {
             path: path,
             isFolder: isFolder
         )
+    }
+}
+
+extension Sequence where Element == PBXFileElement {
+    func sortedLocalizedStandard() -> [Element] {
+        return sortedLocalizedStandard(\.namePathSortString)
     }
 }

tools/generator/test/PathExtensionsTests.swift
@@ -1,22 +1,0 @@
-import PathKit
-import XCTest
-
-@testable import generator
-
-final class PathExtensionsTests: XCTestCase {
-    func test_is_bazel_build_file() {
-        XCTAssertFalse(("/foo" as Path).isBazelBuildFile)
-        XCTAssertFalse(("/ABUILD" as Path).isBazelBuildFile)
-        XCTAssertTrue(("/BUILD.bazel" as Path).isBazelBuildFile)
-        XCTAssertTrue(("/BUILD" as Path).isBazelBuildFile)
-        XCTAssertTrue(("/foo/BUILD" as Path).isBazelBuildFile)
-    }
-
-    func test_explicit_file_type() {
-        XCTAssertNil(("/foo" as Path).explicitFileType)
-        XCTAssertNil(("/ABUILD" as Path).explicitFileType)
-        XCTAssertEqual(("/BUILD.bazel" as Path).explicitFileType, "text.script.python")
-        XCTAssertEqual(("/BUILD" as Path).explicitFileType, "text.script.python")
-        XCTAssertEqual(("/foo/BUILD" as Path).explicitFileType, "text.script.python")
-    }
-}

commit c20f2c24defdd7cc121719040745f902cab2c74b

Fix more sorting non-determinism (#2055)
Regressed with 9041dc2af622df7b3c0221add5ff1cbf07dc5b1b.

We are caching the non-fully created tree result in
`PBXFileElement.namePathSortString`, so we need a new cache for
`PBXBuildFile` explicitly, which can take into account the parent.

Signed-off-by: Brentley Jones <github@brentleyjones.com>

tools/generator/src/Extensions/Sorting.swift
@@ -14,6 +14,31 @@ extension PBXFileElement {
             return cached
         }
 
+        let ret = """
+\(name ?? path ?? "")\t\(name ?? "")\t\(path ?? "")
+"""
+        Self.cache[uuid] = ret
+        return ret
+    }
+}
+
+extension PBXBuildFile {
+    private static var cache: [String: String] = [:]
+    private static let cacheLock = NSRecursiveLock()
+
+    var namePathSortString: String {
+        Self.cacheLock.lock()
+        defer {
+            Self.cacheLock.unlock()
+        }
+        if let cached = Self.cache[uuid] {
+            return cached
+        }
+
+        let name = file!.name
+        let path = file!.path
+        let parent = file!.parent
+
         let parentNamePathSortString = parent?.namePathSortString ?? ""
         let ret = """
 \(name ?? path ?? "")\t\(name ?? "")\t\(path ?? "")\t\(parentNamePathSortString)
@@ -62,7 +87,7 @@ extension Array where Element == PBXFileElement {
 
 extension Sequence where Element == PBXBuildFile {
     func sortedLocalizedStandard() -> [Element] {
-        return sortedLocalizedStandard(\.file!.namePathSortString)
+        return sortedLocalizedStandard(\.namePathSortString)
     }
 }
 

examples/integration/test/fixtures/bwx.xcodeproj/project.pbxproj
@@ -7997,8 +7997,8 @@
 			isa = PBXResourcesBuildPhase;
 			buildActionMask = 2147483647;
 			files = (
-				F2654AC7861464A75EF63853 /* Assets.xcassets in Resources */,
 				7950848A641B9A1086428155 /* Assets.xcassets in Resources */,
+				F2654AC7861464A75EF63853 /* Assets.xcassets in Resources */,
 			);
 			runOnlyForDeploymentPostprocessing = 0;
 		};
@@ -8032,10 +8032,10 @@
 			buildActionMask = 2147483647;
 			files = (
 				A7EE330C67CB794EA5DC8FCE /* Assets.xcassets in Resources */,
-				EB509AB2045BC989DBE6563C /* bucket in Resources */,
 				96536BFC80D7064031732BC1 /* bucket in Resources */,
-				57B6669C9AE0F887A3D54BDA /* bucket in Resources */,
 				130B4EF21F478D0D76C741D0 /* bucket in Resources */,
+				57B6669C9AE0F887A3D54BDA /* bucket in Resources */,
+				EB509AB2045BC989DBE6563C /* bucket in Resources */,
 				A48717BC5F009B090ECFBE3D /* ExampleNestedResources.bundle in Resources */,
 				8FA8E339A4D6550088042CF4 /* nested in Resources */,
 			);
@@ -8058,8 +8058,8 @@
 				5748C8D48BA08EF8C2F33D56 /* Assets.xcassets in Resources */,
 				E278F1DD818736A085E8EF07 /* ExampleNestedResources.bundle in Resources */,
 				9662270A130DFD557C8BEBA5 /* ExampleResources.bundle in Resources */,
-				868A1DC835387DF8B2925C81 /* GoogleMaps.bundle in Resources */,
 				31FB3493B0CBCAB455AFA25D /* GoogleMaps.bundle in Resources */,
+				868A1DC835387DF8B2925C81 /* GoogleMaps.bundle in Resources */,
 				C52713F177F42B0488A82FA1 /* launch_images_ios.xcassets in Resources */,
 				E372FF2EEEDA50F8041AD197 /* Launch.storyboard in Resources */,
 				3DB5B62545E1286F7EE7FF88 /* Localizable.strings in Resources */,
@@ -8129,8 +8129,8 @@
 			files = (
 				C96515E7CF5EB8DFDC4A4D33 /* Assets.xcassets in Resources */,
 				4BAA64EC25430110196B80F8 /* dir in Resources */,
-				C5806E50BE8A7D0EAAB1069F /* ExampleNestedResources.bundle in Resources */,
 				3BA5C2F09EA47A66AFDDCB3F /* ExampleNestedResources.bundle in Resources */,
+				C5806E50BE8A7D0EAAB1069F /* ExampleNestedResources.bundle in Resources */,
 				B113FDC76331C2B59078B69C /* Localizable.strings in Resources */,
 				B5B2DEF7E38E09E7CD67C7EB /* Utils.bundle in Resources */,
 			);