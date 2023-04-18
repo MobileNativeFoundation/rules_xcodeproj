MobileNativeFoundation/rules_xcodeproj

commit d20d87a929f34e58626d30d76c2ed94cf57e45be

Stop retaining some of `input_files` in `XcodeProj` (#2049)
We should retain the minimum needed in the `XcodeProj` provider.

---------

Signed-off-by: Brentley Jones <github@brentleyjones.com>

xcodeproj/internal/input_files.bzl
@@ -118,34 +118,45 @@ def _collect_input_files(
             their resources shouldn't be bundled with `target`.
 
     Returns:
-        A `struct` with the following fields:
+        A `tuple` with two elements:
 
-        *   `srcs`: A `depset` of `File`s that are inputs to `target`'s
-            `srcs`-like attributes.
-        *   `non_arc_srcs`: A `depset` of `File`s that are inputs to
-            `target`'s `non_arc_srcs`-like attributes.
-        *   `hdrs`: A `depset` of `File`s that are inputs to `target`'s
-            `hdrs`-like attributes.
-        *   `resources`: A `depset` of `FilePath`s that are inputs to `target`'s
-            `resources`-like and `structured_resources`-like attributes.
-        *   `xccurrentversions`: A `depset` of `.xccurrentversion` `File`s that
-            are in `resources`.
-        *   `generated`: A `depset` of generated `File`s that are inputs to
-            `target` or its transitive dependencies.
-        *   `important_generated`: A `depset` of important generated `File`s
-            that are inputs to `target` or its transitive dependencies. These
-            differ from `generated` in that they will be generated as part of
-            project generation, to ensure they are created before Xcode is
-            opened. Entitlements are an example of this, as Xcode won't even
-            start a build if they are missing.
-        *   `extra_files`: A `depset` of `FilePath`s that should be included in
-            the project, but aren't necessarily inputs to the target. This also
-            includes some categorized files of transitive dependencies
-            that didn't create an Xcode target.
-        *   `uncategorized`: A `depset` of `FilePath`s that are inputs to
-            `target` didn't fall into one of the more specific (e.g. `srcs`)
-            categories. These will only be included in the Xcode project if this
-            target becomes an input to another target's categorized attribute.
+        *   A `struct`, which will only be used within the aspect, with the
+            following fields:
+
+            *   `generated`: A `depset` of generated `File`s that are inputs to
+                `target` or its transitive dependencies.
+            *   `hdrs`: A `list` of `File`s that are inputs to `target`'s
+                `hdrs`-like attributes.
+            *   `non_arc_srcs`: A `list` of `File`s that are inputs to
+                `target`'s `non_arc_srcs`-like attributes.
+            *   `resources`: A `depset` of `FilePath`s that are inputs to
+                `target`'s `resources`-like and `structured_resources`-like
+                attributes.
+            *   `srcs`: A `list` of `File`s that are inputs to `target`'s
+                `srcs`-like attributes.
+
+        *   A `struct`, which will end up in `XcodeProjInfo.inputs`, with the
+            following fields:
+
+            *   `xccurrentversions`: A `depset` of `.xccurrentversion` `File`s
+                that are in `resources`.
+            *   `generated`: A `depset` of generated `File`s that are inputs to
+                `target` or its transitive dependencies.
+            *   `important_generated`: A `depset` of important generated `File`s
+                that are inputs to `target` or its transitive dependencies.
+                These differ from `generated` in that they will be generated as
+                part of project generation, to ensure they are created before
+                Xcode is opened. Entitlements are an example of this, as Xcode
+                won't even start a build if they are missing.
+            *   `extra_files`: A `depset` of `FilePath`s that should be included
+                in the project, but aren't necessarily inputs to the target.
+                This also includes some categorized files of transitive
+                dependencies that didn't create an Xcode target.
+            *   `uncategorized`: A `depset` of `FilePath`s that are inputs to
+                `target` didn't fall into one of the more specific (e.g. `srcs`)
+                categories. These will only be included in the Xcode project if
+                this target becomes an input to another target's categorized
+                attribute.
     """
     output_files = target.files.to_list()
 
@@ -354,8 +365,8 @@ def _collect_input_files(
     is_resource_bundle_consuming = is_bundle and AppleResourceInfo in target
     label = target.label
 
-    resources = None
-    folder_resources = None
+    resources = tuple()
+    folder_resources = tuple()
     resource_bundles = None
     resource_bundle_dependencies = None
     xccurrentversions = None
@@ -407,9 +418,9 @@ def _collect_input_files(
         if resources_result.dependencies:
             resource_bundle_dependencies = resources_result.dependencies
         if resources_result.resources:
-            resources = depset(resources_result.resources)
+            resources = resources_result.resources
         if resources_result.folder_resources:
-            folder_resources = depset(resources_result.folder_resources)
+            folder_resources = resources_result.folder_resources
     else:
         resource_bundle_labels = depset(
             transitive = [
@@ -658,130 +669,123 @@ def _collect_input_files(
             ],
         )
 
-    return struct(
-        _modulemaps = modulemaps_depset,
-        _non_target_swift_info_modules = non_target_swift_info_modules,
-        _output_group_list = depset(
-            direct_group_list,
-            transitive = [
-                info.inputs._output_group_list
+    return (
+        struct(
+            compiling_files = compiling_files,
+            compiling_output_group_name = compiling_output_group_name,
+            entitlements = entitlements[0] if entitlements else None,
+            folder_resources = folder_resources,
+            generated = generated_depset,
+            has_c_sources = bool(c_srcs),
+            has_cxx_sources = bool(cxx_srcs),
+            hdrs = hdrs,
+            indexstores = indexstores_depset,
+            indexstores_output_group_name = indexstores_output_group_name,
+            linking_output_group_name = linking_output_group_name,
+            non_arc_srcs = non_arc_srcs,
+            pch = pch[0] if pch else None,
+            resource_bundle_dependencies = depset(
+                resource_bundle_dependencies,
+            ),
+            resources = resources,
+            srcs = srcs,
+            unfocused_generated_compiling = unfocused_generated_compiling,
+            unfocused_generated_indexstores = unfocused_generated_indexstores,
+            unfocused_generated_linking = unfocused_generated_linking,
+        ),
+        struct(
+            _modulemaps = modulemaps_depset,
+            _non_target_swift_info_modules = non_target_swift_info_modules,
+            _output_group_list = depset(
+                direct_group_list,
+                transitive = [
+                    info.inputs._output_group_list
+                    for attr, info in transitive_infos
+                    if (info.target_type in
+                        automatic_target_info.xcode_targets.get(attr, [None]))
+                ],
+            ),
+            _product_framework_files = product_framework_files,
+            _resource_bundle_labels = resource_bundle_labels,
+            _resource_bundle_uncategorized = resource_bundle_uncategorized,
+            resource_bundles = depset(
+                resource_bundles,
+                transitive = [
+                    info.inputs.resource_bundles
+                    for attr, info in transitive_infos
+                    if (info.target_type in
+                        automatic_target_info.xcode_targets.get(attr, [None]))
+                ],
+            ),
+            xccurrentversions = depset(
+                [(label, tuple(xccurrentversions))] if xccurrentversions else None,
+                transitive = [
+                    info.inputs.xccurrentversions
+                    for attr, info in transitive_infos
+                    if (info.target_type in
+                        automatic_target_info.xcode_targets.get(attr, [None]))
+                ],
+            ),
+            generated = generated_depset,
+            important_generated = depset(
+                important_generated if important_generated else None,
+                transitive = [
+                    info.inputs.important_generated
+                    for attr, info in transitive_infos
+                    if (info.target_type in
+                        automatic_target_info.xcode_targets.get(attr, [None]))
+                ],
+            ),
+            has_generated_files = bool(generated) or bool([
+                True
                 for attr, info in transitive_infos
-                if (info.target_type in
-                    automatic_target_info.xcode_targets.get(attr, [None]))
-            ],
+                if (info.inputs.has_generated_files and
+                    (info.target_type in
+                     automatic_target_info.xcode_targets.get(attr, [None])))
+            ]),
+            indexstores = indexstores_depset,
+            extra_files = depset(
+                [(label, depset(extra_files))] if extra_files else None,
+                transitive = [
+                    depset(transitive = [info.inputs.extra_files])
+                    for attr, info in transitive_infos
+                    if (info.target_type in
+                        automatic_target_info.xcode_targets.get(attr, [None]))
+                ] + transitive_extra_files,
+            ),
+            uncategorized = depset(
+                [(label, depset(uncategorized))] if uncategorized else None,
+                transitive = [
+                    _collect_transitive_uncategorized(info)
+                    for attr, info in transitive_infos
+                    if (info.target_type in
+                        automatic_target_info.xcode_targets.get(attr, [None]))
+                ],
+            ),
+            unfocused_libraries = unfocused_libraries,
         ),
-        _product_framework_files = product_framework_files,
-        _resource_bundle_labels = resource_bundle_labels,
-        _resource_bundle_uncategorized = resource_bundle_uncategorized,
-        srcs = depset(srcs),
-        non_arc_srcs = depset(non_arc_srcs),
-        hdrs = depset(hdrs),
-        pch = pch[0] if pch else None,
-        has_c_sources = bool(c_srcs),
-        has_cxx_sources = bool(cxx_srcs),
-        resources = resources,
-        folder_resources = folder_resources,
-        resource_bundles = depset(
-            resource_bundles,
-            transitive = [
-                info.inputs.resource_bundles
-                for attr, info in transitive_infos
-                if (info.target_type in
-                    automatic_target_info.xcode_targets.get(attr, [None]))
-            ],
-        ),
-        resource_bundle_dependencies = depset(
-            resource_bundle_dependencies,
-        ),
-        entitlements = entitlements[0] if entitlements else None,
-        xccurrentversions = depset(
-            [(label, tuple(xccurrentversions))] if xccurrentversions else None,
-            transitive = [
-                info.inputs.xccurrentversions
-                for attr, info in transitive_infos
-                if (info.target_type in
-                    automatic_target_info.xcode_targets.get(attr, [None]))
-            ],
-        ),
-        generated = generated_depset,
-        important_generated = depset(
-            important_generated if important_generated else None,
-            transitive = [
-                info.inputs.important_generated
-                for attr, info in transitive_infos
-                if (info.target_type in
-                    automatic_target_info.xcode_targets.get(attr, [None]))
-            ],
-        ),
-        unfocused_generated_compiling = unfocused_generated_compiling,
-        unfocused_generated_indexstores = unfocused_generated_indexstores,
-        unfocused_generated_linking = unfocused_generated_linking,
-        has_generated_files = bool(generated) or bool([
-            True
-            for attr, info in transitive_infos
-            if (info.inputs.has_generated_files and
-                (info.target_type in
-                 automatic_target_info.xcode_targets.get(attr, [None])))
-        ]),
-        compiling_files = compiling_files,
-        indexstores = indexstores_depset,
-        extra_files = depset(
-            [(label, depset(extra_files))] if extra_files else None,
-            transitive = [
-                depset(transitive = [info.inputs.extra_files])
-                for attr, info in transitive_infos
-                if (info.target_type in
-                    automatic_target_info.xcode_targets.get(attr, [None]))
-            ] + transitive_extra_files,
-        ),
-        uncategorized = depset(
-            [(label, depset(uncategorized))] if uncategorized else None,
-            transitive = [
-                _collect_transitive_uncategorized(info)
-                for attr, info in transitive_infos
-                if (info.target_type in
-                    automatic_target_info.xcode_targets.get(attr, [None]))
-            ],
-        ),
-        unfocused_libraries = unfocused_libraries,
-        compiling_output_group_name = compiling_output_group_name,
-        indexstores_output_group_name = indexstores_output_group_name,
-        linking_output_group_name = linking_output_group_name,
     )
 
 def _from_resource_bundle(bundle):
     return struct(
-        _modulemaps = depset(),
-        _non_target_swift_info_modules = depset(),
-        _output_group_list = depset(),
-        _product_framework_files = depset(),
-        _resource_bundle_labels = depset(),
-        _resource_bundle_uncategorized = depset(),
-        srcs = depset(),
-        non_arc_srcs = depset(),
-        hdrs = depset(),
-        pch = None,
+        compiling_output_group_name = None,
+        entitlements = None,
+        folder_resources = bundle.folder_resources,
+        generated = depset(),
         has_c_sources = False,
         has_cxx_sources = False,
-        resources = depset(bundle.resources),
-        folder_resources = depset(bundle.folder_resources),
-        resource_bundles = depset(),
+        hdrs = [],
+        indexstores = depset(),
+        indexstores_output_group_name = None,
+        linking_output_group_name = None,
+        non_arc_srcs = [],
+        pch = None,
         resource_bundle_dependencies = bundle.dependencies,
-        entitlements = None,
-        xccurrentversions = depset(),
-        generated = depset(),
-        important_generated = depset(),
+        resources = bundle.resources,
+        srcs = [],
         unfocused_generated_compiling = None,
         unfocused_generated_indexstores = None,
         unfocused_generated_linking = None,
-        indexstores = depset(),
-        compiling_files = depset(),
-        extra_files = depset(),
-        uncategorized = depset(),
-        unfocused_libraries = depset(),
-        compiling_output_group_name = None,
-        indexstores_output_group_name = None,
-        linking_output_group_name = None,
     )
 
 def _merge_input_files(*, transitive_infos, extra_generated = None):
@@ -834,21 +838,12 @@ def _merge_input_files(*, transitive_infos, extra_generated = None):
                 for _, info in transitive_infos
             ],
         ),
-        srcs = depset(),
-        non_arc_srcs = depset(),
-        hdrs = depset(),
-        pch = None,
-        has_c_sources = False,
-        has_cxx_sources = False,
-        resources = None,
-        folder_resources = None,
         resource_bundles = depset(
             transitive = [
                 info.inputs.resource_bundles
                 for _, info in transitive_infos
             ],
         ),
-        entitlements = None,
         xccurrentversions = depset(
             transitive = [
                 info.inputs.xccurrentversions
@@ -868,9 +863,6 @@ def _merge_input_files(*, transitive_infos, extra_generated = None):
                 for _, info in transitive_infos
             ],
         ),
-        unfocused_generated_compiling = None,
-        unfocused_generated_indexstores = None,
-        unfocused_generated_linking = None,
         has_generated_files = bool([
             True
             for _, info in transitive_infos
@@ -882,7 +874,6 @@ def _merge_input_files(*, transitive_infos, extra_generated = None):
                 for _, info in transitive_infos
             ],
         ),
-        compiling_files = depset(),
         extra_files = depset(
             transitive = [
                 info.inputs.extra_files
@@ -901,9 +892,6 @@ def _merge_input_files(*, transitive_infos, extra_generated = None):
                 for _, info in transitive_infos
             ],
         ),
-        compiling_output_group_name = None,
-        indexstores_output_group_name = None,
-        linking_output_group_name = None,
     )
 
 def _process_output_group_files(

xcodeproj/internal/resources.bzl
@@ -373,8 +373,8 @@ def collect_resources(
             ]
             if bundle
         ],
-        resources = root_bundle.resources,
-        folder_resources = root_bundle.folder_resources,
+        resources = tuple(root_bundle.resources),
+        folder_resources = tuple(root_bundle.folder_resources),
         generated = generated,
         xccurrentversions = xccurrentversions,
         extra_files = extra_files,

xcodeproj/internal/xcode_targets.bzl
@@ -181,9 +181,9 @@ def _lldb_context_key(*, platform, product):
 
 def _to_xcode_target_inputs(inputs):
     return struct(
-        srcs = inputs.srcs,
-        non_arc_srcs = inputs.non_arc_srcs,
-        hdrs = inputs.hdrs,
+        srcs = tuple(inputs.srcs),
+        non_arc_srcs = tuple(inputs.non_arc_srcs),
+        hdrs = tuple(inputs.hdrs),
         pch = inputs.pch,
         entitlements = inputs.entitlements,
         has_c_sources = inputs.has_c_sources,
@@ -815,7 +815,7 @@ def _inputs_to_dto(inputs):
         if value:
             ret[key] = [
                 file.path
-                for file in value.to_list()
+                for file in value
             ]
 
     _process_attr("srcs", "s")
@@ -832,14 +832,14 @@ def _inputs_to_dto(inputs):
         set_if_true(
             ret,
             "r",
-            inputs.resources.to_list(),
+            inputs.resources,
         )
 
     if inputs.folder_resources:
         set_if_true(
             ret,
             "f",
-            inputs.folder_resources.to_list(),
+            inputs.folder_resources,
         )
 
     return ret

xcodeproj/internal/xcodeproj_rule.bzl
@@ -447,10 +447,13 @@ targets.
             # Don't create targets for resource bundles in BwB mode, but still
             # include their files if they aren't unfocused
             focused_targets_extra_files.append(
-                (xcode_target.label, xcode_target.inputs.resources),
+                (xcode_target.label, depset(xcode_target.inputs.resources)),
             )
             focused_targets_extra_folders.append(
-                (xcode_target.label, xcode_target.inputs.folder_resources),
+                (
+                    xcode_target.label,
+                    depset(xcode_target.inputs.folder_resources),
+                ),
             )
             files_only_targets[xcode_target.id] = xcode_target
             continue

xcodeproj/internal/library_targets.bzl
@@ -116,7 +116,7 @@ def process_library_target(
     )
 
     modulemaps = process_modulemaps(swift_info = swift_info)
-    inputs = input_files.collect(
+    (target_inputs, provider_inputs) = input_files.collect(
         ctx = ctx,
         target = target,
         attrs = attrs,
@@ -135,15 +135,15 @@ def process_library_target(
         ctx = ctx,
         debug_outputs = debug_outputs,
         id = id,
-        inputs = inputs,
+        inputs = target_inputs,
         output_group_info = output_group_info,
         swift_info = swift_info,
         transitive_infos = transitive_infos,
     )
 
-    if inputs.pch:
+    if target_inputs.pch:
         build_settings["GCC_PREFIX_HEADER"] = build_setting_path(
-            file = inputs.pch,
+            file = target_inputs.pch,
         )
 
     package_bin_dir = join_paths_ignoring_empty(
@@ -154,8 +154,8 @@ def process_library_target(
     search_paths, conlyopts, cxxopts, swiftcopts, clang_opts = process_opts(
         ctx = ctx,
         build_mode = build_mode,
-        has_c_sources = inputs.has_c_sources,
-        has_cxx_sources = inputs.has_cxx_sources,
+        has_c_sources = target_inputs.has_c_sources,
+        has_cxx_sources = target_inputs.has_cxx_sources,
         target = target,
         implementation_compilation_context = implementation_compilation_context,
         package_bin_dir = package_bin_dir,
@@ -192,7 +192,7 @@ def process_library_target(
         search_paths = search_paths,
         modulemaps = modulemaps,
         swiftmodules = swiftmodules,
-        inputs = inputs,
+        inputs = target_inputs,
         linker_inputs = linker_inputs,
         dependencies = dependencies,
         transitive_dependencies = transitive_dependencies,
@@ -211,7 +211,7 @@ def process_library_target(
         automatic_target_info = automatic_target_info,
         compilation_providers = compilation_providers,
         dependencies = dependencies,
-        inputs = inputs,
+        inputs = provider_inputs,
         library = product.file,
         lldb_context = lldb_context,
         mergable_xcode_library_targets = mergable_xcode_library_targets,

xcodeproj/internal/non_xcode_targets.bzl
@@ -109,23 +109,25 @@ rules_xcodeproj requires {} to have `{}` set.
         if providers._is_xcode_library_target
     ]
 
+    (_, provider_inputs) = input_files.collect(
+        ctx = ctx,
+        target = target,
+        attrs = attrs,
+        unfocused = None,
+        id = None,
+        platform = None,
+        is_bundle = False,
+        product = None,
+        linker_inputs = linker_inputs,
+        automatic_target_info = automatic_target_info,
+        transitive_infos = transitive_infos,
+    )
+
     return processed_target(
         automatic_target_info = automatic_target_info,
         compilation_providers = compilation_providers,
         dependencies = dependencies,
-        inputs = input_files.collect(
-            ctx = ctx,
-            target = target,
-            attrs = attrs,
-            unfocused = None,
-            id = None,
-            platform = None,
-            is_bundle = False,
-            product = None,
-            linker_inputs = linker_inputs,
-            automatic_target_info = automatic_target_info,
-            transitive_infos = transitive_infos,
-        ),
+        inputs = provider_inputs,
         lldb_context = lldb_contexts.collect(
             id = None,
             is_swift = is_swift,

xcodeproj/internal/top_level_targets.bzl
@@ -372,7 +372,7 @@ def process_top_level_target(
         linker_inputs = linker_inputs,
     )
 
-    inputs = input_files.collect(
+    (target_inputs, provider_inputs) = input_files.collect(
         ctx = ctx,
         target = target,
         attrs = attrs,
@@ -393,7 +393,7 @@ def process_top_level_target(
         ctx = ctx,
         debug_outputs = debug_outputs,
         id = id,
-        inputs = inputs,
+        inputs = target_inputs,
         output_group_info = output_group_info,
         swift_info = swift_info,
         top_level_product = product,
@@ -401,9 +401,9 @@ def process_top_level_target(
         transitive_infos = transitive_infos,
     )
 
-    if inputs.entitlements:
+    if target_inputs.entitlements:
         build_settings["CODE_SIGN_ENTITLEMENTS"] = build_setting_path(
-            file = inputs.entitlements,
+            file = target_inputs.entitlements,
         )
 
     package_bin_dir = join_paths_ignoring_empty(
@@ -414,15 +414,15 @@ def process_top_level_target(
     search_paths, conlyopts, cxxopts, swiftcopts, clang_opts = process_opts(
         ctx = ctx,
         build_mode = build_mode,
-        has_c_sources = inputs.has_c_sources,
-        has_cxx_sources = inputs.has_cxx_sources,
+        has_c_sources = target_inputs.has_c_sources,
+        has_cxx_sources = target_inputs.has_cxx_sources,
         target = target,
         implementation_compilation_context = implementation_compilation_context,
         package_bin_dir = package_bin_dir,
         build_settings = build_settings,
     )
 
-    if not inputs.srcs:
+    if not target_inputs.srcs:
         potential_target_merges = [
             struct(
                 src = mergeable_target,
@@ -500,7 +500,7 @@ def process_top_level_target(
         dependencies = dependencies,
         extension_infoplists = extension_infoplists,
         hosted_targets = hosted_targets,
-        inputs = inputs,
+        inputs = provider_inputs,
         is_top_level_target = True,
         is_xcode_required = True,
         lldb_context = lldb_context,
@@ -525,7 +525,7 @@ def process_top_level_target(
             search_paths = search_paths,
             modulemaps = modulemaps,
             swiftmodules = swiftmodules,
-            inputs = inputs,
+            inputs = target_inputs,
             linker_inputs = linker_inputs,
             infoplist = infoplist,
             watch_application = watch_application,

commit 711a72b97072c3fdc611bace6365d8832f385512

Remove duplicate resources from spec.json
Regressed with d20d87a929f34e58626d30d76c2ed94cf57e45be.

Signed-off-by: Brentley Jones <github@brentleyjones.com>

xcodeproj/internal/input_files.bzl
@@ -365,8 +365,8 @@ def _collect_input_files(
     is_resource_bundle_consuming = is_bundle and AppleResourceInfo in target
     label = target.label
 
-    resources = tuple()
-    folder_resources = tuple()
+    resources = None
+    folder_resources = None
     resource_bundles = None
     resource_bundle_dependencies = None
     xccurrentversions = None
@@ -418,9 +418,9 @@ def _collect_input_files(
         if resources_result.dependencies:
             resource_bundle_dependencies = resources_result.dependencies
         if resources_result.resources:
-            resources = resources_result.resources
+            resources = depset(resources_result.resources)
         if resources_result.folder_resources:
-            folder_resources = resources_result.folder_resources
+            folder_resources = depset(resources_result.folder_resources)
     else:
         resource_bundle_labels = depset(
             transitive = [
@@ -770,7 +770,7 @@ def _from_resource_bundle(bundle):
     return struct(
         compiling_output_group_name = None,
         entitlements = None,
-        folder_resources = bundle.folder_resources,
+        folder_resources = depset(bundle.folder_resources),
         generated = depset(),
         has_c_sources = False,
         has_cxx_sources = False,
@@ -781,7 +781,7 @@ def _from_resource_bundle(bundle):
         non_arc_srcs = [],
         pch = None,
         resource_bundle_dependencies = bundle.dependencies,
-        resources = bundle.resources,
+        resources = depset(bundle.resources),
         srcs = [],
         unfocused_generated_compiling = None,
         unfocused_generated_indexstores = None,

xcodeproj/internal/resources.bzl
@@ -373,8 +373,8 @@ def collect_resources(
             ]
             if bundle
         ],
-        resources = tuple(root_bundle.resources),
-        folder_resources = tuple(root_bundle.folder_resources),
+        resources = root_bundle.resources,
+        folder_resources = root_bundle.folder_resources,
         generated = generated,
         xccurrentversions = xccurrentversions,
         extra_files = extra_files,

xcodeproj/internal/xcode_targets.bzl
@@ -832,14 +832,14 @@ def _inputs_to_dto(inputs):
         set_if_true(
             ret,
             "r",
-            inputs.resources,
+            inputs.resources.to_list(),
         )
 
     if inputs.folder_resources:
         set_if_true(
             ret,
             "f",
-            inputs.folder_resources,
+            inputs.folder_resources.to_list(),
         )
 
     return ret

xcodeproj/internal/xcodeproj_rule.bzl
@@ -447,13 +447,10 @@ targets.
             # Don't create targets for resource bundles in BwB mode, but still
             # include their files if they aren't unfocused
             focused_targets_extra_files.append(
-                (xcode_target.label, depset(xcode_target.inputs.resources)),
+                (xcode_target.label, xcode_target.inputs.resources),
             )
             focused_targets_extra_folders.append(
-                (
-                    xcode_target.label,
-                    depset(xcode_target.inputs.folder_resources),
-                ),
+                (xcode_target.label, xcode_target.inputs.folder_resources),
             )
             files_only_targets[xcode_target.id] = xcode_target
             continue