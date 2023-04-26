MobileNativeFoundation/rules_xcodeproj

commit ac59dfa13f90156939ebfab8bc9f3dd6e66dfc1a

Use `args()` for `_write_target_ids_list` (#2086)

xcodeproj/internal/xcodeproj_rule.bzl
@@ -1348,9 +1348,15 @@ def _write_target_ids_list(*, ctx, target_dtos):
         "{}_target_ids".format(ctx.attr.name),
     )
 
+    args = ctx.actions.args()
+    args.add_joined(
+        sorted(target_dtos.keys()),
+        join_with = "\n",
+    )
+
     ctx.actions.write(
         output,
-        "".join([id + "\n" for id in sorted(target_dtos.keys())]),
+        args,
     )
 
     return output

commit 1980948a8862272a7e0cc7f7d113bcd3ada76918

Fix `_write_target_ids_list` regression
Regressed with ac59dfa13f90156939ebfab8bc9f3dd6e66dfc1a.

Signed-off-by: Brentley Jones <github@brentleyjones.com>

xcodeproj/internal/xcodeproj_rule.bzl
@@ -1356,10 +1356,8 @@ def _write_target_ids_list(*, ctx, target_dtos):
     )
 
     args = ctx.actions.args()
-    args.add_joined(
-        sorted(target_dtos.keys()),
-        join_with = "\n",
-    )
+    args.set_param_file_format("multiline")
+    args.add_all(sorted(target_dtos.keys()))
 
     ctx.actions.write(
         output,