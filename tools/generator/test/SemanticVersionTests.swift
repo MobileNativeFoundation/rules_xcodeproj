3ca81e938ed55b744c7265124789e341fa06cc2c
Brentley Jones <github@brentleyjones.com>
2023-05-04 13:35:31 -0500

    Format some tests
    Some minor code cleanup while I was in the area.
    
    Signed-off-by: Brentley Jones <github@brentleyjones.com>

--- tools/generator/test/SemanticVersionTests.swift
+++ tools/generator/test/SemanticVersionTests.swift
@@ -4,36 +4,36 @@ import XCTest
 @testable import generator
 
 // Inspired by https://gist.github.com/mjdescy/a805b5b4c49ed79fb240d3886815d5a2
-public class SemanticVersionNumberTests: XCTestCase {
-    func testInitFromStringWhenStringIsMalformedVerionNumberWithMoreThanThreeNumbers() {
+class SemanticVersionTests: XCTestCase {
+    func test_init_string_moreThanThreeNumbers() {
         let value = SemanticVersion(version: "1.2.3.4")
         XCTAssertNil(value)
     }
 
-    func testInitFromStringWhenStringIsMalformedVerionNumberWithLessThanThreeNumbers() {
+    func test_init_string_lessThanThreeNumbers() {
         let value = SemanticVersion(version: "1.2")
         let expected = SemanticVersion(major: 1, minor: 2, patch: 0)
         XCTAssertEqual(value, expected)
     }
 
-    func testInitFromStringWhenStringIsMalformedVerionNumberWithNonNumbers() {
+    func test_init_string_nonNumbers() {
         let value = SemanticVersion(version: "1.2.X")
         XCTAssertNil(value)
     }
 
-    func testInitFromStringWhenStringIsWellFormedVersionNumber() {
+    func test_init_string_wellFormed_normal() {
         let value = SemanticVersion(version: "1.2.3")
         let expectedValue = SemanticVersion(major: 1, minor: 2, patch: 3)
         XCTAssertEqual(value, expectedValue)
     }
 
-    func testInitFromStringWhenStringIsWellFormedVersionNumberAllZeroes() {
+    func test_init_string_wellFormed_allZeros() {
         let value = SemanticVersion(version: "0.0.0")
         let expectedValue = SemanticVersion(major: 0, minor: 0, patch: 0)
         XCTAssertEqual(value, expectedValue)
     }
 
-    func testConvertToString() {
+    func test_stringDescription() {
         let sver = SemanticVersion(major: 2, minor: 11, patch: 3)
         let value = String(describing: sver)
         let expectedValue = "2.11.3"
@@ -58,31 +58,31 @@ public class SemanticVersionNumberTests: XCTestCase {
         XCTAssertEqual(sver.pretty, expected)
     }
 
-    func testEqualityWhenEqual() {
+    func test_equatable_equal() {
         let lhs = SemanticVersion(major: 2, minor: 11, patch: 3)
         let rhs = SemanticVersion(major: 2, minor: 11, patch: 3)
         XCTAssertEqual(lhs, rhs)
     }
 
-    func testEqualityWhenNotEqual() {
+    func test_equatable_notEqual() {
         let lhs = SemanticVersion(major: 2, minor: 11, patch: 3)
         let rhs = SemanticVersion(major: 2, minor: 0, patch: 4)
         XCTAssertNotEqual(lhs, rhs)
     }
 
-    func testComparisonWhenLHSLessThanRHS() {
+    func test_comparable_lessThan() {
         let lhs = SemanticVersion(major: 2, minor: 0, patch: 4)
         let rhs = SemanticVersion(major: 2, minor: 11, patch: 3)
         XCTAssertLessThan(lhs, rhs)
     }
 
-    func testComparisonWhenLHSGreaterThanRHS() {
+    func test_comparable_greaterThan() {
         let lhs = SemanticVersion(major: 2, minor: 11, patch: 3)
         let rhs = SemanticVersion(major: 2, minor: 0, patch: 4)
         XCTAssertGreaterThan(lhs, rhs)
     }
 
-    func test_encodingAndDecoding() throws {
+    func test_codable() throws {
         let sver: SemanticVersion = "2.11.3"
 
         let encoder = JSONEncoder()
