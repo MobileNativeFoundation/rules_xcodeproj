ac424658802e7a9be8e90adddda4d7e74d653baa
Brentley Jones <github@brentleyjones.com>
2023-05-04 15:37:52 -0500

    Format some tests (#2154)
    Some minor code cleanup while I was in the area.
    
    Signed-off-by: Brentley Jones <github@brentleyjones.com>

--- tools/generator/test/DefaultLoggerTests.swift
+++ tools/generator/test/DefaultLoggerTests.swift
@@ -12,78 +12,187 @@ final class StringOutputStream: TextOutputStream {
 }
 
 final class DefaultLoggerTests: XCTestCase {
-    // MARK: - With Colors
 
-    func test_logDebug_With_Colors() {
+    // MARK: With colors
+
+    func test_logDebug_withColors() {
+        // Arrange
+
         let stderr = StringOutputStream()
         let stdout = StringOutputStream()
-        let logger = DefaultLogger(standardError: stderr, standardOutput: stdout, colorize: true)
+        let logger = DefaultLogger(
+            standardError: stderr,
+            standardOutput: stdout,
+            colorize: true
+        )
+
+        // Act
+
         logger.logDebug("this is a debug message")
+
+        // Assert
+
         XCTAssertEqual(stderr.output, "")
-        XCTAssertEqual(stdout.output, "\u{001B}[33mDEBUG:\u{001B}[0m this is a debug message\n")
+        XCTAssertEqual(
+            stdout.output,
+            "\u{001B}[33mDEBUG:\u{001B}[0m this is a debug message\n"
+        )
     }
 
-    func test_logInfo_With_Colors() {
+    func test_logInfo_withColors() {
+        // Arrange
+
         let stderr = StringOutputStream()
         let stdout = StringOutputStream()
-        let logger = DefaultLogger(standardError: stderr, standardOutput: stdout, colorize: true)
+        let logger = DefaultLogger(
+            standardError: stderr,
+            standardOutput: stdout,
+            colorize: true
+        )
+
+        // Act
+
         logger.logInfo("this is an info message")
+
+        // Assert
+
         XCTAssertEqual(stderr.output, "")
-        XCTAssertEqual(stdout.output, "\u{001B}[32mINFO:\u{001B}[0m this is an info message\n")
+        XCTAssertEqual(
+            stdout.output,
+            "\u{001B}[32mINFO:\u{001B}[0m this is an info message\n"
+        )
     }
 
-    func test_logWarning_With_Colors() {
+    func test_logWarning_withColors() {
+        // Arrange
+
         let stderr = StringOutputStream()
         let stdout = StringOutputStream()
-        let logger = DefaultLogger(standardError: stderr, standardOutput: stdout, colorize: true)
+        let logger = DefaultLogger(
+            standardError: stderr,
+            standardOutput: stdout,
+            colorize: true
+        )
+
+        // Act
+
         logger.logWarning("this is a warning message")
-        XCTAssertEqual(stderr.output, "\u{001B}[35mWARNING:\u{001B}[0m this is a warning message\n")
+
+        // Assert
+
+        XCTAssertEqual(
+            stderr.output,
+            "\u{001B}[35mWARNING:\u{001B}[0m this is a warning message\n"
+        )
         XCTAssertEqual(stdout.output, "")
     }
 
-    func test_logError_With_Colors() {
+    func test_logError_withColors() {
+        // Arrange
+
         let stderr = StringOutputStream()
         let stdout = StringOutputStream()
-        let logger = DefaultLogger(standardError: stderr, standardOutput: stdout, colorize: true)
+        let logger = DefaultLogger(
+            standardError: stderr,
+            standardOutput: stdout,
+            colorize: true
+        )
+
+        // Act
+
         logger.logError("this is an error message")
-        XCTAssertEqual(stderr.output, "\u{001B}[31;1mERROR:\u{001B}[0m this is an error message\n")
+
+        // Assert
+
+        XCTAssertEqual(
+            stderr.output,
+            "\u{001B}[31;1mERROR:\u{001B}[0m this is an error message\n"
+        )
         XCTAssertEqual(stdout.output, "")
     }
 
-    // MARK: - Without Colors
+    // MARK: Without colors
 
-    func test_logDebug_Without_Colors() {
+    func test_logDebug_withoutColors() {
+        // Arrange
+
         let stderr = StringOutputStream()
         let stdout = StringOutputStream()
-        let logger = DefaultLogger(standardError: stderr, standardOutput: stdout, colorize: false)
+        let logger = DefaultLogger(
+            standardError: stderr,
+            standardOutput: stdout,
+            colorize: false
+        )
+
+        // Act
+
         logger.logDebug("this is a debug message")
+
+        // Assert
+
         XCTAssertEqual(stderr.output, "")
         XCTAssertEqual(stdout.output, "DEBUG: this is a debug message\n")
     }
 
-    func test_logInfo_Without_Colors() {
+    func test_logInfo_withoutColors() {
+        // Arrange
+
         let stderr = StringOutputStream()
         let stdout = StringOutputStream()
-        let logger = DefaultLogger(standardError: stderr, standardOutput: stdout, colorize: false)
+        let logger = DefaultLogger(
+            standardError: stderr,
+            standardOutput: stdout,
+            colorize: false
+        )
+
+        // Act
+
         logger.logInfo("this is an info message")
+
+        // Assert
+
         XCTAssertEqual(stderr.output, "")
         XCTAssertEqual(stdout.output, "INFO: this is an info message\n")
     }
 
-    func test_logWarning_Without_Colors() {
+    func test_logWarning_withoutColors() {
+        // Arrange
+
         let stderr = StringOutputStream()
         let stdout = StringOutputStream()
-        let logger = DefaultLogger(standardError: stderr, standardOutput: stdout, colorize: false)
+        let logger = DefaultLogger(
+            standardError: stderr,
+            standardOutput: stdout,
+            colorize: false
+        )
+
+        // Act
+
         logger.logWarning("this is a warning message")
+
+        // Assert
+
         XCTAssertEqual(stderr.output, "WARNING: this is a warning message\n")
         XCTAssertEqual(stdout.output, "")
     }
 
-    func test_logError_Without_Colors() {
+    func test_logError_withoutColors() {
+        // Arrange
+
         let stderr = StringOutputStream()
         let stdout = StringOutputStream()
-        let logger = DefaultLogger(standardError: stderr, standardOutput: stdout, colorize: false)
+        let logger = DefaultLogger(
+            standardError: stderr,
+            standardOutput: stdout,
+            colorize: false
+        )
+
+        // Act
+
         logger.logError("this is an error message")
+
+        // Assert
+
         XCTAssertEqual(stderr.output, "ERROR: this is an error message\n")
         XCTAssertEqual(stdout.output, "")
     }
