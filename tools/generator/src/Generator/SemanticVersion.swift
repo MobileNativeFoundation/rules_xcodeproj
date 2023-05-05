a08239635fe66f9f990831fecb75be9f544401c9
Brentley Jones <github@brentleyjones.com>
2023-05-05 09:26:05 -0500

    SE-0283
    Signed-off-by: Brentley Jones <github@brentleyjones.com>

--- tools/generator/src/Generator/SemanticVersion.swift
+++ tools/generator/src/Generator/SemanticVersion.swift
@@ -47,13 +47,7 @@ public struct SemanticVersion: Equatable, Hashable {
 
 extension SemanticVersion: Comparable {
     public static func < (lhs: SemanticVersion, rhs: SemanticVersion) -> Bool {
-        guard lhs.major == rhs.major else {
-            return lhs.major < rhs.major
-        }
-        guard lhs.minor == rhs.minor else {
-            return lhs.minor < rhs.minor
-        }
-        return lhs.patch < rhs.patch
+        (lhs.major, lhs.minor, lhs.patch) < (rhs.major, rhs.minor, rhs.patch)
     }
 }
 
