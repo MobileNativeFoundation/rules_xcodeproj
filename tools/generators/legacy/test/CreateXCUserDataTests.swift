MobileNativeFoundation/rules_xcodeproj

commit 6b72c5edb5a46dde42fe0a8eeae9848c80d49bdc

Sort schemes in Xcode (#2151)
Fixes
https://github.com/MobileNativeFoundation/rules_xcodeproj/issues/1970

This pr generate
`PROJECT.xcodeproj/xcuserdata/$USER.xcuserdatad/xcschememanagement.plist`
with `orderHint` set to the order of manual specified scheme (user
listed order) > auto-generated top level scheme (alphabetic) >
auto-generated non-top level scheme (alphabetic).

New scheme order with rules_ios example:
<img width="774" alt="Screenshot 2023-05-01 at 12 45 16 PM"
src="https://user-images.githubusercontent.com/37749536/235518894-f6da658e-5458-45dd-85c6-8f330433fe35.png">

---------

Signed-off-by: David-Lam <yuensengd@gmail.com>
Signed-off-by: dlam <dlam@snapchat.com>

tools/generator/src/Generator/CreateXCUserData.swift
@@ -1,0 +1,47 @@
+import XcodeProj
+
+extension Generator {
+    /// Creates an `XCUserData`.
+    static func createXCUserData(
+        userName: String,
+        schemes: [XCScheme],
+        autogeneratedSchemes: [XCScheme],
+        targets: [PBXTarget]
+    ) -> XCUserData {
+        var sortedSchemeUserStates = schemes.enumerated().map { (index, scheme) in
+            return XCSchemeManagement.UserStateScheme(
+                name: "\(scheme.name).xcscheme",
+                shared: true,
+                orderHint: index,
+                isShown: true
+            )
+        }
+        
+        let targetNameDict = Dictionary(uniqueKeysWithValues: targets.map{ ($0.schemeName, $0) })
+        let sortedAutogeneratedSchemes = autogeneratedSchemes.sorted { (schemeA, schemeB) in
+            let isSchemeATopLevel = targetNameDict[schemeA.name]!.isTopLevel
+            let isSchemeBTopLevel = targetNameDict[schemeB.name]!.isTopLevel
+            // xor, prioritze isTopLevel comparsion then alphabetical if both scheme are top level or not top level
+            if (isSchemeATopLevel != isSchemeBTopLevel) {
+                return isSchemeATopLevel
+            }
+            return schemeA.name < schemeB.name
+        }
+        
+        let startingOrderHint = schemes.count
+        sortedSchemeUserStates.append(contentsOf: sortedAutogeneratedSchemes.enumerated().map { (index, scheme) in
+            return XCSchemeManagement.UserStateScheme(
+                name: "\(scheme.name).xcscheme",
+                shared: true,
+                orderHint: startingOrderHint + index,
+                isShown: true
+            )
+        })
+        
+        return XCUserData(
+            userName: userName,
+            schemes: [],
+            schemeManagement: XCSchemeManagement(schemeUserState: sortedSchemeUserStates)
+        )
+    }
+}

tools/generator/src/Generator/Environment.swift
@@ -130,10 +130,18 @@ struct Environment {
     ) throws -> [XCScheme]
 
     let createXCSharedData: (_ schemes: [XCScheme]) -> XCSharedData
+    
+    let createXCUserData: (
+        _ userName: String,
+        _ schemes: [XCScheme],
+        _ autogeneratedSchemes: [XCScheme],
+        _ targets: [PBXTarget]
+    ) -> XCUserData
 
     let createXcodeProj: (
         _ pbxProj: PBXProj,
-        _ sharedData: XCSharedData?
+        _ sharedData: XCSharedData?,
+        _ userData: XCUserData
     ) -> XcodeProj
 
     let writeXcodeProj: (

tools/generator/src/Generator/Generator.swift
@@ -1,3 +1,4 @@
+import Foundation
 import PathKit
 import XcodeProj
 
@@ -24,6 +25,7 @@ class Generator {
         createCustomXCSchemes: Generator.createCustomXCSchemes,
         createAutogeneratedXCSchemes: Generator.createAutogeneratedXCSchemes,
         createXCSharedData: Generator.createXCSharedData,
+        createXCUserData: Generator.createXCUserData,
         createXcodeProj: Generator.createXcodeProj,
         writeXcodeProj: Generator.writeXcodeProj
     )
@@ -183,17 +185,26 @@ class Generator {
             project.envs
         )
         let customSchemeNames = Set(schemes.map(\.name))
-        try schemes.append(contentsOf: environment.createAutogeneratedXCSchemes(
+        let autogeneratedSchemes = try environment.createAutogeneratedXCSchemes(
             project.schemeAutogenerationMode,
             buildMode,
             targetResolver,
             customSchemeNames,
             project.args,
             project.envs
-        ))
+        )
+        
+        let userData = environment.createXCUserData(
+            NSUserName(),
+            schemes,
+            autogeneratedSchemes,
+            Array(pbxTargets.values)
+        )
+        
+        schemes.append(contentsOf: autogeneratedSchemes)
         let sharedData = environment.createXCSharedData(schemes)
 
-        let xcodeProj = environment.createXcodeProj(pbxProj, sharedData)
+        let xcodeProj = environment.createXcodeProj(pbxProj, sharedData, userData)
         try await environment.writeXcodeProj(
             xcodeProj,
             directories,

tools/generator/test/CreateXCUserDataTests.swift
@@ -1,0 +1,87 @@
+import CustomDump
+import XcodeProj
+import XCTest
+
+@testable import generator
+
+final class CreateXCUserDataTests: XCTestCase {
+    func test_basic() throws {
+        // Arrange
+        
+        let userName = "userName"
+        let customSchemes = [
+            XCScheme(name: "User Defined B", lastUpgradeVersion: nil, version: nil),
+            XCScheme(name: "User Defined A", lastUpgradeVersion: nil, version: nil)
+        ]
+        let autogeneratedSchemes = [
+            XCScheme(name: "Non Top Level A", lastUpgradeVersion: nil, version: nil),
+            XCScheme(name: "Top Level B", lastUpgradeVersion: nil, version: nil),
+            XCScheme(name: "Top Level A", lastUpgradeVersion: nil, version: nil),
+            XCScheme(name: "Non Top Level B", lastUpgradeVersion: nil, version: nil)
+        ]
+        let targets = [
+            PBXTarget(name: "Top Level A", productType: .application),
+            PBXTarget(name: "Top Level B", productType: .unitTestBundle),
+            PBXTarget(name: "Non Top Level A"),
+            PBXTarget(name: "Non Top Level B")
+        ]
+        
+        let expectedUserData = XCUserData(
+            userName: userName,
+            schemes: [],
+            schemeManagement: XCSchemeManagement(
+                schemeUserState: [
+                    XCSchemeManagement.UserStateScheme(
+                        name: "User Defined B.xcscheme",
+                        shared: true,
+                        orderHint: 0,
+                        isShown: true
+                    ),
+                    XCSchemeManagement.UserStateScheme(
+                        name: "User Defined A.xcscheme",
+                        shared: true,
+                        orderHint: 1,
+                        isShown: true
+                    ),
+                    XCSchemeManagement.UserStateScheme(
+                        name: "Top Level A.xcscheme",
+                        shared: true,
+                        orderHint: 2,
+                        isShown: true
+                    ),
+                    XCSchemeManagement.UserStateScheme(
+                        name: "Top Level B.xcscheme",
+                        shared: true,
+                        orderHint: 3,
+                        isShown: true
+                    ),
+                    XCSchemeManagement.UserStateScheme(
+                        name: "Non Top Level A.xcscheme",
+                        shared: true,
+                        orderHint: 4,
+                        isShown: true
+                    ),
+                    XCSchemeManagement.UserStateScheme(
+                        name: "Non Top Level B.xcscheme",
+                        shared: true,
+                        orderHint: 5,
+                        isShown: true
+                    )
+                ]
+            )
+        )
+
+        // Act
+
+        let userData = Generator.createXCUserData(
+            userName: userName,
+            schemes: customSchemes,
+            autogeneratedSchemes: autogeneratedSchemes,
+            targets: targets
+        )
+
+        // Assert
+
+        XCTAssertNoDifference(userData, expectedUserData)
+    }
+}

tools/generator/test/GeneratorTests.swift
@@ -205,10 +205,31 @@ final class GeneratorTests: XCTestCase {
         ]
         let allXCSchemes = customXCSchemes + autogeneratedXCSchemes
         let sharedData = XCSharedData(schemes: allXCSchemes)
+        let userName = NSUserName()
+        let testUserData = XCUserData(
+            userName: userName,
+            schemes: [],
+            schemeManagement: XCSchemeManagement(schemeUserState: [
+                XCSchemeManagement.UserStateScheme(
+                    name: "Custom Scheme.xcscheme",
+                    shared: true,
+                    orderHint: 0,
+                    isShown: true
+                ),
+                XCSchemeManagement.UserStateScheme(
+                    name: "Autogenerated Scheme.xcscheme",
+                    shared: true,
+                    orderHint: 1,
+                    isShown: true
+                )
+            ])
+        )
+        let userData = testUserData
         let xcodeProj = XcodeProj(
             workspace: XCWorkspace(),
             pbxproj: pbxProj,
-            sharedData: sharedData
+            sharedData: sharedData,
+            userData: [userData]
         )
 
         let expectedMessagesLogged: [StubLogger.MessageLogged] = []
@@ -752,29 +773,65 @@ final class GeneratorTests: XCTestCase {
         let expectedCreateXCSharedDataCalled = [CreateXCSharedDataCalled(
             schemes: allXCSchemes
         )]
+        
+        // MARK: createXCUserData()
+
+        struct CreateXCUserDataCalled: Equatable {
+            let userName: String
+            let schemes: [XCScheme]
+            let autogeneratedSchemes: [XCScheme]
+            let targets: [PBXTarget]
+        }
+
+        var createXCUserDataCalled: [CreateXCUserDataCalled] = []
+        func createXCUserData(
+            userName: String,
+            schemes: [XCScheme],
+            autogeneratedSchemes: [XCScheme],
+            targets: [PBXTarget]
+        ) -> XCUserData {
+            createXCUserDataCalled.append(.init(
+                userName: userName,
+                schemes: schemes,
+                autogeneratedSchemes: autogeneratedSchemes,
+                targets: targets
+            ))
+            return testUserData
+        }
+
+        let expectedCreateXCUserDataCalled = [CreateXCUserDataCalled(
+            userName: userName,
+            schemes: customXCSchemes,
+            autogeneratedSchemes: autogeneratedXCSchemes,
+            targets: Array(pbxTargets.values)
+        )]
 
         // MARK: createXcodeProj()
 
         struct CreateXcodeProjCalled: Equatable {
             let pbxProj: PBXProj
             let sharedData: XCSharedData?
+            let userData: [XCUserData]
         }
 
         var createXcodeProjCalled: [CreateXcodeProjCalled] = []
         func createXcodeProj(
             for pbxProj: PBXProj,
-            sharedData: XCSharedData?
+            sharedData: XCSharedData?,
+            userData: XCUserData
         ) -> XcodeProj {
             createXcodeProjCalled.append(.init(
                 pbxProj: pbxProj,
-                sharedData: sharedData
+                sharedData: sharedData,
+                userData: [userData]
             ))
             return xcodeProj
         }
 
         let expectedCreateXcodeProjCalled = [CreateXcodeProjCalled(
             pbxProj: pbxProj,
-            sharedData: sharedData
+            sharedData: sharedData,
+            userData: [userData]
         )]
 
         // MARK: writeXcodeProj()
@@ -828,6 +885,7 @@ final class GeneratorTests: XCTestCase {
             createCustomXCSchemes: createCustomXCSchemes,
             createAutogeneratedXCSchemes: createAutogeneratedXCSchemes,
             createXCSharedData: createXCSharedData,
+            createXCUserData: createXCUserData,
             createXcodeProj: createXcodeProj,
             writeXcodeProj: writeXcodeProj
         )
@@ -908,6 +966,10 @@ final class GeneratorTests: XCTestCase {
         XCTAssertNoDifference(
             createXCSharedDataCalled,
             expectedCreateXCSharedDataCalled
+        )
+        XCTAssertNoDifference(
+            createXCUserDataCalled,
+            expectedCreateXCUserDataCalled
         )
         XCTAssertNoDifference(
             createXcodeProjCalled,

commit 2b2350a7a3a54f0259225b8ec37151928006d281

Fix hosted target schemes
Regressed with 6b72c5edb5a46dde42fe0a8eeae9848c80d49bdc.

Signed-off-by: Brentley Jones <github@brentleyjones.com>

tools/generators/legacy/src/Generator/CreateAutogeneratedXCSchemes.swift
@@ -3,6 +3,11 @@ import OrderedCollections
 import PathKit
 import XcodeProj
 
+struct AutogeneratedScheme: Equatable {
+    let scheme: XCScheme
+    let isTopLevel: Bool
+}
+
 extension Generator {
     /// Creates an array of `XCScheme` entries for the specified targets.
     static func createAutogeneratedXCSchemes(
@@ -12,7 +17,7 @@ extension Generator {
         customSchemeNames: Set<String>,
         args: [TargetID: [String]],
         envs: [TargetID: [String: String]]
-    ) throws -> [XCScheme] {
+    ) throws -> [AutogeneratedScheme] {
         let shouldAutogenerateSchemes: Bool
         switch schemeAutogenerationMode {
         case .none:
@@ -26,9 +31,11 @@ extension Generator {
             return []
         }
 
-        // Scheme names collisions can occur with names that differ by case. The scheme name checks
-        // will occur using their lowercased variant.
-        let normalizedCustomSchemeNames = Set(customSchemeNames.map { $0.lowercased() })
+        // Scheme names collisions can occur with names that differ by case. The
+        // scheme name checks will occur using their lowercased variant.
+        let normalizedCustomSchemeNames = Set(
+            customSchemeNames.map { $0.lowercased() }
+        )
 
         return try targetResolver
             .targetInfos.filter(\.pbxTarget.shouldCreateScheme)
@@ -100,12 +107,21 @@ extension Generator {
                     return schemeName
                 }
 
-                // If a custom scheme exists with a colliding name, then preserve the custom scheme.
-                guard !normalizedCustomSchemeNames.contains(schemeInfo.name.lowercased()) else {
+                // If a custom scheme exists with a colliding name, then
+                // preserve the custom scheme
+                guard !normalizedCustomSchemeNames
+                    .contains(schemeInfo.name.lowercased())
+                else {
                     return nil
                 }
 
-                return try XCScheme(buildMode: buildMode, schemeInfo: schemeInfo)
+                return try AutogeneratedScheme(
+                    scheme: XCScheme(
+                        buildMode: buildMode,
+                        schemeInfo: schemeInfo
+                    ),
+                    isTopLevel: pbxTarget.isTopLevel
+                )
             }
     }
 }

tools/generators/legacy/src/Generator/CreateXCUserData.swift
@@ -4,44 +4,37 @@ extension Generator {
     /// Creates an `XCUserData`.
     static func createXCUserData(
         userName: String,
-        schemes: [XCScheme],
-        autogeneratedSchemes: [XCScheme],
-        targets: [PBXTarget]
+        customSchemes: [XCScheme],
+        autogeneratedSchemes: [AutogeneratedScheme]
     ) -> XCUserData {
-        var sortedSchemeUserStates = schemes.enumerated().map { (index, scheme) in
-            return XCSchemeManagement.UserStateScheme(
-                name: "\(scheme.name).xcscheme",
-                shared: true,
-                orderHint: index,
-                isShown: true
-            )
-        }
-        
-        let targetNameDict = Dictionary(uniqueKeysWithValues: targets.map{ ($0.schemeName, $0) })
-        let sortedAutogeneratedSchemes = autogeneratedSchemes.sorted { (schemeA, schemeB) in
-            let isSchemeATopLevel = targetNameDict[schemeA.name]!.isTopLevel
-            let isSchemeBTopLevel = targetNameDict[schemeB.name]!.isTopLevel
-            // xor, prioritze isTopLevel comparsion then alphabetical if both scheme are top level or not top level
-            if (isSchemeATopLevel != isSchemeBTopLevel) {
-                return isSchemeATopLevel
+        let sortedAutogeneratedSchemes = autogeneratedSchemes
+            .sorted { lhs, rhs in
+                // xor, prioritize isTopLevel comparison then alphabetical if
+                // both scheme are top level or not top level
+                guard lhs.isTopLevel == rhs.isTopLevel else {
+                    return lhs.isTopLevel
+                }
+                return lhs.scheme.name < rhs.scheme.name
             }
-            return schemeA.name < schemeB.name
-        }
-        
-        let startingOrderHint = schemes.count
-        sortedSchemeUserStates.append(contentsOf: sortedAutogeneratedSchemes.enumerated().map { (index, scheme) in
-            return XCSchemeManagement.UserStateScheme(
-                name: "\(scheme.name).xcscheme",
-                shared: true,
-                orderHint: startingOrderHint + index,
-                isShown: true
-            )
-        })
+            .map(\.scheme)
+
+        let schemeUserStates = (customSchemes + sortedAutogeneratedSchemes)
+            .enumerated()
+            .map { index, scheme in
+                return XCSchemeManagement.UserStateScheme(
+                    name: "\(scheme.name).xcscheme",
+                    shared: true,
+                    orderHint: index,
+                    isShown: true
+                )
+            }
         
         return XCUserData(
             userName: userName,
             schemes: [],
-            schemeManagement: XCSchemeManagement(schemeUserState: sortedSchemeUserStates)
+            schemeManagement: XCSchemeManagement(
+                schemeUserState: schemeUserStates
+            )
         )
     }
 }

tools/generators/legacy/src/Generator/Environment.swift
@@ -124,15 +124,14 @@ struct Environment {
         _ customSchemeNames: Set<String>,
         _ args: [TargetID: [String]],
         _ envs: [TargetID: [String: String]]
-    ) throws -> [XCScheme]
+    ) throws -> [AutogeneratedScheme]
 
     let createXCSharedData: (_ schemes: [XCScheme]) -> XCSharedData
 
     let createXCUserData: (
         _ userName: String,
-        _ schemes: [XCScheme],
-        _ autogeneratedSchemes: [XCScheme],
-        _ targets: [PBXTarget]
+        _ customSchemes: [XCScheme],
+        _ autogeneratedSchemes: [AutogeneratedScheme]
     ) -> XCUserData
 
     let createXcodeProj: (

tools/generators/legacy/src/Generator/Generator.swift
@@ -171,7 +171,8 @@ class Generator {
             consolidatedTargetKeys: disambiguatedTargets.keys,
             pbxTargets: pbxTargets
         )
-        var schemes = try environment.createCustomXCSchemes(
+
+        var customSchemes = try environment.createCustomXCSchemes(
             project.customXcodeSchemes,
             buildMode,
             project.xcodeConfigurations,
@@ -181,27 +182,30 @@ class Generator {
             project.args,
             project.envs
         )
-        let customSchemeNames = Set(schemes.map(\.name))
         let autogeneratedSchemes = try environment.createAutogeneratedXCSchemes(
             project.schemeAutogenerationMode,
             buildMode,
             targetResolver,
-            customSchemeNames,
+            Set(customSchemes.map(\.name)),
             project.args,
             project.envs
         )
 
         let userData = environment.createXCUserData(
             NSUserName(),
-            schemes,
-            autogeneratedSchemes,
-            Array(pbxTargets.values)
+            customSchemes,
+            autogeneratedSchemes
         )
 
-        schemes.append(contentsOf: autogeneratedSchemes)
-        let sharedData = environment.createXCSharedData(schemes)
+        let sharedData = environment.createXCSharedData(
+            customSchemes + autogeneratedSchemes.map(\.scheme)
+        )
 
-        let xcodeProj = environment.createXcodeProj(pbxProj, sharedData, userData)
+        let xcodeProj = environment.createXcodeProj(
+            pbxProj,
+            sharedData,
+            userData
+        )
         try await environment.writeXcodeProj(
             xcodeProj,
             directories,

tools/generators/legacy/test/CreateAutogeneratedXCSchemesTests.swift
@@ -399,7 +399,7 @@ the archive action buildConfiguration did not match for \(scheme.name)
             args: [:],
             envs: [:]
         )
-        let expected = [XCScheme]()
+        let expected = [AutogeneratedScheme]()
         XCTAssertEqual(schemes, expected)
     }
 
@@ -416,7 +416,7 @@ the archive action buildConfiguration did not match for \(scheme.name)
         XCTAssertEqual(schemes.count, pbxTargetsDict.count - 1)
 
         let schemesDict = Dictionary(
-            uniqueKeysWithValues: schemes.map { ($0.name, $0) }
+            uniqueKeysWithValues: schemes.map { ($0.scheme.name, $0.scheme) }
         )
 
         let buildMode = BuildMode.xcode
@@ -502,14 +502,16 @@ Xcode.IDEFoundation.Launcher.PosixSpawn
     }
 
     func assertBazelSchemes(
-        schemes: [XCScheme],
+        schemes: [AutogeneratedScheme],
         file: StaticString = #filePath,
         line: UInt = #line
     ) throws {
         // -1 since we don't create a scheme for WatchKit Extensions
         XCTAssertEqual(schemes.count, pbxTargetsDict.count - 1)
 
-        let schemesDict = Dictionary(uniqueKeysWithValues: schemes.map { ($0.name, $0) })
+        let schemesDict = Dictionary(
+            uniqueKeysWithValues: schemes.map { ($0.scheme.name, $0.scheme) }
+        )
 
         let buildMode = BuildMode.bazel
 
@@ -663,7 +665,7 @@ Xcode.IDEFoundation.Launcher.PosixSpawn
             args: [:],
             envs: [:]
         )
-        let schemeNames = schemes.map(\.name)
+        let schemeNames = schemes.map(\.scheme.name)
         XCTAssertFalse(schemeNames.contains(customSchemeName))
     }
 

tools/generators/legacy/test/CreateXCUserDataTests.swift
@@ -10,20 +10,50 @@ final class CreateXCUserDataTests: XCTestCase {
         
         let userName = "userName"
         let customSchemes = [
-            XCScheme(name: "User Defined B", lastUpgradeVersion: nil, version: nil),
-            XCScheme(name: "User Defined A", lastUpgradeVersion: nil, version: nil)
+            XCScheme(
+                name: "User Defined B",
+                lastUpgradeVersion: nil,
+                version: nil
+            ),
+            XCScheme(
+                name: "User Defined A",
+                lastUpgradeVersion: nil,
+                version: nil
+            ),
         ]
         let autogeneratedSchemes = [
-            XCScheme(name: "Non Top Level A", lastUpgradeVersion: nil, version: nil),
-            XCScheme(name: "Top Level B", lastUpgradeVersion: nil, version: nil),
-            XCScheme(name: "Top Level A", lastUpgradeVersion: nil, version: nil),
-            XCScheme(name: "Non Top Level B", lastUpgradeVersion: nil, version: nil)
-        ]
-        let targets = [
-            PBXTarget(name: "Top Level A", productType: .application),
-            PBXTarget(name: "Top Level B", productType: .unitTestBundle),
-            PBXTarget(name: "Non Top Level A"),
-            PBXTarget(name: "Non Top Level B")
+            AutogeneratedScheme(
+                scheme: XCScheme(
+                    name: "Non Top Level A",
+                    lastUpgradeVersion: nil,
+                    version: nil
+                ),
+                isTopLevel: false
+            ),
+            AutogeneratedScheme(
+                scheme: XCScheme(
+                    name: "Top Level B",
+                    lastUpgradeVersion: nil,
+                    version: nil
+                ),
+                isTopLevel: true
+            ),
+            AutogeneratedScheme(
+                scheme: XCScheme(
+                    name: "Top Level A",
+                    lastUpgradeVersion: nil,
+                    version: nil
+                ),
+                isTopLevel: true
+            ),
+            AutogeneratedScheme(
+                scheme: XCScheme(
+                    name: "Non Top Level B",
+                    lastUpgradeVersion: nil,
+                    version: nil
+                ),
+                isTopLevel: false
+            ),
         ]
         
         let expectedUserData = XCUserData(
@@ -75,9 +105,8 @@ final class CreateXCUserDataTests: XCTestCase {
 
         let userData = Generator.createXCUserData(
             userName: userName,
-            schemes: customSchemes,
-            autogeneratedSchemes: autogeneratedSchemes,
-            targets: targets
+            customSchemes: customSchemes,
+            autogeneratedSchemes: autogeneratedSchemes
         )
 
         // Assert

tools/generators/legacy/test/GeneratorTests.swift
@@ -202,9 +202,17 @@ final class GeneratorTests: XCTestCase {
         ]
         let customSchemeNames = Set(customXCSchemes.map(\.name))
         let autogeneratedXCSchemes = [
-            XCScheme(name: "Autogenerated Scheme", lastUpgradeVersion: nil, version: nil),
+            AutogeneratedScheme(
+                scheme: XCScheme(
+                    name: "Autogenerated Scheme",
+                    lastUpgradeVersion: nil,
+                    version: nil
+                ),
+                isTopLevel: false
+            ),
         ]
-        let allXCSchemes = customXCSchemes + autogeneratedXCSchemes
+        let allXCSchemes = customXCSchemes +
+            autogeneratedXCSchemes.map(\.scheme)
         let sharedData = XCSharedData(schemes: allXCSchemes)
         let userName = NSUserName()
         let testUserData = XCUserData(
@@ -726,7 +734,7 @@ final class GeneratorTests: XCTestCase {
             customSchemeNames: Set<String>,
             args: [TargetID: [String]],
             envs: [TargetID: [String: String]]
-        ) throws -> [XCScheme] {
+        ) throws -> [AutogeneratedScheme] {
             createAutogeneratedXCSchemesCalled.append(.init(
                 schemeAutogenerationMode: schemeAutogenerationMode,
                 buildMode: buildMode,
@@ -768,22 +776,19 @@ final class GeneratorTests: XCTestCase {
         struct CreateXCUserDataCalled: Equatable {
             let userName: String
             let schemes: [XCScheme]
-            let autogeneratedSchemes: [XCScheme]
-            let targets: [PBXTarget]
+            let autogeneratedSchemes: [AutogeneratedScheme]
         }
 
         var createXCUserDataCalled: [CreateXCUserDataCalled] = []
         func createXCUserData(
             userName: String,
             schemes: [XCScheme],
-            autogeneratedSchemes: [XCScheme],
-            targets: [PBXTarget]
+            autogeneratedSchemes: [AutogeneratedScheme]
         ) -> XCUserData {
             createXCUserDataCalled.append(.init(
                 userName: userName,
                 schemes: schemes,
-                autogeneratedSchemes: autogeneratedSchemes,
-                targets: targets
+                autogeneratedSchemes: autogeneratedSchemes
             ))
             return testUserData
         }
@@ -791,8 +796,7 @@ final class GeneratorTests: XCTestCase {
         let expectedCreateXCUserDataCalled = [CreateXCUserDataCalled(
             userName: userName,
             schemes: customXCSchemes,
-            autogeneratedSchemes: autogeneratedXCSchemes,
-            targets: Array(pbxTargets.values)
+            autogeneratedSchemes: autogeneratedXCSchemes
         )]
 
         // MARK: createXcodeProj()