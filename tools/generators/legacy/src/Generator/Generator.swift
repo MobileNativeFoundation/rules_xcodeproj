3c078face0656b95ba08b0be65f1b2cba36c7926
Brentley Jones <github@brentleyjones.com>
2023-05-23 12:04:22 -0500

    Properly set development region in `PBXProject.knownRegions` (#2228)
    Should have changed this with 8dbd5eb59cfb53b5ee7dc7b593a693beb497ec15.
    
    Signed-off-by: Brentley Jones <github@brentleyjones.com>

--- tools/generators/legacy/src/Generator/Generator.swift
+++ tools/generators/legacy/src/Generator/Generator.swift
@@ -77,6 +77,7 @@ class Generator {
             try environment.createFilesAndGroups(
                 pbxProj,
                 buildMode,
+                project.options.developmentRegion,
                 forFixtures,
                 targets,
                 project.extraFiles,
