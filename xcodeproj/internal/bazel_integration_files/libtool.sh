c18ddcfce85ebba4ddfd1c631d775b15fefe4be5
Brentley Jones <github@brentleyjones.com>
2023-07-13 10:00:40 -0500

    Fix `libtool` version check in Xcode 15 (#2359)
    Fixes #2353.
    
    Signed-off-by: Brentley Jones <github@brentleyjones.com>

--- xcodeproj/internal/bazel_integration_files/libtool.sh
+++ xcodeproj/internal/bazel_integration_files/libtool.sh
@@ -2,9 +2,15 @@
 
 set -euo pipefail
 
+original_args=("$@")
+
 while test $# -gt 0
 do
   case $1 in
+  -V)
+    # Xcode 15+ needs to know the version of libtool
+    exec libtool "${original_args[@]}"
+    ;;
   *_dependency_info.dat)
     printf "\0 \0" > "$1"
     break
