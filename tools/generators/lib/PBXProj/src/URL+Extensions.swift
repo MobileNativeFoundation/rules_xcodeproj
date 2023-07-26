1f1da8224f91081b927199d9752a0330281db01c
Brentley Jones <github@brentleyjones.com>
2023-07-26 09:08:51 -0500

    Add url path to error messages (#2401)

--- tools/generators/lib/PBXProj/src/URL+Extensions.swift
+++ tools/generators/lib/PBXProj/src/URL+Extensions.swift
@@ -9,13 +9,15 @@ extension URL {
         do {
             lines = try String(contentsOf: self).split(separator: "\n")
         } catch {
-            throw PreconditionError(message: error.localizedDescription)
+            throw PreconditionError(message: """
+"\(self.path)": \(error.localizedDescription)
+""")
         }
 
         guard lines.count == 1 else {
             throw PreconditionError(message: """
-The execution_root_file must contain one line: the absolute path to the Bazel \
-execution root.
+"\(self.path)": The execution_root_file must contain one line: the absolute \
+path to the Bazel execution root.
 """)
         }
 
