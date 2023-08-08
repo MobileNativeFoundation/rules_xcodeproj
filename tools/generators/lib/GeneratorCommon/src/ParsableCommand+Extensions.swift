d75a5a17542916a776bc1e20fd67be6d8a982ad1
Brentley Jones <github@brentleyjones.com>
2023-08-08 16:00:55 -0500

    Fix parsing of params files with empty lines
    Signed-off-by: Brentley Jones <github@brentleyjones.com>

--- tools/generators/lib/GeneratorCommon/src/ParsableCommand+Extensions.swift
+++ tools/generators/lib/GeneratorCommon/src/ParsableCommand+Extensions.swift
@@ -30,12 +30,49 @@ extension ParsableCommand {
     private static func parseParamsFile(
         _ path: String
     ) async throws -> [String] {
-        return try await URL(fileURLWithPath: path).lines.collect()
+        return try await URL(fileURLWithPath: path).allLines.collect()
     }
 }
 
 private extension AsyncSequence {
     func collect() async rethrows -> [Element] {
         try await reduce(into: [Element]()) { $0.append($1) }
+    }
+}
+
+private extension URL {
+    var allLines: AsyncThrowingStream<String, Error> {
+        return resourceBytes.allLines
+    }
+}
+
+private extension URL.AsyncBytes {
+    // Inspired by https://developer.apple.com/forums/thread/725162
+    var allLines: AsyncThrowingStream<String, Error> {
+        AsyncThrowingStream { continuation in
+            let bytesTask = Task {
+                var accumulator: [UInt8] = []
+                var iterator = makeAsyncIterator()
+                while let byte = try await iterator.next() {
+                    // 10 == \n
+                    if byte == 10 {
+                        guard !accumulator.isEmpty else {
+                            continuation.yield("")
+                            continue
+                        }
+                        continuation.yield(
+                            String(data: Data(accumulator), encoding: .utf8)!
+                        )
+                        accumulator = []
+                    } else {
+                        accumulator.append(byte)
+                    }
+                }
+                continuation.finish()
+            }
+            continuation.onTermination = { @Sendable _ in
+                bytesTask.cancel()
+            }
+        }
     }
 }
