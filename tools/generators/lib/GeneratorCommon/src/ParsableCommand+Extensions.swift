841df8c550e3e92f9ea0940720a130fb73a7d669
Brentley Jones <github@brentleyjones.com>
2023-08-09 15:25:45 -0500

    Throw
    Signed-off-by: Brentley Jones <github@brentleyjones.com>

--- tools/generators/lib/GeneratorCommon/src/ParsableCommand+Extensions.swift
+++ tools/generators/lib/GeneratorCommon/src/ParsableCommand+Extensions.swift
@@ -42,13 +42,13 @@ private extension AsyncSequence {
 
 private extension URL {
     var allLines: AsyncThrowingStream<String, Error> {
-        return resourceBytes.allLines
+        return resourceBytes.allLines(in: self)
     }
 }
 
 private extension URL.AsyncBytes {
     // Inspired by https://developer.apple.com/forums/thread/725162
-    var allLines: AsyncThrowingStream<String, Error> {
+    func allLines(in url: URL) -> AsyncThrowingStream<String, Error> {
         AsyncThrowingStream { continuation in
             let bytesTask = Task {
                 var accumulator: [UInt8] = []
@@ -60,9 +60,15 @@ private extension URL.AsyncBytes {
                             continuation.yield("")
                             continue
                         }
-                        continuation.yield(
-                            String(data: Data(accumulator), encoding: .utf8)!
-                        )
+                        guard let string = String(
+                            data: Data(accumulator),
+                            encoding: .utf8
+                        ) else {
+                            throw PreconditionError(message: """
+"\(url.path)": invalid (non-UTF8) data
+""")
+                        }
+                        continuation.yield(string)
                         accumulator = []
                     } else {
                         accumulator.append(byte)
