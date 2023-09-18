24702f2f52b3b29aadca9e4b18147170397c5057
Brentley Jones <github@brentleyjones.com>
2023-09-18 09:04:32 -0500

    Use `@autoclosure` with `Dictionary.value()` (#2600)
    Will prevent string creation if there isn’t an error.
    
    Signed-off-by: Brentley Jones <github@brentleyjones.com>

--- tools/generators/lib/PBXProj/src/Dictionary+Extensions.swift
+++ tools/generators/lib/PBXProj/src/Dictionary+Extensions.swift
@@ -13,14 +13,14 @@ extension Dictionary {
 extension Dictionary where Key: Comparable {
     public func value(
         for key: Key,
-        context: String,
+        context: @autoclosure () -> String,
         file: StaticString = #filePath,
         line: UInt = #line
     ) throws -> Value {
         guard let value = self[key] else {
             throw PreconditionError(
                 message: """
-\(context) "\(key)" not found in:
+\(context()) "\(key)" not found in:
 \(keys.sorted())
 """,
                 file: file,
