MobileNativeFoundation/rules_xcodeproj

commit c744087bfae1271149ec8034e163f2b756252b02

Fix nondeterministic product file identifier (#2602)
Signed-off-by: Brentley Jones <github@brentleyjones.com>

tools/generators/legacy/src/Generator/CreateProducts.swift
@@ -1,3 +1,4 @@
+import OrderedCollections
 import PathKit
 import XcodeProj
 
@@ -7,7 +8,9 @@ extension Generator {
         for consolidatedTargets: ConsolidatedTargets
     ) -> (Products, PBXGroup) {
         var products = Products()
-        for (key, target) in consolidatedTargets.targets {
+        for (key, target) in consolidatedTargets.targets.sorted(
+            by: localizedStandardSort
+        ) {
             let fileType: String?
             let name: String?
             let path: String?
@@ -55,7 +58,7 @@ extension Generator {
         }
 
         let group = PBXGroup(
-            children: products.byTarget.sortedLocalizedStandard(),
+            children: products.byTarget.values.elements,
             sourceTree: .group,
             name: "Products"
         )
@@ -72,7 +75,8 @@ struct Products: Equatable {
         let filePaths: Set<FilePath>
     }
 
-    private(set) var byTarget: [ConsolidatedTarget.Key: PBXFileReference] = [:]
+    private(set) var byTarget:
+        OrderedDictionary<ConsolidatedTarget.Key, PBXFileReference> = [:]
     private(set) var byFilePath: [FilePath: PBXFileReference] = [:]
 
     mutating func add(
@@ -95,4 +99,35 @@ extension Products {
             }
         }
     }
+}
+
+private func localizedStandardSort(
+    lhs: (key: ConsolidatedTarget.Key, value: ConsolidatedTarget),
+    rhs: (key: ConsolidatedTarget.Key, value: ConsolidatedTarget)
+) -> Bool {
+    let lhsTarget = lhs.value
+    let rhsTarget = rhs.value
+
+    guard
+        let lhsBasename = lhsTarget.product.basename,
+        let rhsBasename = rhsTarget.product.basename
+    else {
+        return false
+    }
+
+    let basenameCompare = lhsBasename.localizedStandardCompare(rhsBasename)
+    guard basenameCompare == .orderedSame else {
+        return basenameCompare == .orderedAscending
+    }
+
+    guard
+        let lhsProductPath = lhsTarget.sortedTargets.first!.product.path,
+        let rhsProductPath = rhsTarget.sortedTargets.first!.product.path
+    else {
+        return false
+    }
+
+    return lhsProductPath.path.string
+        .localizedStandardCompare(rhsProductPath.path.string) ==
+            .orderedAscending
 }

examples/integration/test/fixtures/bwb.xcodeproj/project.pbxproj
@@ -13333,7 +13333,7 @@
 			);
 			name = watchOS;
 			productName = Lib;
-			productReference = E4976A0A8B1D38CB8334D1E4 /* Lib.framework */;
+			productReference = A9721D8883D73A3F13BF89BB /* Lib.framework */;
 			productType = "com.apple.product-type.framework";
 		};
 		674E716179BE9595CDEE711A /* _AtomicsShims */ = {
@@ -14219,7 +14219,7 @@
 			);
 			name = tvOS;
 			productName = Lib;
-			productReference = A9721D8883D73A3F13BF89BB /* Lib.framework */;
+			productReference = E4976A0A8B1D38CB8334D1E4 /* Lib.framework */;
 			productType = "com.apple.product-type.framework";
 		};
 		EFF8A207F5496A963D6AA99A /* CGRPCZlib */ = {

examples/integration/test/fixtures/bwx.xcodeproj/project.pbxproj
@@ -14009,7 +14009,7 @@
 			);
 			name = tvOS;
 			productName = Lib;
-			productReference = 8DD3F821E51F622E4DA16F5F /* Lib.framework */;
+			productReference = 38D51FF469C531901A0E5BFF /* Lib.framework */;
 			productType = "com.apple.product-type.framework";
 		};
 		197909C893D71098F7D5178C /* UIFramework.tvOS */ = {
@@ -14893,7 +14893,7 @@
 			);
 			name = watchOS;
 			productName = Lib;
-			productReference = 38D51FF469C531901A0E5BFF /* Lib.framework */;
+			productReference = 8DD3F821E51F622E4DA16F5F /* Lib.framework */;
 			productType = "com.apple.product-type.framework";
 		};
 		8D4E430A695D440F45D42A8E /* NIOExtras */ = {

commit fbbbb23f2d6e8c3c84c5ff30abc6cb3661a614ef

Fix legacy tests
Regressed with c744087bfae1271149ec8034e163f2b756252b02, because CI was no longer testing it (which will be enabled in a future change).

Signed-off-by: Brentley Jones <github@brentleyjones.com>

tools/generators/legacy/src/Generator/CreateProducts.swift
@@ -91,7 +91,7 @@ struct Products: Equatable {
 }
 
 extension Products {
-    init(_ products: [ProductKeys: PBXFileReference]) {
+    init(_ products: OrderedDictionary<ProductKeys, PBXFileReference>) {
         for (keys, product) in products {
             byTarget[keys.targetKey] = product
             for filePath in keys.filePaths {

tools/generators/legacy/test/Fixtures.swift
@@ -1313,7 +1313,7 @@ $(BAZEL_OUT)/z/A.link.params
 
         if let group = group {
             // The order products are added to a group matters for uuid fixing
-            products.byTarget.sortedLocalizedStandard().forEach { product in
+            products.byTarget.values.forEach { product in
                 group.addChild(product)
             }
         }