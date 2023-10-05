13cac20a40e42cf23d8dbdbc6f7a380750265d47
Brentley Jones <github@brentleyjones.com>
2023-10-05 14:47:48 -0500

    Improve error reporting of `consumeArg()`
    Signed-off-by: Brentley Jones <github@brentleyjones.com>

--- tools/generators/lib/PBXProj/src/ConsumeArgs.swift
+++ tools/generators/lib/PBXProj/src/ConsumeArgs.swift
@@ -1,42 +1,79 @@
 import ArgumentParser
+import Foundation
 import GeneratorCommon
 
 extension ArraySlice<String> {
     public mutating func consumeArg<Output> (
         _ type: Output.Type,
-        transform: (String) throws -> Output
+        in url: URL,
+        transform: (String) throws -> Output,
+        file: StaticString = #filePath,
+        line: UInt = #line
     ) throws -> Output {
         guard let rawArg = popFirst() else {
-            throw PreconditionError(message: "Expected more arguments")
+            throw PreconditionError(
+                message: #""\#(url.path)": Expected more arguments"#,
+                file: file,
+                line: line
+            )
         }
         return try transform(rawArg)
     }
 
     private static func argumentTransform<Output: ExpressibleByArgument>(
-        _ rawArg: String
+        _ rawArg: String,
+        in url: URL,
+        file: StaticString,
+        line: UInt
     ) throws -> Output {
         guard let arg = Output(argument: rawArg) else {
             throw PreconditionError(
-                message:
-                    #"Failed to parse "\#(rawArg)" as \#(Output.Type.self)"#
+                message: #"""
+"\#(url.path)": Failed to parse "\#(rawArg)" as \#(Output.Type.self)
+"""#,
+                file: file,
+                line: line
             )
         }
         return arg
     }
 
     public mutating func consumeArg<Output: ExpressibleByArgument>(
-        _ type: Output.Type
+        _ type: Output.Type,
+        in url: URL,
+        file: StaticString = #filePath,
+        line: UInt = #line
     ) throws -> Output {
-        return try consumeArg(type, transform: Self.argumentTransform)
+        return try consumeArg(
+            type,
+            in: url,
+            transform: { arg in
+                return try Self.argumentTransform(
+                    arg,
+                    in: url,
+                    file: file,
+                    line: line
+                )
+            },
+            file: file,
+            line: line
+        )
     }
 
     private mutating func consumeArg<Output>(
         _ type: Output.Type,
+        in url: URL,
         transform: (String) throws -> Output,
-        unless: String
+        unless: String,
+        file: StaticString,
+        line: UInt
     ) throws -> Output? {
         guard let rawArg = popFirst() else {
-            throw PreconditionError(message: "Expected more arguments")
+            throw PreconditionError(
+                message: #""\#(url.path)": Expected more arguments"#,
+                file: file,
+                line: line
+            )
         }
 
         guard rawArg != unless else {
@@ -48,14 +85,20 @@ extension ArraySlice<String> {
 
     public mutating func consumeArgs<Output>(
         _ type: Output.Type,
+        in url: URL,
         transform: (String) throws -> Output,
-        terminator: String = "--"
+        terminator: String = "--",
+        file: StaticString = #filePath,
+        line: UInt = #line
     ) throws -> [Output] {
         var args: [Output] = []
         while let arg = try consumeArg(
             type,
+            in: url,
             transform: transform,
-            unless: terminator
+            unless: terminator,
+            file: file,
+            line: line
         ) {
             args.append(arg)
         }
@@ -64,12 +107,25 @@ extension ArraySlice<String> {
 
     public mutating func consumeArgs<Output: ExpressibleByArgument>(
         _ type: Output.Type,
-        terminator: String = "--"
+        terminator: String = "--",
+        in url: URL,
+        file: StaticString = #filePath,
+        line: UInt = #line
     ) throws -> [Output] {
         return try consumeArgs(
             type,
-            transform: Self.argumentTransform,
-            terminator: terminator
+            in: url,
+            transform: { arg in
+                return try Self.argumentTransform(
+                    arg,
+                    in: url,
+                    file: file,
+                    line: line
+                )
+            },
+            terminator: terminator,
+            file: file,
+            line: line
         )
     }
 }
