b61355b6a70c7bf4d70e64566025d56396ed0d1f
Brentley Jones <github@brentleyjones.com>
2023-10-05 14:08:22 -0500

    Remove `Headers` build phase from incremental generators
    This was only needed for BwX support.
    
    Signed-off-by: Brentley Jones <github@brentleyjones.com>

--- tools/generators/lib/PBXProj/src/BuildPhase.swift
+++ tools/generators/lib/PBXProj/src/BuildPhase.swift
@@ -2,7 +2,6 @@ public enum BuildPhase {
     case bazelIntegration
     case createCompileDependencies
     case createLinkDependencies
-    case headers
     case sources
     case copySwiftGeneratedHeader
     case embedAppExtensions
@@ -14,7 +13,6 @@ Copy Bazel Outputs / Generate Bazel Dependencies (Index Build)
 """
         case .createCompileDependencies: return "Create Compile Dependencies"
         case .createLinkDependencies: return "Create Link Dependencies"
-        case .headers: return "Headers"
         case .sources: return "Sources"
         case .copySwiftGeneratedHeader: return "Copy Swift Generated Header"
         case .embedAppExtensions: return "Embed App Extensions"
