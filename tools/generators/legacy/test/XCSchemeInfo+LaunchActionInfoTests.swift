a5b86a6860308b817c429f7a13a775652bba9cb9
Vadim Smal <kenshin312@gmail.com>
2023-10-17 14:53:11 +0100

    Update tools/generators/legacy/test/XCSchemeInfo+LaunchActionInfoTests.swift
    Co-authored-by: Brentley Jones <github@brentleyjones.com>
    Signed-off-by: Vadim Smal <kenshin312@gmail.com>

--- tools/generators/legacy/test/XCSchemeInfo+LaunchActionInfoTests.swift
+++ tools/generators/legacy/test/XCSchemeInfo+LaunchActionInfoTests.swift
@@ -38,7 +38,8 @@ extension XCSchemeInfoLaunchActionInfoTests {
             return
         }
         XCTAssertEqual(preconditionError.message, """
-An `XCSchemeInfo.LaunchActionInfo` should have a launchable `XCSchemeInfo.TargetInfo` value: "\(libraryTargetInfo.label)".
+An `XCSchemeInfo.LaunchActionInfo` should have a launchable \
+`XCSchemeInfo.TargetInfo` value: "\(libraryTargetInfo.label)".
 """)
     }
 }
