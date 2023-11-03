b99694bb0507d071b753278f14101ba3b5a72a0f
Brentley Jones <github@brentleyjones.com>
2023-11-03 15:10:49 -0500

    Improve error reporting for `PBXProj.Write`
    Signed-off-by: Brentley Jones <github@brentleyjones.com>

--- tools/generators/lib/PBXProj/src/Write.swift
+++ tools/generators/lib/PBXProj/src/Write.swift
@@ -12,8 +12,18 @@ public struct Write {
     }
 
     /// Writes `content` to the file designated by `outputPath`.
-    public func callAsFunction(_ content: String, to outputPath: URL) throws {
-        try callable(/*content:*/ content, /*to:*/ outputPath)
+    public func callAsFunction(
+        _ content: String,
+        to outputPath: URL,
+        file: StaticString = #filePath,
+        line: UInt = #line
+    ) throws {
+        try callable(
+            /*content:*/ content,
+            /*to:*/ outputPath,
+            /*file:*/ file,
+            /*line:*/ line
+        )
     }
 }
 
@@ -22,14 +32,22 @@ public struct Write {
 extension Write {
     public typealias Callable = (
         _ content: String,
-        _ outputPath: URL
+        _ outputPath: URL,
+        _ file: StaticString,
+        _ line: UInt
     ) throws -> Void
 
     /// Writes `content` to the file designated by `outputPath`.
     public static func defaultCallable(
         _ content: String,
-        to outputPath: URL
+        to outputPath: URL,
+        file: StaticString = #filePath,
+        line: UInt = #line
     ) throws {
-        return try content.writeCreatingParentDirectories(to: outputPath)
+        return try content.writeCreatingParentDirectories(
+            to: outputPath,
+            file: file,
+            line: line
+        )
     }
 }
--- tools/lib/ToolCommon/src/String+Extensions.swift
+++ tools/lib/ToolCommon/src/String+Extensions.swift
@@ -3,7 +3,11 @@ import Foundation
 extension String {
     /// Writes `self` to the file designated by `url`, creating parent
     /// directories if needed.
-    public func writeCreatingParentDirectories(to url: URL) throws {
+    public func writeCreatingParentDirectories(
+        to url: URL,
+        file: StaticString = #filePath,
+        line: UInt = #line
+    ) throws {
         do {
             // Create parent directory
             try FileManager.default.createDirectory(
@@ -11,18 +15,26 @@ extension String {
                 withIntermediateDirectories: true
             )
         } catch {
-            throw PreconditionError(message: url.prefixMessage("""
+            throw PreconditionError(
+                message: url.prefixMessage("""
 Failed to create parent directories: \(error.localizedDescription)
-"""))
+"""),
+                file: file,
+                line: line
+            )
         }
 
         // Write
         do {
             try write(to: url, atomically: false, encoding: .utf8)
         } catch {
-            throw PreconditionError(message: url.prefixMessage("""
+            throw PreconditionError(
+                message: url.prefixMessage("""
 Failed to write: \(error.localizedDescription)
-"""))
+"""),
+                file: file,
+                line: line
+            )
         }
     }
 }
