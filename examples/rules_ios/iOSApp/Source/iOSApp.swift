fc92189cd587f5872bca813878f6964bc7d870a1
Thiago Cruz <thiago@squareup.com>
2023-11-16 15:19:47 -0500

    Add Xcode Previews example to `examples/rules_ios`
    Signed-off-by: Brentley Jones <github@brentleyjones.com>

--- examples/rules_ios/iOSApp/Source/iOSApp.swift
+++ examples/rules_ios/iOSApp/Source/iOSApp.swift
@@ -2,6 +2,7 @@ import CoreUtils
 import MixedAnswer
 import SwiftUI
 import UI
+import FooPod
 
 @main
 struct iOSApp: App { // swiftlint:disable:this type_name
@@ -18,5 +19,11 @@ extension View {
         Swift.print(value)
         Swift.print(MixedAnswerSwift.swiftMixedAnswer())
         return self
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        Text("foo")
     }
 }
