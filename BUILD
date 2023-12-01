MobileNativeFoundation/rules_xcodeproj

commit 0185fc472031c5a76c63344fd092c3caf59d7588

Apply `out-of-order-load` buildifer fix (#2493)
Signed-off-by: Brentley Jones <github@brentleyjones.com>

BUILD
@@ -49,6 +49,7 @@ _BUILDIFIER_EXCLUDE_PATTERNS = [
 ]
 
 _BUILDIFIER_WARNINGS = [
+    "out-of-order-load",
 ]
 
 buildifier(

examples/integration/iOSApp/Test/UITests/BUILD
@@ -4,12 +4,12 @@ load(
     "ios_ui_test_suite",
 )
 load(
-    "@build_bazel_rules_swift//swift:swift.bzl",
-    "swift_library",
-)
-load(
     "@build_bazel_rules_apple//apple/testing/default_runner:ios_test_runner.bzl",
     "ios_test_runner",
+)
+load(
+    "@build_bazel_rules_swift//swift:swift.bzl",
+    "swift_library",
 )
 
 ios_ui_test(

examples/rules_ios/iOSApp/Test/ObjCUnitTests/BUILD
@@ -1,10 +1,10 @@
 load(
-    "@build_bazel_rules_ios//rules:test.bzl",
-    rules_ios_ios_unit_test = "ios_unit_test",
-)
-load(
     "@build_bazel_rules_apple//apple/testing/default_runner:ios_test_runner.bzl",
     "ios_test_runner",
+)
+load(
+    "@build_bazel_rules_ios//rules:test.bzl",
+    rules_ios_ios_unit_test = "ios_unit_test",
 )
 load(
     "//iOSApp/Test/ObjCUnitTests:macros.bzl",

examples/rules_ios/iOSApp/Test/SwiftUnitTests/BUILD
@@ -1,10 +1,10 @@
 load(
-    "@build_bazel_rules_ios//rules:test.bzl",
-    rules_ios_ios_unit_test = "ios_unit_test",
-)
-load(
     "@build_bazel_rules_apple//apple/testing/default_runner:ios_test_runner.bzl",
     "ios_test_runner",
+)
+load(
+    "@build_bazel_rules_ios//rules:test.bzl",
+    rules_ios_ios_unit_test = "ios_unit_test",
 )
 
 rules_ios_ios_unit_test(

examples/rules_ios/iOSApp/Test/UITests/BUILD
@@ -1,10 +1,10 @@
 load(
-    "@build_bazel_rules_ios//rules:test.bzl",
-    rules_ios_ios_ui_test = "ios_ui_test",
-)
-load(
     "@build_bazel_rules_apple//apple/testing/default_runner:ios_test_runner.bzl",
     "ios_test_runner",
+)
+load(
+    "@build_bazel_rules_ios//rules:test.bzl",
+    rules_ios_ios_ui_test = "ios_ui_test",
 )
 load(
     "//iOSApp/Test/UITests:macros.bzl",

xcodeproj/defs.bzl
@@ -5,14 +5,14 @@ load(
     _project_options = "project_options",
 )
 load(
-    "//xcodeproj/internal:top_level_target.bzl",
-    _top_level_target = "top_level_target",
-    _top_level_targets = "top_level_targets",
-)
-load(
     "//xcodeproj/internal:providers.bzl",
     _XcodeProjAutomaticTargetProcessingInfo = "XcodeProjAutomaticTargetProcessingInfo",
     _XcodeProjInfo = "XcodeProjInfo",
+)
+load(
+    "//xcodeproj/internal:top_level_target.bzl",
+    _top_level_target = "top_level_target",
+    _top_level_targets = "top_level_targets",
 )
 load(
     "//xcodeproj/internal:xcode_provisioning_profile.bzl",

xcodeproj/internal/resources.bzl
@@ -1,7 +1,7 @@
 """ Functions for collecting resource usage information."""
 
-load("@build_bazel_rules_apple//apple:resources.bzl", "resources_common")
 load("@bazel_skylib//lib:paths.bzl", "paths")
+load("@build_bazel_rules_apple//apple:resources.bzl", "resources_common")
 load(":configuration.bzl", "calculate_configuration")
 load(
     ":files.bzl",

xcodeproj/internal/xcodeproj_runner.bzl
@@ -1,7 +1,7 @@
 """Implementation of the `xcodeproj_runner` rule."""
 
-load("@bazel_skylib//rules:common_settings.bzl", "BuildSettingInfo")
 load("@bazel_skylib//lib:paths.bzl", "paths")
+load("@bazel_skylib//rules:common_settings.bzl", "BuildSettingInfo")
 load(":collections.bzl", "uniq")
 load(":execution_root.bzl", "write_execution_root_file")
 load(":providers.bzl", "XcodeProjRunnerOutputInfo")

commit 1296828bf7400caa3a78be50366916a928959d69

Fix buildifier checks
Regressed with 0185fc472031c5a76c63344fd092c3caf59d7588.

Signed-off-by: Brentley Jones <github@brentleyjones.com>

BUILD
@@ -49,7 +49,59 @@ _BUILDIFIER_EXCLUDE_PATTERNS = [
 ]
 
 _BUILDIFIER_WARNINGS = [
+    "attr-cfg",
+    "attr-non-empty",
+    "attr-output-default",
+    "attr-single-file",
+    "build-args-kwargs",
+    "bzl-visibility",
+    "confusing-name",
+    "constant-glob",
+    "ctx-actions",
+    "ctx-args",
+    "deprecated-function",
+    "depset-items",
+    "depset-iteration",
+    "depset-union",
+    "dict-concatenation",
+    "dict-method-named-arg",
+    "duplicated-name",
+    "filetype",
+    "function-docstring",
+    "function-docstring-args",
+    "function-docstring-header",
+    "function-docstring-return",
+    "git-repository",
+    "http-archive",
+    "integer-division",
+    "keyword-positional-params",
+    "list-append",
+    "load",
+    "load-on-top",
+    "module-docstring",
+    "name-conventions",
+    "no-effect",
+    "out-of-order-load",
+    "output-group",
+    "overly-nested-depset",
+    "package-name",
+    "package-on-top",
+    "positional-args",
+    "print",
+    "provider-params",
+    "redefined-variable",
+    "repository-name",
+    "return-value",
+    "rule-impl-return",
+    "same-origin-load",
+    "skylark-comment",
+    "skylark-docstring",
+    "string-iteration",
+    "uninitialized",
+    "unnamed-macro",
+    "unreachable",
     "unsorted-dict-items",
+    "unused-variable",
 ]
 
 buildifier(