96ef20d8c9661a2d4e0712e7fb7c7c5ddb95af4a
Brentley Jones <github@brentleyjones.com>
2024-01-05 10:48:45 -0600

    WIP: Params file for target_build_settings
    Signed-off-by: Brentley Jones <github@brentleyjones.com>

--- tools/generators/lib/PBXProj/src/ConsumeArgs.swift
+++ tools/generators/lib/PBXProj/src/ConsumeArgs.swift
@@ -178,6 +178,182 @@ Failed to parse "\(rawArg)" as \(Output.Type.self) for <\(name)>
     }
 }
 
+extension AsyncThrowingStream<String, Error>.Iterator {
+    // MARK: - consumeArg
+
+    public mutating func consumeArg<Output> (
+        _ name: String,
+        as type: Output.Type,
+        in url: URL? = nil,
+        transform: (String) throws -> Output,
+        file: StaticString = #filePath,
+        line: UInt = #line
+    ) async throws -> Output {
+        guard let rawArg = try await next() else {
+            throw PreconditionError(
+                message: url.prefixMessage("Missing <\(name)>"),
+                file: file,
+                line: line
+            )
+        }
+        return try transform(rawArg)
+    }
+
+    private static func argumentTransform<Output: ExpressibleByArgument>(
+        _ rawArg: String,
+        name: String,
+        in url: URL?,
+        file: StaticString,
+        line: UInt
+    ) throws -> Output {
+        guard let arg = Output(argument: rawArg) else {
+            throw PreconditionError(
+                message: url.prefixMessage("""
+Failed to parse "\(rawArg)" as \(Output.Type.self) for <\(name)>
+"""),
+                file: file,
+                line: line
+            )
+        }
+        return arg
+    }
+
+    public mutating func consumeArg<Output: ExpressibleByArgument>(
+        _ name: String,
+        as type: Output.Type = String.self,
+        in url: URL? = nil,
+        file: StaticString = #filePath,
+        line: UInt = #line
+    ) async throws -> Output {
+        return try await consumeArg(
+            name,
+            as: type,
+            in: url,
+            transform: { arg in
+                return try Self.argumentTransform(
+                    arg,
+                    name: name,
+                    in: url,
+                    file: file,
+                    line: line
+                )
+            },
+            file: file,
+            line: line
+        )
+    }
+
+    public mutating func consumeArg(
+        _ name: String,
+        as type: Bool.Type,
+        in url: URL? = nil,
+        file: StaticString = #filePath,
+        line: UInt = #line
+    ) async throws -> Bool {
+        return try await consumeArg(
+            name,
+            as: Bool.self,
+            in: url,
+            transform: { $0 == "1" },
+            file: file,
+            line: line
+        )
+    }
+
+    // MARK: - consumeArgs
+
+    public mutating func consumeArgs<Output>(
+        _ name: String,
+        as type: Output.Type,
+        count: Int,
+        in url: URL? = nil,
+        transform: (String) throws -> Output,
+        file: StaticString = #filePath,
+        line: UInt = #line
+    ) async throws -> [Output] {
+        var args: [Output] = []
+        for _ in (0..<count) {
+            let arg = try await consumeArg(
+                name,
+                as: type,
+                in: url,
+                transform: transform,
+                file: file,
+                line: line
+            )
+            args.append(arg)
+        }
+        return args
+    }
+
+    public mutating func consumeArgs<Output: ExpressibleByArgument>(
+        _ name: String,
+        as type: Output.Type,
+        count: Int,
+        in url: URL? = nil,
+        file: StaticString = #filePath,
+        line: UInt = #line
+    ) async throws -> [Output] {
+        return try await consumeArgs(
+            name,
+            as: type,
+            count: count,
+            in: url,
+            transform: { arg in
+                return try Self.argumentTransform(
+                    arg,
+                    name: name,
+                    in: url,
+                    file: file,
+                    line: line
+                )
+            },
+            file: file,
+            line: line
+        )
+    }
+
+    public mutating func consumeArgs<Output>(
+        _ name: String,
+        as type: Output.Type,
+        in url: URL? = nil,
+        transform: (String) throws -> Output,
+        file: StaticString = #filePath,
+        line: UInt = #line
+    ) async throws -> [Output] {
+        let count = try await consumeArg("\(name)-count", as: Int.self, in: url)
+
+        return try await consumeArgs(
+            name,
+            as: type,
+            count: count,
+            in: url,
+            transform: transform,
+            file: file,
+            line: line
+        )
+    }
+
+    public mutating func consumeArgs<Output: ExpressibleByArgument>(
+        _ name: String,
+        as type: Output.Type = String.self,
+        in url: URL? = nil,
+        file: StaticString = #filePath,
+        line: UInt = #line
+    ) async throws -> [Output] {
+        let count = try await consumeArg("\(name)-count", as: Int.self, in: url)
+
+        return try await consumeArgs(
+            name,
+            as: type,
+            count: count,
+            in: url,
+            file: file,
+            line: line
+        )
+    }
+}
+
 private extension Optional where Wrapped == URL {
     func prefixMessage(_ message: String) -> String {
         guard let url = self else {
