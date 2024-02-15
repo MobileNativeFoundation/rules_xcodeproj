c3c78920b1ebfc64e0e22b48b8029b0191e93c70
Marcos Griselli <marcosg@lyft.com>
2024-02-14 17:02:22 -0800

    Add support for argument lists in `swiftc_stub`
    Co-authored-by: Brentley Jones <github@brentleyjones.com>
    
    Signed-off-by: Marcos Griselli <marcosg@lyft.com>
    Signed-off-by: Brentley Jones <github@brentleyjones.com>

--- tools/swiftc_stub/main.swift
+++ tools/swiftc_stub/main.swift
@@ -2,6 +2,66 @@ import Foundation
 
 // MARK: - Helpers
 
+enum PathKey: String {
+    case emitModulePath = "-emit-module-path"
+    case emitObjCHeaderPath = "-emit-objc-header-path"
+    case outputFileMap = "-output-file-map"
+    case sdk = "-sdk"
+}
+
+func processArgs(
+    _ args: [String]
+) async throws -> (
+    isPreviewThunk: Bool,
+    isWMO: Bool,
+    paths: [PathKey: URL]
+) {
+    var isPreviewThunk = false
+    var isWMO = false
+    var paths: [PathKey: URL] = [:]
+
+    var previousArg: String?
+    func processArg(_ arg: String) {
+        if let rawPathKey = previousArg,
+            let key = PathKey(rawValue: rawPathKey)
+        {
+            paths[key] = URL(fileURLWithPath: arg)
+            previousArg = nil
+            return
+        }
+
+        if arg == "-wmo" || arg == "-whole-module-optimization" {
+            isWMO = true
+        } else if arg.hasSuffix(".preview-thunk.swift") {
+            isPreviewThunk = true
+        } else {
+            previousArg = arg
+        }
+    }
+
+    for arg in args {
+        if arg.hasPrefix("@") {
+            let argumentFileURL
+                = URL(fileURLWithPath: String(arg.dropFirst()))
+            for try await line in argumentFileURL.lines {
+                if line.hasPrefix(#"""#) && line.hasSuffix(#"""#) {
+                    processArg(String(line.dropFirst().dropLast()))
+                } else {
+                    processArg(String(line))
+                }
+            }
+        } else {
+            processArg(arg)
+        }
+    }
+
+    return (
+        !paths.keys.contains(.outputFileMap) && isPreviewThunk,
+        isWMO,
+        paths
+    )
+}
+
 extension URL {
     mutating func touch() throws {
         let fileManager = FileManager.default
@@ -15,29 +75,11 @@ extension URL {
     }
 }
 
-func findPath(key: String, from args: [String]) -> URL? {
-    var found = false
-    for arg in args {
-        if found {
-            return URL(fileURLWithPath: arg)
-        }
-        if arg == key {
-            found = true
-        }
-    }
-    return nil
-}
+/// Touch the Xcode-required `.d` files
+func touchDepsFiles(isWMO: Bool, paths: [PathKey: URL]) throws {
+    guard let outputFileMapPath = paths[PathKey.outputFileMap] else { return }
 
-func isWMO(args: Set<String>) -> Bool {
-    return args.contains("-wmo") || args.contains("-whole-module-optimization")
-}
-
-/// Touch the Xcode-required .d files
-func touchDepsFiles(args: [String], argsSet: Set<String>) throws {
-    guard let outputFileMapPath = findPath(key: "-output-file-map", from: args)
-    else { return }
-
-    if isWMO(args: argsSet) {
+    if isWMO {
         let dPath = String(
             outputFileMapPath.path.dropLast("-OutputFileMap.json".count) +
             "-master.d"
@@ -65,9 +107,9 @@ func touchDepsFiles(args: [String], argsSet: Set<String>) throws {
     }
 }
 
-/// Touch the Xcode-required .swift{module,doc,sourceinfo} files"
-func touchSwiftmoduleArtifacts(_ args: [String]) throws {
-    if var swiftmodulePath = findPath(key: "-emit-module-path", from: args) {
+/// Touch the Xcode-required `.swift{module,doc,sourceinfo}` files
+func touchSwiftmoduleArtifacts(paths: [PathKey: URL]) throws {
+    if var swiftmodulePath = paths[PathKey.emitModulePath] {
         var swiftdocPath = swiftmodulePath.deletingPathExtension()
             .appendingPathExtension("swiftdoc")
         var swiftsourceinfoPath = swiftmodulePath.deletingPathExtension()
@@ -81,10 +123,7 @@ func touchSwiftmoduleArtifacts(_ args: [String]) throws {
         try swiftinterfacePath.touch()
     }
 
-    if var generatedHeaderPath = findPath(
-        key: "-emit-objc-header-path",
-        from: args
-    ) {
+    if var generatedHeaderPath = paths[PathKey.emitObjCHeaderPath] {
         try generatedHeaderPath.touch()
     }
 }
@@ -98,9 +137,8 @@ func runSubProcess(executable: String, args: [String]) throws -> Int32 {
     return task.terminationStatus
 }
 
-func handleSwiftUIPreviewThunk(_ args: [String]) throws {
-    guard let sdkPath = findPath(key: "-sdk", from: args)?.path
-    else {
+func handleXcodePreviewThunk(args: [String], paths: [PathKey: URL]) throws -> Never {
+    guard let sdkPath = paths[PathKey.sdk]?.path else {
         fputs(
             "error: No such argument '-sdk'. Using /usr/bin/swiftc.",
             stderr
@@ -138,8 +176,6 @@ error: Failed to parse DEVELOPER_DIR from '-sdk'. Using /usr/bin/swiftc.
 // MARK: - Main
 
 let args = CommandLine.arguments
-let argsSet = Set(args)
-
 if args.count == 2, args.last == "-v" {
     guard let path = ProcessInfo.processInfo.environment["PATH"] else {
         fputs("error: PATH not set", stderr)
@@ -165,14 +201,16 @@ if args.count == 2, args.last == "-v" {
     try exit(runSubProcess(executable: swiftcPath, args: ["-v"]))
 }
 
-for arg in args {
-    if arg.hasSuffix(".preview-thunk.swift"),
-        !argsSet.contains("-output-file-map")
-    {
-        // Pass through for SwiftUI Preview thunk compilation
-        try handleSwiftUIPreviewThunk(args)
-    }
+let (
+    isPreviewThunk,
+    isWMO,
+    paths
+) = try await processArgs(args)
+
+guard !isPreviewThunk else {
+    // Pass through for Xcode Preview thunk compilation
+    try handleXcodePreviewThunk(args: args, paths: paths)
 }
 
-try touchDepsFiles(args: args, argsSet: argsSet)
-try touchSwiftmoduleArtifacts(args)
+try touchDepsFiles(isWMO: isWMO, paths: paths)
+try touchSwiftmoduleArtifacts(paths: paths)
