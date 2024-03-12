MobileNativeFoundation/rules_xcodeproj

commit 7399108d66a35e5147970067151ba685861d58fd

Fix performance of `associated_extra_files` with incremental generation mode (#2944)
Signed-off-by: Brentley Jones <github@brentleyjones.com>

xcodeproj/internal/xcodeproj_incremental_rule.bzl
@@ -116,10 +116,16 @@ def _calculate_infos_and_top_level_deps(
 
 def _collect_files(
         *,
+        owned_extra_files,
         resource_bundle_xcode_targets,
         unowned_extra_files,
         unsupported_extra_files,
         xcode_targets):
+    target_extra_files = {
+        target_label_str: files_target.files
+        for files_target, target_label_str in owned_extra_files.items()
+    }
+
     all_targets = xcode_targets.values() + resource_bundle_xcode_targets
 
     compile_stub_needed = False
@@ -134,6 +140,10 @@ def _collect_files(
         transitive_folders.append(xcode_target.inputs.extra_folders)
         transitive_srcs.append(xcode_target.inputs.non_arc_srcs)
         transitive_srcs.append(xcode_target.inputs.srcs)
+
+        extra_files = target_extra_files.get(str(xcode_target.label))
+        if extra_files:
+            transitive_files.append(extra_files)
 
         infoplist_path = xcode_target.inputs.infoplist_path
         if infoplist_path:
@@ -286,6 +296,7 @@ def _write_project_contents(
         install_path,
         minimum_xcode_version,
         name,
+        owned_extra_files,
         pbxnativetargets_generator,
         pbxproj_prefix_generator,
         pbxtargetdependencies_generator,
@@ -331,6 +342,7 @@ def _write_project_contents(
         infoplist_paths,
         srcs,
     ) = _collect_files(
+        owned_extra_files = owned_extra_files,
         resource_bundle_xcode_targets = resource_bundle_xcode_targets,
         unowned_extra_files = unowned_extra_files,
         unsupported_extra_files = unsupported_extra_files,
@@ -596,6 +608,7 @@ After removing unfocused targets, no targets remain. Please check your \
             )
         ),
         name = name,
+        owned_extra_files = ctx.attr.owned_extra_files,
         pbxnativetargets_generator = (
             ctx.executable._pbxnativetargets_generator
         ),
@@ -732,6 +745,7 @@ def _xcodeproj_incremental_attrs(
         "generation_shard_count": attr.int(mandatory = True),
         "install_path": attr.string(mandatory = True),
         "minimum_xcode_version": attr.string(mandatory = True),
+        "owned_extra_files": attr.label_keyed_string_dict(allow_files = True),
         "post_build": attr.string(mandatory = True),
         "pre_build": attr.string(mandatory = True),
         "project_options": attr.string_dict(mandatory = True),

examples/integration/test/fixtures/generated/xcodeproj_bwb/BUILD
@@ -24,6 +24,7 @@ xcodeproj(
     ios_device_cpus = "arm64",
     ios_simulator_cpus = "",
     minimum_xcode_version = "13.0",
+    owned_extra_files = {"@@//Lib:README.md": "@@//Lib:Lib", "@@//iOSApp:ownership.yaml": "@@//iOSApp/Source:iOSApp"},
     post_build = """""",
     pre_build = """set -euo pipefail
 

examples/integration/test/fixtures/generated/xcodeproj_bwb/defs.bzl
@@ -19,7 +19,6 @@ load(
 )
 
 _FOCUSED_LABELS = []
-_OWNED_EXTRA_FILES = {"@@//Lib:README.md": "@@//Lib:Lib", "@@//iOSApp:ownership.yaml": "@@//iOSApp/Source:iOSApp"}
 _UNFOCUSED_LABELS = ["@@//Lib:LibFramework.iOS"]
 
 # Transition
@@ -54,7 +53,6 @@ _aspect = xcodeproj_factory.make_aspect(
     build_mode = "bazel",
     focused_labels = _FOCUSED_LABELS,
     generator_name = "xcodeproj_bwb",
-    owned_extra_files = _OWNED_EXTRA_FILES,
     unfocused_labels = _UNFOCUSED_LABELS,
     use_incremental = False,
 )
@@ -64,7 +62,6 @@ _aspect = xcodeproj_factory.make_aspect(
 xcodeproj = xcodeproj_factory.make_rule(
     focused_labels = _FOCUSED_LABELS,
     is_fixture = True,
-    owned_extra_files = _OWNED_EXTRA_FILES,
     target_transitions = _target_transitions,
     unfocused_labels = _UNFOCUSED_LABELS,
     use_incremental = False,

examples/rules_ios/test/fixtures/generated/xcodeproj_bwb/BUILD
@@ -24,6 +24,7 @@ xcodeproj(
     ios_device_cpus = "arm64",
     ios_simulator_cpus = "",
     minimum_xcode_version = "13.0",
+    owned_extra_files = {},
     post_build = """""",
     pre_build = """""",
     project_name = "bwb",

examples/rules_ios/test/fixtures/generated/xcodeproj_bwb/defs.bzl
@@ -19,7 +19,6 @@ load(
 )
 
 _FOCUSED_LABELS = []
-_OWNED_EXTRA_FILES = {}
 _UNFOCUSED_LABELS = []
 
 # Transition
@@ -54,7 +53,6 @@ _aspect = xcodeproj_factory.make_aspect(
     build_mode = "bazel",
     focused_labels = _FOCUSED_LABELS,
     generator_name = "xcodeproj_bwb",
-    owned_extra_files = _OWNED_EXTRA_FILES,
     unfocused_labels = _UNFOCUSED_LABELS,
     use_incremental = False,
 )
@@ -64,7 +62,6 @@ _aspect = xcodeproj_factory.make_aspect(
 xcodeproj = xcodeproj_factory.make_rule(
     focused_labels = _FOCUSED_LABELS,
     is_fixture = True,
-    owned_extra_files = _OWNED_EXTRA_FILES,
     target_transitions = _target_transitions,
     unfocused_labels = _UNFOCUSED_LABELS,
     use_incremental = False,

xcodeproj/internal/files/incremental_input_files.bzl
@@ -567,8 +567,6 @@ def _collect_incremental_input_files(
         if not file.is_source
     ]
 
-    label_str = str(label)
-
     return (
         struct(
             c_sources = c_sources,
@@ -578,11 +576,7 @@ def _collect_incremental_input_files(
                 extra_file_paths = memory_efficient_depset(extra_file_paths),
                 extra_files = memory_efficient_depset(
                     extra_files,
-                    transitive = [
-                        t.files
-                        for t, file_label_str in ctx.attr._owned_extra_files.items()
-                        if file_label_str == label_str
-                    ] + transitive_extra_files,
+                    transitive = transitive_extra_files,
                 ),
                 extra_folders = extra_folders,
                 infoplist_path = (

xcodeproj/internal/templates/generator.defs.bzl
@@ -3,7 +3,6 @@
 %loads%
 
 _FOCUSED_LABELS = %focused_labels%
-_OWNED_EXTRA_FILES = %owned_extra_files%
 _UNFOCUSED_LABELS = %unfocused_labels%
 
 # Transition
@@ -16,7 +15,6 @@ _aspect = xcodeproj_factory.make_aspect(
     build_mode = "%build_mode%",
     focused_labels = _FOCUSED_LABELS,
     generator_name = "%generator_name%",
-    owned_extra_files = _OWNED_EXTRA_FILES,
     unfocused_labels = _UNFOCUSED_LABELS,
     use_incremental = %use_incremental%,
 )
@@ -26,7 +24,6 @@ _aspect = xcodeproj_factory.make_aspect(
 xcodeproj = xcodeproj_factory.make_rule(
     focused_labels = _FOCUSED_LABELS,
     is_fixture = %is_fixture%,
-    owned_extra_files = _OWNED_EXTRA_FILES,
     target_transitions = _target_transitions,
     unfocused_labels = _UNFOCUSED_LABELS,
     use_incremental = %use_incremental%,

xcodeproj/internal/templates/generator.incremental.BUILD.bazel
@@ -23,6 +23,7 @@ xcodeproj(
     ios_device_cpus = "%ios_device_cpus%",
     ios_simulator_cpus = "%ios_simulator_cpus%",
     minimum_xcode_version = "%minimum_xcode_version%",
+    owned_extra_files = %owned_extra_files%,
     post_build = """%post_build%""",
     pre_build = """%pre_build%""",
     project_options = %project_options%,

commit 80468978891feb91d3cc075ae00f4f19a9dee2fb

Fix handling of resource bundles with incremental generation mode
Regressed with 7399108d66a35e5147970067151ba685861d58fd.

Signed-off-by: Brentley Jones <github@brentleyjones.com>

xcodeproj/internal/incremental_xcode_targets.bzl
@@ -31,6 +31,7 @@ def _from_resource_bundle(bundle):
             non_arc_srcs = EMPTY_DEPSET,
             srcs = EMPTY_DEPSET,
         ),
+        label = None,
     )
 
 def _from_resource_bundles(bundles, *, resource_bundle_ids):

xcodeproj/internal/xcodeproj_incremental_rule.bzl
@@ -141,9 +141,11 @@ def _collect_files(
         transitive_srcs.append(xcode_target.inputs.non_arc_srcs)
         transitive_srcs.append(xcode_target.inputs.srcs)
 
-        extra_files = target_extra_files.get(str(xcode_target.label))
-        if extra_files:
-            transitive_files.append(extra_files)
+        label = xcode_target.label
+        if label:
+            extra_files = target_extra_files.get(str(label))
+            if extra_files:
+                transitive_files.append(extra_files)
 
         infoplist_path = xcode_target.inputs.infoplist_path
         if infoplist_path: