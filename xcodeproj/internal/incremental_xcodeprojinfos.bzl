MobileNativeFoundation/rules_xcodeproj

commit 6f65b40b1c04fc6aa0e8910e8d1597954de95777

Make `xcschemes.top_level_anchor_target` work with `*_build_test` targets (#2945)
Signed-off-by: Brentley Jones <github@brentleyjones.com>

xcodeproj/internal/incremental_xcodeprojinfos.bzl
@@ -247,6 +247,7 @@ def _make_skipped_target_xcodeprojinfo(
         *,
         automatic_target_info,
         rule_attr,
+        skip_type,
         test_env,
         transitive_infos):
     """Passes through existing target info fields, not collecting new ones.
@@ -258,6 +259,7 @@ def _make_skipped_target_xcodeprojinfo(
         automatic_target_info: The `XcodeProjAutomaticTargetProcessingInfo` for
             `the target.
         rule_attr: `ctx.rule.attr`.
+        skip_type: A value returned by `_get_skip_type`.
         test_env: `ctx.configuration.test_env`.
         transitive_infos: A `list` of `depset`s of `XcodeProjInfo`s from the
             transitive dependencies of the target.
@@ -281,6 +283,44 @@ def _make_skipped_target_xcodeprojinfo(
         info
         for _, info in transitive_infos
     ]
+
+    # Test bundles already collect their focused library deps, so we shouldn't
+    # here, otherwise we will override their id with whatever is picked here.
+    # Collecting `top_level_focused_deps` here allows using `*_build_test` rules
+    # in `xcschemes.top_level_anchor_target`.
+    if skip_type != _SKIP_TYPE.apple_test_bundle:
+        first_id = None
+        for info in valid_transitive_infos:
+            if info.xcode_target:
+                # If this target gets listed in something like
+                # `xcschemes.top_level_build_target`, we use the first available
+                # target id. This isn't really a supported use case. We could
+                # maybe handle multiple ids, but that requires a bit more work.
+                first_id = info.xcode_target.id
+                break
+
+        focused_library_deps = {
+            s.label: s.id
+            for s in depset(
+                order = "postorder",
+                transitive = [
+                    info.focused_library_deps
+                    for info in valid_transitive_infos
+                ],
+            ).to_list()
+        }
+        top_level_focused_deps = [
+            struct(
+                id = first_id,
+                label = str(automatic_target_info.label),
+                deps = tuple([
+                    struct(id = id, label = label)
+                    for label, id in focused_library_deps.items()
+                ]),
+            ),
+        ]
+    else:
+        top_level_focused_deps = None
 
     direct_dependencies, transitive_dependencies = dependencies.collect(
         transitive_infos = valid_transitive_infos,
@@ -410,6 +450,7 @@ def _make_skipped_target_xcodeprojinfo(
         ),
         target_type = target_type.compile,
         top_level_focused_deps = memory_efficient_depset(
+            top_level_focused_deps,
             transitive = [
                 info.top_level_focused_deps
                 for info in valid_transitive_infos
@@ -685,6 +726,7 @@ def _make_xcodeprojinfo(
         info_fields = _make_skipped_target_xcodeprojinfo(
             automatic_target_info = automatic_target_info,
             rule_attr = rule_attr,
+            skip_type = target_skip_type,
             test_env = ctx.configuration.test_env,
             transitive_infos = transitive_infos,
         )

commit cbdb236a3ac3b25a865f19a46b78d45372a6e4a2

Fix xcscheme generation when using test suites
Regressed with 6f65b40b1c04fc6aa0e8910e8d1597954de95777.

Signed-off-by: Brentley Jones <github@brentleyjones.com>

xcodeproj/internal/incremental_xcodeprojinfos.bzl
@@ -83,6 +83,11 @@ _SKIP_TYPE = struct(
     test_suite = "test_suite",
 )
 
+_FOCUSED_DEPS_COLLECTION_SKIPPED_TYPES = {
+    _SKIP_TYPE.apple_build_test: None,
+    _SKIP_TYPE.apple_binary_no_deps: None,
+}
+
 def _get_skip_type(*, rule_attr, rule_kind, target):
     """Determines if the given target should be skipped for target generation.
 
@@ -288,7 +293,7 @@ def _make_skipped_target_xcodeprojinfo(
     # here, otherwise we will override their id with whatever is picked here.
     # Collecting `top_level_focused_deps` here allows using `*_build_test` rules
     # in `xcschemes.top_level_anchor_target`.
-    if skip_type != _SKIP_TYPE.apple_test_bundle:
+    if skip_type in _FOCUSED_DEPS_COLLECTION_SKIPPED_TYPES:
         first_id = None
         for info in valid_transitive_infos:
             if info.xcode_target: