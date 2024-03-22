MobileNativeFoundation/rules_xcodeproj

commit e1d058cd871a3cf3f9eb04238f15169c792eae1f

Reduce work that incremental generation mode installer does for generated directories (#2956)
A couple of optimizations:

- We create the list of generated directories with Bazel, allowing it to
be cached
- The generated directories are the unique leaf directories, no longer
including any intermediate parent directories. This is accomplished
using a trie.

Signed-off-by: Brentley Jones <github@brentleyjones.com>

tools/unique_directories/unique_directories.py
@@ -1,0 +1,37 @@
+#!/usr/bin/python3
+
+import os
+import sys
+
+def _insert_into_trie(trie, components):
+    if not components:
+        return
+    _insert_into_trie(trie.setdefault(components[0], {}), components[1:])
+
+def _trie_to_list(trie, prefix_components, output):
+    for component, sub_trie in trie.items():
+        if not sub_trie:
+            output.append(os.sep.join(prefix_components + [component]))
+        else:
+            _trie_to_list(sub_trie, prefix_components + [component], output)
+
+def _main(input_filelist, output_filelist):
+    trie = {}
+
+    with open(input_filelist, 'r', encoding='utf-8') as fp:
+        for directory in fp:
+            _insert_into_trie(trie, directory.rstrip().split(os.sep))
+
+    directories = []
+    _trie_to_list(trie, [], directories)
+
+    with open(output_filelist, 'w', encoding='utf-8') as fp:
+        fp.write("\n".join(directories))
+        fp.write("\n")
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Usage: unique_directories.py <input.filelist> <output.filelist>")
+        exit(1)
+
+    _main(sys.argv[1], sys.argv[2])

tools/BUILD
@@ -566,6 +566,7 @@ filegroup(
         "//" + package_name() + "/import_indexstores:release_files",
         "//" + package_name() + "/params_processors:release_files",
         "//" + package_name() + "/swiftc_stub:release_files",
+        "//" + package_name() + "/unique_directories:release_files",
         "//" + package_name() + "/xccurrentversions_parser:release_files",
     ],
     tags = ["manual"],

tools/unique_directories/BUILD
@@ -1,0 +1,24 @@
+load("@rules_python//python:defs.bzl", "py_binary")
+
+py_binary(
+    name = "unique_directories",
+    srcs = ["unique_directories.py"],
+    python_version = "PY3",
+    srcs_version = "PY3",
+    # TODO: Restrict visibility
+    visibility = ["//visibility:public"],
+)
+
+# Release
+
+filegroup(
+    name = "release_files",
+    srcs = glob(
+        ["**"],
+        exclude = [
+            "**/.*",
+        ],
+    ),
+    tags = ["manual"],
+    visibility = ["//:__subpackages__"],
+)

xcodeproj/internal/files/incremental_input_files.bzl
@@ -579,10 +579,7 @@ def _collect_incremental_input_files(
                     transitive = transitive_extra_files,
                 ),
                 extra_folders = extra_folders,
-                infoplist_path = (
-                    # Removing "bazel-out" prefix
-                    "$(BAZEL_OUT){}".format(infoplist.path[9:]) if infoplist else None
-                ),
+                infoplist = infoplist,
                 non_arc_srcs = memory_efficient_depset(non_arc_srcs),
                 srcs = memory_efficient_depset(srcs),
             ),

xcodeproj/internal/incremental_xcode_targets.bzl
@@ -27,7 +27,7 @@ def _from_resource_bundle(bundle):
             extra_files = bundle.resources,
             extra_file_paths = EMPTY_DEPSET,
             extra_folders = bundle.folder_resources,
-            infoplist_path = None,
+            infoplist = None,
             non_arc_srcs = EMPTY_DEPSET,
             srcs = EMPTY_DEPSET,
         ),
@@ -181,7 +181,7 @@ def _merge_xcode_inputs(*, dest_inputs, mergeable_info):
             ],
         ),
         extra_folders = dest_inputs.extra_folders,
-        infoplist_path = dest_inputs.infoplist_path,
+        infoplist = dest_inputs.infoplist,
         non_arc_srcs = mergeable_info.non_arc_srcs,
         srcs = mergeable_info.srcs,
     )

xcodeproj/internal/pbxproj_partials.bzl
@@ -49,11 +49,23 @@ def _keys_and_files(pair):
     key, file = pair
     return [key, file.path]
 
+def _dirname(file):
+    return file.dirname
+
+def _generated_dirname(file):
+    if file.is_source:
+        return None
+
+    return file.dirname
+
+def _always_generated_file_path(file):
+    return "$(BAZEL_OUT){}".format(file.path[9:])
+
 def _generated_file_path(file):
     if file.is_source:
         return None
 
-    return "$(BAZEL_OUT){}".format(file.path[9:])
+    return _always_generated_file_path(file)
 
 # Partials
 
@@ -526,18 +538,54 @@ def _write_files_and_groups(
         resolved_repositories_file,
     )
 
+def _write_generated_directories_filelist(
+        *,
+        actions,
+        generator_name,
+        infoplists,
+        install_path,
+        srcs,
+        tool):
+    directories_args = actions.args()
+    directories_args.set_param_file_format("multiline")
+    directories_args.use_param_file("%s", use_always = True)
+
+    directories_args.add_all(infoplists, map_each = _dirname)
+    directories_args.add_all(srcs, map_each = _generated_dirname)
+
+    filelist = actions.declare_file(
+        "{}-generated_directories.filelist".format(generator_name),
+    )
+
+    args = actions.args()
+    args.add(filelist)
+
+    message = (
+        "Generating {} generated directories filelist".format(install_path)
+    )
+
+    actions.run(
+        arguments = [directories_args, args],
+        executable = tool,
+        outputs = [filelist],
+        mnemonic = "WriteGeneratedDirectoriesFilelist",
+        progress_message = message,
+    )
+
+    return filelist
+
 def _write_generated_xcfilelist(
         *,
         actions,
         generator_name,
-        infoplist_paths,
+        infoplists,
         srcs):
     args = actions.args()
     args.set_param_file_format("multiline")
 
     # Info.plists are tracked as build files by Xcode, so top-level targets
     # will fail the first time they are built if we don't track them
-    args.add_all(infoplist_paths)
+    args.add_all(infoplists, map_each = _always_generated_file_path)
 
     # Source files are tracked as build files by Xcode, so building targets that
     # directly use generated source files will fail the first time they are
@@ -1325,6 +1373,9 @@ cat "$@" > "{output}"
 
 pbxproj_partials = struct(
     write_files_and_groups = _write_files_and_groups,
+    write_generated_directories_filelist = (
+        _write_generated_directories_filelist
+    ),
     write_generated_xcfilelist = _write_generated_xcfilelist,
     write_pbxproj_prefix = _write_pbxproj_prefix,
     write_pbxtargetdependencies = _write_pbxtargetdependencies,

xcodeproj/internal/templates/incremental_installer.sh
@@ -55,6 +55,7 @@ fi
 
 # Resolve the inputs
 readonly src_generated_xcfilelist="$PWD/%generated_xcfilelist%"
+readonly src_generated_directories_filelist="$PWD/%generated_directories_filelist%"
 readonly src_project_pbxproj="$PWD/%project_pbxproj%"
 readonly src_xcschememanagement="$PWD/%xcschememanagement%"
 readonly src_xcschemes="$PWD/%xcschemes%/"
@@ -179,21 +180,16 @@ readonly indexbuild_exec_root="$output_base/rules_xcodeproj.noindex/indexbuild_o
 mkdir -p "$indexbuild_exec_root"
 
 # Create folder structure in bazel-out to work around Xcode red generated files
-if [[ -f "$dest/rules_xcodeproj/generated.xcfilelist" ]]; then
+if [[ -s "$src_generated_directories_filelist" ]]; then
   cd "$BUILD_WORKSPACE_DIRECTORY"
 
-  readonly nested_build_output_base="$output_base/rules_xcodeproj.noindex/build_output_base"
-  readonly bazel_out="$nested_build_output_base/execroot/$workspace_name/bazel-out"
+  readonly nested_execution_root="$output_base/rules_xcodeproj.noindex/build_output_base/execroot/$workspace_name"
 
   # Create directory structure in bazel-out
-  cd "$bazel_out"
-  sed 's|^\$(BAZEL_OUT)\/\(.*\)\/[^\/]*$|\1|' \
-    "$dest/rules_xcodeproj/generated.xcfilelist" \
-    | uniq \
-    | while IFS= read -r dir
-  do
+  cd "$nested_execution_root"
+  while IFS= read -r dir; do
     mkdir -p "$dir"
-  done
+  done < "$src_generated_directories_filelist"
 fi
 
 echo 'Updated project at "%output_path%"'

xcodeproj/internal/xcodeproj_incremental_rule.bzl
@@ -129,7 +129,7 @@ def _collect_files(
     all_targets = xcode_targets.values() + resource_bundle_xcode_targets
 
     compile_stub_needed = False
-    infoplist_paths = []
+    infoplists = []
     transitive_file_paths = []
     transitive_files = [unsupported_extra_files]
     transitive_folders = []
@@ -147,9 +147,9 @@ def _collect_files(
             if extra_files:
                 transitive_files.append(extra_files)
 
-        infoplist_path = xcode_target.inputs.infoplist_path
-        if infoplist_path:
-            infoplist_paths.append(infoplist_path)
+        infoplist = xcode_target.inputs.infoplist
+        if infoplist:
+            infoplists.append(infoplist)
 
         if xcode_target.compile_stub_needed:
             compile_stub_needed = True
@@ -169,7 +169,7 @@ def _collect_files(
         file_paths,
         files,
         folders,
-        infoplist_paths,
+        infoplists,
         srcs,
     )
 
@@ -244,6 +244,7 @@ def _write_installer(
         bazel_integration_files,
         config,
         contents_xcworkspacedata,
+        generated_directories_filelist,
         generated_xcfilelist,
         install_path,
         is_fixture,
@@ -266,6 +267,9 @@ def _write_installer(
             ),
             "%config%": config,
             "%contents_xcworkspacedata%": contents_xcworkspacedata.short_path,
+            "%generated_directories_filelist%": (
+                generated_directories_filelist.short_path
+            ),
             "%generated_xcfilelist%": generated_xcfilelist.short_path,
             "%is_fixture%": "1" if is_fixture else "0",
             "%output_path%": install_path,
@@ -277,6 +281,7 @@ def _write_installer(
 
     runfiles = bazel_integration_files + [
         contents_xcworkspacedata,
+        generated_directories_filelist,
         generated_xcfilelist,
         project_pbxproj,
         xcschememanagement,
@@ -309,6 +314,7 @@ def _write_project_contents(
         resource_bundle_xcode_targets,
         selected_model_versions_generator,
         target_name_mode,
+        unique_directories,
         unowned_extra_files,
         unsupported_extra_files,
         workspace_directory,
@@ -341,7 +347,7 @@ def _write_project_contents(
         file_paths,
         files,
         folders,
-        infoplist_paths,
+        infoplists,
         srcs,
     ) = _collect_files(
         owned_extra_files = owned_extra_files,
@@ -445,15 +451,26 @@ def _write_project_contents(
 
     # `.xcfilelist`s
 
+    (
+        generated_directories_filelist
+    ) = pbxproj_partials.write_generated_directories_filelist(
+        actions = actions,
+        generator_name = name,
+        infoplists = infoplists,
+        install_path = install_path,
+        srcs = srcs,
+        tool = unique_directories,
+    )
     generated_xcfilelist = pbxproj_partials.write_generated_xcfilelist(
         actions = actions,
         generator_name = name,
-        infoplist_paths = infoplist_paths,
+        infoplists = infoplists,
         srcs = srcs,
     )
 
     return (
         project_pbxproj,
+        generated_directories_filelist,
         generated_xcfilelist,
         consolidation_maps.keys(),
         target_ids_list,
@@ -588,6 +605,7 @@ After removing unfocused targets, no targets remain. Please check your \
 
     (
         project_pbxproj,
+        generated_directories_filelist,
         generated_xcfilelist,
         consolidation_maps,
         target_ids_list,
@@ -637,6 +655,7 @@ After removing unfocused targets, no targets remain. Please check your \
             ctx.executable._selected_model_versions_generator
         ),
         target_name_mode = ctx.attr.target_name_mode,
+        unique_directories = ctx.executable._unique_directories,
         unowned_extra_files = ctx.files.unowned_extra_files,
         unsupported_extra_files = inputs.unsupported_extra_files,
         workspace_directory = workspace_directory,
@@ -697,6 +716,7 @@ After removing unfocused targets, no targets remain. Please check your \
         bazel_integration_files = bazel_integration_files,
         config = config,
         contents_xcworkspacedata = ctx.file._contents_xcworkspacedata,
+        generated_directories_filelist = generated_directories_filelist,
         generated_xcfilelist = generated_xcfilelist,
         install_path = install_path,
         is_fixture = is_fixture,
@@ -861,6 +881,11 @@ def _xcodeproj_incremental_attrs(
             default = Label(
                 "//tools/generators/swift_debug_settings:universal_swift_debug_settings",
             ),
+            executable = True,
+        ),
+        "_unique_directories": attr.label(
+            cfg = "exec",
+            default = Label("//tools/unique_directories"),
             executable = True,
         ),
         "_xcode_config": attr.label(

commit bc2f94ca61d352d2fabb32161e111ef2580e0b76

Fix `unique_directories` for empty `directories` (#2965)
Broke in e1d058cd871a3cf3f9eb04238f15169c792eae1f.

Signed-off-by: Brentley Jones <github@brentleyjones.com>

tools/unique_directories/unique_directories.py
@@ -26,8 +26,11 @@ def _main(input_filelist, output_filelist):
     _trie_to_list(trie, [], directories)
 
     with open(output_filelist, 'w', encoding='utf-8') as fp:
-        fp.write("\n".join(directories))
-        fp.write("\n")
+        if directories:
+            fp.write("\n".join(directories))
+            fp.write("\n")
+        else:
+            fp.write("")
 
 if __name__ == "__main__":
     if len(sys.argv) != 3: