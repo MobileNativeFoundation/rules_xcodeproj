MobileNativeFoundation/rules_xcodeproj

commit 59303f55e135cb9fe51dc2923bb65ef99d200641

Use the same output base for Xcode Build and Index Build (#3074)
By using `--preemptible` Bazel flag on Index Builds, normal builds will
cause them to cancel instead of block for them. This along with Xcode’s
default interrupt means that Index Builds are able to use the same
output base with minimal impact to normal builds.

This change has numerous benefits:

- Indexing completes faster since it can reuse any normal builds that
have happened, including analysis
- Normal builds complete faster because it can leverage the work done by
background Index Builds
- Quickly open won’t show two source paths anymore for generated files,
since there is now only one path
- With only one output base, disk usage is drastically cut down
- There is one less Bazel server, so memory usage is drastically cut
down

Signed-off-by: Brentley Jones <github@brentleyjones.com>

xcodeproj/internal/bazel_integration_files/generate_bazel_dependencies.sh
@@ -206,6 +206,6 @@ done
 if [ -n "${indexstores_filelists:-}" ]; then
   "$BAZEL_INTEGRATION_DIR/import_indexstores" \
     "$INDEXING_PROJECT_DIR__NO" \
-    "${indexstores_filelists[@]/#/$output_path/}" \
+    "${indexstores_filelists[@]/#/$BAZEL_OUT/}" \
     >"$log_dir/import_indexstores.async.log" 2>&1 &
 fi

xcodeproj/internal/bazel_integration_files/generate_index_build_bazel_dependencies.sh
@@ -67,5 +67,5 @@ source "$BAZEL_INTEGRATION_DIR/bazel_build.sh"
 if [ -n "${indexstores_filelists:-}" ]; then
   "$BAZEL_INTEGRATION_DIR/import_indexstores" \
     "$INDEXING_PROJECT_DIR__NO" \
-    "${indexstores_filelists[@]/#/$output_path/}"
+    "${indexstores_filelists[@]/#/$BAZEL_OUT/}"
 fi

tools/generators/legacy/src/Generator/CreateProject.swift
@@ -63,12 +63,6 @@ extension Generator {
         let projectDirComponents = absoluteProjectDirPath
             .split(separator: "/", omittingEmptySubsequences: false)
 
-        // build_output_base/execroot/_main -> indexbuild_output_base/execroot/_main
-        let indexingProjectDirPath =
-            (projectDirComponents.prefix(upTo: projectDirComponents.count - 3) +
-             ["indexbuild_output_base"] +
-             projectDirComponents.suffix(2)).joined(separator: "/")
-
         var buildSettings: [String: Any] = [
             "ALWAYS_SEARCH_USER_PATHS": false,
             "BAZEL_CONFIG": project.bazelConfig,
@@ -101,9 +95,6 @@ $(SYMROOT)/$(CONFIGURATION)$(EFFECTIVE_PLATFORM_NAME)
             "INDEX_DATA_STORE_DIR": "$(INDEX_DATA_STORE_DIR)",
             "INDEX_FORCE_SCRIPT_EXECUTION": true,
             "INDEX_IMPORT": indexImport,
-            "INDEXING_PROJECT_DIR__": "$(INDEXING_PROJECT_DIR__NO)",
-            "INDEXING_PROJECT_DIR__NO": absoluteProjectDirPath,
-            "INDEXING_PROJECT_DIR__YES": indexingProjectDirPath,
             "INSTALL_PATH": "$(BAZEL_PACKAGE_BIN_DIR)/$(TARGET_NAME)/bin",
             "INTERNAL_DIR": """
 $(PROJECT_FILE_PATH)/\(directories.internalDirectoryName)
@@ -114,9 +105,7 @@ $(PROJECT_FILE_PATH)/\(directories.internalDirectoryName)
             // `link.params`
             "LD_RUNPATH_SEARCH_PATHS": "",
             "ONLY_ACTIVE_ARCH": true,
-            "PROJECT_DIR": """
-$(INDEXING_PROJECT_DIR__$(INDEX_ENABLE_BUILD_ARENA))
-""",
+            "PROJECT_DIR": absoluteProjectDirPath,
             "RULES_XCODEPROJ_BUILD_MODE": buildMode.rawValue,
             "SRCROOT": srcRoot,
             // Bazel currently doesn't support Catalyst

tools/generators/legacy/src/Generator/SetTargetConfigurations.swift
@@ -439,8 +439,6 @@ $(BAZEL_OUT)\#(swiftParams.path.string.dropFirst(9))
                         "$(DERIVED_FILE_DIR)/xcode-overlay.yaml",
                         "-Xcc",
                         "-ivfsoverlay",
-                        "-Xcc",
-                        "$(OBJROOT)/bazel-out-overlay.yaml",
                     ])
                 }
 
@@ -463,29 +461,8 @@ $(BAZEL_OUT)\#(swiftParams.path.string.dropFirst(9))
                     swiftFlagsPrefix.append(contentsOf: [
                         "-Xcc",
                         "-ivfsoverlay",
-                        "-Xcc",
-                        "$(OBJROOT)/bazel-out-overlay.yaml",
                     ])
                 }
-            }
-
-            if !swiftFlags.isEmpty {
-                swiftFlagsPrefix.append(contentsOf: [
-                    "-vfsoverlay",
-                    "$(OBJROOT)/bazel-out-overlay.yaml",
-                ])
-            }
-            if !cFlags.isEmpty {
-                cFlagsPrefix.append(contentsOf: [
-                    "-ivfsoverlay",
-                    "$(OBJROOT)/bazel-out-overlay.yaml",
-                ])
-            }
-            if !cxxFlags.isEmpty {
-                cxxFlagsPrefix.append(contentsOf: [
-                    "-ivfsoverlay",
-                    "$(OBJROOT)/bazel-out-overlay.yaml",
-                ])
             }
         }
 

tools/generators/legacy/test/CreateProjectTests.swift
@@ -81,13 +81,6 @@ $(INDEXING_DEPLOYMENT_LOCATION__NO)
 """,
             "INDEXING_DEPLOYMENT_LOCATION__NO": true,
             "INDEXING_DEPLOYMENT_LOCATION__YES": false,
-            "INDEXING_PROJECT_DIR__": "$(INDEXING_PROJECT_DIR__NO)",
-            "INDEXING_PROJECT_DIR__NO": """
-/tmp/bazel-output-base/rules_xcodeproj/build_output_base/execroot/rules_xcodeproj
-""",
-            "INDEXING_PROJECT_DIR__YES": """
-/tmp/bazel-output-base/rules_xcodeproj/indexbuild_output_base/execroot/rules_xcodeproj
-""",
             "INSTALL_PATH": "$(BAZEL_PACKAGE_BIN_DIR)/$(TARGET_NAME)/bin",
             "INTERNAL_DIR": "$(PROJECT_FILE_PATH)/r_xcp",
             "LD_OBJC_ABI_VERSION": "",
@@ -95,7 +88,7 @@ $(INDEXING_DEPLOYMENT_LOCATION__NO)
             "LD_RUNPATH_SEARCH_PATHS": "",
             "ONLY_ACTIVE_ARCH": true,
             "PROJECT_DIR": """
-$(INDEXING_PROJECT_DIR__$(INDEX_ENABLE_BUILD_ARENA))
+/tmp/bazel-output-base/rules_xcodeproj/build_output_base/execroot/rules_xcodeproj
 """,
             "RULES_XCODEPROJ_BUILD_MODE": "xcode",
             "SRCROOT": directories.workspace.string,
@@ -227,13 +220,6 @@ $(BUILD_DIR)/$(BAZEL_PACKAGE_BIN_DIR)
             "INDEX_DATA_STORE_DIR": "$(INDEX_DATA_STORE_DIR)",
             "INDEX_FORCE_SCRIPT_EXECUTION": true,
             "INDEX_IMPORT": "/tmp/index-import",
-            "INDEXING_PROJECT_DIR__": "$(INDEXING_PROJECT_DIR__NO)",
-            "INDEXING_PROJECT_DIR__NO": """
-/tmp/bazel-output-base/rules_xcodeproj/build_output_base/execroot/rules_xcodeproj
-""",
-            "INDEXING_PROJECT_DIR__YES": """
-/tmp/bazel-output-base/rules_xcodeproj/indexbuild_output_base/execroot/rules_xcodeproj
-""",
             "INSTALL_PATH": "$(BAZEL_PACKAGE_BIN_DIR)/$(TARGET_NAME)/bin",
             "INTERNAL_DIR": "$(PROJECT_FILE_PATH)/r_xcp",
             "LD_OBJC_ABI_VERSION": "",
@@ -241,7 +227,7 @@ $(BUILD_DIR)/$(BAZEL_PACKAGE_BIN_DIR)
             "LD_RUNPATH_SEARCH_PATHS": "",
             "ONLY_ACTIVE_ARCH": true,
             "PROJECT_DIR": """
-$(INDEXING_PROJECT_DIR__$(INDEX_ENABLE_BUILD_ARENA))
+/tmp/bazel-output-base/rules_xcodeproj/build_output_base/execroot/rules_xcodeproj
 """,
             "RULES_XCODEPROJ_BUILD_MODE": "bazel",
             "SRCROOT": directories.workspace.string,

tools/generators/legacy/test/Fixtures.swift
@@ -2067,7 +2067,6 @@ touch "$SCRIPT_OUTPUT_FILE_1"
                 "COMPILE_TARGET_NAME": targets["A 1"]!.name,
                 "MACOSX_DEPLOYMENT_TARGET": "10.0",
                 "OTHER_SWIFT_FLAGS": #"""
--vfsoverlay $(OBJROOT)/bazel-out-overlay.yaml \#
 @$(DERIVED_FILE_DIR)/swift.compile.params
 """#,
                 "PRODUCT_NAME": "a",
@@ -2210,8 +2209,6 @@ $(BUILD_DIR)/bazel-out/a1b2c/bin/A 2/A.app/A_ExecutableName
                 "COMPILE_TARGET_NAME": targets["E1"]!.name,
                 "OTHER_SWIFT_FLAGS": #"""
 -Xcc -ivfsoverlay -Xcc $(DERIVED_FILE_DIR)/xcode-overlay.yaml \#
--Xcc -ivfsoverlay -Xcc $(OBJROOT)/bazel-out-overlay.yaml \#
--vfsoverlay $(OBJROOT)/bazel-out-overlay.yaml \#
 @$(DERIVED_FILE_DIR)/swift.compile.params
 """#,
                 "PRODUCT_NAME": "E1",
@@ -2231,7 +2228,6 @@ $(BAZEL_OUT)/E1.swift.compile.params
                 "BAZEL_TARGET_ID[sdk=appletvos*]": "$(BAZEL_TARGET_ID)",
                 "COMPILE_TARGET_NAME": targets["E2"]!.name,
                 "OTHER_SWIFT_FLAGS": #"""
--vfsoverlay $(OBJROOT)/bazel-out-overlay.yaml \#
 @$(DERIVED_FILE_DIR)/swift.compile.params
 """#,
                 "PRODUCT_NAME": "E2",
@@ -2314,7 +2310,6 @@ $(MACOSX_FILES)
 "T/T 3/Ta.c" "T/T 3/Ta.swift"
 """,
                 "OTHER_SWIFT_FLAGS": #"""
--vfsoverlay $(OBJROOT)/bazel-out-overlay.yaml \#
 @$(DERIVED_FILE_DIR)/swift.compile.params
 """#,
                 "PRODUCT_NAME": "t",

commit 95b9c46d741d11215f8148fce6733f1effc1cd39

Fix missed `$INDEXING_PROJECT_DIR__NO` removal
Regressed with 59303f55e135cb9fe51dc2923bb65ef99d200641.

Signed-off-by: Brentley Jones <github@brentleyjones.com>

xcodeproj/internal/bazel_integration_files/generate_bazel_dependencies.sh
@@ -205,7 +205,7 @@ done
 # Import indexes
 if [ -n "${indexstores_filelists:-}" ]; then
   "$BAZEL_INTEGRATION_DIR/import_indexstores" \
-    "$INDEXING_PROJECT_DIR__NO" \
+    "$PROJECT_DIR" \
     "${indexstores_filelists[@]/#/$BAZEL_OUT/}" \
     >"$log_dir/import_indexstores.async.log" 2>&1 &
 fi

xcodeproj/internal/bazel_integration_files/generate_index_build_bazel_dependencies.sh
@@ -66,6 +66,6 @@ source "$BAZEL_INTEGRATION_DIR/bazel_build.sh"
 # Import indexes
 if [ -n "${indexstores_filelists:-}" ]; then
   "$BAZEL_INTEGRATION_DIR/import_indexstores" \
-    "$INDEXING_PROJECT_DIR__NO" \
+    "$PROJECT_DIR" \
     "${indexstores_filelists[@]/#/$BAZEL_OUT/}"
 fi