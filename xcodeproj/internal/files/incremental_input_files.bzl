MobileNativeFoundation/rules_xcodeproj

commit caae410fcd827b4bcc7ae8b794773ce07a1ceb8b

Remove all code related to “folders” (#3100)
We no longer represent any resources as folders, so we can simplify the
code. This has a small performance win.

Signed-off-by: Brentley Jones <github@brentleyjones.com>

xcodeproj/internal/files/incremental_input_files.bzl
@@ -57,22 +57,22 @@ def _inner_merge_input_files(
                 for info in transitive_infos
             ],
         ),
+        _resource_bundle_uncategorized_file_paths = memory_efficient_depset(
+            transitive = [
+                info.inputs._resource_bundle_uncategorized_file_paths
+                for info in transitive_infos
+            ],
+        ),
         _resource_bundle_uncategorized_files = memory_efficient_depset(
             transitive = [
                 info.inputs._resource_bundle_uncategorized_files
                 for info in transitive_infos
             ],
         ),
-        _resource_bundle_uncategorized_folders = memory_efficient_depset(
-            transitive = [
-                info.inputs._resource_bundle_uncategorized_folders
-                for info in transitive_infos
-            ],
-        ),
-        _resource_bundle_uncategorized_generated_folders = (
+        _resource_bundle_uncategorized_generated_file_paths = (
             memory_efficient_depset(
                 transitive = [
-                    info.inputs._resource_bundle_uncategorized_generated_folders
+                    info.inputs._resource_bundle_uncategorized_generated_file_paths
                     for info in transitive_infos
                 ],
             )
@@ -302,9 +302,6 @@ def _collect_incremental_input_files(
                 *   `extra_files`: A `depset` of `File` that aren't covered
                     under the other attributes, but should be included in the
                     project navigator.
-                *   `extra_folders`: A `depset` of folder path strings that
-                    aren't covered under the other attributes, but should be
-                    included in the project navigator.
                 *   `non_arc_srcs`: A `list` of `File`s that are inputs to
                     `target`'s `non_arc_srcs`-like attributes.
                 *   `srcs`: A `list` of `File`s that are inputs to `target`'s
@@ -475,15 +472,15 @@ def _collect_incremental_input_files(
             if label not in bundle_labels
         ]
 
-        resource_bundle_uncategorized_folders = EMPTY_DEPSET
-        resource_bundle_uncategorized_generated_folders = EMPTY_DEPSET
-        extra_folders = memory_efficient_depset(
-            resources_result.folder_resources,
+        resource_bundle_uncategorized_file_paths = EMPTY_DEPSET
+        resource_bundle_uncategorized_generated_file_paths = EMPTY_DEPSET
+        extra_file_paths = memory_efficient_depset(
+            resources_result.resource_file_paths,
             transitive = [
                 d
                 for label, d in depset(
                     transitive = [
-                        info.inputs._resource_bundle_uncategorized_folders
+                        info.inputs._resource_bundle_uncategorized_file_paths
                         for info in transitive_infos
                     ],
                 ).to_list()
@@ -491,15 +488,12 @@ def _collect_incremental_input_files(
             ],
         )
         extra_generated_file_paths = memory_efficient_depset(
-            resources_result.generated_folder_resources,
-        )
-        extra_generated_folders = memory_efficient_depset(
-            resources_result.generated_folder_resources,
+            resources_result.generated_resource_file_paths,
             transitive = [
                 d
                 for label, d in depset(
                     transitive = [
-                        info.inputs._resource_bundle_uncategorized_generated_folders
+                        info.inputs._resource_bundle_uncategorized_generated_file_paths
                         for info in transitive_infos
                     ],
                 ).to_list()
@@ -507,9 +501,8 @@ def _collect_incremental_input_files(
             ],
         )
     else:
-        extra_folders = EMPTY_DEPSET
+        extra_file_paths = EMPTY_DEPSET
         extra_generated_file_paths = EMPTY_DEPSET
-        extra_generated_folders = EMPTY_DEPSET
         resource_bundle_labels = memory_efficient_depset(
             transitive = [
                 info.inputs._resource_bundle_labels
@@ -522,16 +515,16 @@ def _collect_incremental_input_files(
                 for info in transitive_infos
             ],
         )
-        resource_bundle_uncategorized_folders = memory_efficient_depset(
+        resource_bundle_uncategorized_file_paths = memory_efficient_depset(
             transitive = [
-                info.inputs._resource_bundle_uncategorized_folders
+                info.inputs._resource_bundle_uncategorized_file_paths
                 for info in transitive_infos
             ],
         )
-        resource_bundle_uncategorized_generated_folders = (
+        resource_bundle_uncategorized_generated_file_paths = (
             memory_efficient_depset(
                 transitive = [
-                    info.inputs._resource_bundle_uncategorized_generated_folders
+                    info.inputs._resource_bundle_uncategorized_generated_file_paths
                     for info in transitive_infos
                 ],
             )
@@ -550,14 +543,12 @@ def _collect_incremental_input_files(
             cxx_sources = cxx_sources,
             entitlements = entitlements[0] if entitlements else None,
             xcode_inputs = struct(
-                extra_file_paths = memory_efficient_depset(extra_file_paths),
+                extra_file_paths = extra_file_paths,
                 extra_files = memory_efficient_depset(
                     extra_files,
                     transitive = transitive_extra_files,
                 ),
-                extra_folders = extra_folders,
                 extra_generated_file_paths = extra_generated_file_paths,
-                extra_generated_folders = extra_generated_folders,
                 infoplist = infoplist,
                 non_arc_srcs = memory_efficient_depset(non_arc_srcs),
                 srcs = memory_efficient_depset(srcs),
@@ -569,11 +560,11 @@ def _collect_incremental_input_files(
             _resource_bundle_uncategorized_files = (
                 resource_bundle_uncategorized_files
             ),
-            _resource_bundle_uncategorized_folders = (
-                resource_bundle_uncategorized_folders
+            _resource_bundle_uncategorized_file_paths = (
+                resource_bundle_uncategorized_file_paths
             ),
-            _resource_bundle_uncategorized_generated_folders = (
-                resource_bundle_uncategorized_generated_folders
+            _resource_bundle_uncategorized_generated_file_paths = (
+                resource_bundle_uncategorized_generated_file_paths
             ),
             _uncategorized = memory_efficient_depset(
                 uncategorized,
@@ -635,9 +626,6 @@ def _collect_mixed_language_input_files(
             *   `extra_files`: A `depset` of `File` that aren't covered under
                 the other attributes, but should be included in the project
                 navigator.
-            *   `extra_folders`: A `depset` of folder path strings that aren't
-                covered under the other attributes, but should be included in
-                the project navigator.
             *   `non_arc_srcs`: A `list` of `File`s that are inputs to
                 `target`'s `non_arc_srcs`-like attributes.
             *   `srcs`: A `list` of `File`s that are inputs to `target`'s
@@ -664,9 +652,7 @@ def _collect_mixed_language_input_files(
         struct(
             extra_file_paths = mergeable_info.extra_file_paths,
             extra_files = mergeable_info.extra_files,
-            extra_folders = EMPTY_DEPSET,
             extra_generated_file_paths = EMPTY_DEPSET,
-            extra_generated_folders = EMPTY_DEPSET,
             infoplist = None,
             non_arc_srcs = mergeable_info.non_arc_srcs,
             srcs = mergeable_info.srcs,
@@ -679,8 +665,8 @@ def _collect_mixed_language_input_files(
             # them from `mixed_target_infos`
             _resource_bundle_labels = EMPTY_DEPSET,
             _resource_bundle_uncategorized_files = EMPTY_DEPSET,
-            _resource_bundle_uncategorized_folders = EMPTY_DEPSET,
-            _resource_bundle_uncategorized_generated_folders = EMPTY_DEPSET,
+            _resource_bundle_uncategorized_file_paths = EMPTY_DEPSET,
+            _resource_bundle_uncategorized_generated_file_paths = EMPTY_DEPSET,
             _uncategorized = memory_efficient_depset(
                 transitive = [
                     _collect_transitive_uncategorized(info)
@@ -781,8 +767,8 @@ def _collect_unsupported_input_files(
 
     if is_resource_bundle:
         uncategorized_files = []
-        uncategorized_folders = []
-        uncategorized_generated_folders = []
+        uncategorized_file_paths = []
+        uncategorized_generated_file_paths = []
 
         def _process_uncategorized_resource(resource):
             # If a file is a child of a folder-type file, the parent
@@ -790,9 +776,9 @@ def _collect_unsupported_input_files(
             folder_type_prefix = resources_module.folder_type_prefix(resource)
             if folder_type_prefix:
                 if resource.is_source:
-                    uncategorized_folders.append(folder_type_prefix)
+                    uncategorized_file_paths.append(folder_type_prefix)
                 else:
-                    uncategorized_generated_folders.append(
+                    uncategorized_generated_file_paths.append(
                         struct(
                             owner = resource.owner,
                             path = folder_type_prefix,
@@ -814,31 +800,31 @@ def _collect_unsupported_input_files(
         else:
             resource_bundle_uncategorized_files = None
 
-        if uncategorized_folders:
-            resource_bundle_uncategorized_folders = [
+        if uncategorized_file_paths:
+            resource_bundle_uncategorized_file_paths = [
                 (
                     label,
-                    memory_efficient_depset(uncategorized_folders),
+                    memory_efficient_depset(uncategorized_file_paths),
                 ),
             ]
         else:
-            resource_bundle_uncategorized_folders = None
+            resource_bundle_uncategorized_file_paths = None
 
-        if uncategorized_generated_folders:
-            resource_bundle_uncategorized_generated_folders = [
+        if uncategorized_generated_file_paths:
+            resource_bundle_uncategorized_generated_file_paths = [
                 (
                     label,
-                    memory_efficient_depset(uncategorized_generated_folders),
+                    memory_efficient_depset(uncategorized_generated_file_paths),
                 ),
             ]
         else:
-            resource_bundle_uncategorized_generated_folders = None
+            resource_bundle_uncategorized_generated_file_paths = None
 
         uncategorized = None
     else:
         resource_bundle_uncategorized_files = None
-        resource_bundle_uncategorized_folders = None
-        resource_bundle_uncategorized_generated_folders = None
+        resource_bundle_uncategorized_file_paths = None
+        resource_bundle_uncategorized_generated_file_paths = None
 
     return struct(
         _product_framework_files = memory_efficient_depset(
@@ -860,17 +846,17 @@ def _collect_unsupported_input_files(
                 for info in transitive_infos
             ],
         ),
-        _resource_bundle_uncategorized_folders = memory_efficient_depset(
-            resource_bundle_uncategorized_folders,
+        _resource_bundle_uncategorized_file_paths = memory_efficient_depset(
+            resource_bundle_uncategorized_file_paths,
             transitive = [
-                info.inputs._resource_bundle_uncategorized_folders
+                info.inputs._resource_bundle_uncategorized_file_paths
                 for info in transitive_infos
             ],
         ),
-        _resource_bundle_uncategorized_generated_folders = memory_efficient_depset(
-            resource_bundle_uncategorized_generated_folders,
+        _resource_bundle_uncategorized_generated_file_paths = memory_efficient_depset(
+            resource_bundle_uncategorized_generated_file_paths,
             transitive = [
-                info.inputs._resource_bundle_uncategorized_generated_folders
+                info.inputs._resource_bundle_uncategorized_generated_file_paths
                 for info in transitive_infos
             ],
         ),

test/internal/pbxproj_partials/write_files_and_groups_tests.bzl
@@ -18,14 +18,8 @@ _RESOLVED_REPOSITORIES_FILE_DECLARED_FILE = mock_actions.mock_file(
 _FILE_PATHS_FILE = mock_actions.mock_file(
     "a_generator_name_pbxproj_partials/file_paths_file",
 )
-_FOLDER_PATHS_FILE = mock_actions.mock_file(
-    "a_generator_name_pbxproj_partials/folder_paths_file",
-)
 _GENERATED_FILE_PATHS_FILE = mock_actions.mock_file(
     "a_generator_name_pbxproj_partials/generated_file_paths_file",
-)
-_GENERATED_FOLDER_PATHS_FILE = mock_actions.mock_file(
-    "a_generator_name_pbxproj_partials/generated_folder_paths_file",
 )
 
 def _write_files_and_groups_test_impl(ctx):
@@ -40,15 +34,11 @@ def _write_files_and_groups_test_impl(ctx):
         _KNOWN_REGIONS_DECLARED_FILE: None,
         _RESOLVED_REPOSITORIES_FILE_DECLARED_FILE: None,
         _FILE_PATHS_FILE: None,
-        _FOLDER_PATHS_FILE: None,
         _GENERATED_FILE_PATHS_FILE: None,
-        _GENERATED_FOLDER_PATHS_FILE: None,
     }
     expected_inputs = [
         _FILE_PATHS_FILE,
-        _FOLDER_PATHS_FILE,
         _GENERATED_FILE_PATHS_FILE,
-        _GENERATED_FOLDER_PATHS_FILE,
         ctx.attr.execution_root_file,
         ctx.attr.selected_model_versions_file,
     ] + ctx.attr.buildfile_subidentifiers_files
@@ -79,13 +69,6 @@ def _write_files_and_groups_test_impl(ctx):
         )
         for (path, owner) in ctx.attr.generated_file_paths.items()
     ]
-    generated_folders = [
-        struct(
-            path = path,
-            owner = mock_actions.mock_label(owner),
-        )
-        for (path, owner) in ctx.attr.generated_folders.items()
-    ]
 
     # Act
 
@@ -103,9 +86,7 @@ def _write_files_and_groups_test_impl(ctx):
         execution_root_file = ctx.attr.execution_root_file,
         files = depset(files),
         file_paths = depset(ctx.attr.file_paths),
-        folders = depset(ctx.attr.folders),
         generated_file_paths = depset(generated_file_paths),
-        generated_folders = depset(generated_folders),
         generator_name = "a_generator_name",
         install_path = ctx.attr.install_path,
         project_options = ctx.attr.project_options,
@@ -204,10 +185,8 @@ write_files_and_groups_test = unittest.make(
         "execution_root_file": attr.string(mandatory = True),
         "file_paths": attr.string_list(mandatory = True),
         "files": attr.string_list(mandatory = True),
-        "folders": attr.string_list(mandatory = True),
         "generated_file_paths": attr.string_dict(mandatory = True),
         "generated_files": attr.string_dict(mandatory = True),
-        "generated_folders": attr.string_dict(mandatory = True),
         "install_path": attr.string(mandatory = True),
         "project_options": attr.string_dict(mandatory = True),
         "selected_model_versions_file": attr.string(mandatory = True),
@@ -239,10 +218,8 @@ def write_files_and_groups_test_suite(name):
             execution_root_file,
             files = [],
             file_paths = [],
-            folders = [],
             generated_file_paths = {},
             generated_files = {},
-            generated_folders = {},
             install_path,
             project_options,
             selected_model_versions_file,
@@ -262,10 +239,8 @@ def write_files_and_groups_test_suite(name):
             execution_root_file = execution_root_file,
             files = files,
             file_paths = file_paths,
-            folders = folders,
             generated_file_paths = generated_file_paths,
             generated_files = generated_files,
-            generated_folders = generated_folders,
             install_path = install_path,
             project_options = project_options,
             selected_model_versions_file = selected_model_versions_file,
@@ -321,12 +296,8 @@ def write_files_and_groups_test_suite(name):
             "",
             # filePathsFile
             _FILE_PATHS_FILE.path,
-            # folderPathsFile
-            _FOLDER_PATHS_FILE.path,
             # generatedFilePathsFile
             _GENERATED_FILE_PATHS_FILE.path,
-            # generatedFolderPathsFile
-            _GENERATED_FOLDER_PATHS_FILE.path,
             # developmentRegion
             "en",
             # useBaseInternationalization
@@ -338,9 +309,7 @@ def write_files_and_groups_test_suite(name):
         ],
         expected_writes = {
             _FILE_PATHS_FILE: "\n",
-            _FOLDER_PATHS_FILE: "\n",
             _GENERATED_FILE_PATHS_FILE: "\n",
-            _GENERATED_FOLDER_PATHS_FILE: "\n",
         },
     )
 
@@ -365,21 +334,17 @@ def write_files_and_groups_test_suite(name):
             "a/path/to/a/file_path.bundle",
             "another/path/to/another/file_path.framework",
         ],
-        folders = [
-            "a/path/to/a/folder",
-            "another/path/to/another/folder",
-        ],
+        # @unsorted-dict-items
         generated_file_paths = {
             "bazel-out/ios-sim-config/bin/a/path/to/a/generated/file_as_file_path": "//a/path/to/a/generated",
             "bazel-out/ios-sim-config/bin/another/path/to/another/generated/file_as_file_path": "//another/path/to/another/generated",
+            "bazel-out/ios-sim-config/bin/a/path/to/a/generated/some.framework": "//a/path/to/a/generated",
+            "bazel-out/ios-sim-config/bin/another/path/to/another/generated/another.bundle": "//another/path/to/another/generated",
         },
+        # @unsorted-dict-items
         generated_files = {
             "bazel-out/ios-sim-config/bin/a/path/to/a/generated/file": "//a/path/to/a/generated",
             "bazel-out/ios-sim-config/bin/another/path/to/another/generated/file": "//another/path/to/another/generated",
-        },
-        generated_folders = {
-            "bazel-out/ios-sim-config/bin/a/path/to/a/generated/folder": "//a/path/to/a/generated",
-            "bazel-out/ios-sim-config/bin/another/path/to/another/generated/folder": "//another/path/to/another/generated",
         },
         install_path = "best/vision.xcodeproj",
         project_options = {
@@ -415,12 +380,8 @@ def write_files_and_groups_test_suite(name):
             "0",
             # filePathsFile
             _FILE_PATHS_FILE.path,
-            # folderPathsFile
-            _FOLDER_PATHS_FILE.path,
             # generatedFilePathsFile
             _GENERATED_FILE_PATHS_FILE.path,
-            # generatedFolderPathsFile
-            _GENERATED_FOLDER_PATHS_FILE.path,
             # developmentRegion
             "enGB",
             # useBaseInternationalization
@@ -441,10 +402,6 @@ another/path/to/another/file
 a/path/to/a/file_path.bundle
 another/path/to/another/file_path.framework
 """,
-            _FOLDER_PATHS_FILE: """\
-a/path/to/a/folder
-another/path/to/another/folder
-""",
             _GENERATED_FILE_PATHS_FILE: """\
 file
 a/path/to/a/generated
@@ -458,12 +415,10 @@ ios-sim-config
 file_as_file_path
 another/path/to/another/generated
 ios-sim-config
-""",
-            _GENERATED_FOLDER_PATHS_FILE: """\
-folder
+some.framework
 a/path/to/a/generated
 ios-sim-config
-folder
+another.bundle
 another/path/to/another/generated
 ios-sim-config
 """,

tools/BUILD
@@ -83,12 +83,8 @@ _XCSCHEMES = [
                 "1",
                 # filePathsFile
                 "/tmp/pbxproj_partials/file_paths_file",
-                # folderPathsFile
-                "/tmp/pbxproj_partials/folder_paths_file",
                 # generatedFilePathsFile
                 "/tmp/pbxproj_partials/generated_file_paths_file",
-                # generatedFolderPathsFile
-                "/tmp/pbxproj_partials/generated_folder_paths_file",
                 # developmentRegion
                 "en",
                 # useBaseInternationalization

tools/generators/files_and_groups/src/BazelPath+Extensions.swift
@@ -1,7 +1,7 @@
 import PBXProj
 
 extension BazelPath {
-    init(parent: BazelPath, path: String, isFolder: Bool = false) {
+    init(parent: BazelPath, path: String) {
         let newPath: String
         if parent.path.isEmpty {
             newPath = path
@@ -9,6 +9,6 @@ extension BazelPath {
             newPath = "\(parent.path)/\(path)"
         }
 
-        self.init(newPath, isFolder: isFolder)
+        self.init(newPath)
     }
 }

tools/generators/files_and_groups/src/ElementCreator/CollectBazelPaths.swift
@@ -65,11 +65,10 @@ extension ElementCreator.CollectBazelPaths {
         parentBazelPath: BazelPath
     ) -> [BazelPath] {
         switch node {
-        case .file(let name, let isFolder):
+        case .file(let name):
             let bazelPath = BazelPath(
                 parent: parentBazelPath,
-                path: name,
-                isFolder: isFolder
+                path: name
             )
             return [bazelPath]
         case .group(let name, let children):

tools/generators/files_and_groups/src/ElementCreator/CreateFile.swift
@@ -20,7 +20,6 @@ extension ElementCreator {
 
         func callAsFunction(
             name: String,
-            isFolder: Bool,
             bazelPath: BazelPath,
             bazelPathType: BazelPathType,
             transitiveBazelPaths: [BazelPath],
@@ -28,7 +27,6 @@ extension ElementCreator {
         ) -> GroupChild.ElementAndChildren {
             return callable(
                 /*name:*/ name,
-                /*isFolder:*/ isFolder,
                 /*bazelPath:*/ bazelPath,
                 /*bazelPathType:*/ bazelPathType,
                 /*transitiveBazelPaths:*/ transitiveBazelPaths,
@@ -44,7 +42,6 @@ extension ElementCreator {
 extension ElementCreator.CreateFile {
     typealias Callable = (
         _ name: String,
-        _ isFolder: Bool,
         _ bazelPath: BazelPath,
         _ bazelPathType: BazelPathType,
         _ transitiveBazelPaths: [BazelPath],
@@ -54,7 +51,6 @@ extension ElementCreator.CreateFile {
 
     static func defaultCallable(
         name: String,
-        isFolder: Bool,
         bazelPath: BazelPath,
         bazelPathType: BazelPathType,
         transitiveBazelPaths: [BazelPath],

tools/generators/files_and_groups/src/ElementCreator/CreateFileElement.swift
@@ -46,14 +46,6 @@ extension ElementCreator {
 // MARK: - CreateFileElement.Callable
 
 extension ElementCreator.CreateFileElement {
-    private static let folderTypeFileExtensions: Set<String?> = [
-       "bundle",
-       "docc",
-       "framework",
-       "scnassets",
-       "xcassets",
-    ]
-
     private static let explicitFileTypeExtensions: Set<String?> = [
         "bazel",
         "bzl",
@@ -84,16 +76,6 @@ extension ElementCreator.CreateFileElement {
         resolvedRepository: ResolvedRepository?
     ) {
         let impliedExt = ext ?? Xcode.impliedExtension(basename: name)
-        let fileTypeType = calculateFileTypeType(basename: name, extension: impliedExt)
-        let fileType: String
-        let sortOrder: Element.SortOrder
-        if bazelPath.isFolder && !folderTypeFileExtensions.contains(ext) {
-            fileType = "folder"
-            sortOrder = .groupLike
-        } else {
-            fileType = impliedExt.flatMap(Xcode.pbxProjEscapedFileType) ?? "file"
-            sortOrder = .fileLike
-        }
 
         let attributes = createAttributes(
             name: name,
@@ -110,7 +92,9 @@ extension ElementCreator.CreateFileElement {
         }
         let content = """
 {isa = PBXFileReference; \
-\(fileTypeType) = \(fileType); \
+\(calculateFileTypeType(basename: name, extension: impliedExt)) = \(
+    impliedExt.flatMap(Xcode.pbxProjEscapedFileType) ?? "file"
+); \
 \(nameAttribute)\
 path = \(attributes.elementAttributes.path.pbxProjEscaped); \
 sourceTree = \(attributes.elementAttributes.sourceTree.rawValue); }
@@ -128,7 +112,7 @@ sourceTree = \(attributes.elementAttributes.sourceTree.rawValue); }
                     ),
                     content: content
                 ),
-                sortOrder: sortOrder
+                sortOrder: .fileLike
             ),
             resolvedRepository: attributes.resolvedRepository
         )

tools/generators/files_and_groups/src/ElementCreator/CreateGroupChild.swift
@@ -117,15 +117,13 @@ extension ElementCreator.CreateGroupChild {
                 )
             }
 
-        case .file(let name, let isFolder):
+        case .file(let name):
             return .elementAndChildren(
                 createFile(
                     name: name,
-                    isFolder: isFolder,
                     bazelPath: BazelPath(
                         parent: parentBazelPath,
-                        path: name,
-                        isFolder: isFolder
+                        path: name
                     ),
                     bazelPathType: parentBazelPathType,
                     transitiveBazelPaths: []

commit 00ecf36c150d1972296f162ec0fb18a4397ac9f9

Fix missing BUILD files in Project navigator
Regressed in caae410fcd827b4bcc7ae8b794773ce07a1ceb8b.

Signed-off-by: Brentley Jones <github@brentleyjones.com>

xcodeproj/internal/files/incremental_input_files.bzl
@@ -333,9 +333,9 @@ def _collect_incremental_input_files(
     extra_files = [infoplist] if infoplist else []
 
     # Include BUILD files for the project but not for external repos
-    extra_file_paths = []
+    build_file_paths = []
     if not label.workspace_root:
-        extra_file_paths.append(ctx.build_file_path)
+        build_file_paths.append(ctx.build_file_path)
 
     # buildifier: disable=uninitialized
     def _handle_srcs_file(file):
@@ -475,7 +475,7 @@ def _collect_incremental_input_files(
         resource_bundle_uncategorized_file_paths = EMPTY_DEPSET
         resource_bundle_uncategorized_generated_file_paths = EMPTY_DEPSET
         extra_file_paths = memory_efficient_depset(
-            resources_result.resource_file_paths,
+            build_file_paths + resources_result.resource_file_paths,
             transitive = [
                 d
                 for label, d in depset(
@@ -501,7 +501,7 @@ def _collect_incremental_input_files(
             ],
         )
     else:
-        extra_file_paths = EMPTY_DEPSET
+        extra_file_paths = memory_efficient_depset(build_file_paths)
         extra_generated_file_paths = EMPTY_DEPSET
         resource_bundle_labels = memory_efficient_depset(
             transitive = [