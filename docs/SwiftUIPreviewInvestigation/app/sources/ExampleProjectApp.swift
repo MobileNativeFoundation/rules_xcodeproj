1b26019274481fc25b0230142810e533094c126d
Oscar Berggren <oscar.berggren@gotinder.com>
2024-12-10 15:42:39 -0800

    WIP SwiftUI Preview investigation

---
+++ docs/SwiftUIPreviewInvestigation/app/sources/ExampleProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleProjectApp.swift
+//  Core
+//
+//  Created by Oscar Berggren on 12/9/24.
+//
+
+import SwiftUI
+
+@main
+struct ExampleProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
