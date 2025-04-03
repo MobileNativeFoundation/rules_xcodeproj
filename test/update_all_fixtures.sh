3e4f450e486430caa989f98b8baa95f6cf10f68c
Luis Padron <heyluispadron@gmail.com>
2025-04-03 16:23:35 -0400

    Fix CI for Bazel HEAD
    Required changes to fix Bazel @ HEAD
    
    Signed-off-by: Luis Padron <heyluispadron@gmail.com>

--- test/update_all_fixtures.sh
+++ test/update_all_fixtures.sh
@@ -10,17 +10,13 @@ for dir in examples/*/ ; do
       continue
     fi
 
+    # rules_ios only supports up-to Bazel 7
     if [[ "$dir" == "examples/rules_ios/" ]]; then
-      bazel_version="7.4.1"
-    else
-      # See https://github.com/MobileNativeFoundation/rules_xcodeproj/pull/3029
-      #
-      # Temporary change to make CI pass until the fix is in `last_green`
-      bazel_version="dd2464a5933e0a5a6765024573832717b71989bf"
+      export USE_BAZEL_VERSION="7.x"
     fi
 
     echo
     echo "Updating \"${dir%/}\" fixtures"
     echo
-    USE_BAZEL_VERSION="$bazel_version" bazel run --config=cache //test/fixtures:update
+    bazel run --config=cache //test/fixtures:update
 done
