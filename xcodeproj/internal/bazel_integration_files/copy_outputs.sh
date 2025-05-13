f1797b5a5fba606c4d10eb2f3163dfa254b992d3
Luis Padron <lpadron@squareup.com>
2025-05-13 15:40:07 -0400

    Add homebrew paths to `copy_outputs.sh` rsync call
    This works around an issue in macOS 15.4
    
    Signed-off-by: Luis Padron <lpadron@squareup.com>

--- xcodeproj/internal/bazel_integration_files/copy_outputs.sh
+++ xcodeproj/internal/bazel_integration_files/copy_outputs.sh
@@ -31,7 +31,12 @@ if [[ "$ACTION" != indexbuild ]]; then
       ln -sfh "$PWD/$BAZEL_OUTPUTS_PRODUCT_BASENAME" "$TARGET_BUILD_DIR/$PRODUCT_NAME"
     else
       # Product is a bundle
-      rsync \
+      # NOTE: use `which` to find the path to `rsync`.
+      # In macOS 15.4, the system `rsync` is using `openrsync` which contains some permission issues.
+      # This allows users to workaround the issue by overriding the system `rsync` with a working version.
+      # Remove this once we no longer support macOS versions with broken `rsync`.
+      PATH="/opt/homebrew/bin:/usr/local/bin:$PATH" \
+        rsync \
         --copy-links \
         --recursive \
         --times \
