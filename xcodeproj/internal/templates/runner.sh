MobileNativeFoundation/rules_xcodeproj

commit 6bdbcde0a1e04a0379548710be055399b0690f94

Work around stale Xcode version from outer Bazel invocation (#3232)
We were potentially setting a stale `--version` in `runner.sh`, because
not everyone (including the local dev environment of this ruleset) has
code in `tools/bazel` to force Bazel to re-evaluate its Xcode cache when
`xcode-select -p` changes. Now we follow more of
https://github.com/bazelbuild/rules_apple/blob/master/doc/common_info.md#xcode-version-selection-and-invalidation.

Signed-off-by: Brentley Jones <github@brentleyjones.com>

xcodeproj/internal/templates/runner.sh
@@ -22,7 +22,6 @@ readonly execution_root_file="$PWD/%execution_root_file%"
 readonly extra_flags_bazelrc="$PWD/%extra_flags_bazelrc%"
 readonly generator_build_file="$PWD/%generator_build_file%"
 readonly generator_defs_bzl="$PWD/%generator_defs_bzl%"
-readonly schemes_json="$PWD/%schemes_json%"
 readonly xcodeproj_bazelrc="$PWD/%xcodeproj_bazelrc%"
 
 installer_flags=(
@@ -121,7 +120,7 @@ chmod u+w "$generator_package_directory/BUILD"
 cp "$generator_defs_bzl" "$generator_package_directory/defs.bzl"
 chmod u+w "$generator_package_directory/defs.bzl"
 
-cat >> "$generator_package_directory/defs.bzl" <<EOF
+cat <<EOF >> "$generator_package_directory/defs.bzl"
 
 # Constants
 
@@ -146,27 +145,47 @@ if [[ -s "$extra_flags_bazelrc" ]]; then
   bazelrcs+=("--bazelrc=$extra_flags_bazelrc")
 fi
 
+if [[ -n "${DEVELOPER_DIR:-}" ]]; then
+  if [[ ! -f "${DEVELOPER_DIR%/*}/version.plist" ]]; then
+    echo >&2 "DEVELOPER_DIR is set to invalid path: $DEVELOPER_DIR"
+    exit 1
+  fi
+
+  developer_dir="$DEVELOPER_DIR"
+
+  # We can use a fast path when `DEVELOPER_DIR` is set for us
+  xcode_version=$(
+    /usr/libexec/PlistBuddy \
+      -c 'print ProductBuildVersion' \
+      "${DEVELOPER_DIR%/*}/version.plist"
+  )
+elif command -v xcodebuild > /dev/null 2>&1; then
+  developer_dir="$(/usr/bin/xcode-select -p)"
+
+  # Xcode 15.4\nBuild version 15F31d -> 15F31d
+  xcode_version=$(xcodebuild -version | awk '/Build version/{print $NF}')
+else
+  developer_dir=""
+  xcode_version=""
+fi
+
 # We write to a `.bazelrc` file instead of passing flags directly in order to
 # support all Bazel commands via the `common` pseudo-command
-cat > "$pre_xcodeproj_bazelrc_dir/pre_xcodeproj.bazelrc" <<EOF
+if [[ -n "$xcode_version" ]]; then
+  cat <<EOF > "$pre_xcodeproj_bazelrc_dir/pre_xcodeproj.bazelrc"
 # Be explicit about our desired Xcode version
-common:rules_xcodeproj --xcode_version=%xcode_version%
+common:rules_xcodeproj --xcode_version=$xcode_version
 
 # Work around https://github.com/bazelbuild/bazel/issues/8902
 # \`USE_CLANG_CL\` is only used on Windows, we set it here to cause Bazel to
 # re-evaluate the cc_toolchain for a different Xcode version
-common:rules_xcodeproj --repo_env=USE_CLANG_CL=%xcode_version%
-common:rules_xcodeproj --repo_env=XCODE_VERSION=%xcode_version%
+common:rules_xcodeproj --repo_env=USE_CLANG_CL=$xcode_version
+common:rules_xcodeproj --repo_env=XCODE_VERSION=$xcode_version
 EOF
-
-if command -v /usr/bin/xcode-select >/dev/null 2>&1; then
-  developer_dir="$(/usr/bin/xcode-select -p)"
-else
-  developer_dir="${DEVELOPER_DIR:-}"
 fi
 
 if [[ -n "$developer_dir" ]]; then
-  cat >> "$pre_xcodeproj_bazelrc_dir/pre_xcodeproj.bazelrc" <<EOF
+  cat <<EOF>> "$pre_xcodeproj_bazelrc_dir/pre_xcodeproj.bazelrc"
 
 # Set \`DEVELOPER_DIR\` in case a bazel wrapper filters it
 common:rules_xcodeproj --repo_env="DEVELOPER_DIR=$developer_dir"

xcodeproj/internal/xcodeproj_runner.bzl
@@ -6,21 +6,6 @@ load(":collections.bzl", "uniq")
 load(":execution_root.bzl", "write_execution_root_file")
 load(":providers.bzl", "XcodeProjRunnerOutputInfo")
 
-def _get_xcode_product_version(*, xcode_config):
-    raw_version = str(xcode_config.xcode_version())
-    if not raw_version:
-        fail("""\
-`xcode_config.xcode_version` was not set. This is a bazel bug. Try again.
-""")
-
-    version_components = raw_version.split(".")
-    if len(version_components) < 4:
-        # This will result in analysis cache misses, but it's better than
-        # failing
-        return raw_version
-
-    return version_components[3]
-
 def _process_extra_flags(*, attr, content, setting, config, config_suffix):
     extra_flags = getattr(attr, setting)[BuildSettingInfo].value
     if extra_flags:
@@ -272,7 +257,6 @@ def _write_runner(
         package,
         runner_label,
         template,
-        xcode_version,
         xcodeproj_bazelrc):
     output = actions.declare_file("{}-runner.sh".format(name))
 
@@ -358,7 +342,6 @@ def_env+='}}'""".format(
             "%generator_package_name%": generator_package_name,
             "%install_path%": install_path,
             "%runner_label%": runner_label,
-            "%xcode_version%": xcode_version,
             "%xcodeproj_bazelrc%": xcodeproj_bazelrc.short_path,
         },
     )
@@ -380,10 +363,6 @@ def _xcodeproj_runner_impl(ctx):
     install_path = paths.join(
         ctx.attr.install_directory,
         "{}.xcodeproj".format(project_name),
-    )
-
-    xcode_version = _get_xcode_product_version(
-        xcode_config = ctx.attr._xcode_config[apple_common.XcodeVersionConfig],
     )
 
     xcodeproj_bazelrc = _write_xcodeproj_bazelrc(
@@ -443,7 +422,6 @@ def _xcodeproj_runner_impl(ctx):
         install_path = install_path,
         runner_label = runner_label,
         template = ctx.file._runner_template,
-        xcode_version = xcode_version,
         xcodeproj_bazelrc = xcodeproj_bazelrc,
     )
 
@@ -547,12 +525,6 @@ xcodeproj_runner = rule(
         "_runner_template": attr.label(
             allow_single_file = True,
             default = Label("//xcodeproj/internal/templates:runner.sh"),
-        ),
-        "_xcode_config": attr.label(
-            default = configuration_field(
-                name = "xcode_config_label",
-                fragment = "apple",
-            ),
         ),
     },
     executable = True,

commit d1c81ae8998fa9d3f7f28ed5c9ed02109e09c344

Ensure `pre_xcodeproj.bazelrc` is always exists (#3249)
Broke in 6bdbcde0a1e04a0379548710be055399b0690f94 for some workflows
(e.g. Linux).

Signed-off-by: Brentley Jones <github@brentleyjones.com>

xcodeproj/internal/templates/runner.sh
@@ -178,10 +178,12 @@ else
   xcode_version=""
 fi
 
+touch "$pre_xcodeproj_bazelrc_dir/pre_xcodeproj.bazelrc"
+
 # We write to a `.bazelrc` file instead of passing flags directly in order to
 # support all Bazel commands via the `common` pseudo-command
 if [[ -n "$xcode_version" ]]; then
-  cat <<EOF > "$pre_xcodeproj_bazelrc_dir/pre_xcodeproj.bazelrc"
+  cat <<EOF >> "$pre_xcodeproj_bazelrc_dir/pre_xcodeproj.bazelrc"
 # Be explicit about our desired Xcode version
 common:rules_xcodeproj --xcode_version=$xcode_version
 
@@ -194,7 +196,7 @@ EOF
 fi
 
 if [[ -n "$developer_dir" ]]; then
-  cat <<EOF>> "$pre_xcodeproj_bazelrc_dir/pre_xcodeproj.bazelrc"
+  cat <<EOF >> "$pre_xcodeproj_bazelrc_dir/pre_xcodeproj.bazelrc"
 
 # Set \`DEVELOPER_DIR\` in case a bazel wrapper filters it
 common:rules_xcodeproj --repo_env="DEVELOPER_DIR=$developer_dir"